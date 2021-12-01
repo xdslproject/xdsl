1dbbf0d51a197e2832b6124a975bbdd4d5ee83d6
Mathieu Fehr <mathieu.fehr@gmail.com>
2021-12-01 15:30:05 +0000

    Add tests for op_type_rewrite_pattern

--- tests/rewriter_test.py
+++ tests/rewriter_test.py
@@ -19,7 +19,8 @@ def rewrite_and_compare(ctx: MLContext, prog: str, expected_prog: str,
     assert file.getvalue().strip() == expected_prog.strip()
 
 
-def test_simple_rewrite():
+# Test a simple non-recursive rewrite
+def test_non_recursive_rewrite():
     ctx = MLContext()
     builtin = Builtin(ctx)
     std = Std(ctx)
@@ -40,14 +41,79 @@ def test_simple_rewrite():
         def match_and_rewrite(
                 self, op: Operation,
                 new_operands: List[SSAValue]) -> Optional[RewriteAction]:
-            if isinstance(op, Constant) and op.value.value.data == 42:
+            if isinstance(op, Constant):
                 return RewriteAction.from_op_list(
                     [std.constant_from_attr(IntegerAttr.get(43), std.i32)])
             return None
 
-    rewrite_and_compare(ctx, prog, expected,
-                        PatternRewriteWalker(RewriteConst()))
+    rewrite_and_compare(
+        ctx, prog, expected,
+        PatternRewriteWalker(RewriteConst(), apply_recursively=False))
+
+
+def test_op_type_rewrite_pattern_method_decorator():
+    """Test op_type_rewrite_pattern decorator on methods."""
+    ctx = MLContext()
+    builtin = Builtin(ctx)
+    std = Std(ctx)
+
+    prog = \
+"""module() {
+  %0 : !i32 = std.constant() ["value" = 42 : !i32]
+  %1 : !i32 = std.addi(%0 : !i32, %0 : !i32)
+}"""
+
+    expected = \
+"""module() {
+  %0 : !i32 = std.constant() ["value" = 43 : !i64]
+  %1 : !i32 = std.addi(%0 : !i32, %0 : !i32) 
+}"""
+
+    class RewriteConst(RewritePattern):
+        @op_type_rewrite_pattern
+        def match_and_rewrite(
+                self, op: Constant,
+                new_operands: List[SSAValue]) -> Optional[RewriteAction]:
+            return RewriteAction.from_op_list(
+                [std.constant_from_attr(IntegerAttr.get(43), std.i32)])
+
+    rewrite_and_compare(
+        ctx, prog, expected,
+        PatternRewriteWalker(RewriteConst(), apply_recursively=False))
+
+
+def test_op_type_rewrite_pattern_static_decorator():
+    """Test op_type_rewrite_pattern decorator on static functions."""
+    ctx = MLContext()
+    builtin = Builtin(ctx)
+    std = Std(ctx)
+
+    prog = \
+"""module() {
+  %0 : !i32 = std.constant() ["value" = 42 : !i32]
+  %1 : !i32 = std.addi(%0 : !i32, %0 : !i32)
+}"""
+
+    expected = \
+"""module() {
+  %0 : !i32 = std.constant() ["value" = 43 : !i64]
+  %1 : !i32 = std.addi(%0 : !i32, %0 : !i32) 
+}"""
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(
+            op: Constant,
+            new_operands: List[SSAValue]) -> Optional[RewriteAction]:
+        return RewriteAction.from_op_list(
+            [std.constant_from_attr(IntegerAttr.get(43), std.i32)])
+
+    rewrite_and_compare(
+        ctx, prog, expected,
+        PatternRewriteWalker(AnonymousRewritePattern(match_and_rewrite),
+                             apply_recursively=False))
 
 
 if __name__ == "__main__":
-    test_simple_rewrite()
+    test_non_recursive_rewrite()
+    test_op_type_rewrite_pattern_method_decorator()
+    test_op_type_rewrite_pattern_static_decorator()
