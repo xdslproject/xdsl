4ca70e5c1f5bd217fea25393f5939fdc97adb715
Mathieu Fehr <mathieu.fehr@gmail.com>
2021-12-22 23:14:20 +0000

    Move and improve all memref builders

--- src/xdsl/dialects/memref.py
+++ src/xdsl/dialects/memref.py
@@ -1,6 +1,6 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from xdsl.ir import *
-from xdsl.util import OpOrBlockArg, get_ssa_value
 from xdsl.dialects.builtin import *
 from xdsl.irdl import *
 
@@ -21,53 +21,6 @@ class MemRef:
         self.ctx.register_op(GetGlobal)
         self.ctx.register_op(Global)
 
-    def load(self, value, indices: List) -> Operation:
-        return Load.build(operands=[value, indices],
-                          result_types=[SSAValue.get(value).typ.element_type])
-
-    def alloc(self,
-              alignment: int,
-              return_type: Attribute,
-              shape: List = None) -> Operation:
-        if shape is None:
-            shape = [1]
-        return Alloc.build(
-            operands=[[], []],
-            result_types=[MemRefType.from_type_and_list(return_type, shape)],
-            attributes={"alignment": alignment})
-
-    def alloca(self,
-               alignment: int,
-               return_type: Attribute,
-               shape: List = None) -> Operation:
-        if shape is None:
-            shape = [1]
-        return Alloca.build(
-            operands=[[], []],
-            result_types=[MemRefType.from_type_and_list(return_type, shape)],
-            attributes={"alignment": alignment})
-
-    def dealloc(self, memref: OpOrBlockArg) -> Operation:
-        return Dealloc.build(operands=[memref])
-
-    def get_global(self, name, return_type: Attribute) -> Operation:
-        op = GetGlobal.build(result_types=[return_type])
-        op.attributes["name"] = FlatSymbolRefAttr.build(name)
-        return op
-
-    def global_(self,
-                sym_name,
-                typ: Attribute,
-                initial_value: Optional[Attribute],
-                sym_visibility="private") -> Operation:
-        return Global.build(
-            attributes={
-                "sym_name": sym_name,
-                "type": typ,
-                "initial_value": initial_value,
-                "sym_visibility": sym_visibility
-            })
-
 
 @irdl_attr_definition
 class MemRefType(ParametrizedAttribute):
@@ -86,7 +39,9 @@ class MemRefType(ParametrizedAttribute):
     @builder
     def from_type_and_list(
             referenced_type: Attribute,
-            shape: List[Union[int, IntegerAttr]] = [1]) -> Attribute:
+            shape: List[Union[int, IntegerAttr]] = None) -> Attribute:
+        if shape is None:
+            shape = [1]
         return MemRefType([
             ArrayAttr.from_list([IntegerAttr.build(d) for d in shape]),
             referenced_type
@@ -120,6 +75,12 @@ class Load(Operation):
         if self.memref.typ.get_num_dims() != len(self.indices):
             raise Exception("expected an index for each dimension")
 
+    @staticmethod
+    def get(ref: Union[SSAValue, Operation],
+            indices: List[Union[SSAValue, Operation]]) -> Load:
+        return Load.build(operands=[ref, indices],
+                          result_types=[SSAValue.get(ref).typ.element_type])
+
 
 @irdl_op_definition
 class Store(Operation):
@@ -136,6 +97,11 @@ class Store(Operation):
         if self.memref.typ.get_num_dims() != len(self.indices):
             raise Exception("expected an index for each dimension")
 
+    @staticmethod
+    def get(value: Union[Operation, SSAValue], ref: Union[Operation, SSAValue],
+            indices: List[Union[Operation, SSAValue]]):
+        return Store.build(operands=[value, ref, indices])
+
 
 @irdl_op_definition
 class Alloc(Operation):
@@ -150,6 +116,19 @@ class Alloc(Operation):
     alignment = AttributeDef(IntegerAttr)
 
     irdl_options = [AttrSizedOperandSegments()]
+
+    @staticmethod
+    def get(return_type: Attribute,
+            alignment: int,
+            shape: List[Union[int, IntegerAttr]] = None) -> Operation:
+        if shape is None:
+            shape = [1]
+        return Alloc.build(
+            operands=[[], []],
+            result_types=[MemRefType.from_type_and_list(return_type, shape)],
+            attributes={
+                "alignment": IntegerAttr.from_int_and_width(alignment, 64)
+            })
 
 
 @irdl_op_definition
@@ -166,11 +145,28 @@ class Alloca(Operation):
 
     irdl_options = [AttrSizedOperandSegments()]
 
+    @staticmethod
+    def get(return_type: Attribute,
+            alignment: int,
+            shape: List[Union[int, IntegerAttr]] = None) -> Operation:
+        if shape is None:
+            shape = [1]
+        return Alloca.build(
+            operands=[[], []],
+            result_types=[MemRefType.from_type_and_list(return_type, shape)],
+            attributes={
+                "alignment": IntegerAttr.from_int_and_width(alignment, 64)
+            })
+
 
 @irdl_op_definition
 class Dealloc(Operation):
     name = "memref.dealloc"
     memref = OperandDef(MemRefType)
+
+    @staticmethod
+    def get(operand: Union[Operation, SSAValue]) -> Dealloc:
+        return Dealloc.build(operands=[operand])
 
 
 @irdl_op_definition
@@ -188,6 +184,12 @@ class GetGlobal(Operation):
             raise Exception(
                 "expected 'name' attribute to be a FlatSymbolRefAttr")
 
+    @staticmethod
+    def get(name, return_type: Attribute) -> Operation:
+        return GetGlobal.build(
+            result_types=[return_type],
+            attributes={"name": FlatSymbolRefAttr.build(name)})
+
     #TODO how to verify the types, as the global might be defined in another compilation unit
 
 
@@ -202,4 +204,17 @@ class Global(Operation):
     initial_value = AttributeDef(AnyAttr())
 
     # TODO how do we represent these in MLIR-Lite
-    #constant = AttributeDef(UnitAttr)
+    # constant = AttributeDef(UnitAttr)
+
+    @staticmethod
+    def get(sym_name,
+            typ: Attribute,
+            initial_value: Optional[Attribute],
+            sym_visibility="private") -> Operation:
+        return Global.build(
+            attributes={
+                "sym_name": sym_name,
+                "type": typ,
+                "initial_value": initial_value,
+                "sym_visibility": sym_visibility
+            })
