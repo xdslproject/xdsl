0a8cebda521bd137a1d3d6750b610a61c75bf5c3
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-01-16 00:47:59 +0000

    Add rewriter methods to inline block in other blocks

--- tests/rewriter_test.py
+++ tests/rewriter_test.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 from xdsl.dialects.arith import Arith, Constant, Addi
 from xdsl.dialects.builtin import ModuleOp, Builtin, i32
+from xdsl.dialects.scf import Scf
 from xdsl.ir import MLContext
 from xdsl.parser import Parser
 from xdsl.printer import Printer
@@ -14,6 +15,7 @@ def rewrite_and_compare(prog: str, expected_prog: str,
     ctx = MLContext()
     builtin = Builtin(ctx)
     arith = Arith(ctx)
+    scf = Scf(ctx)
 
     parser = Parser(ctx, prog)
     module = parser.parse_op()
@@ -110,5 +112,85 @@ def test_replace_op_new_results():
         add_op = module.ops[1]
 
         rewriter.replace_op(add_op, [], [add_op.input1])
+
+    rewrite_and_compare(prog, expected, transformation)
+
+
+def test_inline_block_at_pos():
+    """Test the inlining of a block at a certain position."""
+    prog = \
+    """module() {
+%0 : !i1 = arith.constant() ["value" = 1 : !i1]
+scf.if(%0 : !i1) {
+  %1 : !i32 = arith.constant() ["value" = 2 : !i32]
+}
+}"""
+
+    expected = \
+"""module() {
+  %0 : !i1 = arith.constant() ["value" = 1 : !i1]
+  %1 : !i32 = arith.constant() ["value" = 2 : !i32]
+  scf.if(%0 : !i1) {}
+}"""
+
+    def transformation(module: ModuleOp, rewriter: Rewriter) -> None:
+        if_op = module.ops[1]
+        module_block = module.regions[0].blocks[0]
+        if_block = if_op.regions[0].blocks[0]
+
+        rewriter.inline_block_at_pos(if_block, module_block, 1)
+
+    rewrite_and_compare(prog, expected, transformation)
+
+
+def test_inline_block_before():
+    """Test the inlining of a block before an operation."""
+    prog = \
+    """module() {
+%0 : !i1 = arith.constant() ["value" = 1 : !i1]
+scf.if(%0 : !i1) {
+  %1 : !i32 = arith.constant() ["value" = 2 : !i32]
+}
+}"""
+
+    expected = \
+"""module() {
+  %0 : !i1 = arith.constant() ["value" = 1 : !i1]
+  %1 : !i32 = arith.constant() ["value" = 2 : !i32]
+  scf.if(%0 : !i1) {}
+}"""
+
+    def transformation(module: ModuleOp, rewriter: Rewriter) -> None:
+        if_op = module.ops[1]
+        if_block = if_op.regions[0].blocks[0]
+
+        rewriter.inline_block_before(if_block, if_op)
+
+    rewrite_and_compare(prog, expected, transformation)
+
+
+def test_inline_block_after():
+    """Test the inlining of a block after an operation."""
+    prog = \
+    """module() {
+%0 : !i1 = arith.constant() ["value" = 1 : !i1]
+scf.if(%0 : !i1) {
+  %1 : !i32 = arith.constant() ["value" = 2 : !i32]
+}
+}"""
+
+    expected = \
+"""module() {
+  %0 : !i1 = arith.constant() ["value" = 1 : !i1]
+  %1 : !i32 = arith.constant() ["value" = 2 : !i32]
+  scf.if(%0 : !i1) {}
+}"""
+
+    def transformation(module: ModuleOp, rewriter: Rewriter) -> None:
+        if_op = module.ops[1]
+        constant_op = module.ops[0]
+        if_block = if_op.regions[0].blocks[0]
+
+        rewriter.inline_block_after(if_block, constant_op)
 
     rewrite_and_compare(prog, expected, transformation)
