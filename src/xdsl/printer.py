58c17a7866f05d4ef10e0e1f21839b95010eceb2
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-03-15 10:53:45 +0000

    Add position tracking in the printer

--- src/xdsl/printer.py
+++ src/xdsl/printer.py
@@ -17,12 +17,23 @@ class Printer:
     _block_names: Dict[Block, int] = field(default_factory=dict, init=False)
     _next_valid_name_id: int = field(default=0, init=False)
     _next_valid_block_id: int = field(default=0, init=False)
+    _current_line: int = field(default=0, init=False)
+    _current_column: int = field(default=0, init=False)
+    _operation_messages: Dict[Operation, str] = field(default_factory=dict,
+                                                      init=False)
 
-    def _print(self, *args, **kwargs):
-        print(*args, **kwargs, file=self.stream)
+    def _print(self, text: Any):
+        text = str(text)
+        lines = text.split('\n')
+        if len(lines) != 1:
+            self._current_line += len(lines) - 1
+            self._current_column = len(lines[-1])
+        else:
+            self._current_column += len(lines[-1])
+        print(text, end='', file=self.stream)
 
     def print_string(self, string) -> None:
-        self._print(string, end='')
+        self._print(string)
 
     T = TypeVar('T')
 
@@ -32,12 +43,12 @@ class Printer:
             return
         print_fn(elems[0])
         for elem in elems[1:]:
-            self._print(", ", end='')
+            self._print(", ")
             print_fn(elem)
 
     def _print_new_line(self) -> None:
-        self._print("")
-        self._print(" " * self._indent * indentNumSpaces, end='')
+        self._print("\n")
+        self._print(" " * self._indent * indentNumSpaces)
 
     def _get_new_valid_name_id(self) -> int:
         self._next_valid_name_id += 1
@@ -49,15 +60,15 @@ class Printer:
 
     def _print_result_value(self, op: Operation, idx: int) -> None:
         val = op.results[idx]
-        self._print("%", end='')
+        self._print("%")
         if val in self._ssa_values.keys():
             name = self._ssa_values[val]
         else:
             name = self._get_new_valid_name_id()
             self._ssa_values[val] = name
-        self._print("%s" % name, end='')
+        self._print("%s" % name)
         if self.print_result_types:
-            self._print(" : ", end='')
+            self._print(" : ")
             self.print_attribute(val.typ)
 
     def _print_results(self, op: Operation) -> None:
@@ -69,28 +80,28 @@ class Printer:
         # One result
         if len(results) == 1:
             self._print_result_value(op, 0)
-            self._print(" = ", end='')
+            self._print(" = ")
             return
 
         # Multiple results
-        self._print("(", end='')
+        self._print("(")
         self._print_result_value(op, 0)
         for idx in range(1, len(results)):
-            self._print(", ", end='')
+            self._print(", ")
             self._print_result_value(op, idx)
-        self._print(") = ", end='')
+        self._print(") = ")
 
     def _print_operand(self, operand: SSAValue) -> None:
         if (self._ssa_values.get(operand) == None):
             raise KeyError(
                 "SSAValue is not part of the IR, are you sure all operations are added before their uses?"
             )
-        self._print("%", end='')
+        self._print("%")
 
-        self._print("%s" % self._ssa_values[operand], end='')
+        self._print("%s" % self._ssa_values[operand])
 
         if self.print_operand_types:
-            self._print(" : ", end='')
+            self._print(" : ")
             self.print_attribute(operand.typ)
 
     def _print_ops(self, ops: List[Operation]) -> None:
@@ -103,46 +114,46 @@ class Printer:
             self._print_new_line()
 
     def print_block_name(self, block: Block) -> None:
-        self._print("^", end='')
+        self._print("^")
         if block not in self._block_names:
             self._block_names[block] = self._get_new_valid_block_id()
-        self._print(self._block_names[block], end='')
+        self._print(self._block_names[block])
 
     def _print_named_block(self, block: Block) -> None:
         self.print_block_name(block)
         if len(block.args) > 0:
-            self._print("(", end='')
+            self._print("(")
             self.print_list(block.args, self._print_block_arg)
-            self._print(")", end='')
-        self._print(":", end='')
+            self._print(")")
+        self._print(":")
         if len(block.ops) > 0:
             self._print_ops(block.ops)
         else:
             self._print_new_line()
 
     def _print_block_arg(self, arg: BlockArgument) -> None:
-        self._print("%", end='')
+        self._print("%")
         name = self._get_new_valid_name_id()
         self._ssa_values[arg] = name
-        self._print("%s : " % name, end='')
+        self._print("%s : " % name)
         self.print_attribute(arg.typ)
 
     def _print_region(self, region: Region) -> None:
         if len(region.blocks) == 0:
-            self._print(" {}", end='')
+            self._print(" {}")
             return
 
         if len(region.blocks) == 1 and len(region.blocks[0].args) == 0:
-            self._print(" {", end='')
+            self._print(" {")
             self._print_ops(region.blocks[0].ops)
-            self._print("}", end='')
+            self._print("}")
             return
 
-        self._print(" {", end='')
+        self._print(" {")
         self._print_new_line()
         for block in region.blocks:
             self._print_named_block(block)
-        self._print("}", end='')
+        self._print("}")
 
     def _print_regions(self, regions: List[Region]) -> None:
         for region in regions:
@@ -150,37 +161,37 @@ class Printer:
 
     def _print_operands(self, operands: List[SSAValue]) -> None:
         if len(operands) == 0:
-            self._print("()", end='')
+            self._print("()")
             return
 
-        self._print("(", end='')
+        self._print("(")
         self._print_operand(operands[0])
         for operand in operands[1:]:
-            self._print(", ", end='')
+            self._print(", ")
             self._print_operand(operand)
-        self._print(")", end='')
+        self._print(")")
 
     def print_attribute(self, attribute: Attribute) -> None:
         if isinstance(attribute, IntegerType):
             width = attribute.parameters[0]
             assert isinstance(width, IntAttr)
-            self._print(f'!i{width.data}', end='')
+            self._print(f'!i{width.data}')
             return
 
         if isinstance(attribute, StringAttr):
-            self._print(f'"{attribute.data}"', end='')
+            self._print(f'"{attribute.data}"')
             return
 
         if isinstance(attribute, FlatSymbolRefAttr):
-            self._print(f'@{attribute.parameters[0].data}', end='')
+            self._print(f'@{attribute.parameters[0].data}')
             return
 
         if isinstance(attribute, IntegerAttr):
             width = attribute.parameters[0]
             typ = attribute.parameters[1]
             assert (isinstance(width, IntAttr))
-            self._print(width.data, end='')
-            self._print(" : ", end='')
+            self._print(width.data)
+            self._print(" : ")
             self.print_attribute(typ)
             return
 
@@ -191,44 +202,44 @@ class Printer:
             return
 
         if isinstance(attribute, Data):
-            self._print(f'!{attribute.name}<', end='')
+            self._print(f'!{attribute.name}<')
             attribute.print(self)
-            self._print(">", end='')
+            self._print(">")
             return
 
         assert isinstance(attribute, ParametrizedAttribute)
 
-        self._print(f'!{attribute.name}', end='')
+        self._print(f'!{attribute.name}')
         if len(attribute.parameters) != 0:
-            self._print("<", end='')
+            self._print("<")
             self.print_list(attribute.parameters, self.print_attribute)
-            self._print(">", end='')
+            self._print(">")
 
     def print_successors(self, successors: List[Block]):
         if len(successors) == 0:
             return
-        self._print(" (", end='')
+        self._print(" (")
         self.print_list(successors, self.print_block_name)
-        self._print(")", end='')
+        self._print(")")
 
     def _print_op_attributes(self, attributes: Dict[str, Attribute]) -> None:
         if len(attributes) == 0:
             return
 
-        self._print(" ", end='')
-        self._print("[", end='')
+        self._print(" ")
+        self._print("[")
 
         attribute_list = [p for p in attributes.items()]
-        self._print("\"%s\" = " % attribute_list[0][0], end='')
+        self._print("\"%s\" = " % attribute_list[0][0])
         self.print_attribute(attribute_list[0][1])
         for (attr_name, attr) in attribute_list[1:]:
-            self._print(", \"%s\" = " % attr_name, end='')
+            self._print(", \"%s\" = " % attr_name)
             self.print_attribute(attr)
-        self._print("]", end='')
+        self._print("]")
 
     def _print_op(self, op: Operation) -> None:
         self._print_results(op)
-        self._print(op.name, end='')
+        self._print(op.name)
         self._print_operands(op.operands)
         self.print_successors(op.successors)
         self._print_op_attributes(op.attributes)
