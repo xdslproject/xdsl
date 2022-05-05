dc1bff5cfb59c6aa835866d76c200e891fddab87
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-05-05 00:04:27 -0500

    Add base file for testing attribute definition

---
+++ tests/attribute_definition_test.py
@@ -0,0 +1,251 @@
+"""
+Test the definition of attributes and their constraints.
+"""
+
+from __future__ import annotations
+from dataclasses import dataclass
+from io import StringIO
+from typing import Any, List, Type, TypeGuard, TypeVar, cast
+
+import pytest
+
+from xdsl.ir import Attribute, Data, ParametrizedAttribute
+from xdsl.irdl import AllOf, AttrConstraint, BaseAttr, GenericData, ParamAttrConstraint, ParameterDef, VerifyException, attr_constr_coercion, irdl_attr_definition, builder, irdl_to_attr_constraint
+from xdsl.parser import Parser
+from xdsl.printer import Printer
+
+#  ____        _
+# |  _ \  __ _| |_ __ _
+# | | | |/ _` | __/ _` |
+# | |_| | (_| | || (_| |
+# |____/ \__,_|\__\__,_|
+#
+
+
+@irdl_attr_definition
+class BoolData(Data[bool]):
+    """An attribute holding a boolean value."""
+    name = "bool"
+
+    @staticmethod
+    def parse_parameter(parser: Parser) -> bool:
+        val = parser.parse_optional_ident()
+        if val == "True":
+            return True
+        elif val == "False":
+            return False
+        else:
+            raise Exception("Wrong argument passed to BoolAttr.")
+
+    @staticmethod
+    def print_parameter(data: bool, printer: Printer):
+        printer.print_string(str(data))
+
+
+def test_simple_data():
+    b = BoolData(True)
+    stream = StringIO()
+    p = Printer(stream=stream)
+    p.print_attribute(b)
+    assert stream.getvalue() == "!bool<True>"
+
+
+def test_simple_data_verifier_failure():
+    with pytest.raises(VerifyException) as e:
+        BoolData(2)
+    assert e.value.args[0] == ("bool data attribute expected type "
+                               "<class 'bool'>, but <class 'int'> given.")
+
+
+class IntListMissingVerifierData(Data[List[int]]):
+    """
+    An attribute holding a list of integers.
+    The definition should fail, since no verifier is provided, and the Data
+    type parameter is not a class.
+    """
+    name = "missing_verifier_data"
+
+    @staticmethod
+    def parse_parameter(parser: Parser) -> List[int]:
+        raise NotImplementedError()
+
+    @staticmethod
+    def print_parameter(data: List[int], printer: Printer) -> None:
+        raise NotImplementedError()
+
+
+def test_data_with_non_class_param_missing_verifier_failure():
+    with pytest.raises(Exception) as e:
+        irdl_attr_definition(IntListMissingVerifierData)
+    assert e.value.args[0] == (
+        'In IntListMissingVerifierData definition: '
+        'Cannot infer "verify" method. Type parameter of Data is not a class.')
+
+
+@irdl_attr_definition
+class IntListData(Data[List[int]]):
+    """
+    An attribute holding a list of integers.
+    """
+    name = "int_list"
+
+    @staticmethod
+    def parse_parameter(parser: Parser) -> List[int]:
+        raise NotImplementedError()
+
+    @staticmethod
+    def print_parameter(data: List[int], printer: Printer) -> None:
+        printer.print_string("[")
+        printer.print_list(data, lambda x: printer.print_string(str(x)))
+        printer.print_string("]")
+
+    def verify(self) -> None:
+        if not isinstance(self.data, list):
+            raise VerifyException("int_list data should hold a list.")
+        for elem in self.data:
+            if not isinstance(elem, int):
+                raise VerifyException(
+                    "int_list list elements should be integers.")
+
+
+def test_non_class_data():
+    attr = IntListData([0, 1, 42])
+    stream = StringIO()
+    p = Printer(stream=stream)
+    p.print_attribute(attr)
+    assert stream.getvalue() == "!int_list<[0, 1, 42]>"
+
+
+def test_simple_data_constructor_failure():
+    with pytest.raises(VerifyException) as e:
+        IntListData([0, 1, 42, ""])
+    assert e.value.args[0] == "int_list list elements should be integers."
+
+
+#   ____                      _      ____        _
+#  / ___| ___ _ __   ___ _ __(_) ___|  _ \  __ _| |_ __ _
+# | |  _ / _ \ '_ \ / _ \ '__| |/ __| | | |/ _` | __/ _` |
+# | |_| |  __/ | | |  __/ |  | | (__| |_| | (_| | || (_| |
+#  \____|\___|_| |_|\___|_|  |_|\___|____/ \__,_|\__\__,_|
+#
+
+_MissingGenericDataData = TypeVar("_MissingGenericDataData")
+
+
+class MissingGenericDataData(Data[_MissingGenericDataData]):
+    name = "array"
+
+    @staticmethod
+    def parse_parameter(parser: Parser) -> _MissingGenericDataData:
+        raise NotImplementedError()
+
+    @staticmethod
+    def print_parameter(data: _MissingGenericDataData,
+                        printer: Printer) -> None:
+        raise NotImplementedError()
+
+    def verify(self) -> None:
+        return
+
+
+def test_data_with_generic_missing_generic_data_failure():
+    with pytest.raises(Exception) as e:
+        irdl_to_attr_constraint(MissingGenericDataData[int])
+    assert e.value.args[0] == (
+        "Generic `Data` type 'array' cannot be converted to an attribute "
+        "constraint. Consider making it inherit from `GenericData` "
+        "instead of `Data`.")
+
+
+A = TypeVar("A", bound=Attribute)
+
+
+@dataclass
+class DataListAttr(AttrConstraint):
+    """
+    A constraint that enforces that the elements of a ListData all respect
+    a constraint.
+    """
+    elem_constr: AttrConstraint
+
+    def verify(self, attr: Attribute) -> None:
+        attr = cast(ListData, attr)
+        for e in attr.data:
+            self.elem_constr.verify(e)
+
+
+@irdl_attr_definition
+class ListData(GenericData[List[A]]):
+    name = "list"
+
+    @staticmethod
+    def parse_parameter(parser: Parser) -> List[A]:
+        raise NotImplementedError()
+
+    @staticmethod
+    def print_parameter(data: List[A], printer: Printer) -> None:
+        printer.print_string("[")
+        printer.print_list(data, printer.print_attribute)
+        printer.print_string("]")
+
+    @staticmethod
+    def generic_constraint_coercion(args: tuple[Any]) -> AttrConstraint:
+        assert len(args) == 1
+        return DataListAttr(irdl_to_attr_constraint(args[0]))
+
+    @staticmethod
+    @builder
+    def from_list(data: List[A]) -> ListData[A]:
+        return ListData(data)
+
+    def verify(self) -> None:
+        if not isinstance(self.data, list):
+            raise VerifyException(
+                f"Wrong type given to attribute {self.name}: got"
+                f" {type(self.data)}, but expected list of"
+                " attributes.")
+        for idx, val in enumerate(self.data):
+            if not isinstance(val, Attribute):
+                raise VerifyException(
+                    f"{self.name} data expects attribute list, but element "
+                    f"{idx} is of type {type(val)}.")
+
+
+def test_generic_data_verifier():
+    attr = ListData([BoolData(True), ListData([BoolData(False)])])
+    stream = StringIO()
+    p = Printer(stream=stream)
+    p.print_attribute(attr)
+    assert stream.getvalue() == "!list<[!bool<True>, !list<[!bool<False>]>]>"
+
+
+def test_generic_data_verifier_fail():
+    with pytest.raises(VerifyException) as e:
+        ListData([0])
+    assert e.value.args[0] == ("list data expects attribute list, but"
+                               " element 0 is of type <class 'int'>.")
+
+
+@irdl_attr_definition
+class ListDataWrapper(ParametrizedAttribute):
+    name = "list_wrapper"
+
+    val: ParameterDef[ListData[BoolData]]
+
+
+def test_generic_data_wrapper_verifier():
+    attr = ListDataWrapper([ListData([BoolData(True), BoolData(False)])])
+    stream = StringIO()
+    p = Printer(stream=stream)
+    p.print_attribute(attr)
+    assert stream.getvalue(
+    ) == "!list_wrapper<!list<[!bool<True>, !bool<False>]>>"
+
+
+def test_generic_data_wrapper_verifier_failure():
+    with pytest.raises(VerifyException) as e:
+        ListDataWrapper(
+            [ListData([BoolData(True),
+                       ListData([BoolData(False)])])])
+    assert e.value.args[
+        0] == "ListData(data=[BoolData(data=False)]) should be of base attribute bool"
