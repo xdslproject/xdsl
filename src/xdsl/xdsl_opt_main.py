9120be86b5e24870a57ddb0832e2d95cfecc7f5b
ShaolunWang <wang.shaolun@gmail.com>
2022-06-22 12:56:36 +0100

    use pattern matching to avoid empty argparse cases

--- src/xdsl/xdsl_opt_main.py
+++ src/xdsl/xdsl_opt_main.py
@@ -68,18 +68,24 @@ class xDSLOptMain:
             try:
                 self.apply_passes(module)
             except DiagnosticException as e:
-                if self.args.verbose_trace:
-                    if len(self.args.verbose_trace) == 0:
-                        f = Frame(e, 3)
-                        f.verbose(1)
-                    elif len(self.args.verbose_trace) == 1:
-                        f = Frame(e, self.args.verbose_trace[1])
-                        f.verbose(1)
-                    else:
-                        f = Frame(e, self.args.verbose_trace[1])
-                        f.verbose(self.args.verbose_trace[0])
-                else:
-                    print(e)
+
+                def get_code_length(e: DiagnosticException, n: int) -> Frame:
+                    return Frame(e, n) if n >= 1 else Frame(e, 3)
+
+                def verbose_frame_output(f: Frame, m: int) -> None:
+                    print("m: ",m)
+                    f.verbose(m) if m > 1 else f.verbose(1)
+
+                match self.args.verbose_trace:
+                    case []:
+                        verbose_frame_output(get_code_length(e, 0), 1)
+                    case [code_length]:
+                        verbose_frame_output(get_code_length(e, code_length), 1)
+                    case [code_length, frame_count, *_]: #if len(self.args.verbose_trace) >= 2:
+                        verbose_frame_output(get_code_length(e, code_length), frame_count)
+                    case _:
+                        print(len(self.args.verbose_trace))
+                        print(e)
                 exit(0)
 
         contents = self.output_resulting_program(module)
@@ -92,68 +98,68 @@ class xDSLOptMain:
         Add other/additional arguments by overloading this function.
         """
         arg_parser.add_argument("input_file",
-                                type=str,
-                                nargs="?",
-                                help="path to input file")
+                type=str,
+                nargs="?",
+                help="path to input file")
 
         targets = [name for name in self.available_targets]
         arg_parser.add_argument("-t",
-                                "--target",
-                                type=str,
-                                required=False,
-                                choices=targets,
-                                help="target",
-                                default="xdsl")
+                "--target",
+                type=str,
+                required=False,
+                choices=targets,
+                help="target",
+                default="xdsl")
 
         frontends = [name for name in self.available_frontends]
         arg_parser.add_argument(
-            "-f",
-            "--frontend",
-            type=str,
-            required=False,
-            choices=frontends,
-            help=
-            f"Frontend to be used for the input. If not set, the xdsl frontend "
-            "or the one for the file extension is used.")
+                "-f",
+                "--frontend",
+                type=str,
+                required=False,
+                choices=frontends,
+                help=
+                f"Frontend to be used for the input. If not set, the xdsl frontend "
+                "or the one for the file extension is used.")
 
         arg_parser.add_argument("--disable-verify",
-                                default=False,
-                                action='store_true')
+                default=False,
+                action='store_true')
         arg_parser.add_argument("-o",
-                                "--output-file",
-                                type=str,
-                                required=False,
-                                help="path to output file")
+                "--output-file",
+                type=str,
+                required=False,
+                help="path to output file")
 
         pass_names = ",".join([name for name in self.available_passes])
         arg_parser.add_argument(
-            "-p",
-            "--passes",
-            required=False,
-            help=
-            f"Delimited list of passes. Available passes are: {pass_names}",
-            type=str,
-            default="")
+                "-p",
+                "--passes",
+                required=False,
+                help=
+                f"Delimited list of passes. Available passes are: {pass_names}",
+                type=str,
+                default="")
 
         arg_parser.add_argument("--print-between-passes",
-                                default=False,
-                                action='store_true',
-                                help="Print the IR between each pass")
+                default=False,
+                action='store_true',
+                help="Print the IR between each pass")
 
         arg_parser.add_argument("--verify-diagnostics",
-                                default=False,
-                                action='store_true',
-                                help="Prints the content of a triggered "
-                                "exception and exits with code 0")
+                default=False,
+                action='store_true',
+                help="Prints the content of a triggered "
+                "exception and exits with code 0")
         arg_parser.add_argument(
-            "--verbose-trace",
-            default=False,
-            type=int,
-            nargs='*',
-            required=False,
-            help="Prints the verbose traceback"
-            "First argument: number of lines of code for the snippet. "
-            "Second argument: number of output frames ")
+                "--verbose-trace",
+                default=False,
+                type=int,
+                nargs='*',
+                required=False,
+                help="Prints the verbose traceback"
+                "First argument: number of lines of code for the snippet. "
+                "Second argument: number of output frames ")
 
     def register_all_dialects(self):
         """
@@ -182,7 +188,7 @@ class xDSLOptMain:
             module = parser.parse_op()
             if not (isinstance(module, ModuleOp)):
                 raise Exception(
-                    "Expected module or program as toplevel operation")
+                        "Expected module or program as toplevel operation")
             return module
 
         self.available_frontends['xdsl'] = parse_xdsl
@@ -226,14 +232,14 @@ class xDSLOptMain:
         Failes, if not all passes are registered.
         """
         pipeline = [
-            str(item) for item in self.args.passes.split(',') if len(item) > 0
-        ]
+                str(item) for item in self.args.passes.split(',') if len(item) > 0
+                ]
         for p in pipeline:
             if p not in self.available_passes:
                 raise Exception(f"Unrecognized pass: {p}")
 
         self.pipeline = [(p, lambda op, p=p: self.available_passes[p]
-                          (self.ctx, op)) for p in pipeline]
+            (self.ctx, op)) for p in pipeline]
 
     def parse_input(self) -> ModuleOp:
         """
