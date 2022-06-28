4a2ba9ce966e70382e602b451866811913f9e19c
purvi-h <s2024596@ed.ac.uk>
2022-06-28 11:51:41 +0100

    fixed errors in cmath and tested cmath operations

--- src/xdsl/dialects/cmath.py
+++ src/xdsl/dialects/cmath.py
@@ -18,19 +18,19 @@ class CMath:
 
 @irdl_attr_definition
 class ComplexType(ParametrizedAttribute):
-    name = "complex"
+    name = "cmath.complex"
     data: ParameterDef[Float64Type | Float32Type]
 
 @irdl_op_definition
 class Norm(Operation):
     name: str = "cmath.norm"
     
-    operands = OperandDef(ParamAttrConstraint(ComplexType, [AnyOf([Float32Type, Float64Type])] ) )
-    results = ResultDef(AnyOf([Float32Type, Float64Type]))
+    op = OperandDef(ParamAttrConstraint(ComplexType, [AnyOf([Float32Type, Float64Type])] ) )
+    res = ResultDef(AnyOf([Float32Type, Float64Type]))
 
     # TODO replace with trait
     def verify_(self) -> None:
-        if self.operands.typ != self.results.typ:
+        if self.op.typ.data != self.res.typ:
             raise VerifyException(
                 "expect all input and output types to be equal")
 
@@ -40,11 +40,11 @@ class Mul(Operation):
     
     lhs = OperandDef(ParamAttrConstraint(ComplexType, [AnyOf([Float32Type, Float64Type])] ) )
     rhs = OperandDef(ParamAttrConstraint(ComplexType, [AnyOf([Float32Type, Float64Type])] ) )
-    results = ResultDef(ParamAttrConstraint(ComplexType, [AnyOf([Float32Type, Float64Type])]))
+    res = ResultDef(ParamAttrConstraint(ComplexType, [AnyOf([Float32Type, Float64Type])]))
 
     # TODO replace with trait
     def verify_(self) -> None:
-        if self.lhs != self.rhs.typ and self.rhs.typ != self.results.typ:
+        if self.lhs != self.rhs.typ and self.rhs.typ != self.res.typ:
             raise VerifyException(
                 "expect all input and output types to be equal")
 
