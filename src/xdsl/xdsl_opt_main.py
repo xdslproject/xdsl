96af946065438a0c129f64c5ffb4d06366bdfb1e
Michel Weber <55622065+webmiche@users.noreply.github.com>
2022-07-15 16:20:16 +0200

    [LLVM] introduce and convert the LLVMStructType (#133)
    So I need the llvm.struct<(i32)> type for my project. Turns out: the llvm dialect is not exposed in the python bindings so I cannot go the usual route. For that reason, I came up with a really mad hack IMO. Feel free to comment if you see any way this could be improved.
    
    Co-authored-by: Fehr Mathieu <mathieu.fehr@gmail.com>

--- src/xdsl/xdsl_opt_main.py
+++ src/xdsl/xdsl_opt_main.py
@@ -14,6 +14,7 @@ from xdsl.dialects.builtin import *
 from xdsl.dialects.cmath import *
 from xdsl.dialects.cf import *
 from xdsl.dialects.irdl import *
+from xdsl.dialects.llvm import LLVM
 
 
 class xDSLOptMain:
@@ -151,6 +152,7 @@ class xDSLOptMain:
         cf = Cf(self.ctx)
         cmath = CMath(self.ctx)
         irdl = IRDL(self.ctx)
+        llvm = LLVM(self.ctx)
 
     def register_all_frontends(self):
         """
