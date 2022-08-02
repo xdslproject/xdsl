3a244801d029a5bd1400541ac47d75c34b3d7077
Alexander Viand <alexander.viand@inf.ethz.ch>
2022-08-02 13:13:53 +0200

    add initial version of symref dialect

---
+++ src/xdsl/dialects/symref.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+from typing import List, Union
+from dataclasses import dataclass
+
+from xdsl.ir import Attribute, MLContext
+from xdsl.irdl import (ResultDef, irdl_op_definition, AttributeDef, AnyAttr,
+                       Operation, OperandDef)
+from xdsl.dialects.builtin import (StringAttr, FlatSymbolRefAttr)
+
+
+@dataclass
+class Symref:
+    ctx: MLContext
+
+    def __post_init__(self):
+        self.ctx.register_op(Declare)
+        self.ctx.register_op(Update)
+        self.ctx.register_op(Fetch)
+
+
+@irdl_op_definition
+class Declare(Operation):
+    name: str = "symref.declare"
+
+    sym_name = AttributeDef(StringAttr)
+
+    @staticmethod
+    def get(sym_name: StringAttr) -> Declare:
+        return Declare.build(attributes={"sym_name": sym_name})
+
+
+@irdl_op_definition
+class Update(Operation):
+    name: str = "symref.update"
+
+    symbol = AttributeDef(FlatSymbolRefAttr)
+    value: OperandDef(AnyAttr())
+
+    @staticmethod
+    def get(symbol: FlatSymbolRefAttr, value: Attribute) -> Update:
+        return Update.build(operands=[value], attributes={"symbol": symbol})
+
+
+@irdl_op_definition
+class Fetch(Operation):
+    name: str = "symref.fetch"
+
+    symbol = AttributeDef(FlatSymbolRefAttr)
+    value: ResultDef(AnyAttr())
+
+    @staticmethod
+    def get(symbol: FlatSymbolRefAttr) -> Update:
+        return Update.build(attributes={"symbol": symbol})
