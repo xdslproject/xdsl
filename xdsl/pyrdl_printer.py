ecaa0569c76db2078b693ca7d040fe4c0e662081
Mathieu Fehr <mathieu.fehr@gmail.com>
2022-10-05 23:31:23 +0200

    Print operation definitions

--- xdsl/pyrdl_printer.py
+++ xdsl/pyrdl_printer.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from io import IOBase
 from typing import Any
 from xdsl.dialects.builtin import ModuleOp
-from xdsl.dialects.irdl import DialectOp
+from xdsl.dialects.irdl import DialectOp, OperationOp
 
 INDENTATION_SIZE: int = 4
 """The number of spaces per identation level"""
@@ -40,9 +40,30 @@ class PyRDLPrinter:
             for op in dialect.get_op_defs()
         ]
 
+        # Print the ctx field
         self._print(' ' * INDENTATION_SIZE, 'ctx: MLContext')
-        self._print('\n')
+        self._print('')
+
+        # Print the op registration
         self._print(' ' * INDENTATION_SIZE, 'def __post_init__(self):')
         for op_py_name in op_py_names:
             self._print(' ' * INDENTATION_SIZE * 2,
                         f'self.ctx.register_op({op_py_name})')
+        self._print('')
+
+        # Print each op definition
+        for op in dialect.get_op_defs():
+            self.print_op(op)
+            self._print('')
+            self._print('')
+
+    def print_op(self, op: OperationOp) -> None:
+        """Convert the op definition to pyrdl."""
+        op_py_name = self.snake_case_to_pascal_case(op.op_name.data)
+        self._print('@irdl_op_definition')
+        self._print(f'class {op_py_name}(Operation):')
+        self._print(' ' * INDENTATION_SIZE, 'name = ', f'"{op.op_name.data}"')
+
+        self._print(' ' * INDENTATION_SIZE, 'ops = VarOperandDef(AnyAttr())')
+        self._print(' ' * INDENTATION_SIZE, 'res = VarResultDef(AnyAttr())')
+        self._print(' ' * INDENTATION_SIZE, 'regs = VarRegionDef(AnyAttr())')
