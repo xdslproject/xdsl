63049530cc6e7c5dd42b6168159fbed9cb84d2ba
meshtag <prathameshtagore@gmail.com>
2023-01-01 14:00:10 +0530

    Testing: Add tests for exception handling

--- tests/dialects/test_vector.py
+++ tests/dialects/test_vector.py
@@ -1,4 +1,6 @@
-from xdsl.dialects.builtin import i32, IntegerType, IndexType, VectorType
+import pytest
+
+from xdsl.dialects.builtin import i32, i64, IntegerType, IndexType, VectorType
 from xdsl.dialects.memref import MemRefType
 from xdsl.dialects.vector import Load, Store
 from xdsl.ir import OpResult
@@ -52,6 +54,32 @@ def test_vector_load_i32_with_dimensions():
     assert load.indices[1] is index2
 
 
+def test_vector_load_verify_type_matching():
+    res_vector_type = VectorType.from_type_and_list(i64)
+
+    i32_memref_type = MemRefType.from_type_and_list(i32, [4, 5])
+    memref_ssa_value = OpResult(i32_memref_type, [], [])
+
+    load = Load.build(operands=[memref_ssa_value, []],
+                      result_types=[res_vector_type])
+
+    with pytest.raises(Exception) as exc_info:
+        load.verify()
+    assert exc_info.value.args[
+        0] == "MemRef element type should match the Vector element type."
+
+
+def test_vector_load_verify_indexing_exception():
+    i32_memref_type = MemRefType.from_type_and_list(i32, [2, 3])
+    memref_ssa_value = OpResult(i32_memref_type, [], [])
+
+    load = Load.get(memref_ssa_value, [])
+
+    with pytest.raises(Exception) as exc_info:
+        load.verify()
+    assert exc_info.value.args[0] == "Expected an index for each dimension."
+
+
 def test_vector_store_i32():
     i32_vector_type = VectorType.from_type_and_list(i32)
     vector_ssa_value = OpResult(i32_vector_type, [], [])
@@ -81,3 +109,32 @@ def test_vector_store_i32_with_dimensions():
     assert store.vector is vector_ssa_value
     assert store.indices[0] is index1
     assert store.indices[1] is index2
+
+
+def test_vector_store_verify_type_matching():
+    i64_vector_type = VectorType.from_type_and_list(i64, [2, 3])
+    vector_ssa_value = OpResult(i64_vector_type, [], [])
+
+    i32_memref_type = MemRefType.from_type_and_list(i32, [4, 5])
+    memref_ssa_value = OpResult(i32_memref_type, [], [])
+
+    store = Store.get(vector_ssa_value, memref_ssa_value, [])
+
+    with pytest.raises(Exception) as exc_info:
+        store.verify()
+    assert exc_info.value.args[
+        0] == "MemRef element type should match the Vector element type."
+
+
+def test_vector_store_verify_indexing_exception():
+    i32_vector_type = VectorType.from_type_and_list(i32, [2, 3])
+    vector_ssa_value = OpResult(i32_vector_type, [], [])
+
+    i32_memref_type = MemRefType.from_type_and_list(i32, [4, 5])
+    memref_ssa_value = OpResult(i32_memref_type, [], [])
+
+    store = Store.get(vector_ssa_value, memref_ssa_value, [])
+
+    with pytest.raises(Exception) as exc_info:
+        store.verify()
+    assert exc_info.value.args[0] == "Expected an index for each dimension."
