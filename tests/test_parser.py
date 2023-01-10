546fda56492da247a89324af92e92cb869b71305
Nick Brown <nick@mesham.com>
2023-01-10 21:40:47 +0000

    xdsl: DictionaryAttr for the builtin dialect (#258)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -13,3 +13,24 @@ def test_int_list_parser(input, expected):
 
     int_list = parser.parse_list(parser.parse_int_literal)
     assert int_list == expected
+
+
+@pytest.mark.parametrize("input,expected", [('"A"=0, "B"=1, "C"=2', {
+    "A": 0,
+    "B": 1,
+    "C": 2
+}), ('"MA"=10, "BR"=7, "Z"=3', {
+    "MA": 10,
+    "BR": 7,
+    "Z": 3
+}), ('"Q"=77, "VV"=12, "AA"=-8', {
+    "Q": 77,
+    "VV": 12,
+    "AA": -8
+})])
+def test_int_dictionary_parser(input, expected):
+    ctx = MLContext()
+    parser = Parser(ctx, input)
+
+    int_dict = parser.parse_dictionary(parser.parse_int_literal)
+    assert int_dict == expected
