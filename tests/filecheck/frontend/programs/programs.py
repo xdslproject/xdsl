63ef31590790dbfbdb8ef0981038e4adc71226c7
George Mitenkov <georgemitenk0v@gmail.com>
2023-01-12 13:44:12 +0000

    frontend: Add initial skeleton (#322)

---
+++ tests/filecheck/frontend/programs/programs.py
@@ -0,0 +1,12 @@
+# RUN: python %s | filecheck %s
+
+from xdsl.frontend.program import FrontendProgram
+from xdsl.frontend.context import CodeContext
+
+p = FrontendProgram()
+with CodeContext(p):
+    # CHECK: builtin.module() {}
+    pass
+
+p.compile(desymref=False)
+print(p.xdsl())
