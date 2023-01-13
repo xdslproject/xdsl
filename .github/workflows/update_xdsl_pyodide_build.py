ca4f92687e20e4ab26a09fc126bedba765683e24
Emilien Bauer <papychacal@gmail.com>
2023-01-13 14:58:07 +0000

    CI: Test JupyterLite build even if not deploying. (#335)
    Co-authored-by: Emilien Bauer <bauer.emilien@gmail.com>

--- .github/workflows/update_xdsl_pyodide_build.py
+++ .github/workflows/update_xdsl_pyodide_build.py
@@ -20,7 +20,7 @@ yaml_doc["requirements"] = {"run": ["frozenlist"]}
 
 # Find the built source distribution. This assumes it is the only thing in xdsl/dist
 xdsl_sdist = os.listdir(os.path.join(xdsl_directory, "dist"))[0]
-xdsl_sdist = os.path.join(xdsl_directory, "dist", xdsl_sdist)
+xdsl_sdist = os.path.abspath(os.path.join(xdsl_directory, "dist", xdsl_sdist))
 sha256_hash = hashlib.sha256()
 with open(xdsl_sdist, "rb") as sdist:
     # Read and update hash string value in blocks of 4K
@@ -28,6 +28,9 @@ with open(xdsl_sdist, "rb") as sdist:
         sha256_hash.update(byte_block)
 
 # Make it build the local xDSL, not the PyPi release. The pyodide build still requires the SHA256 sum.
-yaml_doc["source"] = {"url": xdsl_sdist, "sha256": sha256_hash.hexdigest()}
+yaml_doc["source"] = {
+    "url": f"file://{xdsl_sdist}",
+    "sha256": sha256_hash.hexdigest()
+}
 with open(meta_yaml_path, "w") as f:
     yaml.dump(yaml_doc, f)
