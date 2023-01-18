ac7581fdb81ec438ddedcf3743e9cd78e964e858
George Bisbas <g.bisbas18@imperial.ac.uk>
2023-01-18 16:56:58 +0000

    tests: Cover case of "one" in CustomFormatAttr

--- tests/test_attribute_definition.py
+++ tests/test_attribute_definition.py
@@ -276,11 +276,13 @@ def test_annotated_constraint():
     assert stream.getvalue() == "!positive_int<!int<42>>"
 
 
-def test_annotated_constraint_fail():
-    """Test that the verifier of an annotated constraint can fail."""
-    with pytest.raises(Exception) as e:
-        PositiveIntAttr([IntData(-42)])
-    assert e.value.args[0] == "Expected positive integer, got -42."
+class Test_annotated_constraint_fail:
+
+    def test_fail_I(self):
+        """Test that the verifier of an annotated constraint can fail."""
+        with pytest.raises(Exception) as e:
+            PositiveIntAttr([IntData(-42)])
+        assert e.value.args[0] == "Expected positive integer, got -42."
 
 
 #  _____               __     __          ____                _
@@ -519,25 +521,35 @@ class ListData(GenericData[list[A]]):
 AnyListData: TypeAlias = ListData[Attribute]
 
 
-def test_generic_data_verifier():
-    """
-    Test that a GenericData can be created.
-    """
-    attr = ListData([BoolData(True), ListData([BoolData(False)])])
-    stream = StringIO()
-    p = Printer(stream=stream)
-    p.print_attribute(attr)
-    assert stream.getvalue() == "!list<[!bool<True>, !list<[!bool<False>]>]>"
+class Test_generic_data_verifier:
 
+    def test_generic_data_verifier(self):
+        """
+        Test that a GenericData can be created.
+        """
+        attr = ListData([BoolData(True), ListData([BoolData(False)])])
+        stream = StringIO()
+        p = Printer(stream=stream)
+        p.print_attribute(attr)
+        assert stream.getvalue() == "!list<[!bool<True>, !list<[!bool<False>]>]>"
 
-def test_generic_data_verifier_fail():
-    """
-    Test that a GenericData verifier fails when given wrong parameters.
-    """
-    with pytest.raises(VerifyException) as e:
-        ListData([0])  # type: ignore
-    assert e.value.args[0] == ("list data expects attribute list, but"
-                               " element 0 is of type <class 'int'>.")
+    def test_generic_data_verifier_fail(self):
+        """
+        Test that a GenericData verifier fails when given wrong parameters.
+        """
+        with pytest.raises(VerifyException) as e:
+            ListData([0])  # type: ignore
+        assert e.value.args[0] == ("list data expects attribute list, but"
+                                   " element 0 is of type <class 'int'>.")
+
+    def test_generic_data_verifier_fail_II(self):
+        """
+        Test that a GenericData verifier fails when given wrong parameters.
+        """
+        with pytest.raises(VerifyException) as e:
+            ListData((0))  # type: ignore
+        assert e.value.args[0] == ("Wrong type given to attribute list: "
+                                   "got <class 'int'>, but expected list of attributes.")
 
 
 @irdl_attr_definition
