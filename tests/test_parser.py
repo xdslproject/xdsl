b6678eeaab05ac79209e3e7ba41d3098e0cbd59b
Sasha Lopoukhine <superlopuh@gmail.com>
2023-01-23 12:27:52 +0000

    xdsl: parse dictionary as non-optional (#342)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -15,15 +15,15 @@ def test_int_list_parser(input: str, expected: list[int]):
     assert int_list == expected
 
 
-@pytest.mark.parametrize("input,expected", [('"A"=0, "B"=1, "C"=2', {
+@pytest.mark.parametrize("input,expected", [('{"A"=0, "B"=1, "C"=2}', {
     "A": 0,
     "B": 1,
     "C": 2
-}), ('"MA"=10, "BR"=7, "Z"=3', {
+}), ('{"MA"=10, "BR"=7, "Z"=3}', {
     "MA": 10,
     "BR": 7,
     "Z": 3
-}), ('"Q"=77, "VV"=12, "AA"=-8', {
+}), ('{"Q"=77, "VV"=12, "AA"=-8}', {
     "Q": 77,
     "VV": 12,
     "AA": -8
@@ -32,6 +32,6 @@ def test_int_dictionary_parser(input: str, expected: dict[str, int]):
     ctx = MLContext()
     parser = Parser(ctx, input)
 
-    int_dict = parser.parse_dictionary(parser.parse_optional_str_literal,
-                                       parser.parse_optional_int_literal)
+    int_dict = parser.parse_dictionary(parser.parse_str_literal,
+                                       parser.parse_int_literal)
     assert int_dict == expected
