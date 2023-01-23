db1b24a0f4f72470bc3970bfd3318cfd550a4cbe
Sasha Lopoukhine <superlopuh@gmail.com>
2023-01-23 14:25:37 +0000

    add tests and __init__.py files

---
+++ docs/Toy/tests/test_toy.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import pytest
+
+from ..toy.parser import Parser
+
+
+def test_memreftype():
+    ast_toy = Path() / 'docs' / 'Toy' / 'examples' / 'ast.toy'
+
+    with open(ast_toy, 'r') as f:
+        parser = Parser(ast_toy, f.read())
+
+    parsed_module_ast = parser.parseModule()
+
+    assert parsed_module_ast is not None
