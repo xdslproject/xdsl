2d69772166dd72016387b9f54c2f0a05acf8eafe
Sasha Lopoukhine <superlopuh@gmail.com>
2023-01-24 12:37:03 +0000

    misc: add Toy chapter 1 python code, examples and notebook (#354)

---
+++ docs/Toy/toy/tests/test_toy.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from ..parser import Parser
+
+
+def test_parse_ast():
+    ast_toy = Path('docs/Toy/examples/ast.toy')
+
+    with open(ast_toy, 'r') as f:
+        parser = Parser(ast_toy, f.read())
+
+    parsed_module_ast = parser.parseModule()
+
+    assert parsed_module_ast is not None
