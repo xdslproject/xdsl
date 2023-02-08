65c424228304ed463999d0a8d69ab563f283241f
Fehr Mathieu <mathieu.fehr@gmail.com>
2023-02-08 15:50:01 +0100

    API: Fix pyright issue in printer.py (#419)

--- xdsl/printer.py
+++ xdsl/printer.py
@@ -446,8 +446,7 @@ class Printer:
         # Dense resources have an alias in MLIR, but not in xDSL
         if isinstance(attribute, DenseResourceAttr):
             handle = attribute.resource_handle.data
-            type = attribute.type
-            self.print(f"dense_resource<{handle}> : ", type)
+            self.print(f"dense_resource<{handle}> : ", attribute.type)
             return
 
         # vector types have an alias in MLIR, but not in xDSL
