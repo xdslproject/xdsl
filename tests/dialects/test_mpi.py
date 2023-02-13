ef8b5d3770b73778bf2eaebcf76e694f5d56c44d
George Bisbas <g.bisbas18@imperial.ac.uk>
2023-02-13 19:51:04 +0000

    xdsl: Init MPI

---
+++ tests/dialects/test_mpi.py
@@ -0,0 +1,28 @@
+import pytest
+from conftest import assert_print_op
+from xdsl.dialects.mpi import MPIRequestOp, MPIRequest, MPI_Wait, MPIStatus
+from xdsl.ir import Region
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.printer import Printer
+
+
+def test_MPIRequest():
+    # This test constructs and MPIRequest and feeds it as input to
+    # an MPI_Wait operation
+    request = MPIRequest()
+    request_op = MPIRequestOp.from_attr(request)
+    mpiwait = MPI_Wait.from_callable(request_op)
+
+    assert request_op.results[0].typ is request
+    assert mpiwait.operands[0] is request_op.results[0]
+
+    op0 = ModuleOp.from_region_or_ops([request_op, mpiwait])
+    expected0 = \
+    """
+    builtin.module() {
+  %0 : !MPI_Request = mpi.MPIRequestOp()
+  %1 : !MPI_Status = mpi.MPI_Wait(%0 : !MPI_Request)
+}
+    """
+
+    assert_print_op(op0, expected0, None)
--- xdsl/dialects/mpi.py
+++ xdsl/dialects/mpi.py
@@ -1,44 +1,110 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import List, Union
+from typing import Annotated, List, TYPE_CHECKING, Union
 
-from xdsl.dialects.builtin import IntegerAttr, StringAttr, FunctionType, Attribute, FlatSymbolRefAttr
-from xdsl.ir import MLContext, SSAValue
-from xdsl.irdl import (OptAttributeDef, irdl_op_definition, VarOperandDef,
-                       AnyAttr, Block, RegionDef, Region, Operation,
-                       AttributeDef, VarResultDef)
+from xdsl.dialects.builtin import IntegerAttr, Attribute
+from xdsl.dialects.memref import MemRefType
+from xdsl.ir import Data, OpResult, SSAValue, Dialect
+from xdsl.ir import ParametrizedAttribute
 
+from xdsl.irdl import (OpAttr, irdl_op_definition, Block, Region, Operation, Operand, OpResult,
+                       builder, irdl_attr_definition, AnyAttr)
 
-@dataclass
-class MpiCall:
-    ctx: MLContext
-
-    def __post_init__(self):
-        self.ctx.register_op(MPI_Send)
+if TYPE_CHECKING:
+    from xdsl.parser import BaseParser
+    from utils.exceptions import ParseError
+    from xdsl.printer import Printer
 
 
 @irdl_op_definition
-class MPI_Send(Operation):
-    name: str = "func.MPI_Send"
+class MPI_ISend(Operation):
+    name: str = "mpi.MPI_ISend"
 
-    body = RegionDef()
-    sym_name = AttributeDef(StringAttr)
-    function_type = AttributeDef(FunctionType)
-    sym_visibility = OptAttributeDef(StringAttr)
+    buffer: Annotated[Operand, MemRefType]
+    count: Annotated[Operand, IntegerAttr]
+    recipient: Annotated[Operand, IntegerAttr]
+    tag: Annotated[Operand, IntegerAttr]
+    communicator: Annotated[Operand, IntegerAttr]
+    request: Annotated[Operand, IntegerAttr]
+
 
     @staticmethod
-    def from_callable(name: str,
-                      count: IntegerAttr,
-                      input_types: List[Attribute],
-                      return_types: List[Attribute],
-                      func: Block.BlockCallback) -> MPI_Send:
+    def from_callable(buffer: Union[Operation, SSAValue],
+                      count: Union[Operation, SSAValue],
+                      recipient: Union[Operation, SSAValue],
+                      tag: Union[Operation, SSAValue],
+                      communicator: Union[Operation, SSAValue],
+                      request: Union[Operation, SSAValue]) -> MPI_ISend:
         type_attr = FunctionType.from_lists(input_types, return_types)
         attributes = {
             "sym_name": name,
             "function_type": type_attr,
             "sym_visibility": "private"
         }
-        op = MPI_Send.build(attributes=attributes,
-                            regions=[Region.from_block_list(
+        op = MPI_ISend.build(attributes=attributes,
+                             regions=[Region.from_block_list(
                                      [Block.from_callable(input_types, func)])])
         return op
+
+
+@irdl_attr_definition
+class MPIIntAttr(Data[int]):
+    name = "mpi_int"
+
+    @staticmethod
+    def parse_parameter(parser: BaseParser) -> int:
+        data = parser.parse_int_literal()
+        return data
+
+    @staticmethod
+    def print_parameter(data: int, printer: Printer) -> None:
+        printer.print_string(f'{data}')
+
+    @staticmethod
+    @builder
+    def from_int(data: int) -> MPIIntAttr:
+        return MPIIntAttr(data)
+
+
+@irdl_attr_definition
+class MPIRequest(ParametrizedAttribute):
+    name = "MPI_Request"
+
+
+@irdl_attr_definition
+class MPIStatus(ParametrizedAttribute):
+    name = "MPI_Status"
+
+
+@irdl_op_definition
+class MPIRequestOp(Operation):
+    # Name of the type. This is used for printing and parsing.
+    name: str = "mpi.MPIRequestOp"
+    request: Annotated[Operand, AnyAttr()]
+    # output: Annotated[OpResult, Attribute]
+
+    @staticmethod
+    def from_attr(request: AnyAttr) -> MPIRequestOp:
+        return MPIRequestOp.create(operands=[],
+                                   result_types=[request])
+
+
+@irdl_op_definition
+class MPI_Wait(Operation):
+    name: str = "mpi.MPI_Wait"
+
+    request: Annotated[Operand, AnyAttr()]
+    status: Annotated[OpResult, MPIStatus]
+
+    @staticmethod
+    def from_callable(request: Operation) -> MPI_Wait:
+        op1 = SSAValue.get(request)
+        return MPI_Wait.build(operands=[op1], result_types=[MPIStatus()])
+
+
+MPI = Dialect(
+    [MPI_Wait, MPI_ISend, MPIRequestOp],
+    [
+        MPIRequest,
+        MPIStatus
+    ])
