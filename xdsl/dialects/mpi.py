737982b1295cf5c4ec5f94b63afbb676cc1c5bd3
Anton Lydike <me@antonlydike.de>
2023-02-14 10:02:36 +0000

    dialects: mpi: add irecv

--- xdsl/dialects/mpi.py
+++ xdsl/dialects/mpi.py
@@ -41,11 +41,41 @@ class ISend(MPIBaseOp):
     request: Annotated[OpResult, Request()]
 
     @classmethod
-    def get(cls, buff: Operand, dest: Operand, tag: int):
+    def get(cls, buff: Operand, dest: Operand, tag: int | None):
+        attrs = {}
+
+        if tag is not None:
+            attrs['tag'] = IntegerAttr.from_params(tag, t_int)
+
         return cls.build(
             operands=[buff, dest],
-            attributes={'tag': IntegerAttr.from_params(tag, t_int)},
+            attributes=attrs,
             result_types=[Request()]
+        )
+
+
+@irdl_op_definition
+class IRecv(MPIBaseOp):
+    name = "mpi.irecv"
+
+    source: Annotated[Operand, t_int]
+
+    tag: OptOpAttr[IntegerAttr]
+
+    buffer: Annotated[OpResult, MemRefType[AnyNumericAttr]]
+    request: Annotated[OpResult, Request()]
+
+    @classmethod
+    def get(cls, source: Operand, dtype: MemRefType[AnyNumericAttr], tag: int | None = None):
+        attrs = {}
+
+        if tag is not None:
+            attrs['tag'] = IntegerAttr.from_params(tag, t_int)
+
+        return cls.build(
+            operands=[source],
+            attributes=attrs,
+            result_types=[dtype, Request()]
         )
 
 
@@ -53,7 +83,8 @@ if __name__ == '__main__':
     reg = Region.from_operation_list([
         memref := Alloc.get(f64, 32, [100, 14, 14]),
         dest := Constant.from_int_and_width(1, t_int),
-        req := ISend.get(memref, dest, 1)
+        req := ISend.get(memref, dest, 1),
+        res := IRecv.get(dest, memref.results[0].typ, 1)
     ])
 
     printer.print_region(reg)
@@ -67,7 +98,7 @@ if __name__ == '__main__':
 
 // example irecv
 // %source is the source rank (si32)
-%data, %request = "mpi.irecv"(%source) {"tag" = 1, "dtype" = !memref<3x2x2xi64>} : (!si32) -> (!memref<3x2x2xi64>, !mpi.request)
+%data, %request = "mpi.irecv"(%source) {"tag" = 1} : (!si32) -> (!memref<3x2x2xi64>, !mpi.request)
 
 // example test
 // %request is an !mpi.request
