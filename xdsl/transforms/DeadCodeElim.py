501107a52e76e6a5202a4cd42bd89e1a467f76dd
Michel Weber <michel.weber@inf.ethz.ch>
2023-02-27 19:46:04 +0100

    address comments

--- xdsl/transforms/DeadCodeElim.py
+++ xdsl/transforms/DeadCodeElim.py
@@ -5,16 +5,18 @@ from xdsl.ir import MLContext, Operation
 from xdsl.dialects.builtin import ModuleOp
 
 
-class DCEImpl(RewritePattern):
+class UnusedOperationRemover(RewritePattern):
 
-    def match_and_rewrite(self, op: Operation, rewriter: PatternRewriter, /):
+    def match_and_rewrite(self, op: NoSideEffect, rewriter: PatternRewriter,
+                          /):
         if isinstance(op, NoSideEffect) and all(
             [len(res.uses) == 0 for res in op.results]):
             rewriter.erase_matched_op()
 
 
 def dce(ctx: MLContext, module: ModuleOp):
-    walker = PatternRewriteWalker(GreedyRewritePatternApplier([DCEImpl()]),
+    walker = PatternRewriteWalker(GreedyRewritePatternApplier(
+        [UnusedOperationRemover()()]),
                                   walk_regions_first=True,
                                   apply_recursively=False,
                                   walk_reverse=True)
