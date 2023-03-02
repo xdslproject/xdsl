155bc7ca6b2173cfc072d576835a2af2920abdd1
Michel Weber <55622065+webmiche@users.noreply.github.com>
2023-03-02 16:07:44 +0100

    bug: Fix error message on missing Operands in the context of variadic Operands (#495)

--- xdsl/irdl.py
+++ xdsl/irdl.py
@@ -780,8 +780,8 @@ def get_variadic_sizes_from_attr(op: Operation,
         if not isinstance(arg_def, VariadicDef) and arg_size != 1:
             raise VerifyException(
                 f"non-variadic {get_construct_name(construct)} {arg_name} is expected "
-                f"to be of size 0 or 1 in {size_attribute_name}, but got "
-                f"{arg_size}")
+                f"to be of size 1 in {size_attribute_name}, but got {arg_size}"
+            )
 
         if isinstance(arg_def, VariadicDef):
             variadic_sizes.append(arg_size)
