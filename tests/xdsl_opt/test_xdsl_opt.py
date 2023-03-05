b0c973eb5ad70c53aa3d08db4ea22ac9a5735023
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-03-05 16:40:11 +0100

    Close open files in xdsl-opt and tests

--- tests/xdsl_opt/test_xdsl_opt.py
+++ tests/xdsl_opt/test_xdsl_opt.py
@@ -23,8 +23,9 @@ def test_empty_program():
     with redirect_stdout(f):
         opt.run()
 
-    expected = open(filename, 'r').read()
-    assert f.getvalue().strip() == expected.strip()
+    with open(filename, 'r') as file:
+        expected = file.read()
+        assert f.getvalue().strip() == expected.strip()
 
 
 @pytest.mark.parametrize(
@@ -67,11 +68,14 @@ def test_wrong_target():
 def test_print_to_file():
     filename_in = 'tests/xdsl_opt/empty_program.xdsl'
     filename_out = 'tests/xdsl_opt/empty_program.out'
-    opt = xDSLOptMain(args=[filename_in, '-o', filename_out])
 
-    inp = open(filename_in, 'r').read()
+    opt = xDSLOptMain(args=[filename_in, '-o', filename_out])
     opt.run()
-    expected = open(filename_out, 'r').read()
+
+    with open(filename_in, 'r') as file:
+        inp = file.read()
+    with open(filename_out, 'r') as file:
+        expected = file.read()
 
     assert inp.strip() == expected.strip()
 
@@ -94,6 +98,7 @@ def test_operation_deletion():
     f = StringIO("")
     with redirect_stdout(f):
         opt.run()
-    expected = open(filename_out, 'r').read()
+    with open(filename_out, 'r') as file:
+        expected = file.read()
 
     assert f.getvalue().strip() == expected.strip()
