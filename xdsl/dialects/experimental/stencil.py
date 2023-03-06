54afef07d4980172f189a5a3a801d57264ce8801
meshtag <prathameshtagore@gmail.com>
2023-03-06 09:40:39 +0530

    Fix lb and ub in Combine op

--- xdsl/dialects/experimental/stencil.py
+++ xdsl/dialects/experimental/stencil.py
@@ -337,8 +337,8 @@ class Combine(Operation):
     lower_ext: Annotated[VarOperand, TempType]
     upper_ext: Annotated[VarOperand, TempType]
 
-    # lb = OptOpAttr[Stencil_Index]
-    # ub = OptOpAttr[Stencil_Index]
+    lb: OptOpAttr[Stencil_Index]
+    ub: OptOpAttr[Stencil_Index]
 
     region: Region
     res: VarOpResult
