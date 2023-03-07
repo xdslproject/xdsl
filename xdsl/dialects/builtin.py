f9678337ad7cf1b99543023b239c63d447129039
Sasha Lopoukhine <superlopuh@gmail.com>
2023-03-07 17:18:00 +0000

    add method to parse dictionary attr MLIR style to BaseParser

--- xdsl/dialects/builtin.py
+++ xdsl/dialects/builtin.py
@@ -405,9 +405,7 @@ class DictionaryAttr(GenericData[dict[str, Attribute]]):
 
     @staticmethod
     def parse_parameter(parser: BaseParser) -> dict[str, Attribute]:
-        # force MLIR style parsing of attribute
-        from xdsl.parser import MLIRParser
-        return MLIRParser.parse_optional_attr_dict(parser)
+        return parser.parse_optional_dictionary_attr_dict()
 
     @staticmethod
     def print_parameter(data: dict[str, Attribute], printer: Printer) -> None:
