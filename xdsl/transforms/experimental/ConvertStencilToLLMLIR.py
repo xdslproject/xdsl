de91291e78f06b893de93b7d00fb6e2903f3644d
Emilien Bauer <papychacal@gmail.com>
2023-03-14 17:35:23 +0000

    dialects: Add a stencil.CastOp -> memref.Cast lowering. (#545)

--- xdsl/transforms/experimental/ConvertStencilToLLMLIR.py
+++ xdsl/transforms/experimental/ConvertStencilToLLMLIR.py
@@ -1,67 +1,76 @@
 from typing import TypeVar
 
 from xdsl.pattern_rewriter import (PatternRewriter, PatternRewriteWalker,
-                                   RewritePattern, GreedyRewritePatternApplier)
-from xdsl.ir import MLContext, Operation
+                                   RewritePattern, GreedyRewritePatternApplier,
+                                   op_type_rewrite_pattern)
+from xdsl.ir import MLContext
 from xdsl.irdl import Attribute
-from xdsl.dialects.builtin import ArrayAttr, FunctionType, IntegerAttr, ModuleOp, AnyIntegerAttr, IndexType
+from xdsl.dialects.builtin import FunctionType, ModuleOp
 from xdsl.dialects.func import FuncOp
 from xdsl.dialects.memref import MemRefType
+from xdsl.dialects import memref
 
-from xdsl.dialects.experimental.stencil import FieldType, IndexAttr
+from xdsl.dialects.experimental.stencil import CastOp, FieldType, IndexAttr
 
 _TypeElement = TypeVar("_TypeElement", bound=Attribute)
 
 
 def GetMemRefFromField(
         inputFieldType: FieldType[_TypeElement]) -> MemRefType[_TypeElement]:
-    memref_shape_integer_attr_list: list[AnyIntegerAttr] = []
-    for i in inputFieldType.shape.data:
-        memref_shape_integer_attr_list.append(
-            IntegerAttr.from_params(i.value.data, i.typ))
+    dims = [i.value.data for i in inputFieldType.shape.data]
 
-    memref_shape_array_attr = ArrayAttr[AnyIntegerAttr](
-        memref_shape_integer_attr_list)
-
-    return MemRefType.from_params(inputFieldType.element_type,
-                                  memref_shape_array_attr)
+    return MemRefType.from_element_type_and_shape(inputFieldType.element_type,
+                                                  dims)
 
 
 def GetMemRefFromFieldWithLBAndUB(memref_element_type: _TypeElement,
                                   lb: IndexAttr,
                                   ub: IndexAttr) -> MemRefType[_TypeElement]:
-    memref_shape_integer_attr_list: list[AnyIntegerAttr] = []
-    for i in range(len(lb.array.data)):
-        memref_shape_integer_attr_list.append(
-            IntegerAttr.from_params(
-                ub.array.data[i].value.data - lb.array.data[i].value.data,
-                IndexType()))
+    # lb and ub defines the minimum and maximum coordinates of the resulting memref,
+    # so its shape is simply ub - lb, computed here.
+    dims = [
+        ub.value.data - lb.value.data
+        for lb, ub in zip(lb.array.data, ub.array.data)
+    ]
 
-    memref_shape_array_attr = ArrayAttr(memref_shape_integer_attr_list)
+    return MemRefType.from_element_type_and_shape(memref_element_type, dims)
 
-    return MemRefType.from_params(memref_element_type, memref_shape_array_attr)
+
+class CastOpToMemref(RewritePattern):
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: CastOp, rewriter: PatternRewriter, /):
+        if not isinstance(op.field.typ, FieldType):
+            return
+
+        field_typ: FieldType[Attribute] = op.field.typ
+
+        result_typ = GetMemRefFromFieldWithLBAndUB(field_typ.element_type,
+                                                   op.lb, op.ub)
+
+        rewriter.replace_matched_op(memref.Cast.get(op.field, result_typ))
 
 
 class StencilTypeConversionFuncOp(RewritePattern):
 
-    def match_and_rewrite(self, op: Operation, rewriter: PatternRewriter, /):
-        if (isinstance(op, FuncOp)):
-            inputs: list[Attribute] = []
-            for arg in op.body.blocks[0].args:
-                if isinstance(arg.typ, FieldType):
-                    typ: FieldType[Attribute] = arg.typ
-                    memreftyp = GetMemRefFromField(typ)
-                    rewriter.modify_block_argument_type(arg, memreftyp)
-                    inputs.append(memreftyp)
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: FuncOp, rewriter: PatternRewriter, /):
+        inputs: list[Attribute] = []
+        for arg in op.body.blocks[0].args:
+            if isinstance(arg.typ, FieldType):
+                typ: FieldType[Attribute] = arg.typ
+                memreftyp = GetMemRefFromField(typ)
+                rewriter.modify_block_argument_type(arg, memreftyp)
+                inputs.append(memreftyp)
 
-            op.attributes["function_type"] = FunctionType(
-                [ArrayAttr(inputs),
-                 ArrayAttr(op.function_type.outputs.data)])
+        op.attributes["function_type"] = FunctionType.from_lists(
+            inputs, list(op.function_type.outputs.data))
 
 
 def ConvertStencilToLLMLIR(ctx: MLContext, module: ModuleOp):
     walker = PatternRewriteWalker(GreedyRewritePatternApplier(
-        [StencilTypeConversionFuncOp()]),
+        [StencilTypeConversionFuncOp(),
+         CastOpToMemref()]),
                                   walk_regions_first=True,
                                   apply_recursively=False,
                                   walk_reverse=True)
