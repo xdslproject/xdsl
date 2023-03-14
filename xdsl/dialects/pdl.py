6412ca92eb5277dd301cbfca652174106c50e280
Sasha Lopoukhine <superlopuh@gmail.com>
2023-03-14 15:13:49 +0000

    pdl.types returns a range type

--- xdsl/dialects/pdl.py
+++ xdsl/dialects/pdl.py
@@ -225,7 +225,7 @@ class TypesOp(Operation):
     """
     name: str = "pdl.types"
     constant_types: Annotated[OptOperand, ArrayAttr[TypeType]]
-    result: Annotated[OpResult, ArrayAttr[TypeType] | RangeType[TypeType]]
+    result: Annotated[OpResult, RangeType[TypeType]]
 
 
 PDL = Dialect([
