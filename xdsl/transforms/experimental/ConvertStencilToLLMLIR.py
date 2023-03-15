9937073b5ddc51db2870edc7fd5d1c9ca1ebdbb4
Emilien Bauer <bauer.emilien@gmail.com>
2023-03-15 15:17:27 +0000

    Add a stencil.ApplyOp to scf.ParallelOp lowering.

--- xdsl/transforms/experimental/ConvertStencilToLLMLIR.py
+++ xdsl/transforms/experimental/ConvertStencilToLLMLIR.py
@@ -4,24 +4,24 @@ from warnings import warn
 from xdsl.pattern_rewriter import (PatternRewriter, PatternRewriteWalker,
                                    RewritePattern, GreedyRewritePatternApplier,
                                    op_type_rewrite_pattern)
-from xdsl.ir import MLContext
+from xdsl.ir import MLContext, Region
 from xdsl.irdl import Attribute
 from xdsl.dialects.builtin import ArrayAttr, FunctionType, IntegerAttr, ModuleOp, i64
 from xdsl.dialects.func import FuncOp
 from xdsl.dialects.memref import MemRefType
-from xdsl.dialects import memref
+from xdsl.dialects import memref, arith, scf, builtin
 
-from xdsl.dialects.experimental.stencil import CastOp, FieldType, IndexAttr, LoadOp
+from xdsl.dialects.experimental.stencil import ApplyOp, CastOp, FieldType, IndexAttr, LoadOp, TempType
 
 _TypeElement = TypeVar("_TypeElement", bound=Attribute)
 
 
 def GetMemRefFromField(
-        inputFieldType: FieldType[_TypeElement]) -> MemRefType[_TypeElement]:
-    dims = [i.value.data for i in inputFieldType.shape.data]
+    inputType: FieldType[_TypeElement] | TempType[_TypeElement]
+) -> MemRefType[_TypeElement]:
+    dims = [i.value.data for i in inputType.shape.data]
 
-    return MemRefType.from_element_type_and_shape(inputFieldType.element_type,
-                                                  dims)
+    return MemRefType.from_element_type_and_shape(inputType.element_type, dims)
 
 
 def GetMemRefFromFieldWithLBAndUB(memref_element_type: _TypeElement,
@@ -29,10 +29,7 @@ def GetMemRefFromFieldWithLBAndUB(memref_element_type: _TypeElement,
                                   ub: IndexAttr) -> MemRefType[_TypeElement]:
     # lb and ub defines the minimum and maximum coordinates of the resulting memref,
     # so its shape is simply ub - lb, computed here.
-    dims = [
-        ub.value.data - lb.value.data
-        for lb, ub in zip(lb.array.data, ub.array.data)
-    ]
+    dims = IndexAttr.sizeFromBounds(lb, ub)
 
     return MemRefType.from_element_type_and_shape(memref_element_type, dims)
 
@@ -74,6 +71,47 @@ class LoadOpToMemref(RewritePattern):
         rewriter.replace_matched_op([], [op.field.owner.dest])
 
 
+class ApplyOpToParallel(RewritePattern):
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: ApplyOp, rewriter: PatternRewriter, /):
+        if (op.lb is None) or (op.ub is None):
+            print(f"ub:{op.ub}, lb:{op.ub}")
+            return
+        # # First replace all TempField arguments by memref.
+        entry = op.region.blocks[0]
+        # for arg in entry.args:
+        #     if isinstance(arg.typ, TempType):
+        #         arg_typ: TempType[Attribute] = arg.typ
+        #         rewriter.modify_block_argument_type(
+        #             arg, GetMemRefFromField(arg_typ))
+
+        for arg in entry.args:
+            for use in arg.uses:
+                use.operation.replace_operand(use.index, op.args[use.index])
+            entry.erase_arg(arg)
+
+        entry.insert_arg(builtin.IndexType(), 0)
+        entry.insert_arg(builtin.IndexType(), 0)
+        entry.insert_arg(builtin.IndexType(), 0)
+
+        #Then lower to scf.parallel
+        dims = IndexAttr.sizeFromBounds(op.lb, op.ub)
+        zero = arith.Constant.from_int_and_width(0, builtin.IndexType())
+        one = arith.Constant.from_int_and_width(1, builtin.IndexType())
+        upperBounds = [
+            arith.Constant.from_int_and_width(x, builtin.IndexType())
+            for x in dims
+        ]
+
+        body = rewriter.move_region_contents_to_new_regions(op.region)
+        p = scf.ParallelOp.get(lowerBounds=[zero, zero, zero],
+                               upperBounds=upperBounds,
+                               steps=[one, one, one],
+                               body=body)
+        rewriter.replace_matched_op([zero, one, *upperBounds, p])
+
+
 class StencilTypeConversionFuncOp(RewritePattern):
 
     @op_type_rewrite_pattern
@@ -93,9 +131,11 @@ class StencilTypeConversionFuncOp(RewritePattern):
 
 
 def ConvertStencilToLLMLIR(ctx: MLContext, module: ModuleOp):
-    walker = PatternRewriteWalker(GreedyRewritePatternApplier(
-        [StencilTypeConversionFuncOp(),
-         CastOpToMemref(),
-         LoadOpToMemref()]),
+    walker = PatternRewriteWalker(GreedyRewritePatternApplier([
+        StencilTypeConversionFuncOp(),
+        CastOpToMemref(),
+        LoadOpToMemref(),
+        ApplyOpToParallel()
+    ]),
                                   walk_regions_first=True)
     walker.rewrite_module(module)
