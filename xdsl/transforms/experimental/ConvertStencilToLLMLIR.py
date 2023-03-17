e01b75d3e4af71d0fcf9e7a2914c469705e70587
Emilien Bauer <bauer.emilien@gmail.com>
2023-03-17 17:20:05 +0000

    Add a stencil.ApplyOp to gpu.LaunchOp lowering.
    This is trial-and-error code, it needs a lot of cleanup.

--- xdsl/transforms/experimental/ConvertStencilToLLMLIR.py
+++ xdsl/transforms/experimental/ConvertStencilToLLMLIR.py
@@ -2,15 +2,17 @@ from dataclasses import dataclass
 from typing import TypeVar
 from warnings import warn
 
+import functools
+
 from xdsl.pattern_rewriter import (PatternRewriter, PatternRewriteWalker,
                                    RewritePattern, GreedyRewritePatternApplier,
                                    op_type_rewrite_pattern)
-from xdsl.ir import MLContext, Operation
+from xdsl.ir import Block, MLContext, Operation
 from xdsl.irdl import Attribute
 from xdsl.dialects.builtin import FunctionType, ModuleOp
 from xdsl.dialects.func import FuncOp
 from xdsl.dialects.memref import MemRefType
-from xdsl.dialects import memref, arith, scf, builtin
+from xdsl.dialects import memref, arith, scf, builtin, cf, gpu
 
 from xdsl.dialects.experimental.stencil import AccessOp, ApplyOp, CastOp, FieldType, IndexAttr, LoadOp, ReturnOp, StoreOp, TempType
 
@@ -40,6 +42,7 @@ def GetMemRefFromFieldWithLBAndUB(memref_element_type: _TypeElement,
 class CastOpToMemref(RewritePattern):
 
     return_target: dict[ReturnOp, CastOp | memref.Cast]
+    gpu: bool = False
 
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: CastOp, rewriter: PatternRewriter, /):
@@ -56,6 +59,12 @@ class CastOpToMemref(RewritePattern):
             if v == op:
                 self.return_target[k] = cast
 
+        if gpu:
+            unranked = memref.Cast.get(
+                cast.dest,
+                memref.UnrankedMemrefType.from_type(field_typ.element_type))
+            register = gpu.HostRegisterOp.from_memref(unranked.dest)
+            rewriter.insert_op_after_matched_op([unranked, register])
         rewriter.replace_matched_op(cast)
 
 
@@ -76,7 +85,7 @@ class ReturnOpToMemref(RewritePattern):
     def match_and_rewrite(self, op: ReturnOp, rewriter: PatternRewriter, /):
 
         parallel = op.parent_op()
-        assert isinstance(parallel, scf.ParallelOp)
+        assert isinstance(parallel, scf.ParallelOp | gpu.LaunchOp)
 
         cast = self.return_target[op]
         assert isinstance(cast, CastOp)
@@ -84,7 +93,7 @@ class ReturnOpToMemref(RewritePattern):
         offsets = cast.lb
         assert isinstance(offsets, IndexAttr)
 
-        block = parallel.body.blocks[0]
+        assert (block := op.parent_block()) is not None
 
         off_const_ops = [
             arith.Constant.from_int_and_width(-x.value.data,
@@ -110,32 +119,120 @@ class LoadOpToMemref(RewritePattern):
         rewriter.replace_matched_op([], list(op.field.owner.results))
 
 
+def prepare_apply_body(op: ApplyOp, rewriter: PatternRewriter):
+    assert (op.lb is not None) and (op.ub is not None)
+
+    # First replace all current arguments by their definition
+    # and erase them from the block. (We are changing the op
+    # to a loop, which has access to them either way)
+    entry = op.region.blocks[0]
+
+    for arg in entry.args:
+        arg_uses = set(arg.uses)
+        for use in arg_uses:
+            use.operation.replace_operand(use.index, op.args[use.index])
+        entry.erase_arg(arg)
+
+    dim = len(op.lb.array.data)
+
+    for _ in range(dim):
+        rewriter.insert_block_argument(entry, 0, builtin.IndexType())
+
+    return rewriter.move_region_contents_to_new_regions(op.region)
+
+
+class ApplyOpToLaunch(RewritePattern):
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: ApplyOp, rewriter: PatternRewriter, /):
+
+        assert (op.lb is not None) and (op.ub is not None)
+
+        body = prepare_apply_body(op, rewriter)
+        dim = len(op.lb.array.data)
+
+        threads_per_dim = [[1024, 32, 8][dim - 1]] * dim + [1] * (3 - dim)
+        cst_tpd = [
+            arith.Constant.from_int_and_width(tpd, builtin.IndexType())
+            for tpd in threads_per_dim
+        ]
+
+        dims = IndexAttr.size_from_bounds(op.lb, op.ub)
+        one = arith.Constant.from_int_and_width(1, builtin.IndexType())
+
+        bounds_cst = [
+            arith.Constant.from_int_and_width(dims[i], builtin.IndexType())
+            for i in range(dim)
+        ]
+
+        blocks_divs = [
+            arith.CeilDivUI.get(bound, tpd)
+            for bound, tpd in zip(bounds_cst, cst_tpd)
+        ]
+
+        launch = gpu.LaunchOp.get(body, blocks_divs + [one] * (3 - dim),
+                                  cst_tpd)
+
+        index_compute_block = Block.from_arg_types([builtin.IndexType()] * 12)
+        block_mul = [
+            arith.Muli.get(index_compute_block.args[i],
+                           index_compute_block.args[9 + i]) for i in range(dim)
+        ]
+        thread_add = [
+            arith.Addi.get(index_compute_block.args[3 + i], block_mul[i])
+            for i in range(dim)
+        ]
+
+        thread_int = [
+            arith.IndexCastOp.get(ta, builtin.IntegerType.from_width(64))
+            for ta in thread_add
+        ]
+
+        bounds_int = [
+            arith.IndexCastOp.get(bc, builtin.IntegerType.from_width(64))
+            for bc in bounds_cst
+        ]
+
+        cmpis: list[arith.Cmpi | arith.AndI] = [
+            arith.Cmpi.from_mnemonic(ti, bc, "ult")
+            for ti, bc in zip(thread_int, bounds_int)
+        ]
+
+        if len(cmpis) >= 2:
+            cmpis.append(arith.AndI.get(cmpis[0], cmpis[1]))
+        for i in range(2, len(cmpis) - 1):
+            cmpis.append(arith.AndI.get(cmpis[i], cmpis[-1]))
+
+        else_block = Block.from_ops([gpu.TerminatorOp.get()])
+
+        body_branch = cf.ConditionalBranch.get(cmpis[-1], body.blocks[0],
+                                               list(thread_add), else_block,
+                                               [])
+
+        index_compute_block.add_ops([
+            *block_mul, *thread_add, *thread_int, *bounds_int, *cmpis,
+            body_branch
+        ])
+
+        body.insert_block(index_compute_block, 0)
+        body.add_block(else_block)
+        launch.body.blocks[1].add_op(gpu.TerminatorOp.get())
+
+        # Replace with the loop and necessary constants.
+        rewriter.insert_op_before_matched_op(
+            [*cst_tpd, *bounds_cst, *blocks_divs, one, launch])
+        rewriter.erase_matched_op(False)
+
+
 class ApplyOpToParallel(RewritePattern):
 
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: ApplyOp, rewriter: PatternRewriter, /):
 
-        if (op.lb is None) or (op.ub is None):
-            warn(
-                "stencil.apply should have lb and ub attributes before being lowered to "
-                "scf.parallel.")
-            return
+        assert (op.lb is not None) and (op.ub is not None)
 
-        # First replace all current arguments by their definition
-        # and erase them from the block. (We are changing the op
-        # to a loop, which has access to them either way)
-        entry = op.region.blocks[0]
-
-        for arg in entry.args:
-            arg_uses = set(arg.uses)
-            for use in arg_uses:
-                use.operation.replace_operand(use.index, op.args[use.index])
-            entry.erase_arg(arg)
-
+        body = prepare_apply_body(op, rewriter)
         dim = len(op.lb.array.data)
-
-        for _ in range(dim):
-            rewriter.insert_block_argument(entry, 0, builtin.IndexType())
 
         #Then create the corresponding scf.parallel
         dims = IndexAttr.size_from_bounds(op.lb, op.ub)
@@ -147,7 +244,6 @@ class ApplyOpToParallel(RewritePattern):
         ]
 
         # Move the body to the loop
-        body = rewriter.move_region_contents_to_new_regions(op.region)
         body.blocks[0].add_op(scf.Yield.get())
         p = scf.ParallelOp.get(lowerBounds=[zero] * dim,
                                upperBounds=upperBounds,
@@ -212,6 +308,45 @@ class StencilTypeConversionFuncOp(RewritePattern):
 
         op.attributes["function_type"] = FunctionType.from_lists(
             inputs, list(op.function_type.outputs.data))
+
+
+def ConvertStencilToGPU(ctx: MLContext, module: ModuleOp):
+
+    return_target: dict[ReturnOp, CastOp | memref.Cast] = {}
+
+    def map_returns(op: Operation) -> None:
+        if not isinstance(op, ReturnOp):
+            return
+
+        apply = op.parent_op()
+        assert isinstance(apply, ApplyOp)
+
+        res = list(apply.res)[0]
+
+        if (len(res.uses) > 1) or (not isinstance(
+            (store := list(res.uses)[0].operation), StoreOp)):
+            warn("Only single store result atm")
+            return
+
+        cast = store.field.owner
+        assert isinstance(cast, CastOp)
+
+        return_target[op] = cast
+
+    module.walk(map_returns)
+
+    the_one_pass = PatternRewriteWalker(GreedyRewritePatternApplier([
+        ApplyOpToLaunch(),
+        StencilTypeConversionFuncOp(),
+        CastOpToMemref(return_target, gpu=True),
+        LoadOpToMemref(),
+        AccessOpToMemref(),
+        ReturnOpToMemref(return_target),
+        StoreOpCleanup()
+    ]),
+                                        apply_recursively=False,
+                                        walk_reverse=True)
+    the_one_pass.rewrite_module(module)
 
 
 def ConvertStencilToLLMLIR(ctx: MLContext, module: ModuleOp):
