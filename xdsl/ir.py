522a0743c8d1b93189badd2334c2c09ae45a3f85
Sasha Lopoukhine <superlopuh@gmail.com>
2023-03-17 10:37:03 +0000

    remove unused import

--- xdsl/ir.py
+++ xdsl/ir.py
@@ -6,8 +6,8 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from io import StringIO
 from itertools import chain
-from typing import (TYPE_CHECKING, Any, Callable, Generic, Optional, Protocol,
-                    Sequence, TypeVar, cast, Iterator, ClassVar)
+from typing import (TYPE_CHECKING, Any, Callable, Generic, Protocol, Sequence,
+                    TypeVar, cast, Iterator, ClassVar)
 
 # Used for cyclic dependencies in type hints
 if TYPE_CHECKING:
