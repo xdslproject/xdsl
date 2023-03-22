5ca46e61ffe7187d4113cead0ea0082c567fa3b8
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-03-22 10:41:54 +0100

    Change tests to make sense

--- tests/test_traits.py
+++ tests/test_traits.py
@@ -1,61 +1,81 @@
 from __future__ import annotations
 from abc import ABC
 from dataclasses import dataclass
+from typing import Annotated
 
 import pytest
 
 from xdsl.ir import OpResult, OpTrait, Operation
-from xdsl.irdl import VarOpResult, VarOperand, irdl_op_definition
+from xdsl.irdl import Operand, irdl_op_definition
 from xdsl.utils.exceptions import VerifyException
-from xdsl.dialects.builtin import i32
+from xdsl.dialects.builtin import IntegerType, i1, i32, i64
 
 
 @dataclass(frozen=True)
-class NoResultsTrait(OpTrait):
+class LargerResultTrait(OpTrait):
+    """Check that the only result has a larger bitwidth than the operand."""
+
+    def verify(self, op: Operation) -> None:
+        # This function is never called in this test
+        raise NotImplementedError()
+
+
+class LargerOperandTrait(OpTrait):
+    """Check that the only operand has a larger bitwidth than the result."""
 
     def verify(self, op: Operation):
-        if len(op.results) != 0:
-            raise VerifyException(
-                f"Op has {len(op.results)} results, should have none")
+        # These asserts should be exceptions in a non-testing environment.
+        assert len(op.results) == 1
+        assert len(op.operands) == 1
+        assert isinstance(op.results[0].typ, IntegerType)
+        assert isinstance(op.operands[0].typ, IntegerType)
+        if op.results[0].typ.width.data >= op.operands[0].typ.width.data:
+            raise VerifyException("Operation has a result bitwidth greater "
+                                  "or equal to the operand bitwidth.")
 
 
 @dataclass(frozen=True)
-class NoOperandsTrait(OpTrait):
+class BitwidthSumLessThanTrait(OpTrait):
+    """
+    Check that the sum of the bitwidths of the
+    operands and results is less than a given value.
+    """
+
+    max_sum: int
 
     def verify(self, op: Operation):
-        if len(op.operands) != 0:
-            raise VerifyException(
-                f"Op has {len(op.operands)} operands, should have none")
+        sum_bitwidth = 0
+        for operand in op.operands:
+            # This assert should be an exception in a non-testing environment.
+            assert isinstance(operand.typ, IntegerType)
+            sum_bitwidth += operand.typ.width.data
+        for result in op.results:
+            # This assert should be an exception in a non-testing environment.
+            assert isinstance(result.typ, IntegerType)
+            sum_bitwidth += result.typ.width.data
 
-
-@dataclass(frozen=True)
-class NOperandsTrait(OpTrait):
-    parameter: int
-
-    def verify(self, op: Operation):
-        if len(op.operands) != self.parameter:
-            raise VerifyException(
-                f"Op has {len(op.operands)} operands, should have {self.parameter}"
-            )
+        if sum_bitwidth >= self.max_sum:
+            raise VerifyException("Operation has a bitwidth sum "
+                                  f"greater or equal to {self.max_sum}.")
 
 
 @irdl_op_definition
-class MyOp(Operation):
-    name = "test.my_op"
-    traits = frozenset([NoResultsTrait(), NOperandsTrait(1)])
+class TestOp(Operation):
+    name = "test.test"
+    traits = frozenset([LargerOperandTrait(), BitwidthSumLessThanTrait(64)])
 
-    ops: VarOperand
-    res: VarOpResult
+    ops: Annotated[Operand, IntegerType]
+    res: Annotated[OpResult, IntegerType]
 
 
 def test_has_trait_object():
     """
     Test the `has_trait` `Operation` method on a simple operation definition.
     """
-    assert MyOp.has_trait(NoResultsTrait())
-    assert not MyOp.has_trait(NoOperandsTrait())
-    assert not MyOp.has_trait(NOperandsTrait(0))
-    assert MyOp.has_trait(NOperandsTrait(1))
+    assert TestOp.has_trait(LargerOperandTrait())
+    assert not TestOp.has_trait(LargerResultTrait())
+    assert not TestOp.has_trait(BitwidthSumLessThanTrait(0))
+    assert TestOp.has_trait(BitwidthSumLessThanTrait(64))
 
 
 def test_get_traits_of_type():
@@ -63,45 +83,66 @@ def test_get_traits_of_type():
     Test the `get_traits_of_type` `Operation` method
     on a simple operation definition.
     """
-    assert MyOp.get_traits_of_type(NoResultsTrait) == [NoResultsTrait()]
-    assert MyOp.get_traits_of_type(NoOperandsTrait) == []
-    assert MyOp.get_traits_of_type(NOperandsTrait) == [NOperandsTrait(1)]
+    assert TestOp.get_traits_of_type(LargerOperandTrait) == [
+        LargerOperandTrait()
+    ]
+    assert TestOp.get_traits_of_type(LargerResultTrait) == []
+    assert TestOp.get_traits_of_type(BitwidthSumLessThanTrait) == [
+        BitwidthSumLessThanTrait(64)
+    ]
 
 
 def test_verifier():
     """
     Check that the traits verifier are correctly called.
     """
-    operand = OpResult(i32, [], [])  # type: ignore
-    op = MyOp.create(operands=[operand], result_types=[i32])
-    with pytest.raises(VerifyException):
+    operand64 = OpResult(i64, [], [])  # type: ignore
+    operand32 = OpResult(i32, [], [])  # type: ignore
+    operand1 = OpResult(i1, [], [])  # type: ignore
+    op = TestOp.create(operands=[operand1], result_types=[i32])
+    with pytest.raises(VerifyException) as e:
         op.verify()
+    assert e.value.args[0] == ("Operation has a result bitwidth greater"
+                               " or equal to the operand bitwidth.")
 
-    op = MyOp.create(operands=[], result_types=[])
-    with pytest.raises(VerifyException):
+    op = TestOp.create(operands=[operand64], result_types=[i32])
+    with pytest.raises(VerifyException) as e:
         op.verify()
+    assert e.value.args[0] == ("Operation has a bitwidth sum "
+                               "greater or equal to 64.")
 
-    op = MyOp.create(operands=[operand], result_types=[])
+    op = TestOp.create(operands=[operand32], result_types=[i1])
     op.verify()
 
 
-class NoOperandsOp(Operation, ABC):
-    traits = frozenset([NoOperandsTrait()])
+def test_verifier_order():
+    """
+    Check that trait verifiers are called after IRDL verifiers.
+    """
+    op = TestOp.create(operands=[], result_types=[i1])
+    with pytest.raises(VerifyException) as e:
+        op.verify()
+    assert e.value.args[0] == ("Expected 1 operand, but got 0")
+
+
+class LargerOperandOp(Operation, ABC):
+    traits = frozenset([LargerOperandTrait()])
 
 
 @irdl_op_definition
-class NoResultNorOperandOp(NoOperandsOp):
-    name = "test.no_result_nor_operand_op"
+class TestCopyOp(LargerOperandOp):
+    name = "test.test_copy"
 
-    traits = NoOperandsOp.traits.union([NoResultsTrait()])
+    traits = LargerOperandOp.traits.union([BitwidthSumLessThanTrait(64)])
 
 
 def test_trait_inheritance():
     """
     Check that traits are correctly inherited from parent classes.
     """
-    assert NoResultNorOperandOp.traits == frozenset(
-        [NoResultsTrait(), NoOperandsTrait()])
+    assert TestCopyOp.traits == frozenset(
+        [LargerOperandTrait(),
+         BitwidthSumLessThanTrait(64)])
 
 
 @irdl_op_definition
