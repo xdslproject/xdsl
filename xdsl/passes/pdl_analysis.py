6dfa2e5a986eecbd5204d67fc79fd03e69808ed1
Martin Lücke <martin.luecke@ed.ac.uk>
2023-03-23 13:38:51 +0100

    add initial portions of dominance analysis + tests

--- xdsl/passes/pdl_analysis.py
+++ xdsl/passes/pdl_analysis.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
-from typing import Type
+from typing import NamedTuple, Type
 import warnings
 from warnings import warn
 from xdsl.dialects import pdl
@@ -35,6 +35,7 @@ def debug(msg: str):
 
 @dataclass()
 class UseInfo():
+    used_by: list[AnalyzedPDLOperation] = field(default_factory=list)
     dominated_by: list[AnalyzedPDLOperation] = field(default_factory=list)
 
 
@@ -49,11 +50,50 @@ class AnalyzedPDLOperation:
     matched: bool = True
     erased_by: pdl.EraseOp | None = None
     replaced_by: list[SSAValue] | AnalyzedPDLOperation | None = None
-    use_info: UseInfo = UseInfo()
+    use_info: UseInfo = field(default_factory=lambda: UseInfo())
+    # op_results always stem from a pdl.ResultOp
+    op_results: list[OpResult] = field(default_factory=list)
 
     @property
     def is_terminator(self):
         return issubclass(self.op_type, TerminatorOp)
+
+
+class AnalyzedValue(NamedTuple):
+    val: SSAValue
+    owner: AnalyzedPDLOperation | None
+
+
+@dataclass
+class Scope():
+    vals_in_scope: list[AnalyzedValue] = field(default_factory=list)
+
+    def add_vals_defined_by(self, op: AnalyzedPDLOperation):
+        for result in op.pdl_op.results:
+            self.vals_in_scope.append(AnalyzedValue(result, op))
+
+    def add_val_and_owner(self, val: SSAValue,
+                          owner: AnalyzedPDLOperation | None):
+        self.vals_in_scope.append(AnalyzedValue(val, owner))
+
+    def add_val(self, val: SSAValue):
+        self.vals_in_scope.append(AnalyzedValue(val, None))
+
+    def remove_val(self, val: SSAValue):
+        for v in self.vals_in_scope:
+            if v.val == val:
+                self.vals_in_scope.remove(v)
+                return
+        debug("Value to remove not found in scope!")
+
+    def is_in_scope(self, val: SSAValue) -> bool:
+        return any(v.val == val for v in self.vals_in_scope)
+
+    def get_owner(self, val: SSAValue) -> AnalyzedPDLOperation | None:
+        for v in self.vals_in_scope:
+            if v.val == val:
+                return v.owner
+        return None
 
 
 @dataclass(init=False)
@@ -74,6 +114,11 @@ class PDLAnalysis:
     rewrite_op: pdl.RewriteOp
     analyzed_ops: list[AnalyzedPDLOperation] = field(default_factory=list)
     visited_ops: set[Operation] = field(default_factory=set)
+    #TMP:
+    # vals_defined_during_matching: list[tuple[SSAValue, AnalyzedPDLOperation
+    #                                          | None]] = field(
+    #                                              default_factory=list)
+    matching_scope: Scope = field(default_factory=lambda: Scope())
 
     @property
     def matched_ops(self):
@@ -99,6 +144,8 @@ class PDLAnalysis:
         self.pattern_op = pattern_op
         self.analyzed_ops = []
         self.visited_ops = set()
+        self.vals_defined_during_matching = []
+        self.matching_scope = Scope()
 
         if isinstance(pattern_op.body.ops[-1], pdl.RewriteOp):
             self.rewrite_op: pdl.RewriteOp = pattern_op.body.ops[-1]
@@ -106,9 +153,11 @@ class PDLAnalysis:
             raise Exception(
                 "pdl.PatternOp must have a pdl.RewriteOp as terminator!")
 
-        if isinstance(pattern_op.body.ops[-2], pdl.OperationOp):
-            self.root_op: pdl.OperationOp = pattern_op.body.ops[-2]
-        else:
+        for lhs_op in reversed(pattern_op.body.ops):
+            if isinstance(lhs_op, pdl.OperationOp):
+                self.root_op: pdl.OperationOp = lhs_op
+                break
+        if self.root_op is None:  # type: ignore
             raise Exception(
                 "pdl.PatternOp must have a pdl.OperationOp as root!")
 
@@ -194,20 +243,68 @@ class PDLAnalysis:
 
         debug("Terminator Analysis finished.")
 
-    def def_use_analysis(self):
-        """
-        
+    def dominance_analysis(self):
         """
 
-        debug("Dominance Analysis not implemented!")
-        # analyze dominance of generated ops
-        # for op in self.generated_ops:
-        #     for operand in op.operands:
-        #         if operand.op in self.matched_ops:
-        #             self._add_analysis_result_to_op(op.pdl_op,
-        #                                             "dominance_broken")
 
-        # analyze dominance changes through erasures
+        Dominance can be broken by PDL in # ways:
+            1: An erased op is a dominator of a matched op + that matched op persists
+            2: A new op is generated that uses the root op + root op not replaced. (Is it legal to not modify the root?)
+            3: An op which still has uses in the matched code is erased
+        """
+
+        current_scope = self.matching_scope
+        # This is basically abstract interpretation and could be handled using the
+        # interpreter we are currently developing
+        for rhs_op in self.rewrite_op.body.ops:
+            if isinstance(rhs_op, pdl.EraseOp):
+                # get the op that is erased
+                if not current_scope.is_in_scope(erased_op := rhs_op.opValue):
+                    self._add_analysis_result_to_op(rhs_op,
+                                                    "out_of_scope_erasure")
+                    debug(f"Out of scope erasure: {erased_op}")
+                    return
+
+                if not (analyzed_op := current_scope.get_owner(erased_op)):
+                    raise Exception(
+                        "Only ops can be erased, PDL rewrite malformed!")
+
+                # check whether erased op is still in use
+                for result in analyzed_op.op_results:
+                    for (_, owner) in current_scope.vals_in_scope:
+                        if owner:
+                            if result in owner.pdl_op.operandValues:
+                                self._add_analysis_result_to_op(
+                                    rhs_op, "erased_op_still_in_use")
+                                debug(f"Erased op still in use: {erased_op}")
+                                return
+
+                # Delete erased op and all of its results from the scope:
+                for result in analyzed_op.op_results:
+                    current_scope.remove_val(result)
+                current_scope.remove_val(erased_op)
+            if isinstance(rhs_op, pdl.OperationOp):
+                # check whether operands are in scope
+                for operand in rhs_op.operandValues:
+                    if not current_scope.is_in_scope(operand):
+                        self._add_analysis_result_to_op(
+                            rhs_op, "out_of_scope_operand")
+                        debug(f"Out of scope operand: {operand}")
+                # add the op to the scope
+                analyzed_rhs_op = self.get_analysis_for_pdl_op(rhs_op)
+                assert analyzed_rhs_op
+                current_scope.add_vals_defined_by(analyzed_rhs_op)
+            # if isinstance(rhs_op, pdl.ResultOp):
+
+            # if isinstance(rhs_op, pdl.ReplaceOp):
+            #     # check whether operands are in scope
+            #     for operand in rhs_op.opValue.operandValues:
+            #         if not current_scope.is_in_scope(operand):
+            #             self._add_analysis_result_to_op(
+            #                 rhs_op, "out_of_scope_operand")
+            #             debug(f"Out of scope operand: {operand}")
+            #     # add the op to the scope
+            #     current_scope.add_vals_defined_by(rhs_op.opValue)
 
     def _trace_match_operation_op(
             self, pdl_operation_op: pdl.OperationOp) -> AnalyzedPDLOperation:
@@ -234,8 +331,15 @@ class PDLAnalysis:
                 analyzed_operands.append(analyzed_operand)
 
         analyzed_pdl_op = AnalyzedPDLOperation(pdl_operation_op, op_type)
+        # Register that all operands are used by this operation
+        # And that this operation is dominated by all its operands
         analyzed_pdl_op.use_info.dominated_by.extend(analyzed_operands)
+        for analyzed_operand in analyzed_operands:
+            analyzed_operand.use_info.used_by.append(analyzed_pdl_op)
         self.analyzed_ops.append(analyzed_pdl_op)
+        # Add the operation to the scope
+        self.matching_scope.add_val_and_owner(pdl_operation_op.results[0],
+                                              analyzed_pdl_op)
         return analyzed_pdl_op
 
     def _trace_matching_op(self,
@@ -261,13 +365,19 @@ class PDLAnalysis:
                 )
             used_op: pdl.OperationOp = used_op_result.op
             self.visited_ops.add(pdl_op)
-            return self._trace_matching_op(used_op)
+            analyzed_used_op = self._trace_matching_op(used_op)
+            assert analyzed_used_op
+            analyzed_used_op.op_results.append(pdl_op.val)  # TODO: needed?
+            self.matching_scope.add_val_and_owner(pdl_op.val, analyzed_used_op)
+            return analyzed_used_op
         elif isinstance(pdl_op, pdl.AttributeOp):
             debug(f"lhs: Found attribute: {pdl_op.name}")
         elif isinstance(pdl_op, pdl.TypeOp):
             debug(f"lhs: Found type: {pdl_op.name}")
         elif isinstance(pdl_op, pdl.OperandOp):
             debug(f"lhs: Found operand: {pdl_op.name}")
+            # Add val to list of vals defined during matching + no information about its definition
+            self.matching_scope.add_val(pdl_op.value)
         else:
             debug(f"lhs: unsupported PDL op: {pdl_op.name}")
 
@@ -379,7 +489,7 @@ class PDLAnalysis:
             # We can ignore the type issues here since we actually check all the types.
             # Only the type checker doesn't get that.
             if not all([isinstance(attr, StringAttr)
-                        for attr in tmp]):  # type: ignore
+                        for attr in tmp.data]):  # type: ignore
                 raise Exception(
                     "pdl_analysis attribute must be an array of strings!")
             analysis_results.extend(
@@ -394,6 +504,7 @@ def pdl_analysis_pass(ctx: MLContext, prog: ModuleOp):
             debug(f"Found pattern:{op.name}")
             analysis = PDLAnalysis(ctx, op)
             analysis.terminator_analysis()
+            analysis.dominance_analysis()
 
 
 if __name__ == '__main__':
@@ -460,7 +571,6 @@ if __name__ == '__main__':
     }) : () -> ()
     """
 
-    # TODO: here 5 is dominated by 4. Why did the system add this?
     erasing_and_adding_terminator = """
     "builtin.module"() ({
         "pdl.pattern"() ({
@@ -470,16 +580,42 @@ if __name__ == '__main__':
       %3 = "pdl.result"(%2) {index = 0 : i32} : (!pdl.operation) -> !pdl.value
       %4 = "pdl.operation"(%3) {attributeValueNames = [], operand_segment_sizes = array<i32: 1, 0, 0>} : (!pdl.value) -> !pdl.operation
       "pdl.rewrite"(%4) ({
-        %5 = "pdl.operation"() {attributeValueNames = [], opName = "func.return", operand_segment_sizes = array<i32: 0, 0, 0>} : () -> !pdl.operation
+        %test = "pdl.result"(%4) {index = 0 : i32} : (!pdl.operation) -> !pdl.value
+        %5 = "pdl.operation"(%test) {attributeValueNames = [], opName = "func.return", operand_segment_sizes = array<i32: 1, 0, 0>} : () -> !pdl.operation
         "pdl.erase"(%4) : (!pdl.operation) -> ()
       }) {operand_segment_sizes = array<i32: 1, 0>} : (!pdl.operation) -> ()
     }) {benefit = 3 : i16} : () -> ()
     }) : () -> ()
     """
 
-    parser = Parser(ctx=ctx,
-                    prog=erasing_and_adding_terminator,
-                    source=Source.MLIR)
+    erasing_a_used_op = """
+    "builtin.module"() ({
+    "pdl.pattern"() ({
+      %0 = "pdl.type"() : () -> !pdl.type
+      %1 = "pdl.attribute"() : () -> !pdl.attribute
+      %2 = "pdl.operation"(%1, %0) {attributeValueNames = ["value"], opName = "arith.constant", operand_segment_sizes = array<i32: 0, 1, 1>} : (!pdl.attribute, !pdl.type) -> !pdl.operation
+      %3 = "pdl.result"(%2) {index = 0 : i32} : (!pdl.operation) -> !pdl.value
+      %4 = "pdl.operand"() : () -> !pdl.value
+      %5 = "pdl.operation"(%3, %4, %0) {attributeValueNames = [], opName = "arith.addi", operand_segment_sizes = array<i32: 2, 0, 1>} : (!pdl.value, !pdl.value, !pdl.type) -> !pdl.operation
+      "pdl.rewrite"(%5) ({
+        "pdl.erase"(%2) : (!pdl.operation) -> ()
+        "pdl.erase"(%2) : (!pdl.operation) -> ()
+      }) {operand_segment_sizes = array<i32: 1, 0>} : (!pdl.operation) -> ()
+    }) {benefit = 1 : i16, sym_name = "required"} : () -> ()
+  }) {sym_name = "patterns"} : () -> ()"""
+
+    double_erasure = """
+    "builtin.module"() ({
+    "pdl.pattern"() ({
+      %0 = "pdl.operation"() {attributeValueNames = [], operand_segment_sizes = array<i32: 0, 0, 0>} : () -> !pdl.operation
+      "pdl.rewrite"(%0) ({
+        "pdl.erase"(%0) : (!pdl.operation) -> ()
+        "pdl.erase"(%0) : (!pdl.operation) -> ()
+      }) {operand_segment_sizes = array<i32: 1, 0>} : (!pdl.operation) -> ()
+    }) {benefit = 2 : i16, sym_name = "required"} : () -> ()
+    }) {sym_name = "patterns"} : () -> ()"""
+
+    parser = Parser(ctx=ctx, prog=double_erasure, source=Source.MLIR)
     program = parser.parse_op()
     assert isinstance(program, ModuleOp)
     pdl_analysis_pass(ctx, program)
