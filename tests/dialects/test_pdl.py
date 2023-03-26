8ddd35e3f34ec6b39dc95a2816f1e6b5c40a7e38
Sasha Lopoukhine <superlopuh@gmail.com>
2023-03-26 14:03:26 +0100

    split pdl test

--- tests/dialects/test_pdl.py
+++ tests/dialects/test_pdl.py
@@ -10,81 +10,92 @@ attribute_type = pdl.AttributeType()
 value_type = pdl.ValueType()
 operation_type = pdl.OperationType()
 
+block = Block.from_arg_types([
+    type_type,
+    attribute_type,
+    value_type,
+    operation_type,
+])
 
-def test_build_ops():
+type_val, attr_val, val_val, op_val = block.args
 
-    block = Block.from_arg_types([
-        type_type,
-        attribute_type,
-        value_type,
-        operation_type,
-    ])
-    arg0, arg1, arg2, arg3 = block.args
 
-    anc = pdl.ApplyNativeConstraintOp.get('anc', [arg0])
+def test_build_anc():
+    anc = pdl.ApplyNativeConstraintOp.get('anc', [type_val])
 
     assert anc.attributes['name'] == StringAttr('anc')
-    assert anc.args == (arg0, )
+    assert anc.args == (type_val, )
 
-    anr = pdl.ApplyNativeRewriteOp.get('anr', [arg0], [attribute_type])
+
+def test_build_anr():
+    anr = pdl.ApplyNativeRewriteOp.get('anr', [type_val], [attribute_type])
 
     assert anr.attributes['name'] == StringAttr('anr')
-    assert anr.args == (arg0, )
+    assert anr.args == (type_val, )
     assert len(anr.results) == 1
     assert [r.typ for r in anr.results] == [attribute_type]
 
+
+def test_build_rewrite():
     r = pdl.RewriteOp.get(StringAttr('r'),
                           root=None,
-                          external_args=[arg0, arg1],
+                          external_args=[type_val, attr_val],
                           body=None)
 
     assert r.attributes['name'] == StringAttr('r')
-    assert r.externalArgs == (arg0, arg1)
+    assert r.externalArgs == (type_val, attr_val)
     assert len(r.results) == 0
 
+
+def test_build_operation_replace():
     operation = pdl.OperationOp.get(opName=StringAttr('operation'),
                                     attributeValueNames=ArrayAttr(
                                         [StringAttr('name')]),
-                                    operandValues=[arg2],
-                                    attributeValues=[arg1],
-                                    typeValues=[arg0])
+                                    operandValues=[val_val],
+                                    attributeValues=[attr_val],
+                                    typeValues=[type_val])
 
     assert operation.opName == StringAttr('operation')
     assert operation.attributeValueNames == ArrayAttr([StringAttr('name')])
-    assert operation.operandValues == (arg2, )
-    assert operation.attributeValues == (arg1, )
-    assert operation.typeValues == (arg0, )
+    assert operation.operandValues == (val_val, )
+    assert operation.attributeValues == (attr_val, )
+    assert operation.typeValues == (type_val, )
 
-    res = pdl.ResultOp.get(IntegerAttr.from_int_and_width(1, 32), parent=arg3)
-
-    assert res.index == IntegerAttr.from_int_and_width(1, 32)
-    assert res.parent_ == arg3
-
-    operand = pdl.OperandOp.get(arg2)
-
-    assert operand.valueType == arg2
-
-    replace = pdl.ReplaceOp.get(opValue=arg3,
+    replace = pdl.ReplaceOp.get(opValue=op_val,
                                 replOperation=operation.results[0])
     replace.verify()
 
-    assert replace.opValue == arg3
+    assert replace.opValue == op_val
     assert replace.replOperation == operation.results[0]
     assert replace.replValues == ()
 
-    replace = pdl.ReplaceOp.get(opValue=arg3, replValues=[arg2])
+    replace = pdl.ReplaceOp.get(opValue=op_val, replValues=[val_val])
     replace.verify()
 
-    assert replace.opValue == arg3
+    assert replace.opValue == op_val
     assert replace.replOperation == None
-    assert replace.replValues == (arg2, )
+    assert replace.replValues == (val_val, )
 
     with pytest.raises(VerifyException):
-        replace = pdl.ReplaceOp.get(opValue=arg3)
+        replace = pdl.ReplaceOp.get(opValue=op_val)
         replace.verify()
 
     with pytest.raises(VerifyException):
-        replace = pdl.ReplaceOp.get(opValue=arg3,
+        replace = pdl.ReplaceOp.get(opValue=op_val,
                                     replOperation=operation.results[0],
-                                    replValues=[arg2])
+                                    replValues=[val_val])
         replace.verify()
+
+
+def test_build_result():
+    res = pdl.ResultOp.get(IntegerAttr.from_int_and_width(1, 32),
+                           parent=op_val)
+
+    assert res.index == IntegerAttr.from_int_and_width(1, 32)
+    assert res.parent_ == op_val
+
+
+def test_build_operand():
+    operand = pdl.OperandOp.get(val_val)
+
+    assert operand.valueType == val_val
