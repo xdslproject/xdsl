a51b7193ee7eefe40314412321aba7712d359e53
Anton Lydike <me@AntonLydike.de>
2023-03-27 17:43:09 +0100

    mpi: Move to preallocated request objects + collectives (#590)
    * MPI vector type with create and get operations
    
    Isend and Irecv now accept request type as operand rather than result type
    waitall operation
    
    * LLVM PtrToIntOp operation
    
    * Renamed status to statuses for MPI Waitall operation and some reformatting for yapf
    
    * Lowerings for allocate MPI array type, get element, waitall, wait, and updates to isend and irecv
    
    * Rename ArrayGetOp to VectorGetOp
    
    * Support for MPI Reduce
    
    * Support for MPI Allreduce
    
    * Support for MPI Bcast
    
    * Send buffer for MPI allreduce is optional, this then uses MPI_IN_PLACE for comms
    
    * mpi: move OperationType constants into an enum
    
    * mpi: make VectorType generic
    
    * mpi: fix pyright in the lower-mpi
    
    * tests: update test_mpi_lowering to comply with changes in mpi.py
    
    * mpi: update tests
    
    * mpi: add tests for all new lowerings
    
    * minor: fix error introduced by merge
    
    * mpi: fix wrong assumption in lowering
    
    * format: move from class- to staticmethod
    
    * misc: many thanks to smart merging tools
    
    * mpi: rename WaitAll to Waitall
    
    * mpi: rename WaitAll to Waitall in rewrites too
    
    ---------
    
    Co-authored-by: Nick Brown <nick@mesham.com>
    Co-authored-by: Nick Brown <n.brown@epcc.ed.ac.uk>

--- tests/dialects/test_mpi.py
+++ tests/dialects/test_mpi.py
@@ -10,11 +10,16 @@ def test_mpi_baseop():
     alloc0 = memref.Alloc.get(f64, 32, [100, 14, 14])
     dest = Constant.from_int_and_width(1, i32)
     unwrap = mpi.UnwrapMemrefOp.get(alloc0)
+    req_vec = mpi.AllocateTypeOp.get(mpi.RequestType, dest)
+    req_obj = mpi.VectorGetOp.get(req_vec, dest)
     tag = Constant.from_int_and_width(1, i32)
-    send = mpi.ISend.get(unwrap.ptr, unwrap.len, unwrap.typ, dest, tag)
+    send = mpi.Isend.get(unwrap.ptr, unwrap.len, unwrap.typ, dest, tag,
+                         req_obj)
     wait = mpi.Wait.get(send.request, ignore_status=False)
-    recv = mpi.IRecv.get(unwrap.ptr, unwrap.len, unwrap.typ, dest, tag)
+    recv = mpi.Irecv.get(unwrap.ptr, unwrap.len, unwrap.typ, dest, tag,
+                         req_obj)
     test_res = mpi.Test.get(recv.request)
+    assert wait.status is not None
     source = mpi.GetStatusField.get(wait.status,
                                     mpi.StatusTypeField.MPI_SOURCE)
 
