921ed91656752a8e3b0bc29dac7befe1e7db4f06
meshtag <prathameshtagore@gmail.com>
2023-03-29 22:08:23 +0530

    Retrigger CI build

--- benchmarks/dialects/experimental/stencil/run-laplace-benchmark.sh
+++ benchmarks/dialects/experimental/stencil/run-laplace-benchmark.sh
@@ -10,7 +10,7 @@ xdsl-opt ./benchmarks/dialects/experimental/stencil/laplace_xdsl.mlir -t mlir -p
 mlir-opt --arith-expand --convert-scf-to-cf --convert-memref-to-llvm --convert-func-to-llvm --reconcile-unrealized-casts laplace_xdsl_ll.mlir | mlir-translate --mlir-to-llvmir | llc --filetype=obj -o laplace_xdsl_lowered.o
 
 # Get object file used for benchmarking and comparing both implementations.
-mlir-opt --lower-affine --arith-expand --convert-scf-to-cf --convert-vector-to-llvm --convert-memref-to-llvm --convert-func-to-llvm --reconcile-unrealized-casts ./benchmarks/dialects/experimental/stencil/laplace_xdsl_oec.mlir | mlir-translate --mlir-to-llvmir | llc --filetype=obj -o laplace_xdsl_oec_lowered.o
+mlir-opt --lower-affine --arith-expand --convert-scf-to-cf --convert-vector-to-llvm --convert-memref-to-llvm --convert-func-to-llvm --reconcile-unrealized-casts ./benchmarks/dialects/experimental/stencil/laplace_xdsl_oec.mlir | mlir-translate --mlir-to-llvmir | llc --filetype=obj -o laplace_xdsl_oec_lowered.o 
 
 # Link above object files and libmlir_c_runner_utils to get the resultant executable.
 gcc -O3 -o laplace_benchmark laplace_xdsl_oec_lowered.o laplace_oec_lowered.o laplace_xdsl_lowered.o -lmlir_c_runner_utils
