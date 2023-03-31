93a79fc317b9612bedd50b9e0d8470cd3dedb534
Sasha Lopoukhine <superlopuh@gmail.com>
2023-03-31 15:10:04 +0100

    add tests for implicit region

--- tests/dialects/test_op_builder.py
+++ tests/dialects/test_op_builder.py
@@ -77,3 +77,55 @@ def test_build_callable_region():
     assert isinstance(ops[1], Constant)
     assert isinstance(ops[1].value, IntegerAttr)
     assert ops[1].value.value is two
+
+
+def test_build_implicit_region():
+
+    one = IntAttr(1)
+    two = IntAttr(2)
+
+    @Builder.implicit_region
+    def region():
+        Constant.from_int_and_width(one, i32)
+        Constant.from_int_and_width(two, i32)
+
+    assert len(region.blocks) == 1
+
+    ops = region.ops
+
+    assert len(ops) == 2
+
+    assert isinstance(ops[0], Constant)
+    assert isinstance(ops[0].value, IntegerAttr)
+    assert ops[0].value.value is one
+
+    assert isinstance(ops[1], Constant)
+    assert isinstance(ops[1].value, IntegerAttr)
+    assert ops[1].value.value is two
+
+
+def test_build_implicit_callable_region():
+
+    one = IntAttr(1)
+    two = IntAttr(2)
+
+    @Builder.implicit_region([i32])
+    def region(args: tuple[BlockArgument, ...]):
+        assert len(args) == 1
+
+        Constant.from_int_and_width(one, i32)
+        Constant.from_int_and_width(two, i32)
+
+    assert len(region.blocks) == 1
+
+    ops = region.ops
+
+    assert len(ops) == 2
+
+    assert isinstance(ops[0], Constant)
+    assert isinstance(ops[0].value, IntegerAttr)
+    assert ops[0].value.value is one
+
+    assert isinstance(ops[1], Constant)
+    assert isinstance(ops[1].value, IntegerAttr)
+    assert ops[1].value.value is two
