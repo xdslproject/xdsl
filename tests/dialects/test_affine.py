a63d34f6c648032becf6233d9ed3146e0d65be29
Sasha Lopoukhine <superlopuh@gmail.com>
2023-04-04 10:39:45 +0100

    misc: lint test_affine.py

--- tests/dialects/test_affine.py
+++ tests/dialects/test_affine.py
@@ -2,17 +2,17 @@ import pytest
 
 from xdsl.dialects.affine import For, Yield
 from xdsl.dialects.builtin import IndexType, IntegerAttr, IntegerType
-from xdsl.ir import Region, OpResult, Block
+from xdsl.ir import Attribute, Region, Block
 
 
 def test_simple_for():
-    f = For.from_callable([], 0, 5, lambda x: [])
+    f = For.from_callable([], 0, 5, lambda *args: [])
     assert f.lower_bound.value.data == 0
     assert f.upper_bound.value.data == 5
 
 
 def test_for_mismatch_operands_results_counts():
-    attributes = {
+    attributes: dict[str, Attribute] = {
         "lower_bound": IntegerAttr.from_index_int_value(0),
         "upper_bound": IntegerAttr.from_index_int_value(5),
         "step": IntegerAttr.from_index_int_value(1)
@@ -20,7 +20,7 @@ def test_for_mismatch_operands_results_counts():
     f = For.create(operands=[],
                    regions=[Region.from_block_list([])],
                    attributes=attributes,
-                   result_types=[IndexType])
+                   result_types=[IndexType()])
     with pytest.raises(Exception) as e:
         f.verify()
     assert e.value.args[
@@ -28,16 +28,17 @@ def test_for_mismatch_operands_results_counts():
 
 
 def test_for_mismatch_operands_results_types():
-    attributes = {
+    attributes: dict[str, Attribute] = {
         "lower_bound": IntegerAttr.from_index_int_value(0),
         "upper_bound": IntegerAttr.from_index_int_value(5),
         "step": IntegerAttr.from_index_int_value(1)
     }
-    inp = OpResult(IntegerType(32), [], [])
+    b = Block.from_arg_types((IntegerType(32), ))
+    inp = b.args[0]
     f = For.create(operands=[inp],
                    regions=[Region.from_block_list([])],
                    attributes=attributes,
-                   result_types=[IndexType])
+                   result_types=[IndexType()])
     with pytest.raises(Exception) as e:
         f.verify()
     assert e.value.args[
@@ -45,19 +46,20 @@ def test_for_mismatch_operands_results_types():
 
 
 def test_for_mismatch_blockargs():
-    attributes = {
+    attributes: dict[str, Attribute] = {
         "lower_bound": IntegerAttr.from_index_int_value(0),
         "upper_bound": IntegerAttr.from_index_int_value(5),
         "step": IntegerAttr.from_index_int_value(1)
     }
-    inp = OpResult(IndexType, [], [])
+    b = Block.from_arg_types((IndexType(), ))
+    inp = b.args[0]
     f = For.create(operands=[inp],
                    regions=[
                        Region.from_block_list(
-                           [Block.from_callable([], lambda: [])])
+                           [Block.from_callable([], lambda *args: [])])
                    ],
                    attributes=attributes,
-                   result_types=[IndexType])
+                   result_types=[IndexType()])
     with pytest.raises(Exception) as e:
         f.verify()
     assert e.value.args[
