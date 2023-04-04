e953781a7e34b05601374b047bce7b9ea1ccfe27
Sasha Lopoukhine <superlopuh@gmail.com>
2023-04-04 14:45:58 +0100

    RFC: implicit builder regions (#634)
    This PR proposes an API for a clutter-free IR building experience. By
    introducing some global mutable state, we can construct IRs without
    explicit `b.insert` calls. Obviously this is super error-prone, so I
    added some asserts in the code to make sure that the right thing is
    going on, and try to generate useful errors when something goes wrong.
    Please take a look at the tests for usage.

--- docs/Toy/toy/tests/test_ir_gen.py
+++ docs/Toy/toy/tests/test_ir_gen.py
@@ -23,50 +23,44 @@ def test_convert_ast():
     generated_module_op = ir_gen.ir_gen_module(module_ast)
 
     @ModuleOp.from_region_or_ops
-    @Builder.region
-    def module_op(builder: Builder):
+    @Builder.implicit_region
+    def module_op():
         unrankedf64TensorType = toy.UnrankedTensorType.from_type(f64)
 
         multiply_transpose_type = FunctionType.from_lists(
             [unrankedf64TensorType, unrankedf64TensorType],
             [unrankedf64TensorType])
 
-        @Builder.region(multiply_transpose_type.inputs)
-        def multiply_transpose(builder: Builder, args: tuple[BlockArgument,
-                                                             ...]) -> None:
+        @Builder.implicit_region(multiply_transpose_type.inputs)
+        def multiply_transpose(args: tuple[BlockArgument, ...]) -> None:
             a, b = args
-            a_t = builder.insert(toy.TransposeOp.from_input(a)).res
-            b_t = builder.insert(toy.TransposeOp.from_input(b)).res
-            prod = builder.insert(toy.MulOp.from_summands(a_t, b_t)).res
-            builder.insert(toy.ReturnOp.from_input(prod))
+            a_t = toy.TransposeOp.from_input(a).res
+            b_t = toy.TransposeOp.from_input(b).res
+            prod = toy.MulOp.from_summands(a_t, b_t).res
+            toy.ReturnOp.from_input(prod)
 
-        def call_multiply_transpose(builder: Builder, a: SSAValue,
-                                    b: SSAValue) -> OpResult:
-            return builder.insert(
-                toy.GenericCallOp.get("multiply_transpose", [a, b],
-                                      [unrankedf64TensorType])).res[0]
+        def call_multiply_transpose(a: SSAValue, b: SSAValue) -> OpResult:
+            return toy.GenericCallOp.get("multiply_transpose", [a, b],
+                                         [unrankedf64TensorType]).res[0]
 
         main_type = FunctionType.from_lists([], [])
 
-        @Builder.region
-        def main(builder: Builder) -> None:
-            a = builder.insert(
-                toy.ConstantOp.from_list([1, 2, 3, 4, 5, 6], [2, 3])).res
-            b_0 = builder.insert(
-                toy.ConstantOp.from_list([1, 2, 3, 4, 5, 6], [6])).res
-            b = builder.insert(toy.ReshapeOp.from_input(b_0, [2, 3])).res
-            c = call_multiply_transpose(builder, a, b)
-            call_multiply_transpose(builder, b, a)
-            call_multiply_transpose(builder, b, c)
-            a_t = builder.insert(toy.TransposeOp.from_input(a)).res
-            call_multiply_transpose(builder, a_t, c)
-            builder.insert(toy.ReturnOp.from_input())
+        @Builder.implicit_region
+        def main() -> None:
+            a = toy.ConstantOp.from_list([1, 2, 3, 4, 5, 6], [2, 3]).res
+            b_0 = toy.ConstantOp.from_list([1, 2, 3, 4, 5, 6], [6]).res
+            b = toy.ReshapeOp.from_input(b_0, [2, 3]).res
+            c = call_multiply_transpose(a, b)
+            call_multiply_transpose(b, a)
+            call_multiply_transpose(b, c)
+            a_t = toy.TransposeOp.from_input(a).res
+            call_multiply_transpose(a_t, c)
+            toy.ReturnOp.from_input()
 
-        builder.insert(
-            toy.FuncOp.from_region("multiply_transpose",
-                                   multiply_transpose_type,
-                                   multiply_transpose,
-                                   private=True))
-        builder.insert(toy.FuncOp.from_region("main", main_type, main))
+        toy.FuncOp.from_region("multiply_transpose",
+                               multiply_transpose_type,
+                               multiply_transpose,
+                               private=True)
+        toy.FuncOp.from_region("main", main_type, main)
 
     assert module_op.is_structurally_equivalent(generated_module_op)
