eba93dc689f5d927e4e08bde4332151e99be1ae9
Anton Lydike <me@antonlydike.de>
2023-04-11 18:17:02 +0100

    format: yapf :)

--- xdsl/utils/hints.py
+++ xdsl/utils/hints.py
@@ -40,7 +40,8 @@ def isa(arg: Any, hint: type[_T]) -> TypeGuard[_T]:
             return all(isa(elem, elem_hints[0]) for elem in arg_tuple)
         else:
             assert len(elem_hints) == len(arg_tuple)
-            return all(isa(elem, hint) for elem, hint in zip(arg_tuple, elem_hints))
+            return all(
+                isa(elem, hint) for elem, hint in zip(arg_tuple, elem_hints))
 
     if origin is dict:
         if not isinstance(arg, dict):
