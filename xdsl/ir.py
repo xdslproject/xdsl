484896eb5861d344e7572ac4204b641b8b909e88
Tobias Grosser <tobias@grosser.es>
2023-04-18 07:09:43 +0100

    introduce block method

--- xdsl/ir.py
+++ xdsl/ir.py
@@ -1209,7 +1209,7 @@ class Region(IRNode):
             raise ValueError(
                 "'ops' property of Region class is only available "
                 "for single-block regions.")
-        return self.blocks[0].ops
+        return self.block.ops
 
     @property
     def op(self) -> Operation:
@@ -1220,7 +1220,18 @@ class Region(IRNode):
         if len(self.blocks) != 1 or len(self.blocks[0].ops) != 1:
             raise ValueError("'op' property of Region class is only available "
                              "for single-operation single-block regions.")
-        return self.blocks[0].ops[0]
+        return self.block.ops[0]
+
+    @property
+    def block(self) -> Operation:
+        """
+        Get the block of a single-block region.
+        Returns an exception if the region is not single-block.
+        """
+        if len(self.blocks) != 1:
+            raise ValueError("'block' property of Region class is only available "
+                             "for single-block regions.")
+        return self.blocks[0]
 
     def _attach_block(self, block: Block) -> None:
         """Attach a block to the region, and check that it has no parents."""
