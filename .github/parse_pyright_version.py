760dc2b4883e366513d62031136787f537fe1ef6
Michel Weber <michel.weber@inf.ethz.ch>
2023-04-24 10:07:34 +0200

    address comments

--- .github/parse_pyright_version.py
+++ .github/parse_pyright_version.py
@@ -6,5 +6,5 @@ line = f.readlines()[-1]
 
 version = line.split("==")[1].strip()
 
-version_file = open("pyright-version.txt", "w")
+version_file = open(".github/pyright-version.txt", "w")
 version_file.write(version)
