007061b6cf2811d202dab768434cc03ef1d17b18
George Mitenkov <georgemitenk0v@gmail.com>
2023-04-28 14:40:25 +0200

    frontend: Add support for If and IfExpr (#799)
    This PR adds support for handling Python `if` and `if` expressions.

---
+++ tests/filecheck/frontend/dialects/scf.py
@@ -0,0 +1,82 @@
+# RUN: python %s | filecheck %s
+
+from xdsl.frontend.exception import CodeGenerationException
+from xdsl.frontend.program import FrontendProgram
+from xdsl.frontend.context import CodeContext
+from xdsl.frontend.dialects.builtin import i1, i32, f32
+
+p = FrontendProgram()
+with CodeContext(p):
+    # CHECK:      %{{.*}} : !i32 = scf.if(%{{.*}} : !i1) {
+    # CHECK-NEXT:   %{{.*}} : !i32 = symref.fetch() ["symbol" = @x]
+    # CHECK-NEXT:   scf.yield(%{{.*}} : !i32)
+    # CHECK-NEXT: } {
+    # CHECK-NEXT:   %{{.*}} : !i32 = symref.fetch() ["symbol" = @y]
+    # CHECK-NEXT:   scf.yield(%{{.*}} : !i32)
+    # CHECK-NEXT: }
+    def test_if_expr(cond: i1, x: i32, y: i32) -> i32:
+        return x if cond else y
+
+    # CHECK:      scf.if(%{{.*}} : !i1) {
+    # CHECK-NEXT:   scf.yield()
+    # CHECK-NEXT: } {
+    # CHECK-NEXT:   scf.yield()
+    # CHECK-NEXT: }
+    def test_if_I(cond: i1):
+        if cond:
+            pass
+        else:
+            pass
+
+    # CHECK:       %{{.*}} : !i1 = symref.fetch() ["symbol" = @a]
+    # CHECK-NEXT:  scf.if(%{{.*}} : !i1) {
+    # CHECK-NEXT:   scf.yield()
+    # CHECK-NEXT: } {
+    # CHECK-NEXT:   %{{.*}} : !i1 = symref.fetch() ["symbol" = @b]
+    # CHECK-NEXT:   scf.if(%{{.*}} : !i1) {
+    # CHECK-NEXT:     scf.yield()
+    # CHECK-NEXT:   } {
+    # CHECK-NEXT:     %{{.*}} : !i1 = symref.fetch() ["symbol" = @c]
+    # CHECK-NEXT:     scf.if(%{{.*}} : !i1) {
+    # CHECK-NEXT:       scf.yield()
+    # CHECK-NEXT:     } {
+    # CHECK-NEXT:       scf.yield()
+    # CHECK-NEXT:     }
+    # CHECK-NEXT:     scf.yield()
+    # CHECK-NEXT:   }
+    # CHECK-NEXT:   scf.yield()
+    # CHECK-NEXT: }
+    def test_if_II(a: i1, b: i1, c: i1):
+        if a:
+            pass
+        elif b:
+            pass
+        elif c:
+            pass
+
+    # CHECK:      %{{.*}} : !i1 = symref.fetch() ["symbol" = @cond]
+    # CHECK-NEXT: scf.if(%{{.*}} : !i1) {
+    # CHECK-NEXT:   scf.yield()
+    # CHECK-NEXT: } {
+    # CHECK-NEXT:   scf.yield()
+    # CHECK-NEXT: }
+    def test_if_III(cond: i1):
+        if cond:
+            pass
+        return
+
+
+p.compile(desymref=False)
+print(p.xdsl())
+
+
+try:
+    with CodeContext(p):
+        # CHECK: Expected the same types for if expression, but got i32 and f32.
+        def test_type_mismatch_in_if_expr(cond: i1, x: i32, y: f32) -> i32:
+            return x if cond else y  # type: ignore
+
+    p.compile(desymref=False)
+    print(p.xdsl())
+except CodeGenerationException as e:
+    print(e.msg)
--- xdsl/frontend/code_generation.py
+++ xdsl/frontend/code_generation.py
@@ -1,6 +1,7 @@
 import ast
 import xdsl.dialects.builtin as builtin
 import xdsl.dialects.func as func
+import xdsl.dialects.scf as scf
 import xdsl.frontend.symref as symref
 
 from dataclasses import dataclass, field
@@ -244,6 +245,66 @@ class CodegGenerationVisitor(ast.NodeVisitor):
             mnemonic = python_AST_cmpop_to_mnemonic[op_name]
             op = op(lhs, rhs, mnemonic)
 
+        self.inserter.insert_op(op)
+
+    def visit_If(self, node: ast.If):
+        # Get the condition.
+        self.visit(node.test)
+        cond = self.inserter.get_operand()
+        cond_block = self.inserter.insertion_point
+
+        def visit_region(stmts: list[ast.stmt]) -> Region:
+            region = Region([Block()])
+            self.inserter.set_insertion_point_from_region(region)
+            for stmt in stmts:
+                self.visit(stmt)
+            return region
+
+        # Generate code for both branches.
+        true_region = visit_region(node.body)
+        false_region = visit_region(node.orelse)
+
+        # In our case, if statement never returns a value and therefore we can
+        # simply yield nothing. It is the responsibility of subsequent passes to
+        # ensure SSA-form of IR and that values are yielded correctly.
+        true_region.blocks[-1].add_op(scf.Yield.get())
+        false_region.blocks[-1].add_op(scf.Yield.get())
+        op = scf.If.get(cond, [], true_region, false_region)
+
+        # Reset insertion point and insert a new operation.
+        self.inserter.set_insertion_point_from_block(cond_block)
+        self.inserter.insert_op(op)
+
+    def visit_IfExp(self, node: ast.IfExp) -> Any:
+        self.visit(node.test)
+        cond = self.inserter.get_operand()
+        cond_block = self.inserter.insertion_point
+
+        def visit_expr(expr: ast.expr) -> tuple[Attribute, Region]:
+            region = Region([Block()])
+            self.inserter.set_insertion_point_from_region(region)
+            self.visit(expr)
+            result = self.inserter.get_operand()
+            self.inserter.insert_op(scf.Yield.get(result))
+            return result.typ, region
+
+        # Generate code for both branches.
+        true_type, true_region = visit_expr(node.body)
+        false_type, false_region = visit_expr(node.orelse)
+
+        # Check types are the same for this to be a valid if statement.
+        if true_type != false_type:
+            raise CodeGenerationException(
+                self.file,
+                node.lineno,
+                node.col_offset,
+                f"Expected the same types for if expression,"
+                f" but got {true_type} and {false_type}.",
+            )
+        op = scf.If.get(cond, [true_type], true_region, false_region)
+
+        # Reset insertion point to add scf.if.
+        self.inserter.set_insertion_point_from_block(cond_block)
         self.inserter.insert_op(op)
 
     def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
