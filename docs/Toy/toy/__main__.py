194001b2995708dc3c1f1d5e8ed8f714f54859d7
Sasha Lopoukhine <superlopuh@gmail.com>
2023-05-01 20:58:17 +0100

    add ir printing

--- docs/Toy/toy/__main__.py
+++ docs/Toy/toy/__main__.py
@@ -1,14 +1,18 @@
 import argparse
 from pathlib import Path
 
-from frontend.parser import Parser
+from xdsl.printer import Printer
+
+from .frontend.ir_gen import IRGen
+from .frontend.parser import Parser
+
 
 parser = argparse.ArgumentParser(description="Process Toy file")
 parser.add_argument("source", type=Path, help="toy source file")
 parser.add_argument(
     "--emit",
     dest="emit",
-    choices=["ast"],
+    choices=["ast", "ir"],
     default="ast",
     help="Action to perform on source file (default: interpret file)",
 )
@@ -24,6 +28,14 @@ def main(path: Path, emit: str):
     if emit == "ast":
         print(ast.dump())
         return
+
+    ir_gen = IRGen()
+
+    module_op = ir_gen.ir_gen_module(ast)
+
+    if emit == "ir":
+        printer = Printer()
+        printer.print_op(module_op)
 
     print(f"Unknown option {emit}")
 
