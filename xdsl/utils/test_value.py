eab5a4382c9c0ad1887a4bbd5cea59b2d9746519
Sasha Lopoukhine <superlopuh@gmail.com>
2023-05-01 11:53:17 +0100

    remove test value changes

--- xdsl/utils/test_value.py
+++ xdsl/utils/test_value.py
@@ -5,10 +5,3 @@ class TestSSAValue(SSAValue):
     @property
     def owner(self) -> Operation | Block:
         assert False, "Attempting to get the owner of a `TestSSAValue`"
-
-    def __eq__(self, other: object) -> bool:
-        return self is other
-
-    # This might be problematic, as the superclass is not hashable ...
-    def __hash__(self) -> int:  # type: ignore
-        return id(self)
