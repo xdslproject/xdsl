cd0b3809e2b3ee53397f18b0a7731e9a6c863f4e
Fehr Mathieu <mathieu.fehr@gmail.com>
2023-05-01 15:37:43 +0100

    tests: Move more tests to MLIR syntax (#831)

--- tests/rewriting/composable_rewriting/immutable_ir/test_immutable_ir.py
+++ tests/rewriting/composable_rewriting/immutable_ir/test_immutable_ir.py
@@ -1,7 +1,7 @@
 import pytest
 
 from xdsl.ir import MLContext, Operation
-from xdsl.parser import XDSLParser
+from xdsl.parser import MLIRParser
 from xdsl.dialects.builtin import Builtin
 from xdsl.dialects.func import Func
 from xdsl.dialects.arith import Arith
@@ -10,50 +10,65 @@ from xdsl.rewriting.composable_rewriting.immutable_ir.immutable_ir import (
     get_immutable_copy,
 )  # noqa
 
-program_region = """builtin.module() {
-  %0 : !i32 = arith.constant() ["value" = 1 : !i32]
-}
+program_region = """
+"builtin.module"() ({
+  %0 = "arith.constant"() {"value" = 1 : i32} : () -> i32
+}) : () -> ()
 """
-program_region_2 = """builtin.module() {
-  %0 : !i32 = arith.constant() ["value" = 2 : !i32]
-}
+
+program_region_2 = """
+"builtin.module"() ({
+  %0 = "arith.constant"() {"value" = 2 : i32} : () -> i32
+}) : () -> ()
 """
-program_region_2_diff_name = """builtin.module() {
-  %cst : !i32 = arith.constant() ["value" = 2 : !i32]
-}
+
+program_region_2_diff_name = """
+"builtin.module"() ({
+  %cst = "arith.constant"() {"value" = 2 : i32} : () -> i32
+}) : () -> ()
 """
-program_region_2_diff_type = """builtin.module() {
-  %0 : !i64 = arith.constant() ["value" = 2 : !i64]
-}
+
+program_region_2_diff_type = """
+"builtin.module"() ({
+  %0 = "arith.constant"() {"value" = 2 : i64} : () -> i64
+}) : () -> ()
 """
-program_add = """builtin.module() {
-%0 : !i32 = arith.constant() ["value" = 1 : !i32]
-%1 : !i32 = arith.constant() ["value" = 2 : !i32]
-%2 : !i32 = arith.addi(%0 : !i32, %1 : !i32)
-}
+
+program_add = """
+"builtin.module"() ({
+  %0 = "arith.constant"() {"value" = 1 : i32} : () -> i32
+  %1 = "arith.constant"() {"value" = 2 : i32} : () -> i32
+  %2 = "arith.addi"(%0, %1) : (i32, i32) -> i32
+}) : () -> ()
 """
-program_add_2 = """builtin.module() {
-%0 : !i32 = arith.constant() ["value" = 1 : !i32]
-%1 : !i32 = arith.constant() ["value" = 2 : !i32]
-%2 : !i32 = arith.addi(%1 : !i32, %0 : !i32)
-}
+
+program_add_2 = """
+"builtin.module"() ({
+  %0 = "arith.constant"() {"value" = 1 : i32} : () -> i32
+  %1 = "arith.constant"() {"value" = 2 : i32} : () -> i32
+  %2 = "arith.addi"(%1, %0) : (i32, i32) -> i32
+}) : () -> ()
 """
-program_func = """builtin.module() {
-  func.func() ["sym_name" = "test", "type" = !fun<[!i32, !i32], [!i32]>, "sym_visibility" = "private"] {
-  ^0(%0 : !i32, %1 : !i32):
-    %2 : !i32 = arith.addi(%0 : !i32, %1 : !i32)
-    func.return(%2 : !i32)
-  }
-}
+
+program_func = """
+"builtin.module"() ({
+  "func.func"() ({
+  ^0(%0 : i32, %1 : i32):
+    %2 = "arith.addi"(%0, %1) : (i32, i32) -> i32
+    "func.return"(%2) : (i32) -> ()
+  }) {"sym_name" = "test", "function_type" = (i32, i32) -> i32, "sym_visibility" = "private"} : () -> ()
+}) : () -> ()
 """
-program_successors = """builtin.module() {
-    func.func() ["sym_name" = "unconditional_br", "function_type" = !fun<[], []>, "sym_visibility" = "private"] {
-    ^0:
-        cf.br() (^1)
-    ^1:
-        cf.br() (^0)
-    }
-}
+
+program_successors = """
+"builtin.module"() ({
+  "func.func"() ({
+  ^0:
+    "cf.br"() [^1] : () -> ()
+  ^1:
+    "cf.br"() [^0] : () -> ()
+  }) {"sym_name" = "unconditional_br", "function_type" = () -> (), "sym_visibility" = "private"} : () -> ()
+}) : () -> ()
 """
 
 
@@ -77,7 +92,7 @@ def test_immutable_ir(program_str: str):
     ctx.register_dialect(Arith)
     ctx.register_dialect(Cf)
 
-    parser = XDSLParser(ctx, program_str)
+    parser = MLIRParser(ctx, program_str)
     program: Operation = parser.parse_op()
     immutable_program = get_immutable_copy(program)
     mutable_program = immutable_program.to_mutable()
