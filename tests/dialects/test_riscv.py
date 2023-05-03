5fd9c800b0f73682b9850bd210ae61521d7b1d68
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2023-05-03 11:12:03 +0100

    [wip] Add riscv.op verification tests

--- tests/dialects/test_riscv.py
+++ tests/dialects/test_riscv.py
@@ -1,6 +1,12 @@
 from xdsl.utils.test_value import TestSSAValue
 from xdsl.dialects import riscv
 
+from xdsl.dialects.builtin import IntegerAttr, i32
+
+from xdsl.utils.exceptions import VerifyException
+
+import pytest
+
 
 def test_add_op():
     a1 = TestSSAValue(riscv.RegisterType(riscv.Register("a1")))
@@ -16,3 +22,17 @@ def test_add_op():
     assert a0.typ.data.name == "a0"
     assert a1.typ.data.name == "a1"
     assert a2.typ.data.name == "a2"
+
+
+def test_csr_op():
+    a1 = TestSSAValue(riscv.RegisterType(riscv.Register("a1")))
+    csr = IntegerAttr(16, i32)
+    op0 = riscv.CsrrwOp(rs1=a1, csr=csr, writeonly=IntegerAttr(0, i32), rd="a2")
+    op0.verify()
+    op1 = riscv.CsrrwOp(rs1=a1, csr=csr, writeonly=IntegerAttr(0, i32), rd="zero")
+    op1.verify()
+    op2 = riscv.CsrrwOp(rs1=a1, csr=csr, writeonly=IntegerAttr(1, i32), rd="zero")
+    op2.verify()
+    op3 = riscv.CsrrwOp(rs1=a1, csr=csr, writeonly=IntegerAttr(1, i32), rd="a2")
+    with pytest.raises(VerifyException):
+        op3.verify()
