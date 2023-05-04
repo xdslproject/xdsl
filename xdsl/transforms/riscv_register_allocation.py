4ae07c57fbe72b62aab16ad7b0fb598978cfaa2a
Sasha Lopoukhine <superlopuh@gmail.com>
2023-05-04 16:26:20 +0100

    walk

--- xdsl/transforms/riscv_register_allocation.py
+++ xdsl/transforms/riscv_register_allocation.py
@@ -1,7 +1,26 @@
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.dialects.riscv import Register, RegisterType, RISCVOp
-from xdsl.ir import MLContext
+from xdsl.ir import MLContext, Operation
 from xdsl.passes import ModulePass
+
+
+class _RegisterIndex:
+    idx: int
+
+    def __init__(self, idx: int):
+        self.idx = idx
+
+
+def _allocate_registers(op: Operation, idx: _RegisterIndex) -> None:
+    if not isinstance(op, RISCVOp):
+        # Don't perform register allocations on non-RISCV-ops
+        return
+
+    for result in op.results:
+        assert isinstance(result.typ, RegisterType)
+        if result.typ.data.name is None:
+            result.typ = RegisterType(Register(f"j{idx.idx}"))
+            idx.idx += 1
 
 
 def allocate_registers(op: ModuleOp) -> None:
@@ -9,17 +28,10 @@ def allocate_registers(op: ModuleOp) -> None:
     Allocates unallocated registers in the module. Currently sets them to an infinite set
     of `j` registers.
     """
-    idx: int = 0
-    for operation in op.ops:
-        # Don't perform register allocations on non-RISCV-ops
-        if not isinstance(operation, RISCVOp):
-            continue
 
-        for result in operation.results:
-            assert isinstance(result.typ, RegisterType)
-            if result.typ.data.name is None:
-                result.typ = RegisterType(Register(f"j{idx}"))
-                idx += 1
+    idx = _RegisterIndex(0)
+
+    op.walk(lambda op: _allocate_registers(op, idx))
 
 
 class RISCVRegisterAllocation(ModulePass):
