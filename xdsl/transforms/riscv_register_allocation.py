9f84168f4f4bc55ff386d55ee0bb7c98a2d077d1
Josse Van Delm <josse.vandelm@gmail.com>
2023-05-04 14:44:55 +0100

    dialects: Implement register allocation without PatternRewriteWalker

--- xdsl/transforms/riscv_register_allocation.py
+++ xdsl/transforms/riscv_register_allocation.py
@@ -2,37 +2,23 @@ from xdsl.dialects.builtin import ModuleOp
 from xdsl.dialects.riscv import Register, RegisterType, RISCVOp
 from xdsl.ir import MLContext
 from xdsl.passes import ModulePass
-from xdsl.pattern_rewriter import (
-    RewritePattern,
-    PatternRewriter,
-    PatternRewriteWalker,
-    op_type_rewrite_pattern,
-)
-
-
-class AllocateRegisters(RewritePattern):
-    def __init__(self) -> None:
-        super().__init__()
-        self.idx: int = 0
-
-    @op_type_rewrite_pattern
-    def match_and_rewrite(
-        self,
-        op: RISCVOp,
-        rewriter: PatternRewriter,
-    ):
-        for result in op.results:
-            assert isinstance(result.typ, RegisterType)
-            if result.typ.data.name is None:
-                result.typ = RegisterType(Register(f"j{self.idx}"))
-                self.idx += 1
 
 
 class RISCVRegisterAllocation(ModulePass):
     name = "riscv-allocate-registers"
 
+    def __init__(self) -> None:
+        super().__init__()
+        self.idx: int = 0
+
     def apply(self, ctx: MLContext, op: ModuleOp) -> None:
-        walker = PatternRewriteWalker(
-            AllocateRegisters(), apply_recursively=True, walk_reverse=False
-        )
-        walker.rewrite_module(op)
+        for operation in op.ops:
+            # Don't perform register allocations on non-RISCV-ops
+            if not isinstance(operation, RISCVOp):
+                continue
+            else:
+                for result in operation.results:
+                    assert isinstance(result.typ, RegisterType)
+                    if result.typ.data.name is None:
+                        result.typ = RegisterType(Register(f"j{self.idx}"))
+                self.idx += 1
