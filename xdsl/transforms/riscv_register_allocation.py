0ff495cee6006150ed6b021a93b225ed4b9981be
Sasha Lopoukhine <superlopuh@gmail.com>
2023-05-04 16:08:05 +0100

    dialects: register allocation always starts at 0

--- xdsl/transforms/riscv_register_allocation.py
+++ xdsl/transforms/riscv_register_allocation.py
@@ -4,18 +4,31 @@ from xdsl.ir import MLContext
 from xdsl.passes import ModulePass
 
 
+def allocate_registers(op: ModuleOp) -> None:
+    """
+    Allocates unallocated registers in the module. Currently sets them to an infinite set
+    of `j` registers.
+    """
+    idx: int = 0
+    for operation in op.ops:
+        # Don't perform register allocations on non-RISCV-ops
+        if not isinstance(operation, RISCVOp):
+            continue
+        else:
+            for result in operation.results:
+                assert isinstance(result.typ, RegisterType)
+                if result.typ.data.name is None:
+                    result.typ = RegisterType(Register(f"j{idx}"))
+                    idx += 1
+
+
 class RISCVRegisterAllocation(ModulePass):
+    """
+    Allocates unallocated registers in the module. Currently sets them to an infinite set
+    of `j` registers.
+    """
+
     name = "riscv-allocate-registers"
 
     def apply(self, ctx: MLContext, op: ModuleOp) -> None:
-        idx: int = 0
-        for operation in op.ops:
-            # Don't perform register allocations on non-RISCV-ops
-            if not isinstance(operation, RISCVOp):
-                continue
-            else:
-                for result in operation.results:
-                    assert isinstance(result.typ, RegisterType)
-                    if result.typ.data.name is None:
-                        result.typ = RegisterType(Register(f"j{idx}"))
-                idx += 1
+        allocate_registers(op)
