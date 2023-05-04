838b01a00c846113f0fc9edb18e039ade1284fd5
Sasha Lopoukhine <superlopuh@gmail.com>
2023-05-04 18:04:50 +0100

    dialects: register allocation skips indices (#876)
    Small usability issue with the infinite register allocation. Not a bug
    per se, but the register allocation would skip indices, which makes it a
    bit less readable when debugging.

--- tests/filecheck/dialects/riscv/riscv_register_allocation.mlir
+++ tests/filecheck/dialects/riscv/riscv_register_allocation.mlir
@@ -2,12 +2,12 @@
 
 "builtin.module"() ({
   %0 = "riscv.li"() {"immediate" = 6 : i32} : () -> !riscv.reg<>
-  %1 = "riscv.li"() {"immediate" = 5 : i32} : () -> !riscv.reg<>
+  %1 = "riscv.li"() {"immediate" = 5 : i32} : () -> !riscv.reg<s0>
   %2 = "riscv.add"(%0, %1) : (!riscv.reg<>, !riscv.reg<>) -> !riscv.reg<>
 }) : () -> ()
 
 // CHECK:      "builtin.module"() ({
 // CHECK-NEXT:  %{{\d+}} = "riscv.li"() {"immediate" = 6 : i32} : () -> !riscv.reg<j0>
-// CHECK-NEXT:  %{{\d+}} = "riscv.li"() {"immediate" = 5 : i32} : () -> !riscv.reg<j1>
-// CHECK-NEXT:  %{{\d+}} = "riscv.add"(%{{\d+}}, %{{\d+}}) : (!riscv.reg<j0>, !riscv.reg<j1>) -> !riscv.reg<j2>
+// CHECK-NEXT:  %{{\d+}} = "riscv.li"() {"immediate" = 5 : i32} : () -> !riscv.reg<s0>
+// CHECK-NEXT:  %{{\d+}} = "riscv.add"(%{{\d+}}, %{{\d+}}) : (!riscv.reg<j0>, !riscv.reg<s0>) -> !riscv.reg<j1>
 // CHECK-NEXT: }) : () -> ()
--- xdsl/transforms/riscv_register_allocation.py
+++ xdsl/transforms/riscv_register_allocation.py
@@ -1,21 +1,43 @@
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.dialects.riscv import Register, RegisterType, RISCVOp
-from xdsl.ir import MLContext
+from xdsl.ir import MLContext, Operation
 from xdsl.passes import ModulePass
 
 
+class RegisterAllocator:
+    idx: int
+
+    def __init__(self) -> None:
+        self.idx = 0
+
+    def _allocate_registers(self, op: Operation) -> None:
+        if not isinstance(op, RISCVOp):
+            # Don't perform register allocations on non-RISCV-ops
+            return
+
+        for result in op.results:
+            assert isinstance(result.typ, RegisterType)
+            if result.typ.data.name is None:
+                result.typ = RegisterType(Register(f"j{self.idx}"))
+                self.idx += 1
+
+    def allocate_registers(self, module: ModuleOp) -> None:
+        """
+        Allocates unallocated registers in the module. Currently sets them to an infinite set
+        of `j` registers.
+        """
+
+        module.walk(self._allocate_registers)
+
+
 class RISCVRegisterAllocation(ModulePass):
+    """
+    Allocates unallocated registers in the module. Currently sets them to an infinite set
+    of `j` registers.
+    """
+
     name = "riscv-allocate-registers"
 
     def apply(self, ctx: MLContext, op: ModuleOp) -> None:
-        idx: int = 0
-        for operation in op.ops:
-            # Don't perform register allocations on non-RISCV-ops
-            if not isinstance(operation, RISCVOp):
-                continue
-            else:
-                for result in operation.results:
-                    assert isinstance(result.typ, RegisterType)
-                    if result.typ.data.name is None:
-                        result.typ = RegisterType(Register(f"j{idx}"))
-                idx += 1
+        allocator = RegisterAllocator()
+        allocator.allocate_registers(op)
