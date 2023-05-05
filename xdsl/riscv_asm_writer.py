d8e0be076633f906ce591154198d5c559df5badb
Josse Van Delm <josse.vandelm@gmail.com>
2023-05-05 15:17:13 +0100

    dialects: Add csr printing and remove comment printing

--- xdsl/riscv_asm_writer.py
+++ xdsl/riscv_asm_writer.py
@@ -1,7 +1,7 @@
 from typing import IO
 from xdsl.ir import Operation
 from xdsl.dialects.riscv import RISCVOp, RegisterType, AnyIntegerAttr
-from xdsl.dialects.builtin import StringAttr, IntegerAttr, ModuleOp
+from xdsl.dialects.builtin import IntegerAttr, ModuleOp
 from xdsl.utils.hints import isa
 
 
@@ -22,6 +22,11 @@ def print_assembly_instruction(op: Operation, output: IO[str]) -> None:
         assert isinstance(result.typ.data.name, str)
         components.append(result.typ.data.name)
 
+    if "csr" in op.attributes:
+        csr_attr = getattr(op, "csr")
+        assert isinstance(csr_attr, IntegerAttr)
+        components.append(str(csr_attr.value.data))
+
     for operand in op.operands:
         assert isinstance(operand.typ, RegisterType)
         assert isinstance(operand.typ.data.name, str)
@@ -29,8 +34,8 @@ def print_assembly_instruction(op: Operation, output: IO[str]) -> None:
 
     if "offset" in op.attributes:
         label_attr = getattr(op, "offset")
-        assert isinstance(label_attr, StringAttr)
-        components.append(label_attr.data)
+        assert isinstance(label_attr, IntegerAttr)
+        components.append(str(label_attr.value.data))
 
     if "immediate" in op.attributes:
         immediate_attr = getattr(op, "immediate")
@@ -38,19 +43,5 @@ def print_assembly_instruction(op: Operation, output: IO[str]) -> None:
         if isa(immediate_attr, AnyIntegerAttr):
             components.append(str(immediate_attr.value.data))
 
-    if "comment" in op.attributes:
-        comment = getattr(op, "comment")
-        assert isinstance(comment, StringAttr)
-    else:
-        comment = None
-
-    def append_comment(line: str, comment: StringAttr | None) -> str:
-        if comment is None:
-            return line
-
-        padding = " " * max(0, 48 - len(line))
-
-        return f"{line}{padding} # {comment.data}"
-
     code = f"    {instruction_name} {', '.join(components)}"
-    print(append_comment(code, comment), file=output)
+    print(code, file=output)
