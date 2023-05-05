7cfacf414498b5e0919d5dce22ac29f2bc1e35a4
Josse Van Delm <josse.vandelm@gmail.com>
2023-05-05 18:23:00 +0100

    dialects: Get rid of getattr in printer

--- xdsl/riscv_asm_writer.py
+++ xdsl/riscv_asm_writer.py
@@ -1,10 +1,11 @@
 from typing import IO
 from xdsl.ir import Operation, SSAValue
 from xdsl.dialects.riscv import RISCVOp, RegisterType, AnyIntegerAttr
-from xdsl.dialects.builtin import IntegerAttr, ModuleOp
+from xdsl.dialects.builtin import ModuleOp
 from xdsl.utils.hints import isa
 
 from xdsl.dialects import riscv
+
 
 def print_riscv_module(module: ModuleOp, output: IO[str]):
     for op in module.ops:
@@ -17,54 +18,48 @@ def print_assembly_instruction(op: Operation, output: IO[str]) -> None:
     assert op.name.startswith("riscv.")
     instruction_name = op.name[6:]
 
-
-    components: list[AnyIntegerAttr | riscv.LabelAttr | SSAValue | str] = []
+    components: list[AnyIntegerAttr | riscv.LabelAttr | SSAValue | str | None] = []
 
     match op:
         case riscv.NullaryOperation():
             pass
+        case riscv.RdRsRsOperation():
+            components = [op.rd, op.rs1, op.rs2]
+        case riscv.RdRsImmOperation():
+            components = [op.rd, op.rs1, op.immediate]
+        case riscv.RdImmOperation():
+            components = [op.rd, op.immediate]
+        case riscv.RsRsImmOperation():
+            components = [op.rs1, op.rs2, op.immediate]
+        case riscv.RsRsOffOperation():
+            components = [op.rs1, op.rs2, op.offset]
+        case riscv.CsrReadWriteImmOperation():
+            components = [op.rd, op.csr, op.immediate]
+        case riscv.CsrReadWriteOperation():
+            components = [op.rd, op.csr, op.rs1]
+        case riscv.CsrBitwiseImmOperation():
+            components = [op.rd, op.csr, op.immediate]
+        case riscv.CsrBitwiseOperation():
+            components = [op.rd, op.csr, op.rs1]
         case _:
-
-
-            for result in op.results:
-                assert isinstance(result.typ, RegisterType)
-                assert isinstance(result.typ.data.name, str)
-                components.append(result.typ.data.name)
-
-            if "csr" in op.attributes:
-                csr_attr = getattr(op, "csr")
-                assert isinstance(csr_attr, IntegerAttr)
-                components.append(str(csr_attr.value.data))
-
-            for operand in op.operands:
-                assert isinstance(operand.typ, RegisterType)
-                assert isinstance(operand.typ.data.name, str)
-                components.append(operand.typ.data.name)
-
-            if "offset" in op.attributes:
-                label_attr = getattr(op, "offset")
-                assert isinstance(label_attr, IntegerAttr)
-                components.append(str(label_attr.value.data))
-
-            if "immediate" in op.attributes:
-                immediate_attr = getattr(op, "immediate")
-                assert isinstance(immediate_attr, IntegerAttr)
-                if isa(immediate_attr, AnyIntegerAttr):
-                    components.append(str(immediate_attr.value.data))
+            raise ValueError(f"Unknown RISCV operation type :{type(op)}")
 
     component_strs: list[str] = []
 
-
     for component in components:
-        if isa(component, AnyIntegerAttr):
-            component_strs.append(f"{component.value}")
+        if component is None:
+            continue
+        elif isa(component, AnyIntegerAttr):
+            component_strs.append(f"{component.value.data}")
         elif isinstance(component, riscv.LabelAttr):
             component_strs.append(component.data)
+        elif isinstance(component, str):
+            component_strs.append(component)
         else:
             assert isinstance(component.typ, RegisterType)
             reg = component.typ.data.name
             assert reg is not None
             component_strs.append(reg)
 
-    code = f"    {instruction_name} {', '.join(components)}"
+    code = f"    {instruction_name} {', '.join(component_strs)}"
     print(code, file=output)
