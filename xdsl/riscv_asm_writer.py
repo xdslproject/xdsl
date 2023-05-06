dd000b42e7dc6dab9f2c17d088ab09c8db046163
Sasha Lopoukhine <superlopuh@gmail.com>
2023-05-06 16:52:46 +0100

    dialects: add ability to print comments in riscv assembly

--- xdsl/riscv_asm_writer.py
+++ xdsl/riscv_asm_writer.py
@@ -1,7 +1,8 @@
+from io import StringIO
 from typing import IO
 from xdsl.ir import Operation, SSAValue
 from xdsl.dialects.riscv import RISCVOp, RegisterType, AnyIntegerAttr
-from xdsl.dialects.builtin import ModuleOp
+from xdsl.dialects.builtin import ModuleOp, StringAttr
 from xdsl.utils.hints import isa
 
 from xdsl.dialects import riscv
@@ -12,40 +13,65 @@ def print_riscv_module(module: ModuleOp, output: IO[str]):
         print_assembly_instruction(op, output)
 
 
+def append_comment(line: str, comment: StringAttr | None) -> str:
+    if comment is None:
+        return line
+
+    padding = " " * max(0, 48 - len(line))
+
+    return f"{line}{padding} # {comment.data}"
+
+
 def print_assembly_instruction(op: Operation, output: IO[str]) -> None:
     # default assembly code generator
     assert isinstance(op, RISCVOp)
     assert op.name.startswith("riscv.")
     instruction_name = op.name[6:]
 
-    components: list[AnyIntegerAttr | riscv.LabelAttr | SSAValue | str | None] = []
+    components: list[AnyIntegerAttr | riscv.LabelAttr | SSAValue | str | None]
+    comment: StringAttr | None
 
     match op:
         case riscv.GetRegisterOp():
             # Don't print assembly for creating a SSA value representing register
             return
+        case riscv.CommentOp():
+            desc = f"    # {op.comment.data}"
+            print(desc, file=output)
+            return
         case riscv.NullaryOperation():
-            pass
+            components = []
+            comment = op.comment
         case riscv.RdRsRsOperation():
             components = [op.rd, op.rs1, op.rs2]
+            comment = op.comment
         case riscv.RdRsImmOperation():
             components = [op.rd, op.rs1, op.immediate]
+            comment = op.comment
         case riscv.RdImmOperation():
             components = [op.rd, op.immediate]
+            comment = op.comment
         case riscv.RsRsImmOperation():
             components = [op.rs1, op.rs2, op.immediate]
+            comment = op.comment
         case riscv.RsRsOffOperation():
             components = [op.rs1, op.rs2, op.offset]
+            comment = op.comment
         case riscv.RdRsOperation():
             components = [op.rd, op.rs]
+            comment = op.comment
         case riscv.CsrReadWriteImmOperation():
             components = [op.rd, op.csr, op.immediate]
+            comment = op.comment
         case riscv.CsrReadWriteOperation():
             components = [op.rd, op.csr, op.rs1]
+            comment = op.comment
         case riscv.CsrBitwiseImmOperation():
             components = [op.rd, op.csr, op.immediate]
+            comment = op.comment
         case riscv.CsrBitwiseOperation():
             components = [op.rd, op.csr, op.rs1]
+            comment = op.comment
         case _:
             raise ValueError(f"Unknown RISCV operation type :{type(op)}")
 
@@ -65,5 +91,14 @@ def print_assembly_instruction(op: Operation, output: IO[str]) -> None:
             reg = component.typ.register_name
             component_strs.append(reg)
 
-    code = f"    {instruction_name} {', '.join(component_strs)}"
+    code = f"    {instruction_name}"
+    if len(component_strs):
+        code += f" {', '.join(component_strs)}"
+    code = append_comment(code, comment)
     print(code, file=output)
+
+
+def riscv_code(module: ModuleOp) -> str:
+    stream = StringIO()
+    print_riscv_module(module, stream)
+    return stream.getvalue()
