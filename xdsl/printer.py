72f25f272c47bb19769979def5377d3ea715cee9
ShaolunWang <wang.shaolun@gmail.com>
2023-05-08 17:33:03 +0100

    - Adding two new functions to printer.py - Updating floatAttr printing

--- xdsl/printer.py
+++ xdsl/printer.py
@@ -26,6 +26,7 @@ from xdsl.ir import (
     Data,
     ParametrizedAttribute,
 )
+from xdsl.irdl import Operand, VarOpResult, VarOperand
 from xdsl.utils.diagnostic import Diagnostic
 from xdsl.dialects.builtin import (
     AnyIntegerAttr,
@@ -452,8 +453,10 @@ class Printer:
         if isinstance(attribute, DenseIntOrFPElementsAttr):
 
             def print_one_elem(val: Attribute):
-                if isinstance(val, IntegerAttr | FloatAttr):
+                if isinstance(val, IntegerAttr):
                     self.print(val.value.data)
+                elif isinstance(val, FloatAttr):
+                    self.print(f"{val.value.data:.6e}")                    
                 else:
                     raise Exception(
                         "unexpected attribute type "
@@ -702,3 +705,19 @@ class Printer:
             op.print(self)
         else:
             self.print_op_with_default_format(op)
+
+    def print_optional_attr(self, attributes: Dict[str, Attribute],elided_attrs: Sequence[str] = []) -> None:         
+
+        new_attr: Dict[str, Attribute]
+        new_attr = attributes.copy()
+        for e in elided_attrs:
+            new_attr.pop(e) 
+        self._print_op_attributes(new_attr)
+
+    def print_list_with_comma(self, inputs: Sequence[Operand]):
+        # it prints args in the form of
+        # ($arg1, $arg2, .... $argN)
+        for i in range(len(inputs)):
+            self.print(inputs[i])
+            if i != len(inputs) - 1:
+                self.print(", ")
