06063d6c61a6422226ce794b56ab40919d8fbda8
Sasha Lopoukhine <superlopuh@gmail.com>
2023-05-11 22:35:49 +0100

    documentation: add Toy optimisations (#933)
    This is the last thing we need before adding Chapter 3

--- docs/Toy/toy/__main__.py
+++ docs/Toy/toy/__main__.py
@@ -2,9 +2,12 @@ import argparse
 from pathlib import Path
 
 from xdsl.printer import Printer
+from xdsl.parser import Parser as IRParser
 
 from .frontend.ir_gen import IRGen
-from .frontend.parser import Parser
+from .frontend.parser import Parser as ToyParser
+from .compiler import context
+from .rewrites.optimise_toy import OptimiseToy
 
 
 parser = argparse.ArgumentParser(description="Process Toy file")
@@ -12,34 +15,53 @@ parser.add_argument("source", type=Path, help="toy source file")
 parser.add_argument(
     "--emit",
     dest="emit",
-    choices=["ast", "ir"],
+    choices=["ast", "ir-toy"],
     default="ast",
     help="Action to perform on source file (default: interpret file)",
 )
+parser.add_argument(
+    "--opt", dest="opt", action="store_true", help="Optimise IR before printing"
+)
 
 
-def main(path: Path, emit: str):
+def main(path: Path, emit: str, opt: bool):
+    ctx = context()
+
     path = args.source
 
     with open(path, "r") as f:
-        parser = Parser(path, f.read())
-        ast = parser.parseModule()
+        match path.suffix:
+            case ".toy":
+                parser = ToyParser(path, f.read())
+                ast = parser.parseModule()
+                if emit == "ast":
+                    print(ast.dump())
+                    return
 
-    if emit == "ast":
-        print(ast.dump())
+                ir_gen = IRGen()
+                module_op = ir_gen.ir_gen_module(ast)
+            case ".mlir":
+                parser = IRParser(ctx, f.read(), name=f"{path}")
+                module_op = parser.parse_module()
+            case _:
+                print(f"Unknown file format {path}")
+                return
+
+    if emit == "ir-toy" and not opt:
+        printer = Printer()
+        printer.print(module_op)
         return
 
-    ir_gen = IRGen()
+    OptimiseToy().apply(ctx, module_op)
 
-    module_op = ir_gen.ir_gen_module(ast)
-
-    if emit == "ir":
+    if emit == "ir-toy" and opt:
         printer = Printer()
-        printer.print_op(module_op)
+        printer.print(module_op)
+        return
 
     print(f"Unknown option {emit}")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    main(args.source, args.emit)
+    main(args.source, args.emit, args.opt)
