2c50e33780a184f7557cb1a580f5edab86316b58
Sasha Lopoukhine <superlopuh@gmail.com>
2023-05-13 14:42:37 +0100

    cast works

--- docs/Toy/toy/rewrites/inline_toy.py
+++ docs/Toy/toy/rewrites/inline_toy.py
@@ -1,6 +1,7 @@
 from xdsl.ir import MLContext
 from xdsl.dialects.builtin import (
     ModuleOp,
+    StringAttr,
 )
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
@@ -38,24 +39,59 @@ class InlineFunctions(RewritePattern):
 
         # Clone called function
         impl = self.func_op(module, op.callee.string_value()).clone()
-        _ = impl
+        impl_block = impl.body.block
 
-        # Insert function definition before matched op
-        # rewriter.insert_op_before_matched_op(impl)
+        # Cast operands to unranked
+        inputs = [toy.CastOp(operand) for operand in op.operands]
 
-        # Replace uses of arguments with operands
+        # Insert casts before matched op
+        rewriter.insert_op_before_matched_op(inputs)
 
-        # Remove arguments
+        # Replace block args with operand casts
+        for i, arg in zip(inputs, impl_block.args):
+            arg.replace_by(i.res)
 
-        # Inline block before function operation
+        # remove block args
+        while len(impl_block.args):
+            assert not len(impl_block.args[-1].uses)
+            rewriter.erase_block_argument(impl_block.args[-1])
 
-        # Replace return op with casts
-        casts: list[toy.CastOp] = []
+        # Inline function definition before matched op
+        rewriter.inline_block_before_matched_op(impl_block)
 
-        # Replace results
-        new_results = [cast.res for cast in casts]
+        # Get return from function definition
+        return_op = op.prev_op
+        assert return_op is not None
 
-        rewriter.replace_op(op, [], new_results)
+        rewriter.replace_matched_op([], return_op.operands)
+        rewriter.erase_op(return_op)
+
+
+class RemoveUnusedPrivateFunctions(RewritePattern):
+    _func_ops_to_remove: set[str] | None = None
+
+    def should_remove_op(self, op: toy.FuncOp) -> bool:
+        if self._func_ops_to_remove is None:
+            # Get module
+            module = op.parent_op()
+            assert isinstance(module, ModuleOp)
+
+            private_func_names = set(
+                op.sym_name.data
+                for op in module.ops
+                if isinstance(op, toy.FuncOp)
+                and op.sym_visibility == StringAttr("private")
+            )
+
+            self._func_ops_to_remove = private_func_names.difference(
+                op.callee.string_value() for op in module.walk_filter(toy.GenericCallOp)
+            )
+        return op.sym_name.data in self._func_ops_to_remove
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: toy.FuncOp, rewriter: PatternRewriter):
+        if self.should_remove_op(op):
+            rewriter.erase_matched_op()
 
 
 class InlineToyPass(ModulePass):
@@ -63,4 +99,5 @@ class InlineToyPass(ModulePass):
 
     def apply(self, ctx: MLContext, op: ModuleOp) -> None:
         PatternRewriteWalker(InlineFunctions()).rewrite_module(op)
+        PatternRewriteWalker(RemoveUnusedPrivateFunctions()).rewrite_module(op)
         dce(op)
--- docs/Toy/toy/tests/test_inliner.py
+++ docs/Toy/toy/tests/test_inliner.py
@@ -8,25 +8,6 @@ from ..dialects import toy
 from ..rewrites.inline_toy import InlineToyPass
 from ..rewrites.optimise_toy import OptimiseToy
 
-"""
-toy.func @multiply_transpose(%arg0: tensor<*xf64>, %arg1: tensor<*xf64>) -> tensor<*xf64> {
-  %0 = toy.transpose(%arg0 : tensor<*xf64>) to tensor<*xf64>
-  %1 = toy.transpose(%arg1 : tensor<*xf64>) to tensor<*xf64>
-  %2 = toy.mul %0, %1 : tensor<*xf64>
-  toy.return %2 : tensor<*xf64>
-}
-toy.func @main() {
-  %0 = toy.constant dense<[[1.000000e+00, 2.000000e+00, 3.000000e+00], [4.000000e+00, 5.000000e+00, 6.000000e+00]]> : tensor<2x3xf64>
-  %1 = toy.reshape(%0 : tensor<2x3xf64>) to tensor<2x3xf64>
-  %2 = toy.constant dense<[1.000000e+00, 2.000000e+00, 3.000000e+00, 4.000000e+00, 5.000000e+00, 6.000000e+00]> : tensor<6xf64>
-  %3 = toy.reshape(%2 : tensor<6xf64>) to tensor<2x3xf64>
-  %4 = toy.generic_call @multiply_transpose(%1, %3) : (tensor<2x3xf64>, tensor<2x3xf64>) -> tensor<*xf64>
-  %5 = toy.generic_call @multiply_transpose(%3, %1) : (tensor<2x3xf64>, tensor<2x3xf64>) -> tensor<*xf64>
-  toy.print %5 : tensor<*xf64>
-  toy.return
-}
-"""
-
 
 @ModuleOp
 @Builder.implicit_region
@@ -72,21 +53,6 @@ def toy_0():
     toy.FuncOp("main", main_type, main)
 
 
-"""
-toy.func @main() {
-  %0 = toy.constant dense<[[1.000000e+00, 2.000000e+00, 3.000000e+00], [4.000000e+00, 5.000000e+00, 6.000000e+00]]> : tensor<2x3xf64>
-  %1 = toy.constant dense<[[1.000000e+00, 2.000000e+00, 3.000000e+00], [4.000000e+00, 5.000000e+00, 6.000000e+00]]> : tensor<2x3xf64>
-  %2 = toy.cast %1 : tensor<2x3xf64> to tensor<*xf64>
-  %3 = toy.cast %0 : tensor<2x3xf64> to tensor<*xf64>
-  %4 = toy.transpose(%2 : tensor<*xf64>) to tensor<*xf64>
-  %5 = toy.transpose(%3 : tensor<*xf64>) to tensor<*xf64>
-  %6 = toy.mul %4, %5 : tensor<*xf64>
-  toy.print %6 : tensor<*xf64>
-  toy.return
-}
-"""
-
-
 @ModuleOp
 @Builder.implicit_region
 def toy_1():
@@ -95,7 +61,7 @@ def toy_1():
     @Builder.implicit_region
     def main() -> None:
         x0 = toy.ConstantOp.from_list([1, 2, 3, 4, 5, 6], [2, 3]).res
-        x1 = toy.ConstantOp.from_list([1, 2, 3, 4, 5, 6], [3, 2]).res
+        x1 = toy.ConstantOp.from_list([1, 2, 3, 4, 5, 6], [2, 3]).res
         x2 = toy.CastOp(x1).res
         x3 = toy.CastOp(x0).res
         x4 = toy.TransposeOp(x2).res
