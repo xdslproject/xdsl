46aec483ec2c0f5191257ea9219ebf1e90015590
Anton Lydike <me@antonlydike.de>
2023-05-16 18:53:07 +0100

    passes: add tokenizer for passes

---
+++ tests/test_pass_lexer.py
@@ -0,0 +1,35 @@
+import pytest
+
+from xdsl.utils.parse_pipeline import tokenize_pass, Kind, PassPipelineParseError
+
+
+def test_pass_lexer():
+    tokens = list(
+        tokenize_pass(
+            'pass-1,pass-2{arg1=1 arg2=test arg3="test-str" arg-4=-34.4e-12},pass-3'
+        )
+    )
+
+    assert [t.kind for t in tokens] == [
+        Kind.IDENT, Kind.COMMA,  # pass-1,
+        Kind.IDENT, Kind.L_BRACE,  # pass-2{
+        Kind.IDENT, Kind.EQUALS, Kind.NUMBER, Kind.SPACE,  # arg1=1
+        Kind.IDENT, Kind.EQUALS, Kind.IDENT, Kind.SPACE,  # arg2=test
+        Kind.IDENT, Kind.EQUALS, Kind.STRING_LIT, Kind.SPACE,  # arg3="test-str"
+        Kind.IDENT, Kind.EQUALS, Kind.NUMBER,  # arg-4=-34.4e-12
+        Kind.R_BRACE, Kind.COMMA,  # },
+        Kind.IDENT,  # pass-3
+    ]  # fmt: skip
+
+    assert tokens[-1].span.text == "pass-3"
+    assert tokens[1].span.text == ","
+    assert tokens[3].span.text == "{"
+    assert tokens[18].span.text == "-34.4e-12"
+
+
+def test_pass_lex_errors():
+    with pytest.raises(PassPipelineParseError, match="Unknown token"):
+        list(tokenize_pass("pass-1["))
+
+    with pytest.raises(PassPipelineParseError, match="Unknown token"):
+        list(tokenize_pass("pass-1{thing$=1}"))
---
+++ xdsl/utils/parse_pipeline.py
@@ -0,0 +1,68 @@
+import re
+from dataclasses import dataclass
+from typing import Iterable
+from xdsl.utils.lexer import Input, Span
+from enum import Enum, auto
+
+
+class Kind(Enum):
+    EOF = auto()
+
+    IDENT = auto()
+    L_BRACE = auto()
+    R_BRACE = auto()
+    EQUALS = auto()
+    NUMBER = auto()
+    SPACE = auto()
+    STRING_LIT = auto()
+    COMMA = auto()
+
+
+@dataclass
+class Token:
+    span: Span
+    kind: Kind
+
+
+_lexer_rules: list[tuple[re.Pattern[str], Kind]] = [
+    (re.compile(r"[-+]?[0-9]+(\.[0-9]*([eE][-+]?[0-9]+)?)?"), Kind.NUMBER),
+    (re.compile(r"[A-Za-z0-9_-]+"), Kind.IDENT),
+    (re.compile(r'"(\\[nfvtr"\\]|[^\n\f\v\r"\\])*"'), Kind.STRING_LIT),
+    (re.compile(r"\{"), Kind.L_BRACE),
+    (re.compile(r"}"), Kind.R_BRACE),
+    (re.compile(r"="), Kind.EQUALS),
+    (re.compile(r"\s+"), Kind.SPACE),
+    (re.compile(r","), Kind.COMMA),
+]
+
+
+def tokenize_pass(input_str: str) -> Iterable[Token]:
+    """
+    This tokenizes a pass declaration string
+    """
+    input = Input(input_str, "pass-pipeline")
+    pos = 0
+    end = len(input_str)
+
+    while True:
+        token: Token | None = None
+        for pattern, kind in _lexer_rules:
+            if (match := pattern.match(input_str, pos)) is not None:
+                token = Token(Span(match.start(), match.end(), input), kind)
+                pos = match.end()
+                break
+        if token is None:
+            raise PassPipelineParseError(
+                Token(Span(pos, pos + 1, input), Kind.IDENT), "Unknown token"
+            )
+        yield token
+        if pos >= end:
+            return
+
+
+class PassPipelineParseError(BaseException):
+    def __init__(self, token: Token, msg: str):
+        super().__init__(
+            "Error parsing pass pipeline specification:\n"
+            + token.span.print_with_context(msg)
+        )
