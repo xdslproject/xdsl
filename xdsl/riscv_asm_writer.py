8ef2ebe1acca1627b5de9e70f632a9486366323d
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-05-19 15:00:42 +0100

    riscv: only walk the body when printing the assembly

--- xdsl/riscv_asm_writer.py
+++ xdsl/riscv_asm_writer.py
@@ -9,9 +9,8 @@ from xdsl.dialects import riscv
 
 
 def print_riscv_module(module: ModuleOp, output: IO[str]):
-    for op in module.walk():
-        if isa(op, RISCVOp):
-            print_assembly_instruction(op, output)
+    for op in module.body.walk():
+        print_assembly_instruction(op, output)
 
 
 def append_comment(line: str, comment: StringAttr | None) -> str:
