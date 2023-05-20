554c5ade8a8ddb13574ce38bd37555d6f7b2e5c3
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-05-20 20:50:21 +0200

    core: Allow to print regions with arguments separately

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -8,6 +8,7 @@ from typing import Annotated
 from xdsl.dialects.arith import Arith, Addi, Constant
 from xdsl.dialects.builtin import Builtin, IntAttr, IntegerType, ModuleOp, UnitAttr, i32
 from xdsl.dialects.func import Func
+from xdsl.dialects.test import TestOp
 from xdsl.ir import (
     Attribute,
     MLContext,
@@ -15,6 +16,7 @@ from xdsl.ir import (
     Operation,
     ParametrizedAttribute,
     Block,
+    Region,
 )
 from xdsl.irdl import (
     Operand,
@@ -362,6 +364,92 @@ def test_print_custom_block_arg_name():
     p = Printer(stream=io)
     p.print_block(block)
     assert io.getvalue() == """\n^0(%test : i32, %0 : i32):"""
+
+
+def test_print_block_argument():
+    """Print a block argument."""
+    block = Block(arg_types=[i32, i32])
+
+    io = StringIO()
+    p = Printer(stream=io)
+    p.print_block_argument(block.args[0])
+    p.print(", ")
+    p.print_block_argument(block.args[0], print_type=False)
+    assert io.getvalue() == """%0 : i32, %1"""
+
+
+def test_print_block():
+    """Print a block."""
+    block = Block(arg_types=[i32, i32])
+    block.add_op(TestOp(operands=(block.args[1],), result_types=[[]], regions=[[]]))
+
+    # Print block arguments inside the block
+    io = StringIO()
+    p = Printer(stream=io)
+    p.print_block(block)
+    assert (
+        io.getvalue() == """\n^0(%0 : i32, %1 : i32):\n  "test.op"(%1) : (i32) -> ()"""
+    )
+
+
+def test_print_block_without_arguments():
+    """Print a block and its arguments separately."""
+    block = Block(arg_types=[i32, i32])
+    block.add_op(TestOp(operands=(block.args[1],), result_types=[[]], regions=[[]]))
+
+    # Print block arguments separately from the block
+    io = StringIO()
+    p = Printer(stream=io)
+    p.print_block_argument(block.args[0])
+    p.print(", ")
+    p.print_block_argument(block.args[1])
+    p.print_block(block, print_block_args=False)
+    assert io.getvalue() == """%0 : i32, %1 : i32\n  "test.op"(%1) : (i32) -> ()"""
+
+
+def test_print_region():
+    """Print a region."""
+    block = Block(arg_types=[i32, i32])
+    block.add_op(TestOp(operands=(block.args[1],), result_types=[[]], regions=[[]]))
+    region = Region(block)
+
+    io = StringIO()
+    p = Printer(stream=io)
+    p.print_region(region)
+    assert (
+        io.getvalue()
+        == """{\n^0(%0 : i32, %1 : i32):\n  "test.op"(%1) : (i32) -> ()\n}"""
+    )
+
+
+def test_print_region_without_arguments():
+    """Print a region and its arguments separately."""
+    block = Block(arg_types=[i32, i32])
+    block.add_op(TestOp(operands=(block.args[1],), result_types=[[]], regions=[[]]))
+    region = Region(block)
+
+    io = StringIO()
+    p = Printer(stream=io)
+    p.print_block_argument(block.args[0])
+    p.print(", ")
+    p.print_block_argument(block.args[1])
+    p.print(" ")
+    p.print_region(region, print_entry_block_args=False)
+    assert io.getvalue() == """%0 : i32, %1 : i32 {\n  "test.op"(%1) : (i32) -> ()\n}"""
+
+
+def test_print_region_empty_block():
+    """
+    Print a region with an empty block, and specify that
+    empty entry blocks shouldn't be printed.
+    """
+    block = Block()
+    region = Region(block)
+
+    io = StringIO()
+    p = Printer(stream=io)
+    p.print_region(region, print_empty_block=False)
+    assert io.getvalue() == """{\n}"""
 
 
 #   ____          _                  _____                          _
