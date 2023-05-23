f75d8be84e995189d812a6e391ccfa6e9120ee9a
Emilien Bauer <bauer.emilien@gmail.com>
2023-05-23 19:25:35 +0100

    Use name_hint.

--- xdsl/dialects/arith.py
+++ xdsl/dialects/arith.py
@@ -173,7 +173,9 @@ class BinaryOperation(IRDLOperation, Generic[_T]):
         return cls(lhs, rhs, result_type)
 
     def print(self, printer: Printer):
-        printer.print(f" %{self.lhs.name}, %{self.rhs.name} : {self.result.typ}")
+        printer.print(
+            f" %{self.lhs.name_hint}, %{self.rhs.name_hint} : {self.result.typ}"
+        )
 
     # TODO replace with trait
     def verify_(self) -> None:
