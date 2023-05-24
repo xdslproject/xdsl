a3366cf69590742723a0e20997508f8ff468db36
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2023-05-24 08:53:06 +0100

    dialects/riscv: Add optional regions to LabelOp

--- xdsl/dialects/riscv.py
+++ xdsl/dialects/riscv.py
@@ -1586,10 +1586,12 @@ class LabelOp(IRDLOperation, RISCVOp):
     name = "riscv.label"
     label: OpAttr[LabelAttr]
     comment: OptOpAttr[StringAttr]
+    data: OptSingleBlockRegion
 
     def __init__(
         self,
         label: str | LabelAttr,
+        region: OptSingleBlockRegion = None,
         *,
         comment: str | StringAttr | None = None,
     ):
@@ -1597,12 +1599,15 @@ class LabelOp(IRDLOperation, RISCVOp):
             label = LabelAttr(label)
         if isinstance(comment, str):
             comment = StringAttr(comment)
+        if region is None:
+            region = Region()
 
         super().__init__(
             attributes={
                 "label": label,
                 "comment": comment,
-            }
+            },
+            regions=[region],
         )
 
 
