38dd1efb611c35444c2230cdecd6473ceda2d0c7
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-05-24 11:04:15 +0100

    dialects: riscv: Add a naive register allocator strategy

---
+++ docs/Toy/toy/tests/test_regalloc.py
@@ -0,0 +1,166 @@
+from xdsl.builder import Builder
+from xdsl.dialects import riscv
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.ir import MLContext
+from xdsl.riscv_asm_writer import riscv_code
+
+from xdsl.transforms.riscv_register_allocation import (
+    RISCVRegisterAllocation,
+    RegisterAllocationAlgorithm,
+)
+
+from ..emulator.emulator_iop import run_riscv
+
+ALLOCATION_STRATEGIES = [
+    RegisterAllocationAlgorithm.GlobalJRegs,
+    RegisterAllocationAlgorithm.BlockNaive,
+]
+
+def context() -> MLContext:
+    ctx = MLContext()
+    return ctx
+
+# Handwritten riscv dialect code to test register allocation
+
+@ModuleOp
+@Builder.implicit_region
+def simple_branching_riscv():
+    """
+    The following riscv dialect IR is generated from the following C code:
+
+    int main() {
+        int a = 5;
+        int b = 77777;
+        int c = 6;
+        if (a==b) {
+            c = a * a;
+        } else {
+            c = b + b;
+        }
+
+        return c;
+    }
+
+    The goal of this test is to check that the register allocator is able to handle very simple branching code with multiple basic blocks.
+    Morever it uses some reserved registers (ra, s0) to check that the register allocator does not use them.
+    """
+
+    @Builder.implicit_region
+    def text_region():
+        @Builder.implicit_region
+        def main_region() -> None:
+            sp = riscv.GetRegisterOp(riscv.Registers.SP).res
+            riscv.AddiOp(sp, -32, rd=riscv.Registers.SP).rd
+            ra = riscv.GetRegisterOp(riscv.Registers.RA).res
+            riscv.SwOp(ra, sp, 28)
+            s0 = riscv.GetRegisterOp(riscv.Registers.S0).res
+            riscv.SwOp(s0, sp, 24)
+            a = riscv.AddiOp(sp, 32).rd
+            b = riscv.LiOp(5).rd
+            riscv.SwOp(b, a, -12)
+            c = riscv.LuiOp(19).rd
+            d = riscv.AddiOp(c, -47).rd
+            riscv.SwOp(d, a, -16)
+            e = riscv.LiOp(6).rd
+            riscv.SwOp(e, a, -20)
+            f = riscv.LwOp(a, -12).rd
+            g = riscv.LwOp(a, -16).rd
+            riscv.BneOp(f, g, riscv.LabelAttr("LBB0_2"))
+            riscv.JOp(riscv.LabelAttr("LBB0_1"))
+
+            @Builder.implicit_region
+            def true_branch() -> None:
+                f = riscv.LwOp(a, -12).rd
+                f = riscv.MulOp(f, f).rd
+                riscv.SwOp(f, a, -20)
+                riscv.JOp(riscv.LabelAttr("LBB0_3"))
+            riscv.LabelOp("LBB0_1", true_branch)
+
+            @Builder.implicit_region
+            def false_branch() -> None:
+                f = riscv.LwOp(a, -16).rd
+                f = riscv.AddOp(f, f).rd
+                riscv.SwOp(f, a, -20)
+                riscv.JOp(riscv.LabelAttr("LBB0_3"))
+            riscv.LabelOp("LBB0_2", false_branch)
+
+            @Builder.implicit_region
+            def merge_if() -> None:
+                f = riscv.LwOp(a, -20).rd
+                riscv.LwOp(sp, 28, rd=riscv.Registers.RA).rd
+                riscv.LwOp(sp, 24, rd=riscv.Registers.S0).rd
+                riscv.AddiOp(sp, 32, rd=riscv.Registers.SP).rd
+                riscv.MVOp(f, rd=riscv.Registers.A0)
+                zero = riscv.GetRegisterOp(riscv.Registers.ZERO).res
+                riscv.AddiOp(zero, 93, rd=riscv.Registers.A7).rd
+                riscv.EcallOp()
+            riscv.LabelOp("LBB0_3", merge_if)
+
+        riscv.LabelOp("main", main_region)
+    riscv.DirectiveOp(".text", None, text_region)
+
+
+@ModuleOp
+@Builder.implicit_region
+def simple_linear_riscv():
+    """
+    The following riscv dialect IR is generated from the following C code:
+
+    int main() {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        int d = a - b * c;
+        int f = a * b + c + d;
+        int g = a - b + c * d + f;
+        int h = a * b * c * d * f * g;
+        return a + b * c - d + f * g;
+    }
+
+    The goal of this test is to check that the register allocator is able to handle very simple linear code with no branching.
+    """
+
+    @Builder.implicit_region
+    def text_region():
+        @Builder.implicit_region
+        def main_region() -> None:
+            zero = riscv.GetRegisterOp(riscv.Registers.ZERO).res
+            v0 = riscv.AddiOp(zero, 1).rd
+            v1 = riscv.AddiOp(zero, 2).rd
+
+            v3 = riscv.AddOp(v1, v0).rd
+            v4 = riscv.MulOp(v3, v1).rd
+            v5 = riscv.SubOp(v0, v4).rd
+            v6 = riscv.MulOp(v1, v0).rd
+            v7 = riscv.AddOp(v6, v3).rd
+            v8 = riscv.AddOp(v7, v5).rd
+            v9 = riscv.SubOp(v0, v1).rd
+            v10 = riscv.MulOp(v5, v3).rd
+            v11 = riscv.AddOp(v9, v10).rd
+            v12 = riscv.AddOp(v11, v8).rd
+            v13 = riscv.AddOp(v4, v0).rd
+            v14 = riscv.SubOp(v13, v5).rd
+            v15 = riscv.MulOp(v12, v8).rd
+            v16 = riscv.AddOp(v14, v15).rd
+
+            riscv.MVOp(v16, rd=riscv.Registers.A0)
+            riscv.AddiOp(zero, 93, rd=riscv.Registers.A7).rd
+            riscv.EcallOp()
+        riscv.LabelOp("main", main_region)
+    riscv.DirectiveOp(".text", None, text_region)
+
+def test_allocate_simple_branching():
+    for allocation_strategy in ALLOCATION_STRATEGIES:
+        RISCVRegisterAllocation(allocation_strategy).apply(context(), simple_branching_riscv)
+        code = riscv_code(simple_branching_riscv)
+        assert (
+            run_riscv(code, unlimited_regs=True, setup_stack=True, verbosity=1)
+            == 155554
+        )
+
+
+def test_allocate_simple_linear():
+    for allocation_strategy in ALLOCATION_STRATEGIES:
+        RISCVRegisterAllocation(allocation_strategy).apply(context(), simple_linear_riscv)
+        code = riscv_code(simple_linear_riscv)
+        assert run_riscv(code, unlimited_regs=True, setup_stack=True, verbosity=1) == 12
--- xdsl/transforms/riscv_register_allocation.py
+++ xdsl/transforms/riscv_register_allocation.py
@@ -1,10 +1,70 @@
+from enum import Enum
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.dialects.riscv import Register, RegisterType, RISCVOp
 from xdsl.ir import MLContext
 from xdsl.passes import ModulePass
 
 
-class RegisterAllocator:
+class RegisterAllocatorStrategy:
+    """
+    Base class for register allocation strategies.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def allocate_registers(self, module: ModuleOp) -> None:
+        """
+        Allocates unallocated registers in the module.
+        """
+
+        raise NotImplementedError()
+
+
+class RegisterAllocatorBlockNaive(RegisterAllocatorStrategy):
+    idx: int
+
+    def __init__(self) -> None:
+        self.idx = 0
+
+        """
+        Since we've got neither right now a handling of a consistent ABI nor of a calling convention,
+        let's just assume that we have all the registers available for our use except the one explicitly reserved by the default riscv ABI.
+        """
+
+        self.available_registers = set(Register.ABI_INDEX_BY_NAME.keys())
+        reserved_registers = set(["zero", "sp", "gp", "tp", "fp", "s0"])
+        self.available_registers -= reserved_registers
+
+    def allocate_registers(self, module: ModuleOp) -> None:
+        """
+        Sets unallocated registers for each block to a finite set of real available registers.
+        When it runs out of real registers for a block, it allocates j registers.
+        """
+
+        for region in module.regions:
+            for block in region.blocks:
+                block_registers = self.available_registers.copy()
+
+                for op in block.walk():
+                    if not isinstance(op, RISCVOp):
+                        # Don't perform register allocations on non-RISCV-ops
+                        continue
+
+                    for result in op.results:
+                        assert isinstance(result.typ, RegisterType)
+                        if result.typ.data.name is None:
+                            # If we run out of real registers, allocate a j register
+                            if block_registers == set():
+                                result.typ = RegisterType(Register(f"j{self.idx}"))
+                                self.idx += 1
+                            else:
+                                result.typ = RegisterType(
+                                    Register(block_registers.pop())
+                                )
+
+
+class RegisterAllocatorJRegs(RegisterAllocatorStrategy):
     idx: int
 
     def __init__(self) -> None:
@@ -12,8 +72,7 @@ class RegisterAllocator:
 
     def allocate_registers(self, module: ModuleOp) -> None:
         """
-        Allocates unallocated registers in the module. Currently sets them to an infinite set
-        of `j` registers.
+        Sets unallocated registers to an infinite set of `j` registers
         """
         for op in module.walk():
             if not isinstance(op, RISCVOp):
@@ -27,14 +86,33 @@ class RegisterAllocator:
                     self.idx += 1
 
 
+class RegisterAllocationAlgorithm(Enum):
+    """
+    The register allocation algorithm to use.
+    """
+
+    GlobalJRegs = 0
+    BlockNaive = 1
+
+
 class RISCVRegisterAllocation(ModulePass):
     """
-    Allocates unallocated registers in the module. Currently sets them to an infinite set
-    of `j` registers.
+    Allocates unallocated registers in the module.
     """
 
     name = "riscv-allocate-registers"
 
+    def __init__(
+        self,
+        allocation_type: RegisterAllocationAlgorithm = RegisterAllocationAlgorithm.GlobalJRegs,
+    ) -> None:
+        self.allocation_type = allocation_type
+
     def apply(self, ctx: MLContext, op: ModuleOp) -> None:
-        allocator = RegisterAllocator()
+        match self.allocation_type:
+            case RegisterAllocationAlgorithm.GlobalJRegs:
+                allocator = RegisterAllocatorJRegs()
+            case RegisterAllocationAlgorithm.BlockNaive:
+                allocator = RegisterAllocatorBlockNaive()
+
         allocator.allocate_registers(op)
