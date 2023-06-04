93004a5812bb00ec443eef598cfd44b632aee9c6
Sasha Lopoukhine <superlopuh@gmail.com>
2023-06-04 17:31:51 +0100

    use timeraw for import check

--- benchmarks/import_xdsl_opt.py
+++ benchmarks/import_xdsl_opt.py
@@ -2,11 +2,11 @@
 # See "Writing benchmarks" in the asv docs for more information.
 
 
-class ImportXDSLOptSuite:
+def timeraw_import_inspect():
     """
     A benchmark that measures the time to import xdsl_opt_main. This is most of the
     constant cost of running xdsl-opt.
     """
-
-    def time_import_xdsl_opt_main(self):
-        from xdsl import xdsl_opt_main as _
+    return """
+    from xdsl.xdsl_opt_main import xDSLOptMain
+    """
