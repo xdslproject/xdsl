e9bb42d07679dffc815077aabad8f271155824d7
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-06-06 10:25:25 +0100

    dialects: (riscv): basic linear scan regalloc

--- docs/Toy/toy/tests/test_regalloc.py
+++ docs/Toy/toy/tests/test_regalloc.py
@@ -1,12 +1,17 @@
+from collections import OrderedDict
 from io import StringIO
+from typing import List
 from xdsl.builder import Builder
 from xdsl.dialects import riscv
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.ir import MLContext
 
 from xdsl.transforms.riscv_register_allocation import (
+    LiveInterval,
     RISCVRegisterAllocation,
+    RegisterSet,
 )
+from xdsl.utils.test_value import TestSSAValue
 
 
 def context() -> MLContext:
@@ -21,6 +26,14 @@ def riscv_code(module: ModuleOp) -> str:
 
 
 # Handwritten riscv dialect code to test register allocation
+RESERVERD_REGISTERS = set(["zero", "sp", "gp", "tp", "fp", "s0"])
+AVAILABLE_REGISTERS = RegisterSet(
+    [
+        reg
+        for reg in list(riscv.Register.ABI_INDEX_BY_NAME.keys())
+        if reg not in RESERVERD_REGISTERS
+    ]
+)
 
 
 @ModuleOp
@@ -89,7 +102,6 @@ def simple_linear_riscv_allocated():
             zero = riscv.GetRegisterOp(riscv.Registers.ZERO).res
             v0 = riscv.AddiOp(zero, 1, rd=riscv.Registers.T6).rd
             v1 = riscv.AddiOp(zero, 2, rd=riscv.Registers.T5).rd
-
             v3 = riscv.AddOp(v1, v0, rd=riscv.Registers.T4).rd
             v4 = riscv.MulOp(v3, v1, rd=riscv.Registers.T3).rd
             v5 = riscv.SubOp(v0, v4, rd=riscv.Registers.S11).rd
@@ -118,3 +130,64 @@ def test_allocate_simple_linear():
     RISCVRegisterAllocation("BlockNaive").apply(context(), simple_linear_riscv)
 
     assert riscv_code(simple_linear_riscv) == riscv_code(simple_linear_riscv_allocated)
+
+
+# Check that the linear scan register allocator is able to allocate given hardcodded live ranges
+
+
+def test_dummy_linear_scan_allocator():
+    def get_test_variable() -> TestSSAValue:
+        return TestSSAValue(riscv.RegisterType(riscv.Register()))
+
+    a = LiveInterval(get_test_variable(), 1, 10)
+    b = LiveInterval(get_test_variable(), 1, 4)
+    c = LiveInterval(get_test_variable(), 1, 3)
+    d = LiveInterval(get_test_variable(), 2, 8)
+    e = LiveInterval(get_test_variable(), 3, 6)
+    f = LiveInterval(get_test_variable(), 3, 10)
+    g = LiveInterval(get_test_variable(), 4, 8)
+
+    """
+    a: 1 2 3 4 5 6 7 8 9 10
+    b: 1 2 3 4
+    c: 1 2 3
+    d:   2 3 4 5 6 7 8
+    e:     3 4 5 6
+    f:     3 4 5 6 7 8 9 10
+    g:       4 5 6 7 8
+    """
+
+    intervals: List[LiveInterval] = [a, b, c, d, e, f, g]
+
+    @ModuleOp
+    @Builder.implicit_region
+    def empty_module():
+        pass
+
+    # create the condition for spilling
+
+    AVAILABLE_REGISTERS.reset()
+    AVAILABLE_REGISTERS.limit_free_registers(3)
+
+    RISCVRegisterAllocation("LinearScan").apply(
+        context(), empty_module, intervals, AVAILABLE_REGISTERS
+    )
+
+    assert a.register == None
+    assert a.abstract_stack_location == 0
+    assert b.register == riscv.Registers.T0
+    assert c.register == riscv.Registers.RA
+    assert d.register == None
+    assert d.abstract_stack_location == 1
+    assert e.register == riscv.Registers.T1
+    assert f.register == None
+    assert f.abstract_stack_location == 2
+    assert g.register == riscv.Registers.RA
+
+    assert a.allocated() == riscv.RegisterType(riscv.Register("j0"))
+    assert b.allocated() == riscv.RegisterType(riscv.Registers.T0)
+    assert c.allocated() == riscv.RegisterType(riscv.Registers.RA)
+    assert d.allocated() == riscv.RegisterType(riscv.Register("j1"))
+    assert e.allocated() == riscv.RegisterType(riscv.Registers.T1)
+    assert f.allocated() == riscv.RegisterType(riscv.Register("j2"))
+    assert g.allocated() == riscv.RegisterType(riscv.Registers.RA)
--- xdsl/transforms/riscv_register_allocation.py
+++ xdsl/transforms/riscv_register_allocation.py
@@ -1,9 +1,59 @@
 from abc import ABC
+from collections import OrderedDict
 from dataclasses import dataclass
+from typing import Any, List, Set
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.dialects.riscv import Register, RegisterType, RISCVOp
-from xdsl.ir import MLContext
+from xdsl.ir import MLContext, SSAValue
 from xdsl.passes import ModulePass
+
+
+class RegisterSet:
+    def __init__(self, registers: List[str]) -> None:
+        self.registers = registers
+        self.free: List[str] = list(registers)
+        self.occupied: Set[str] = set()
+
+    def get_free(self) -> str | None:
+        if not self.free:
+            return None
+
+        reg = self.free.pop()
+        self.occupied.add(reg)
+        return reg
+
+    def set_free(self, reg: str) -> None:
+        self.occupied.remove(reg)
+        self.free.append(reg)
+
+    def set_occupied(self, reg: str) -> None:
+        assert reg in self.free
+        self.free.remove(reg)
+        self.occupied.add(reg)
+
+    def reset(self) -> None:
+        self.free = list(self.registers)
+        self.occupied = set()
+
+    def num_free_registers(self) -> int:
+        return len(self.free)
+
+    def has_available_registers(self) -> bool:
+        return not self.free
+
+    def limit_free_registers(self, limit: int) -> None:
+        self.free = self.free[:limit]
+        self.occupied = set(self.registers) - set(self.free)
+
+
+_DEFAULT_RESERVED_REGISTERS = set(["zero", "sp", "gp", "tp", "fp", "s0"])
+_DEFAULT_REGISTER_SET = RegisterSet(
+    [
+        reg
+        for reg in list(Register.ABI_INDEX_BY_NAME.keys())
+        if reg not in _DEFAULT_RESERVED_REGISTERS
+    ]
+)
 
 
 class AbstractRegisterAllocator(ABC):
@@ -24,20 +74,11 @@ class AbstractRegisterAllocator(ABC):
 
 class RegisterAllocatorBlockNaive(AbstractRegisterAllocator):
     idx: int
+    register_set: RegisterSet
 
-    def __init__(self) -> None:
+    def __init__(self, register_set: RegisterSet = _DEFAULT_REGISTER_SET) -> None:
         self.idx = 0
-
-        """
-        Since we've got neither right now a handling of a consistent ABI nor of a calling convention,
-        let's just assume that we have all the registers available for our use except the one explicitly reserved by the default riscv ABI.
-        """
-
-        self.available_registers = list(Register.ABI_INDEX_BY_NAME.keys())
-        reserved_registers = set(["zero", "sp", "gp", "tp", "fp", "s0"])
-        self.available_registers = [
-            reg for reg in self.available_registers if reg not in reserved_registers
-        ]
+        self.register_set = register_set
 
     def allocate_registers(self, module: ModuleOp) -> None:
         """
@@ -47,8 +88,7 @@ class RegisterAllocatorBlockNaive(AbstractRegisterAllocator):
 
         for region in module.regions:
             for block in region.blocks:
-                block_registers = self.available_registers.copy()
-
+                self.register_set.reset()
                 for op in block.walk():
                     if not isinstance(op, RISCVOp):
                         # Don't perform register allocations on non-RISCV-ops
@@ -58,12 +98,12 @@ class RegisterAllocatorBlockNaive(AbstractRegisterAllocator):
                         assert isinstance(result.typ, RegisterType)
                         if result.typ.data.name is None:
                             # If we run out of real registers, allocate a j register
-                            if not block_registers:
+                            if self.register_set.has_available_registers():
                                 result.typ = RegisterType(Register(f"j{self.idx}"))
                                 self.idx += 1
                             else:
                                 result.typ = RegisterType(
-                                    Register(block_registers.pop())
+                                    Register(self.register_set.get_free())
                                 )
 
 
@@ -89,6 +129,157 @@ class RegisterAllocatorJRegs(AbstractRegisterAllocator):
                     self.idx += 1
 
 
+class LiveInterval:
+    ssa_value: SSAValue
+    register: Register | None
+    start: int
+    abstract_stack_location: int | None
+    end: int
+
+    def __init__(self, ssa_value: SSAValue, start: int, end: int) -> None:
+        self.ssa_value = ssa_value
+
+        if (
+            isinstance(ssa_value.typ, RegisterType)
+            and ssa_value.typ.data.name is not None
+        ):
+            self.register = ssa_value.typ.data
+        else:
+            self.register = None
+
+        self.abstract_stack_location = None
+        self.start = start
+        self.end = end
+
+    def regalloc(self, register: Register) -> None:
+        self.abstract_stack_location = None
+        self.register = register
+
+    def spill(self, stack_location: int) -> None:
+        self.register = None
+        self.abstract_stack_location = stack_location
+
+    def allocated(self) -> RegisterType | None:
+        assert isinstance(self.ssa_value.typ, RegisterType)
+        return self.ssa_value.typ
+
+    def __repr__(self) -> str:
+        return f"LiveInterval({self.ssa_value}, {self.start}, {self.end}) -> Register: {self.register}, Stack Location: {self.abstract_stack_location}"
+
+
+class SSAValueInfo:
+    register: Register | None
+
+
+class RegisterAllocatorLinearScan(AbstractRegisterAllocator):
+    """
+    Linear scan register allocation strategy.
+
+    Basic form of the algorithm based on the paper "Linear Scan Register Allocation"
+    by Massimiliano Poletto and Vivek Sarkar.
+    """
+
+    abstract_stack_location: int
+    intervals: list[LiveInterval]
+    active: OrderedDict[LiveInterval, None]
+    register_set: RegisterSet
+
+    def __init__(
+        self,
+        intervals: list[LiveInterval],
+        register_set: RegisterSet = _DEFAULT_REGISTER_SET,
+    ) -> None:
+        self.active = OrderedDict()
+        self.intervals = sorted(intervals, key=lambda x: x.start)
+        self.register_set = register_set
+
+        # already registers are removed from the available registers
+        for interval in self.intervals:
+            if interval.register is not None and interval.register.name is not None:
+                self.register_set.set_occupied(interval.register.name)
+
+        self.abstract_stack_location = 0
+
+    # TO:DO - Refactor the following methods to use a proper SortedSet (C++ fashion)
+    # This requires an extra dependency, so right now stick with this subpar implementation
+
+    ###
+    def insert_active_interval(self, interval: LiveInterval) -> None:
+        self.active[interval] = None
+        self.sort_active_intervals()
+
+    def remove_active_interval(self, interval: LiveInterval) -> None:
+        self.active.pop(interval)
+        self.sort_active_intervals()
+
+    def sort_active_intervals(self) -> None:
+        self.active = OrderedDict(sorted(self.active.items(), key=lambda x: x[0].end))
+
+    ###
+
+    def allocate_registers(self, module: ModuleOp) -> None:
+        """
+        Allocates unallocated registers in the module.
+        """
+
+        def expire_old_intervals(i: LiveInterval) -> None:
+            for j in list(filter(lambda j: j.end < i.start, self.active.keys())):
+                assert j.register is not None
+                assert j.register.name is not None
+
+                self.remove_active_interval(j)
+                self.register_set.set_free(j.register.name)
+
+        def fresh_stack_location() -> int:
+            old_stack_location = self.abstract_stack_location
+            self.abstract_stack_location += 1
+            return old_stack_location
+
+        def spill_at_interval(i: LiveInterval) -> None:
+            # Spill the interval with the furthest endpoint
+            spill = next(reversed(self.active.keys()))
+            if spill.end > i.end:
+                i.register = spill.register
+                self.remove_active_interval(spill)
+                spill.spill(fresh_stack_location())
+                self.insert_active_interval(i)
+            else:
+                i.spill(fresh_stack_location())
+
+        num_registers = self.register_set.num_free_registers()
+        for iv in self.intervals:
+            """
+            Expire all intervals whose endpoint is smaller than the start point of the current interval being processed.
+            This means that the current interval can be mapped to a register that was previously assigned
+            to one of the expired intervals, which is no longer needed because it has expired.
+            """
+
+            expire_old_intervals(iv)
+            if len(self.active) >= num_registers:
+                spill_at_interval(iv)
+            else:
+                iv.regalloc(Register(self.register_set.get_free()))
+                self.insert_active_interval(iv)
+
+        # final pass to assign registers to intervals
+        for iv in self.intervals:
+            assert isinstance(iv.ssa_value.typ, RegisterType)
+
+            # if the interval is not allocated to a register, it must be spilled
+            if iv.register is None:
+                assert iv.abstract_stack_location is not None
+                # still use j-register for now
+                iv.ssa_value.typ = RegisterType(
+                    Register(f"j{iv.abstract_stack_location}")
+                )
+            elif iv.ssa_value.typ.data.name is None:
+                iv.ssa_value.typ = RegisterType(iv.register)
+            else:
+                raise ValueError("Register already allocated")
+
+        return
+
+
 @dataclass
 class RISCVRegisterAllocation(ModulePass):
     """
@@ -99,10 +290,11 @@ class RISCVRegisterAllocation(ModulePass):
 
     allocation_strategy: str = "GlobalJRegs"
 
-    def apply(self, ctx: MLContext, op: ModuleOp) -> None:
+    def apply(self, ctx: MLContext, op: ModuleOp, *args: Any) -> None:
         allocator_strategies = {
             "GlobalJRegs": RegisterAllocatorJRegs,
             "BlockNaive": RegisterAllocatorBlockNaive,
+            "LinearScan": RegisterAllocatorLinearScan,
         }
 
         if self.allocation_strategy not in allocator_strategies:
@@ -111,5 +303,5 @@ class RISCVRegisterAllocation(ModulePass):
                 f"Available allocation types: {allocator_strategies.keys()}"
             )
 
-        allocator = allocator_strategies[self.allocation_strategy]()
+        allocator = allocator_strategies[self.allocation_strategy](*args)
         allocator.allocate_registers(op)
