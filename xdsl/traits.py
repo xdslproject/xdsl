41cc21f63f29824cbedb3aa717cff5c60ef26589
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2023-06-07 13:41:45 +0100

    Add verification at the trait

--- xdsl/traits.py
+++ xdsl/traits.py
@@ -68,3 +68,10 @@ class IsTerminator(OpTrait):
 
     https://mlir.llvm.org/docs/Traits/#terminator
     """
+
+    def verify(self, op: Operation) -> None:
+        """Check that the operation satisfies the IsTerminator trait requirements."""
+        if op.parent is not None and op.parent.last_op != op:
+            raise VerifyException(
+                f"'{op.name}' must be the last operation in the parent block"
+            )
