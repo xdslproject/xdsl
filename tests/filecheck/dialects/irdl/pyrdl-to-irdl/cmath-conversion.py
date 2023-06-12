8ec5d87c256de2f7884e051c1280eb904dced699
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-06-13 00:57:55 +0200

    Fix test

--- tests/filecheck/dialects/irdl/pyrdl-to-irdl/cmath-conversion.py
+++ tests/filecheck/dialects/irdl/pyrdl-to-irdl/cmath-conversion.py
@@ -8,23 +8,23 @@ print(dialect_to_irdl(CMath, "cmath"))
 # CHECK:      irdl.dialect @cmath {
 
 # CHECK-NEXT:   irdl.attribute @cmath.complex {
-# CHECK-NEXT:     %{} = irdl.any
-# CHECK-NEXT:     irdl.parameters(%{})
+# CHECK-NEXT:     %{{.*}} = irdl.any
+# CHECK-NEXT:     irdl.parameters(%{{.*}})
 # CHECK-NEXT:   }
 
 # CHECK-NEXT:   irdl.operation @cmath.norm {
-# CHECK-NEXT:     %{} = irdl.any
-# CHECK-NEXT:     irdl.operands(%{})
-# CHECK-NEXT:     %{} = irdl.any
-# CHECK-NEXT:     irdl.results(%{})
+# CHECK-NEXT:     %{{.*}} = irdl.any
+# CHECK-NEXT:     irdl.operands(%{{.*}})
+# CHECK-NEXT:     %{{.*}} = irdl.any
+# CHECK-NEXT:     irdl.results(%{{.*}})
 # CHECK-NEXT:   }
 
 # CHECK-NEXT:   irdl.operation @cmath.mul {
-# CHECK-NEXT:     %{} = irdl.any
-# CHECK-NEXT:     %{} = irdl.any
-# CHECK-NEXT:     irdl.operands(%{}, %{})
-# CHECK-NEXT:     %{} = irdl.any
-# CHECK-NEXT:     irdl.results(%{})
+# CHECK-NEXT:     %{{.*}} = irdl.any
+# CHECK-NEXT:     %{{.*}} = irdl.any
+# CHECK-NEXT:     irdl.operands(%{{.*}}, %{{.*}})
+# CHECK-NEXT:     %{{.*}} = irdl.any
+# CHECK-NEXT:     irdl.results(%{{.*}})
 # CHECK-NEXT:   }
 
 # CHECK-NEXT: }
