b2ce0857ddbf1ab81adacf8a9e6cabc754bd4702
Sasha Lopoukhine <superlopuh@gmail.com>
2023-06-15 08:56:29 +0100

    fix pyright issues

--- docs/Toy/toy/interpreter.py
+++ docs/Toy/toy/interpreter.py
@@ -85,7 +85,7 @@ class ToyFunctions(InterpreterFunctions):
         (arg,) = args
         assert isinstance(arg, Tensor)
         result_typ = op.results[0].typ
-        assert isinstance(result_typ, VectorType | TensorType)
+        assert isinstance(result_typ, (VectorType, TensorType))
         new_shape = list(result_typ.get_shape())
 
         return (Tensor(arg.data, new_shape),)
