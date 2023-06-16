07dae3c6fdc044362172794901d51acb7d470daa
Emilien Bauer <bauer.emilien@gmail.com>
2023-06-16 17:17:56 +0100

    Pyright.

--- xdsl/dialects/memref.py
+++ xdsl/dialects/memref.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import (
     TYPE_CHECKING,
     Iterable,
-    Mapping,
     Sequence,
     TypeVar,
     Optional,
@@ -32,8 +31,6 @@ from xdsl.dialects.builtin import (
 )
 from xdsl.ir import (
     Attribute,
-    Block,
-    Region,
     TypeAttribute,
     Operation,
     SSAValue,
