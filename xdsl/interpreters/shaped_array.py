097be14259278dd10b5f909fe9211b4d503eb1bf
Sasha Lopoukhine <superlopuh@gmail.com>
2023-06-27 17:15:01 +0100

    interpreter: introduce ShapedArray class for interpreter functions (#1199)
    Also use in Toy

---
+++ xdsl/interpreters/shaped_array.py
@@ -0,0 +1,63 @@
+from itertools import accumulate
+from math import prod
+import operator
+from typing import Generic, TypeVar
+from attr import dataclass
+
+_T = TypeVar("_T")
+
+
+@dataclass
+class ShapedArray(Generic[_T]):
+    """
+    A helper structure to represent instances of type MemrefType, TensorType, VectorType, etc.
+    in the interpreter.
+    """
+
+    data: list[_T]
+    shape: list[int]
+
+    def __post__init__(self):
+        assert prod(self.shape) == len(self.data)
+
+    def offset(self, indices: tuple[int, ...]) -> int:
+        """
+        Returns the index of the element in `self.data` for a given tuple of indices
+        """
+        if len(indices) != len(self.shape):
+            raise ValueError(f"Invalid indices {indices} for shape {self.shape}")
+        # For each dimension, the number of elements in the nested arrays
+        prod_dims: list[int] = list(
+            accumulate(reversed(self.shape), operator.mul, initial=1)
+        )[:-1]
+        offsets = map(operator.mul, reversed(indices), prod_dims)
+        offset = sum(offsets)
+        return offset
+
+    def load(self, indices: tuple[int, ...]) -> _T:
+        """
+        Returns the element for a given tuple of indices
+        """
+        return self.data[self.offset(indices)]
+
+    def store(self, indices: tuple[int, ...], value: _T) -> None:
+        """
+        Returns the element for a given tuple of indices
+        """
+        self.data[self.offset(indices)] = value
+
+    def __format__(self, __format_spec: str) -> str:
+        prod_dims: list[int] = list(accumulate(reversed(self.shape), operator.mul))
+        assert prod_dims[-1] == len(self.data)
+        result = "[" * len(self.shape)
+
+        for i, d in enumerate(self.data):
+            if i:
+                n = sum(not i % p for p in prod_dims)
+                result += "]" * n
+                result += ", "
+                result += "[" * n
+            result += f"{d}"
+
+        result += "]" * len(self.shape)
+        return result
