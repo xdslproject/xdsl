f6434327878d43b23c3a7e40dc7442c1f6b30917
Sasha Lopoukhine <superlopuh@gmail.com>
2023-07-03 17:56:55 +0100

    interpreter: (scf) add some scf function implementations

---
+++ xdsl/interpreters/scf.py
@@ -0,0 +1,33 @@
+from typing import Any
+from xdsl.dialects import scf
+
+from xdsl.interpreter import (
+    Interpreter,
+    InterpreterFunctions,
+    PythonValues,
+    ReturnedValues,
+    impl,
+    impl_terminator,
+    register_impls,
+)
+
+
+@register_impls
+class ScfFunctions(InterpreterFunctions):
+    @impl(scf.For)
+    def run_for(
+        self, interpreter: Interpreter, op: scf.For, args: PythonValues
+    ) -> PythonValues:
+        lb, ub, step, *loop_args = args
+        loop_args = tuple(loop_args)
+
+        for i in range(lb, ub, step):
+            loop_args = interpreter.run_ssacfg_region(
+                op.body, (i, *loop_args), "for_loop"
+            )
+
+        return loop_args
+
+    @impl_terminator(scf.Yield)
+    def run_br(self, interpreter: Interpreter, op: scf.Yield, args: tuple[Any, ...]):
+        return ReturnedValues(args), ()
