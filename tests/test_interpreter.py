9d183d1ec144affda69263c03c716f27d2f68263
Sasha Lopoukhine <superlopuh@gmail.com>
2023-07-13 00:04:39 +0700

    interpreter: add cast mechanism

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,9 +1,16 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Any, NamedTuple
 
 import pytest
 
-from xdsl.dialects.builtin import ModuleOp
-from xdsl.interpreter import Interpreter, InterpreterFunctions, register_impls
+from xdsl.dialects.builtin import IndexType, IntegerType, ModuleOp, i32
+from xdsl.interpreter import (
+    Interpreter,
+    InterpreterFunctions,
+    impl_cast,
+    register_impls,
+)
+from xdsl.utils.exceptions import InterpretationError
 
 
 def test_import_functions():
@@ -24,3 +31,43 @@ def test_import_functions():
         i.register_implementations(A())
 
     assert e.value.args[0] == "Use `@register_impls` on class A"
+
+
+def test_cast():
+    @dataclass
+    class Integer:
+        value: int = field()
+
+    @dataclass
+    class Index:
+        value: int
+
+    @register_impls
+    @dataclass
+    class CastImpls(InterpreterFunctions):
+        @impl_cast(IntegerType, IndexType)
+        def cast_integer_to_index(
+            self,
+            input_type: IntegerType,
+            output_type: IndexType,
+            value: Any,
+        ) -> Any:
+            assert isinstance(value, Integer)
+            return Index(value.value)
+
+    # Dummy module
+    module = ModuleOp([])
+
+    interpreter = Interpreter(module)
+
+    interpreter.register_implementations(CastImpls())
+
+    integer = Integer(42)
+    index = interpreter.cast_value(i32, IndexType(), integer)
+    assert isinstance(index, Index)
+
+    with pytest.raises(
+        InterpretationError,
+        match="Could not find cast implementation for types index, i32",
+    ):
+        integer = interpreter.cast_value(IndexType(), i32, index)
