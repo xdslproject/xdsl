29a0c6a1439ce8a5f0c7cdedb6c6ead9af646d2b
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-07-12 10:40:59 +0100

    use manual walk

--- xdsl/transforms/reconcile_unrealized_casts.py
+++ xdsl/transforms/reconcile_unrealized_casts.py
@@ -8,113 +8,98 @@ from xdsl.dialects.builtin import ModuleOp
 from xdsl.ir import MLContext
 from xdsl.ir.core import Use
 from xdsl.passes import ModulePass
-from xdsl.pattern_rewriter import (
-    PatternRewriter,
-    PatternRewriteWalker,
-    RewritePattern,
-    op_type_rewrite_pattern,
-)
+from xdsl.pattern_rewriter import PatternRewriter
 
 
-class ReconcileUnrealizedCasts(RewritePattern):
-    """
-    An `unrealized_conversion_cast` operation represents an unrealized conversion
-    from one set of types to another, that is used to enable the inter-mixing
-    of different type systems.
+def _try_remove_cast(op: builtin.UnrealizedConversionCastOp):
+    # We remove several casts in a single pass, which should not happen
+    # with the current implementation of the pattern rewriter.
+    # The rewriter is not notified of our deletions, so we need to
+    # ignore casts that have already been removed earlier in the pass
+    # (i.e: no parent anymore).
 
-    This pattern rewriter removes `unrealized_conversion_cast` operations
-    that are not needed.
-    """
+    if op.parent is not None:
+        # casts that either have no uses or have at least
+        # one user that isn't an unrealized cast.
+        finalized_casts = list[builtin.UnrealizedConversionCastOp]()
 
-    @op_type_rewrite_pattern
-    def match_and_rewrite(
-        self, op: builtin.UnrealizedConversionCastOp, rewriter: PatternRewriter
-    ) -> None:
-        # We remove several casts in a single pass, which should not happen
-        # with the current implementation of the pattern rewriter.
-        # The rewriter is not notified of our deletions, so we need to
-        # ignore casts that have already been removed earlier in the pass
-        # (i.e: no parent anymore).
+        # casts with only other unrealized casts as users.
+        pending_casts = deque[builtin.UnrealizedConversionCastOp]()
 
-        if op.parent is not None:
-            # casts that either have no uses or have at least
-            # one user that isn't an unrealized cast.
-            finalized_casts = list[builtin.UnrealizedConversionCastOp]()
+        # casts that neeed to be visited in the DFS traversal of the use-def chain.
+        casts_to_visit = [op]
 
-            # casts with only other unrealized casts as users.
-            pending_casts = deque[builtin.UnrealizedConversionCastOp]()
+        def gen_all_uses_cast(
+            node: builtin.UnrealizedConversionCastOp,
+        ) -> Iterable[Use]:
+            for result in node.results:
+                yield from result.uses
 
-            # casts that neeed to be visited in the DFS traversal of the use-def chain.
-            casts_to_visit = [op]
+        # DFS traversal of the use-def chain.
+        while casts_to_visit:
+            cast = casts_to_visit.pop()
 
-            def gen_all_uses_cast(
-                node: builtin.UnrealizedConversionCastOp,
-            ) -> Iterable[Use]:
-                for result in node.results:
-                    yield from result.uses
+            is_live = False
+            has_any_uses = False
 
-            # DFS traversal of the use-def chain.
-            while casts_to_visit:
-                cast = casts_to_visit.pop()
-
-                is_live = False
-                has_any_uses = False
-
-                for use in gen_all_uses_cast(cast):
-                    if isinstance(use.operation, builtin.UnrealizedConversionCastOp):
-                        # early check, it's definitely not an unifiable cast
-                        if any(
-                            r != i
-                            for r, i in itertools.zip_longest(
-                                use.operation.inputs, cast.results
-                            )
-                        ):
-                            warn(
-                                f"Unable to remove cast {cast} because "
-                                "it is not unifiable with its uses"
-                            )
-                            return
-                        casts_to_visit.append(use.operation)
-                    else:
-                        is_live = True
-                    has_any_uses = True
-
-                # check that either there is a trivial cycle we can remove
-                # because types are homogeneous (e.g. {A -> B, B -> A})
-                # otherwise it means the cast is not unifiable with its uses
-                assert len(cast.results) == len(op.inputs)
-                has_trivial_cycle = all(
-                    r.type == i.type for r, i in zip(cast.results, op.inputs)
-                )
-                if is_live and not has_trivial_cycle:
-                    warn(
-                        "Unable to remove cast "
-                        f"{cast} because it is not unifiable "
-                        "with its uses"
-                    )
-                    return
-
-                if not has_any_uses or is_live:
-                    finalized_casts.append(cast)
+            for use in gen_all_uses_cast(cast):
+                if isinstance(use.operation, builtin.UnrealizedConversionCastOp):
+                    # early check, it's definitely not an unifiable cast
+                    if any(
+                        r != i
+                        for r, i in itertools.zip_longest(
+                            use.operation.inputs, cast.results
+                        )
+                    ):
+                        warn(
+                            f"Unable to remove cast {cast} because "
+                            "it is not unifiable with its uses"
+                        )
+                        return
+                    casts_to_visit.append(use.operation)
                 else:
-                    pending_casts.appendleft(cast)
+                    is_live = True
+                has_any_uses = True
 
-            for cast in filter(lambda c: c.parent is not None, finalized_casts):
-                # replace the uses of this cast by the inputs of the root cast
-                PatternRewriter(cast).replace_matched_op([], op.inputs)
+            # check that either there is a trivial cycle we can remove
+            # because types are homogeneous (e.g. {A -> B, B -> A})
+            # otherwise it means the cast is not unifiable with its uses
+            assert len(cast.results) == len(op.inputs)
+            has_trivial_cycle = all(
+                r.type == i.type for r, i in zip(cast.results, op.inputs)
+            )
+            if is_live and not has_trivial_cycle:
+                warn(
+                    "Unable to remove cast "
+                    f"{cast} because it is not unifiable "
+                    "with its uses"
+                )
+                return
 
-            for cast in filter(lambda c: c.parent is not None, pending_casts):
-                # remove other casts in the chain in the right order
-                PatternRewriter(cast).erase_matched_op()
+            if not has_any_uses or is_live:
+                finalized_casts.append(cast)
+            else:
+                pending_casts.appendleft(cast)
+
+        for cast in filter(lambda c: c.parent is not None, finalized_casts):
+            # replace the uses of this cast by the inputs of the root cast
+            PatternRewriter(cast).replace_matched_op([], op.inputs)
+
+        for cast in filter(lambda c: c.parent is not None, pending_casts):
+            # remove other casts in the chain in the right order
+            PatternRewriter(cast).erase_matched_op()
 
 
-def reconcile_unrealized_casts(op: ModuleOp):
+def reconcile_unrealized_casts(module: ModuleOp):
     """
     Removes all `builtin.unrealized_conversion_cast` operations
     that are not needed anymore in a module.
     """
-    walker = PatternRewriteWalker(ReconcileUnrealizedCasts())
-    walker.rewrite_module(op)
+    for op in filter(
+        lambda op: isinstance(op, builtin.UnrealizedConversionCastOp), module.walk()
+    ):
+        assert isinstance(op, builtin.UnrealizedConversionCastOp)
+        _try_remove_cast(op)
 
 
 class ReconcileUnrealizedCastsPass(ModulePass):
