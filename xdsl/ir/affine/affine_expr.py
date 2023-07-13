f32ebdea44f21936e12ba4274476f81de20ab7ef
Groverkss <groverkss@gmail.com>
2023-07-13 16:55:39 +0530

    Fix pre-commit

--- xdsl/ir/affine/affine_expr.py
+++ xdsl/ir/affine/affine_expr.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import Enum, auto
-
 from typing import TYPE_CHECKING
 
 # Used for cyclic dependencies in type hints
