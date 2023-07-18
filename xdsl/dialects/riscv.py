dcf2ff6dafd59212a0acca7ca68f2f0766223c3c
Sasha Lopoukhine <superlopuh@gmail.com>
2023-07-18 07:29:46 +0100

    ()

--- xdsl/dialects/riscv.py
+++ xdsl/dialects/riscv.py
@@ -96,7 +96,7 @@ class RISCVRegisterType(Data[str], TypeAttribute, ABC):
     @classmethod
     @abstractmethod
     def abi_index_by_name(cls) -> dict[str, int]:
-        raise NotImplementedError
+        raise NotImplementedError()
 
 
 @irdl_attr_definition
