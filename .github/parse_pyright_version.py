4c2777bbec8dc1bf9d41d761ac87b787e8bdd396
Anton Lydike <me@AntonLydike.de>
2023-07-20 12:46:06 +0100

    CI: remove `parse_pyright_version.py` and change to simple sed script (#1276)
    This should be less brittle than before (and removes a python script
    from the project)
    
    ---------
    
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

--- .github/parse_pyright_version.py
+++
@@ -1,8 +0,0 @@
-f = open("requirements-optional.txt")
-
-line = f.readlines()[-1]
-
-version = line.split("==")[1].strip()
-
-version_file = open(".github/pyright-version.txt", "w")
-version_file.write(version)
