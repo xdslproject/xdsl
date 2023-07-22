de3fcad5e65a8d78c927de2a22357a07f7eb32a0
Gabriel RodrÃiguez-Canal <gabriel.rodcanal@ed.ac.uk>
2023-07-22 01:20:18 +0100

    Added pass to transform ApplyOp into the for loops required by the 3D buffer pattern. It takes into account chunking in the Y index

--- xdsl/transforms/experimental/hls_convert_stencil_to_ll_mlir.py
+++ xdsl/transforms/experimental/hls_convert_stencil_to_ll_mlir.py
@@ -10,10 +10,10 @@ from xdsl.pattern_rewriter import (
 from xdsl.ir import MLContext, Operation, OpResult
 from xdsl.dialects.builtin import i32, f64, i64
 from xdsl.dialects.func import FuncOp, Call
-from xdsl.dialects import arith, builtin
+from xdsl.dialects import arith, builtin, scf
 from xdsl.dialects.arith import Constant
 
-from xdsl.dialects.stencil import ExternalLoadOp, AccessOp, ApplyOp
+from xdsl.dialects.stencil import ExternalLoadOp, AccessOp, ApplyOp, ReturnOp
 
 from xdsl.dialects.experimental.hls import HLSStream, HLSStreamType, HLSStreamRead
 
@@ -29,9 +29,11 @@ from xdsl.dialects.llvm import (
     LLVMArrayType,
     GEPOp,
     LoadOp,
+    AllocaOp,
 )
 
-from xdsl.ir.core import BlockArgument
+from xdsl.ir.core import BlockArgument, Block, Region
+from xdsl.transforms.experimental.convert_stencil_to_ll_mlir import prepare_apply_body
 
 IN = 0
 OUT = 1
@@ -107,6 +109,8 @@ class StencilExternalLoadToHLSExternalLoad(RewritePattern):
         shape_x = Constant.from_int_and_width(shape[0], i32)
         shape_y = Constant.from_int_and_width(shape[1], i32)
         shape_z = Constant.from_int_and_width(shape[2], i32)
+
+        qualify_apply_op_with_shapes(op.parent_op(), shape_x, shape_y, shape_z)
 
         two_int = Constant.from_int_and_width(2, i32)
         shift_shape_x = arith.Subi(shape_x, two_int)
@@ -225,6 +229,21 @@ class StencilExternalLoadToHLSExternalLoad(RewritePattern):
             self.load_data_declaration = True
 
 
+def qualify_apply_op_with_shapes(
+    stencil_func: FuncOp,
+    shape_x: arith.Constant,
+    shape_y: arith.Constant,
+    shape_z: arith.Constant,
+):
+    block = stencil_func.body.block
+
+    for op in block.ops:
+        if isinstance(op, ApplyOp):
+            op.attributes["shape_x"] = shape_x.value
+            op.attributes["shape_y"] = shape_y.value
+            op.attributes["shape_z"] = shape_z.value
+
+
 @dataclass
 class ApplyOpToHLS(RewritePattern):
     module: builtin.ModuleOp
@@ -259,6 +278,130 @@ class ApplyOpToHLS(RewritePattern):
             read_op = HLSStreamRead(stream_to_read)
 
             rewriter.insert_op_at_start(read_op, op.region.block)
+
+        # Transform ApplyOp into for loops
+        get_number_chunks = FuncOp.external(
+            "get_number_chunks",
+            [i32, LLVMPointerType.typed(i32)],
+            [i32],
+        )
+
+        get_chunk_size = FuncOp.external(
+            "get_chunk_size",
+            [i32, i32, i32, i32],
+            [i32],
+        )
+
+        res_type = op.res[0].typ
+        # Get this apply's ReturnOp
+        body_block = op.region.blocks[0]
+        return_op = next(o for o in body_block.ops if isinstance(o, ReturnOp))
+        rewriter.erase_op(return_op)
+
+        body = prepare_apply_body(op, rewriter)
+        body.block.add_op(scf.Yield.get())
+        dim = res_type.get_num_dims()
+
+        size_x = Constant.from_int_and_width(
+            op.attributes["shape_x"].value.data, builtin.IndexType()
+        )
+        size_y = Constant.from_int_and_width(
+            op.attributes["shape_y"].value.data, builtin.IndexType()
+        )
+        size_z = Constant.from_int_and_width(
+            op.attributes["shape_z"].value.data, builtin.IndexType()
+        )
+        one_int = Constant.from_int_and_width(1, i32)
+        two = Constant.from_int_and_width(2, builtin.IndexType())
+        zero = Constant.from_int_and_width(0, builtin.IndexType())
+        one = Constant.from_int_and_width(1, builtin.IndexType())
+
+        size_x_2 = arith.Subi(size_x, two)
+        size_y_1 = arith.Subi(size_y, one)
+
+        lower_x = Constant.from_int_and_width(2, builtin.IndexType())
+        lower_y = Constant.from_int_and_width(1, builtin.IndexType())
+        lower_z = Constant.from_int_and_width(1, builtin.IndexType())
+        upper_x = size_x_2
+        upper_y = size_y_1
+        upper_z = Constant.from_int_and_width(
+            op.attributes["shape_z"].value.data, builtin.IndexType()
+        )
+
+        p_remainder = AllocaOp.get(one_int, i32)
+
+        call_get_number_chunks = Call.get(
+            "get_number_chunks", [upper_x, p_remainder], [builtin.IndexType()]
+        )
+
+        lower_chunks = zero
+        upper_chunks = call_get_number_chunks
+
+        lowerBounds = [lower_chunks, lower_x, lower_y, lower_z]
+        upperBounds = [upper_chunks, upper_x, upper_y, upper_z]
+
+        # The for loop for the y index receives its trip variable from the get_chunk_size function, since the chunking
+        # is happening in the y axis. TODO: this is currently intended for the 3D case. It should be extended to the
+        # 1D and 2D cases as well.
+        y_for_op = None
+
+        current_region = body
+        for i in range(1, dim + 1):
+            for_op = scf.For.get(
+                lb=lowerBounds[-i],
+                ub=upperBounds[-i],
+                step=one,
+                iter_args=[],
+                body=current_region,
+            )
+            block = Block(
+                ops=[for_op, scf.Yield.get()], arg_types=[builtin.IndexType()]
+            )
+            current_region = Region(block)
+
+            if i == 2:
+                y_for_op = for_op
+
+        p = scf.ParallelOp.get(
+            lowerBounds=[lowerBounds[0]],
+            upperBounds=[upperBounds[0]],
+            steps=[one],
+            body=current_region,
+        )
+
+        chunk_num = p.body.block.args[0]
+
+        MAX_Y_SIZE = 16
+        max_chunk_length = Constant.from_int_and_width(MAX_Y_SIZE, i32)
+
+        remainder = LoadOp.get(p_remainder)
+
+        call_get_chunk_size = Call.get(
+            "get_chunk_size",
+            [chunk_num, call_get_number_chunks, max_chunk_length, remainder],
+            [builtin.IndexType()],
+        )
+
+        p.body.block.insert_op_before(call_get_chunk_size, p.body.block.first_op)
+
+        old_operands_lst = [old_operand for old_operand in y_for_op.operands]
+        y_for_op.operands = [call_get_chunk_size.res[0]] + old_operands_lst[1:]
+
+        rewriter.insert_op_before_matched_op(
+            [
+                size_x,
+                size_y,
+                one_int,
+                one,
+                two,
+                max_chunk_length,
+                p_remainder,
+                remainder,
+                *lowerBounds,
+                *upperBounds,
+                p,
+            ]
+        )
 
 
 @dataclass
