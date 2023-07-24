9683ca44b66ebd8a1a21dc7f7d756bda570df9bc
Emilien Bauer <bauer.emilien@gmail.com>
2023-07-24 11:01:07 +0100

    Make `fir.global` a Symbol.

--- xdsl/dialects/experimental/fir.py
+++ xdsl/dialects/experimental/fir.py
@@ -37,6 +37,7 @@ from xdsl.irdl import (
 )
 from xdsl.parser import AttrParser
 from xdsl.printer import Printer
+from xdsl.traits import SymbolOpInterface
 
 
 @irdl_attr_definition
@@ -1413,6 +1414,8 @@ class Global(IRDLOperation):
     )
     linkName: StringAttr | None = opt_attr_def(StringAttr)
     constant: UnitAttr | None = opt_attr_def(UnitAttr)
+
+    traits = frozenset([SymbolOpInterface()])
 
 
 @irdl_op_definition
