75023571dff6e7ce926c57f7a24fe792d0a5f8bd
Emilien Bauer <bauer.emilien@gmail.com>
2023-07-25 12:30:13 +0100

    Get initial draft on this branch.

---
+++ xdsl/interpreters/experimental/wgpu.py
@@ -0,0 +1,76 @@
+import array
+from io import StringIO
+from math import prod
+from typing import Any, cast
+
+import wgpu
+import wgpu.backends.rs
+from wgpu.utils import compute_with_buffers
+from xdsl.dialects import arith, gpu
+from xdsl.dialects.builtin import AnyFloatAttr, AnyIntegerAttr, IndexType
+from xdsl.dialects.memref import MemRefType
+from xdsl.interpreter import Interpreter, InterpreterFunctions, impl, register_impls
+from xdsl.interpreters.experimental.wgsl_printer import WGSLPrinter
+from xdsl.interpreters.memref import MemrefValue
+from xdsl.interpreters.shaped_array import ShapedArray
+from xdsl.ir.core import Attribute, SSAValue
+from xdsl.utils.hints import isa
+
+
+@register_impls
+class WGPUFunctions(InterpreterFunctions):
+    def prepare_operand(self, interpreter: Interpreter, operand: SSAValue):
+        if isa(operand.type, MemRefType[Attribute]):
+            element_type = operand.type.element_type
+            if isinstance(element_type, IndexType):
+                shaped_array = interpreter.get_values((operand,))[0]
+                assert isinstance(shaped_array, ShapedArray)
+                values = (shaped_array.load(index) for index in shaped_array.indices())
+                return (
+                    array.array("I", (v if isinstance(v, int) else 0 for v in values)),
+                    (*shaped_array.shape, "I"),
+                )
+
+    def process_outputs(self, interpreter: Interpreter, operand: SSAValue, output: Any):
+        if isa(operand.type, MemRefType[Attribute]):
+            element_type = operand.type.element_type
+            if isinstance(element_type, IndexType):
+                assert isinstance(output, memoryview)
+                shaped_array = interpreter.get_values((operand,))[0]
+                assert isinstance(shaped_array, ShapedArray)
+                for index in shaped_array.indices():
+                    shaped_array.store(index, output[index])
+                    pass
+
+    @impl(gpu.LaunchFuncOp)
+    def run_launch_func(
+        self, interpreter: Interpreter, op: gpu.LaunchFuncOp, args: tuple[Any, ...]
+    ):
+        if op.asyncToken is not None:
+            raise NotImplementedError(
+                "The WGPU interpreter does not handle asynchronous GPU regions at the moment."
+            )
+
+        gridSize = interpreter.get_values((op.gridSizeX, op.gridSizeY, op.gridSizeZ))
+        blockSize = interpreter.get_values(
+            (op.blockSizeX, op.blockSizeY, op.blockSizeZ)
+        )
+        dispatch_count = tuple(g * b for g, b in zip(gridSize, blockSize))
+        kernel_operands = op.kernelOperands
+
+        func = interpreter.get_op_for_symbol(op.kernel.string_value().split(".")[-1])
+        printer = WGSLPrinter()
+        wgsl_source = StringIO("")
+        printer.print(func, wgsl_source)
+        operands_dict = {}
+        outputs_dict = {}
+        for i, o in enumerate(kernel_operands):
+            operand, output = WGPUFunctions.prepare_operand(self, interpreter, o)
+            operands_dict[i + 1] = operand
+            outputs_dict[i + 1] = output
+        outputs = compute_with_buffers(
+            operands_dict, outputs_dict, wgsl_source.getvalue(), dispatch_count
+        )
+        for i, o in enumerate(kernel_operands):
+            WGPUFunctions.process_outputs(self, interpreter, o, outputs[i + 1])
+        return ()
