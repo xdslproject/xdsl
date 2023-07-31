eab2657a459b3aa78efda7a4401677d19c770ae9
Ka Wing, Li <68145845+kingiler@users.noreply.github.com>
2023-07-31 22:51:52 +0100

    core: (utils) rename better private fields

--- xdsl/utils/lazy.py
+++ xdsl/utils/lazy.py
@@ -9,29 +9,25 @@ _T = TypeVar("_T")
 
 @dataclass(frozen=True, init=False)
 class Lazy(Generic[_T]):
-    _func: Callable[[], _T]
-    _unresolved: bool
-    _data: _T = field(hash=False)
+    _lazy_func: Callable[[], _T]
+    _lazy_unresolved: bool
+    _lazy_data: _T = field(hash=False)
 
     def __init__(self, func: Callable[[], _T]):
-        object.__setattr__(self, "_func", func)
-        object.__setattr__(self, "_unresolved", True)
-
-    def __get__(self, instance: object, owner: type | None = None):
-        # self._resolve()
-        return self._data
+        object.__setattr__(self, "_lazy_func", func)
+        object.__setattr__(self, "_lazy_unresolved", True)
 
     def __getattribute__(self, name: str) -> Any:
         try:
             return object.__getattribute__(self, name)
         except AttributeError:
             self._resolve()
-            return getattr(object.__getattribute__(self, "_data"), name)
+            return getattr(object.__getattribute__(self, "_lazy_data"), name)
 
     def _resolve(self):
-        if self._unresolved:
-            object.__setattr__(self, "_data", self._func())
-            object.__setattr__(self, "_unresolved", False)
+        if self._lazy_unresolved:
+            object.__setattr__(self, "_lazy_data", self._lazy_func())
+            object.__setattr__(self, "_lazy_unresolved", False)
 
 
 def lazy(func: Callable[[], _T]) -> _T:
