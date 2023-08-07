ef5c11cb319e1026beb63ee75bfb2c30e71f9cfd
Sasha Lopoukhine <superlopuh@gmail.com>
2023-08-07 08:08:46 +0100

    dialects: (riscv_scf) add while op

--- xdsl/dialects/riscv_scf.py
+++ xdsl/dialects/riscv_scf.py
@@ -22,7 +22,7 @@ from xdsl.irdl import (
     var_operand_def,
     var_result_def,
 )
-from xdsl.traits import IsTerminator, SingleBlockImplicitTerminator
+from xdsl.traits import HasParent, IsTerminator, SingleBlockImplicitTerminator
 
 
 @irdl_op_definition
@@ -74,10 +74,63 @@ class ForOp(IRDLOperation):
         )
 
 
+@irdl_op_definition
+class WhileOp(IRDLOperation):
+    name = "riscv_scf.while"
+    arguments: VarOperand = var_operand_def(IntRegisterType)
+
+    res: VarOpResult = var_result_def(IntRegisterType)
+    before_region: Region = region_def()
+    after_region: Region = region_def()
+
+    def __init__(
+        self,
+        arguments: Sequence[SSAValue | Operation],
+        result_types: Sequence[IntRegisterType],
+        before_region: Region | Sequence[Operation] | Sequence[Block],
+        after_region: Region | Sequence[Operation] | Sequence[Block],
+    ):
+        super().__init__(
+            operands=[arguments],
+            result_types=[result_types],
+            regions=[before_region, after_region],
+        )
+
+    # TODO verify dependencies between riscv_scf.condition, riscv_scf.yield and the regions
+    def verify_(self):
+        for idx, arg in enumerate(self.arguments):
+            if self.before_region.block.args[idx].type != arg.type:
+                raise Exception(
+                    f"Block arguments with wrong type, expected {arg.type}, "
+                    f"got {self.before_region.block.args[idx].type}"
+                )
+
+        for idx, res in enumerate(self.res):
+            if self.after_region.block.args[idx].type != res.type:
+                raise Exception(
+                    f"Block arguments with wrong type, expected {res.type}, "
+                    f"got {self.after_region.block.args[idx].type}"
+                )
+
+
+@irdl_op_definition
+class ConditionOp(IRDLOperation):
+    name = "riscv_scf.condition"
+    cond: Operand = operand_def(IntRegisterType)
+    arguments: VarOperand = var_operand_def(IntRegisterType)
+
+    traits = frozenset([HasParent(WhileOp), IsTerminator()])
+
+    def __init__(self, cond: SSAValue | Operation, *output_ops: SSAValue | Operation):
+        super().__init__(operands=[cond, output_ops])
+
+
 RISCV_Scf = Dialect(
     [
         YieldOp,
         ForOp,
+        WhileOp,
+        ConditionOp,
     ],
     [],
 )
