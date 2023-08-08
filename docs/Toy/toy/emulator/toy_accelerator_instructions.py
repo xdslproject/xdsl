1f75b3ff3a9fd0df5174fc3e980268ad985db41c
Sasha Lopoukhine <superlopuh@gmail.com>
2023-08-08 11:47:18 +0100

    remove unused accelerator instruction functions

--- docs/Toy/toy/emulator/toy_accelerator_instructions.py
+++ docs/Toy/toy/emulator/toy_accelerator_instructions.py
@@ -1,4 +1,9 @@
-# pyright: reportMissingTypeStubs=false
+"""
+This file contains the implementations of the custom accelerator instructions for riscemu.
+
+These are temporary, until xDSL supports all the functionality necessary to lower from
+builtin+printf to riscv.
+"""
 
 from functools import reduce
 from typing import IO, ClassVar
@@ -56,57 +61,7 @@ class ToyAccelerator(InstructionSet):
     def buffer_copy(self, /, source: int, destination: int, count: int):
         self.mmu.write(destination, count * 4, self.mmu.read(source, count * 4))
 
-    # Vector helpers
-
-    # A vector is represented as an array of ints, where the first int is the count:
-    # [] -> [0]
-    # [1] -> [1, 1]
-    # [1, 2, 3] -> [3, 1, 2, 3]
-
-    def vector_count(self, ptr: int) -> int:
-        return self.ptr_read(ptr)
-
-    def vector_data(self, ptr: int) -> list[int]:
-        count = self.vector_count(ptr)
-        return self.buffer_read(ptr, count, offset=1)
-
-    # Tensor helpers
-
-    # The tensor is represented as a vector, containing two pointers to vectors:
-    # shape and data
-    # [] -> [-> [0], -> [0]] (rank: 0, shape: [], count: 0, data: [])
-    # [1, 2] -> [-> [1, 2], -> [2, 1, 2]] (rank: 1, shape: [2], count: 2, data: [1, 2])
-    # [[1, 2, 3], [4, 5, 6]]
-    #   -> [-> [2, 2, 3], -> [6, 1, 2, 3, 4, 5, 6]] (
-    #       rank: 2,
-    #       shape: [2, 3],
-    #       count: 2,
-    #       data: [1, 2, 3, 4, 5, 6]
-    #   )
-
-    # Where rank is the length of the shape subarray, and count is the length of data.
-
-    def tensor_shape(self, ptr: int) -> int:
-        return self.ptr_read(ptr, offset=0)
-
-    def tensor_data(self, ptr: int) -> int:
-        return self.ptr_read(ptr, offset=1)
-
     # Custom instructions
-
-    def instruction_tensor_print(self, ins: Instruction):
-        """
-        This instruction prints a formatted tensor
-        [[1, 2, 3], [4, 5, 6]]
-        """
-        # get the input register
-        t_ptr_reg = ins.get_reg(0)
-        t_ptr = self.get_reg(t_ptr_reg)
-
-        shape = self.vector_data(self.tensor_shape(t_ptr))
-        data = self.vector_data(self.tensor_data(t_ptr))
-
-        print(tensor_description(shape, data), file=type(self).stream)
 
     def instruction_tensor_print1d(self, ins: Instruction):
         """
@@ -229,81 +184,6 @@ class ToyAccelerator(InstructionSet):
         self.ptr_write(heap_ptr, value=new_heap_count)
 
         self.set_reg(destination_ptr_reg, result_ptr)
-
-    def allocate_count(self, bytes: int, destination_ptr_reg: str):
-        # Magic value of the start of the address space
-        # The .bss instruction is the first one inserted in the code, and
-        # 'heap' is the first label, so this will point to the start of the
-        # address space.
-        heap_ptr = 0x100
-
-        # The first word of the heap contains the size in words of the used space
-        heap_count = self.ptr_read(heap_ptr)
-
-        # The second word of the heap is the start of the allocated space
-        heap_start = heap_ptr + 4
-
-        # The first element past the end of the allocated space
-        result_ptr = heap_start + heap_count * 4
-
-        # Update the allocates space counter
-        new_heap_count = heap_count + bytes
-        self.ptr_write(heap_ptr, value=new_heap_count)
-
-        self.set_reg(destination_ptr_reg, result_ptr)
-
-    def instruction_vector_copy(self, ins: Instruction):
-        """
-        Custom instruction to allocate a new vector and copy input contents.
-        """
-
-        d_ptr_reg = ins.get_reg(0)
-        s_ptr_reg = ins.get_reg(1)
-
-        s_ptr = self.get_reg(s_ptr_reg)
-        count = self.vector_count(s_ptr)
-
-        self.allocate_count((count + 1) * 4, d_ptr_reg)
-
-        d_ptr = self.get_reg(d_ptr_reg)
-
-        self.buffer_copy(s_ptr, d_ptr, count + 1)
-
-    def instruction_vector_add(self, ins: Instruction):
-        """
-        Custom instruction to allocate a new vector and copy input contents.
-        """
-
-        d_ptr_reg = ins.get_reg(0)
-        s_ptr_reg = ins.get_reg(1)
-
-        d_ptr = self.get_reg(d_ptr_reg)
-        s_ptr = self.get_reg(s_ptr_reg)
-
-        s_data = self.vector_data(s_ptr)
-        d_data = self.vector_data(d_ptr)
-
-        self.buffer_write(
-            d_ptr, data=[l_el + r_el for l_el, r_el in zip(s_data, d_data)], offset=1
-        )
-
-    def instruction_vector_mul(self, ins: Instruction):
-        """
-        Custom instruction to allocate a new vector and copy input contents.
-        """
-
-        d_ptr_reg = ins.get_reg(0)
-        s_ptr_reg = ins.get_reg(1)
-
-        d_ptr = self.get_reg(d_ptr_reg)
-        s_ptr = self.get_reg(s_ptr_reg)
-
-        s_data = self.vector_data(s_ptr)
-        d_data = self.vector_data(d_ptr)
-
-        self.buffer_write(
-            d_ptr, data=[l_el * r_el for l_el, r_el in zip(s_data, d_data)], offset=1
-        )
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, ToyAccelerator):
