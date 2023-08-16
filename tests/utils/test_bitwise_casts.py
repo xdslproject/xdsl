23db5e10353e65a82bb37746c17b7dee2c807ce1
Sasha Lopoukhine <superlopuh@gmail.com>
2023-08-16 12:31:43 +0100

    use struct.pack

--- tests/utils/test_bitwise_casts.py
+++ tests/utils/test_bitwise_casts.py
@@ -1,3 +1,5 @@
+import struct
+
 import pytest
 
 from xdsl.utils.bitwise_casts import convert_f32_to_u32, convert_u32_to_f32
@@ -15,5 +17,4 @@ from xdsl.utils.bitwise_casts import convert_f32_to_u32, convert_u32_to_f32
 )
 def test_float_bitwise_casts(i: int, f: float):
     assert convert_f32_to_u32(f) == i
-    # N.B. this is an IEEE float comparison, 0.0 == -0.0, so there may be false positives
-    assert convert_u32_to_f32(i) == f
+    assert struct.pack(">f", convert_u32_to_f32(i)) == struct.pack(">f", f)
