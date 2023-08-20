a0139f8894857246d202bfa8f91ac51e5e2ab2ea
Sasha Lopoukhine <superlopuh@gmail.com>
2023-08-20 08:41:55 +0100

    remove CanonicalizationPattern

--- xdsl/transforms/canonicalize/canonicalize_pass.py
+++ xdsl/transforms/canonicalize/canonicalize_pass.py
@@ -1,40 +1,12 @@
-from collections import defaultdict
-from dataclasses import dataclass, field
-
 from xdsl.dialects import builtin
-from xdsl.ir import MLContext, Operation
+from xdsl.ir import MLContext
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     GreedyRewritePatternApplier,
-    PatternRewriter,
     PatternRewriteWalker,
 )
 from xdsl.traits import HasCanonicalisationPatternsTrait
 from xdsl.transforms.dead_code_elimination import dce
-
-
-@dataclass
-class CanonicalizationPattern(GreedyRewritePatternApplier):
-    """
-    A variant on the GreedyRewritePatternApplier that applies at most
-    n times per op to prevent infinite loops.
-    """
-
-    op_match_count: dict[Operation, int] = field(
-        default_factory=lambda: defaultdict(int)
-    )
-    repeat_apply_limit: int = field(default=0)
-
-    def match_and_rewrite(self, op: Operation, rewriter: PatternRewriter):
-        # Apply at most n times per op, if the limit is set
-        if (
-            self.repeat_apply_limit
-            and self.op_match_count[op] > self.repeat_apply_limit
-        ):
-            return
-        self.op_match_count[op] += 1
-        # Invoke the greedy rewrite pattern applier
-        super().match_and_rewrite(op, rewriter)
 
 
 class CanonicalizationPass(ModulePass):
@@ -51,5 +23,5 @@ class CanonicalizationPass(ModulePass):
             if (trait := ctx_op.get_trait(HasCanonicalisationPatternsTrait)) is not None
             for pattern in trait.get_canonicalization_patterns()
         ]
-        PatternRewriteWalker(CanonicalizationPattern(patterns)).rewrite_module(op)
+        PatternRewriteWalker(GreedyRewritePatternApplier(patterns)).rewrite_module(op)
         dce(op)
