5d39811e90f9ba698b3527725387c26f3acd9be7
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2023-09-05 14:26:09 +0100

    Add preallocated registers to the set of reserved registers

--- xdsl/backend/riscv/register_allocation.py
+++ xdsl/backend/riscv/register_allocation.py
@@ -104,6 +104,9 @@ class RegisterAllocatorLivenessBlockNaive(RegisterAllocator):
             preallocated = _gather_allocated(func)
 
             for pa_reg in preallocated:
+                if isinstance(pa_reg, IntRegisterType | FloatRegisterType):
+                    self.available_registers.reserved_registers.add(pa_reg)
+
                 if pa_reg in self.available_registers.available_int_registers:
                     self.available_registers.available_int_registers.remove(pa_reg)
                 if pa_reg in self.available_registers.available_float_registers:
@@ -196,6 +199,9 @@ class RegisterAllocatorBlockNaive(RegisterAllocator):
             preallocated = _gather_allocated(func)
 
             for pa_reg in preallocated:
+                if isinstance(pa_reg, IntRegisterType | FloatRegisterType):
+                    self.available_registers.reserved_registers.add(pa_reg)
+
                 if pa_reg in self.available_registers.available_int_registers:
                     self.available_registers.available_int_registers.remove(pa_reg)
                 if pa_reg in self.available_registers.available_float_registers:
