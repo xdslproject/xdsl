c180115fe1efd5db4a19b8d5b426d474fb804db8
Anton Lydike <me@antonlydike.de>
2023-10-11 11:22:54 +0100

    migrate mpi lowering to insert_symbol_if_not_exists

--- xdsl/transforms/lower_mpi.py
+++ xdsl/transforms/lower_mpi.py
@@ -15,6 +15,7 @@ from xdsl.pattern_rewriter import (
     RewritePattern,
     op_type_rewrite_pattern,
 )
+from xdsl.traits import SymbolTable
 from xdsl.utils.hints import isa
 
 
@@ -759,9 +760,8 @@ class MpiAddExternalFuncDefs(RewritePattern):
 
         # for each func found, add a FuncOp to the top of the module.
         for name, types in funcs_to_emit.items():
-            arg, res = types
-            rewriter.insert_op_at_end(
-                func.FuncOp.external(name, arg, res), module.body.block
+            SymbolTable.insert_symbol_if_not_exists(
+                module, func.FuncOp.external(name, *types)
             )
 
 
