fcdfcc1ee86c5b9e9b0940b587434d7662631527
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-10-12 01:32:05 +0100

    qRevert "Remove native constraints"
    This reverts commit c86790353ae087c8da3de7ecf367aa8412127591.

--- tests/interpreters/test_pdl_interpreter.py
+++ tests/interpreters/test_pdl_interpreter.py
@@ -13,6 +13,7 @@ from xdsl.dialects.builtin import (
 )
 from xdsl.interpreter import Interpreter
 from xdsl.interpreters.experimental.pdl import (
+    PDLMatcher,
     PDLRewriteFunctions,
     PDLRewritePattern,
 )
@@ -426,3 +427,49 @@ def test_erase_op():
     pattern_walker.rewrite_module(input_module)
 
     assert input_module.is_structurally_equivalent(ModuleOp([]))
+
+
+def test_native_constraint():
+    @ModuleOp
+    @Builder.implicit_region
+    def input_module_true():
+        test.TestOp.create(properties={"attr": StringAttr("foo")})
+
+    @ModuleOp
+    @Builder.implicit_region
+    def input_module_false():
+        test.TestOp.create(properties={"attr": StringAttr("fooo")})
+
+    @ModuleOp
+    @Builder.implicit_region
+    def pdl_module():
+        with ImplicitBuilder(pdl.PatternOp(42, None).body):
+            attr = pdl.AttributeOp().output
+            pdl.ApplyNativeConstraintOp("even_length_string", [attr])
+            op = pdl.OperationOp(
+                op_name=None,
+                attribute_value_names=ArrayAttr([StringAttr("attr")]),
+                attribute_values=[attr],
+            ).op
+            with ImplicitBuilder(pdl.RewriteOp(op).body):
+                pdl.EraseOp(op)
+
+    pdl_rewrite_op = next(
+        op for op in pdl_module.walk() if isinstance(op, pdl.RewriteOp)
+    )
+
+    ctx = MLContext()
+    PDLMatcher.native_constraints["even_length_string"] = (
+        lambda attr: isinstance(attr, StringAttr) and len(attr.data) == 4
+    )
+
+    pattern_walker = PatternRewriteWalker(PDLRewritePattern(pdl_rewrite_op, ctx))
+
+    new_input_module_true = input_module_true.clone()
+    pattern_walker.rewrite_module(new_input_module_true)
+
+    new_input_module_false = input_module_false.clone()
+    pattern_walker.rewrite_module(new_input_module_false)
+
+    assert new_input_module_false.is_structurally_equivalent(ModuleOp([]))
+    assert new_input_module_true.is_structurally_equivalent(input_module_true)
