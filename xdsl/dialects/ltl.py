f8aab596ae3355dd7986a0b12232a8b24a965411
Luisa Cicolini <luisacicolini@gmail.com>
2023-10-12 11:43:32 +0100

    test AND operator

--- xdsl/dialects/ltl.py
+++ xdsl/dialects/ltl.py
@@ -1,30 +1,33 @@
 from __future__ import annotations
+from xdsl.dialects.builtin import ContainerOf
+from xdsl.ir import Attribute, SSAValue
 
-from xdsl.ir.core import ParametrizedAttribute, TypeAttribute
+from xdsl.ir.core import Attribute, ParametrizedAttribute, TypeAttribute
 from xdsl.irdl import (
     IRDLOperation,
     irdl_attr_definition,
     irdl_op_definition,
 )
 
+from typing import Annotated
+from xdsl.irdl import (
+    IRDLOperation,
+    irdl_attr_definition,
+    irdl_op_definition,
+    operand_def,
+    AnyOf,
+    ConstraintVar
+
+)
+
+from xdsl.dialects.arith import signlessIntegerLike
+from xdsl.irdl.irdl import result_def
+
+
+
 """
 Implementation of the LTL dialect by CIRCT. Documentation: https://circt.llvm.org/docs/Dialects/LTL/
 """
-
-
-@irdl_attr_definition
-class propertytype(ParametrizedAttribute, TypeAttribute):
-    """
-     The `ltl.property` type represents a verifiable property built from linear
-    temporal logic sequences and quantifiers, for example, *"if you see sequence
-    A, eventually you will see sequence B"*.
-    Note that this type explicitly identifies a *property*. However, a boolean
-    value (`i1`) or a sequence (`ltl.sequence`) is also a valid property.
-    Operations that accept a property as an operand will use the `AnyProperty`
-    constraint, which also accepts `ltl.sequence` and `i1`.
-    """
-
-    name = "ltl.propertytype"
 
 
 @irdl_attr_definition
@@ -55,6 +58,8 @@ class sequence(ParametrizedAttribute, TypeAttribute):
     name = "ltl.sequencetype"
 
 
+ltltype = ContainerOf(AnyOf([sequence, property, signlessIntegerLike]))
+
 @irdl_op_definition
 class AndOp(IRDLOperation):
     """
@@ -63,3 +68,21 @@ class AndOp(IRDLOperation):
     """
 
     name = "ltl.and"
+
+    T = Annotated[Attribute, ConstraintVar("T"), sequence | property | signlessIntegerLike]
+
+    input = operand_def(T)
+
+    result = result_def(T)
+
+    def __init__(
+            self, 
+            operand: SSAValue, 
+    ):
+        super().__init__(operands = [operand])
+
+    def verify_(self):
+        if isinstance(self.input, property) ^ isinstance(self.result, property):
+            raise ValueError("AndOp: property type mismatch")
+        if isinstance(self.input, sequence) ^ isinstance(self.result, sequence):
+            raise ValueError("AndOp: sequence type mismatch")
