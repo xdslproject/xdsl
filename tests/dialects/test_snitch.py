c82bb8b1b8245dbdaba6fb04034575f2c44d440c
Sasha Lopoukhine <superlopuh@gmail.com>
2023-10-26 15:54:14 +0100

    SsrSetStreamConfigOperation has dm attr instead of stream operand

--- tests/dialects/test_snitch.py
+++ tests/dialects/test_snitch.py
@@ -7,30 +7,30 @@ from xdsl.utils.test_value import TestSSAValue
 
 
 def test_csr_op():
-    stream = TestSSAValue(riscv.Registers.A1)
     value = TestSSAValue(riscv.Registers.A1)
+    stream = IntAttr(0)
     valid = IntAttr(snitch.SnitchResources.dimensions - 1)
     invalid = IntAttr(snitch.SnitchResources.dimensions)
 
-    snitch.SsrSetDimensionBoundOp(stream=stream, value=value, dimension=valid).verify()
+    snitch.SsrSetDimensionBoundOp(value=value, dm=stream, dimension=valid).verify()
     with pytest.raises(VerifyException):
         snitch.SsrSetDimensionBoundOp(
-            stream=stream, value=value, dimension=invalid
+            value=value, dm=stream, dimension=invalid
         ).verify()
-    snitch.SsrSetDimensionStrideOp(stream=stream, value=value, dimension=valid).verify()
+    snitch.SsrSetDimensionStrideOp(value=value, dm=stream, dimension=valid).verify()
     with pytest.raises(VerifyException):
         snitch.SsrSetDimensionStrideOp(
-            stream=stream, value=value, dimension=invalid
+            value=value, dm=stream, dimension=invalid
         ).verify()
-    snitch.SsrSetDimensionSourceOp(stream=stream, value=value, dimension=valid).verify()
+    snitch.SsrSetDimensionSourceOp(value=value, dm=stream, dimension=valid).verify()
     with pytest.raises(VerifyException):
         snitch.SsrSetDimensionSourceOp(
-            stream=stream, value=value, dimension=invalid
+            value=value, dm=stream, dimension=invalid
         ).verify()
     snitch.SsrSetDimensionDestinationOp(
-        stream=stream, value=value, dimension=valid
+        value=value, dm=stream, dimension=valid
     ).verify()
     with pytest.raises(VerifyException):
         snitch.SsrSetDimensionDestinationOp(
-            stream=stream, value=value, dimension=invalid
+            value=value, dm=stream, dimension=invalid
         ).verify()
