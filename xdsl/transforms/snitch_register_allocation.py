4ac05a636340df6cf8a018dfe29a4f61b966b11f
Sasha Lopoukhine <superlopuh@gmail.com>
2023-11-02 17:06:09 +0000

    backend: (snitch) add snitch register allocation

--- xdsl/transforms/snitch_register_allocation.py
+++ xdsl/transforms/snitch_register_allocation.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Any, cast
 
 from xdsl.dialects import riscv, snitch_stream, stream
 from xdsl.dialects.builtin import ModuleOp
@@ -12,27 +13,41 @@ from xdsl.pattern_rewriter import (
 )
 
 
+class AllocateSnitchStridedStreamRegisters(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(
+        self,
+        op: snitch_stream.StridedReadOp | snitch_stream.StridedWriteOp,
+        rewriter: PatternRewriter,
+        /,
+    ):
+        stream_type = op.stream.type
+        assert isinstance(
+            stream_type, stream.ReadableStreamType | stream.WritableStreamType
+        )
+        stream_type = cast(stream.StreamType[Any], stream_type)
+        op.stream.type = type(stream_type)(riscv.Registers.FT[op.dm.data])
+
+
 class AllocateSnitchGenericRegisters(RewritePattern):
     @op_type_rewrite_pattern
     def match_and_rewrite(
         self, op: snitch_stream.GenericOp, rewriter: PatternRewriter, /
     ):
         block = op.body.block
+
+        for arg, input in zip(block.args, op.inputs):
+            assert isinstance(input.type, stream.ReadableStreamType)
+            input_type: stream.ReadableStreamType[Any] = input.type
+            arg.type = input_type.element_type
+
         yield_op = block.last_op
         assert isinstance(yield_op, snitch_stream.YieldOp)
 
-        for a, arg in enumerate(block.args):
-            arg.type = riscv.Registers.FT[a]
-
-        for i, arg in enumerate(op.inputs):
-            arg.type = stream.ReadableStreamType(riscv.Registers.FT[i])
-
-        input_count = len(op.inputs)
-
-        for o, arg in enumerate(op.outputs):
-            t = riscv.Registers.FT[o + input_count]
-            arg.type = stream.WritableStreamType(t)
-            yield_op.operands[o].type = t
+        for arg, output in zip(yield_op.values, op.outputs):
+            assert isinstance(output.type, stream.WritableStreamType)
+            output_type: stream.WritableStreamType[Any] = output.type
+            arg.type = output_type.element_type
 
 
 @dataclass
@@ -44,6 +59,10 @@ class SnitchRegisterAllocation(ModulePass):
     name = "snitch-allocate-registers"
 
     def apply(self, ctx: MLContext, op: ModuleOp) -> None:
+        PatternRewriteWalker(
+            AllocateSnitchStridedStreamRegisters(),
+            apply_recursively=False,
+        ).rewrite_module(op)
         PatternRewriteWalker(
             AllocateSnitchGenericRegisters(),
             apply_recursively=False,
