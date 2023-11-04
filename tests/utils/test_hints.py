fd8dbb70c8c6c3b35c8c312375edfc9fa536ac06
Emilien Bauer <bauer.emilien@gmail.com>
2023-11-04 10:20:28 +0000

    MFE?

---
+++ tests/utils/test_hints.py
@@ -0,0 +1,33 @@
+from typing import Generic, TypeVar
+
+import pytest
+
+from xdsl.utils.hints import get_type_var_mapping
+
+T = TypeVar("T")
+
+
+class GenThing(Generic[T]):
+    pass
+
+
+class GenThingy(GenThing[T]):
+    pass
+
+
+class IntThing(GenThing[int]):
+    pass
+
+
+class IntThingy(GenThingy[int]):
+    pass
+
+
+@pytest.mark.parametrize(
+    "specialized_type, type_var_mapping",
+    [(IntThing, (GenThing, {T: int})), (IntThingy, (GenThingy, {T: int}))],
+)
+def test_type_var_mapping(
+    specialized_type: type, type_var_mapping: tuple[type, dict[TypeVar, type]]
+):
+    assert get_type_var_mapping(specialized_type) == type_var_mapping
