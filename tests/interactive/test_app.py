e7ddacd41f9ce101d85ab8b46d6bddc55e48e387
Emilien Bauer <bauer.emilien@gmail.com>
2023-12-11 13:31:47 +0000

    Update GUI test.

--- tests/interactive/test_app.py
+++ tests/interactive/test_app.py
@@ -16,7 +16,7 @@ from xdsl.dialects.builtin import (
 )
 from xdsl.interactive.app import InputApp
 from xdsl.ir import Block, Region
-from xdsl.transforms import mlir_opt, printf_to_llvm, scf_parallel_loop_tiling
+from xdsl.transforms import mlir_opt, printf_to_llvm, scf_parallel_loop_tiling, stencil_unroll
 from xdsl.transforms.experimental import (
     hls_convert_stencil_to_ll_mlir,
 )
@@ -258,6 +258,7 @@ async def test_buttons():
                 mlir_opt.MLIROptPass,
                 printf_to_llvm.PrintfToLLVM,
                 scf_parallel_loop_tiling.ScfParallelLoopTilingPass,
+                stencil_unroll.StencilUnrollPass,
             )
         )
 
