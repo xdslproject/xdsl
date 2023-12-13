8715ba86f5d1289e967b8513d38acdde975e3eb5
Dalia Shaaban <dalia.shaaban08@gmail.com>
2023-12-13 15:31:52 +0000

    add cute test

--- tests/interactive/test_pass_metrics.py
+++ tests/interactive/test_pass_metrics.py
@@ -6,7 +6,9 @@ from xdsl.dialects.builtin import (
     ModuleOp,
 )
 from xdsl.interactive.pass_metrics import count_number_of_operations
-from xdsl.ir import Block, Region
+from xdsl.ir import Block, MLContext, Region
+from xdsl.parser import Parser
+from xdsl.tools.command_line_tool import get_all_dialects
 
 
 def test_operation_counter():
@@ -27,6 +29,34 @@ def test_operation_counter():
         "func.func": 1,
         "arith.constant": 2,
         "arith.muli": 3,
+        "func.return": 1,
+        "builtin.module": 1,
+    }
+
+    res = count_number_of_operations(module)
+    assert res == expected_res
+
+
+def test_operation_counter_with_parsing_text():
+    text = """builtin.module {
+  func.func @hello(%n : index) -> index {
+    %two = arith.constant 2 : index
+    %res = arith.muli %n, %two : index
+    func.return %res : index
+  }
+}
+"""
+
+    ctx = MLContext(True)
+    for dialect in get_all_dialects():
+        ctx.load_dialect(dialect)
+    parser = Parser(ctx, text)
+    module = parser.parse_module()
+
+    expected_res = {
+        "func.func": 1,
+        "arith.constant": 1,
+        "arith.muli": 1,
         "func.return": 1,
         "builtin.module": 1,
     }
