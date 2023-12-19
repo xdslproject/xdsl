a010fa67bb4bef03a6a7a16d6985783aef1a2930
Sasha Lopoukhine <superlopuh@gmail.com>
2023-12-19 09:44:28 +0100

    backend: (riscv) reserve iter arg from allocation inside of riscv_scf body (#1875)
    The iteration arguments live in registers that currently are not
    protected from being clobbered by the register allocator. This PR fixes
    this by reserving them form being reused inside the body, by using the
    newly introduced reserve/unreserve mechanism.

--- xdsl/backend/riscv/register_allocation.py
+++ xdsl/backend/riscv/register_allocation.py
@@ -186,8 +186,18 @@ class RegisterAllocatorLivenessBlockNaive(RegisterAllocator):
         for operand in loop.operands:
             self.allocate(operand)
 
+        # Reserve the loop carried variables for allocation within the body
+        for iter_arg in loop.iter_args:
+            assert isinstance(iter_arg.type, IntRegisterType | FloatRegisterType)
+            self.available_registers.reserve_register(iter_arg.type)
+
         for op in loop.body.block.ops_reverse:
             self.process_operation(op)
+
+        # Unreserve the loop carried variables for allocation outside of the body
+        for iter_arg in loop.iter_args:
+            assert isinstance(iter_arg.type, IntRegisterType | FloatRegisterType)
+            self.available_registers.unreserve_register(iter_arg.type)
 
     def allocate_func(self, func: riscv_func.FuncOp) -> None:
         if not func.body.blocks:
