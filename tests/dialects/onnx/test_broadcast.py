efd9b84950506d79866a9c6287f5fb80173b5f25
kayode-gif <kayodeoke417@gmail.com>
2024-01-13 16:50:18 +0000

    update factor broadcast helper

--- tests/dialects/onnx/test_broadcast.py
+++ tests/dialects/onnx/test_broadcast.py
@@ -20,6 +20,21 @@ u_test_cases: list[tuple[tuple[list[int], list[int]], list[int]]] = [
     (([2, 3, 4, 5], [1, 3, 1, 5]), [2, 3, 4, 5]),
 ]
 
+m_fail_test_cases: list[tuple[tuple[list[int], list[int]], list[int]]] = [
+    (([2, 3, 4, 5], []), []),
+    (([2, 3, 4, 5], [5]), [2, 3, 4, 5]),
+    (([4, 5], [2, 3, 4, 5]), [2, 3, 4, 5]),
+    (([1, 4, 5], [2, 3, 1, 5]), [2, 4, 5, 5]),
+    (([3, 4, 5], [2, 1, 1, 1]), [3, 3, 4, 5]),
+]
+
+u_fail_test_cases: list[tuple[tuple[list[int], list[int]], list[int]]] = [
+    (([], [2, 3, 4, 5]), [2, 3, 4, 5]),
+    (([5], [2, 3, 4, 5]), [2, 3, 4, 5]),
+    (([2, 1, 1, 5], [2, 3, 4, 5]), [2, 3, 4, 5]),
+    (([2, 3, 5], [1, 3, 1, 5]), [2, 3, 4, 5]),
+]
+
 
 # Multidirectional Broadcasting Tests
 @pytest.mark.parametrize("input_shapes, expected_result", m_test_cases)
@@ -31,7 +46,7 @@ def multi_test_broadcast_shape(
     assert result == expected_result
 
 
-# Multidirectional Broadcasting Tests
+# Unidirectional Broadcasting Tests
 @pytest.mark.parametrize("input_shapes, expected_result", u_test_cases)
 def uni_test_broadcast_shape(
     input_shapes: tuple[list[int], list[int]], expected_result: list[int]
@@ -39,3 +54,23 @@ def uni_test_broadcast_shape(
     lhs, rhs = input_shapes
     result = unidirectional_broadcast_shape(lhs, rhs)
     assert result == expected_result
+
+
+# Multidirectional Broadcasting Tests (Failing Cases)
+@pytest.mark.parametrize("input_shapes, expected_result", m_fail_test_cases)
+def multi_fail_test_broadcast_shape(
+    input_shapes: tuple[list[int], list[int]], expected_result: list[int]
+):
+    lhs, rhs = input_shapes
+    result = multidirectional_broadcast_shape(lhs, rhs)
+    assert result != expected_result
+
+
+# Unidirectional Broadcasting Tests (Failing Cases)
+@pytest.mark.parametrize("input_shapes, expected_result", u_fail_test_cases)
+def uni_fail_test_broadcast_shape(
+    input_shapes: tuple[list[int], list[int]], expected_result: list[int]
+):
+    lhs, rhs = input_shapes
+    result = unidirectional_broadcast_shape(lhs, rhs)
+    assert result != expected_result
