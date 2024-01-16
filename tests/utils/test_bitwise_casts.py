5590211ad0bcae7324146da6bfd40fc4daf20cb4
Josse Van Delm <josse.vandelm@gmail.com>
2024-01-16 14:47:50 +0100

    Add pytest check for is_power_of_two

--- tests/utils/test_bitwise_casts.py
+++ tests/utils/test_bitwise_casts.py
@@ -2,7 +2,11 @@ import struct
 
 import pytest
 
-from xdsl.utils.bitwise_casts import convert_f32_to_u32, convert_u32_to_f32
+from xdsl.utils.bitwise_casts import (
+    convert_f32_to_u32,
+    convert_u32_to_f32,
+    is_power_of_two,
+)
 
 
 # http://bartaz.github.io/ieee754-visualization/
@@ -18,3 +22,9 @@ from xdsl.utils.bitwise_casts import convert_f32_to_u32, convert_u32_to_f32
 def test_float_bitwise_casts(i: int, f: float):
     assert convert_f32_to_u32(f) == i
     assert struct.pack(">f", convert_u32_to_f32(i)) == struct.pack(">f", f)
+
+
+def test_is_power_of_two():
+    powers = [is_power_of_two(i) for i in range(-2, 6)]
+    # [-2, -1, 0, 1, 2, 3, 4, 5]
+    assert powers == [False, False, False, True, True, False, True, False]
