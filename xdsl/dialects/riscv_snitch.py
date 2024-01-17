d9e02cf52da9cc2a5a739c35aa4585ff346d7591
Sasha Lopoukhine <superlopuh@gmail.com>
2024-01-17 13:08:45 +0000

    backend: (snitch) add iteration arguments to frep (#1982)
    Adds iteration arguments to frep inner and outer, this lets us express
    things like dot products in frep. This PR also updates the reigster
    allocator to take these into account, and to treat them similarly to the
    carried variables in scf for loops.

--- xdsl/dialects/riscv_snitch.py
+++ xdsl/dialects/riscv_snitch.py
@@ -6,9 +6,7 @@ from typing import Annotated, cast
 from typing_extensions import Self
 
 from xdsl.dialects import riscv, stream
-from xdsl.dialects.builtin import (
-    IntAttr,
-)
+from xdsl.dialects.builtin import IntAttr, UnrealizedConversionCastOp
 from xdsl.dialects.riscv import (
     AssemblyInstructionArg,
     IntRegisterType,
@@ -18,7 +16,11 @@ from xdsl.dialects.riscv import (
     RISCVInstruction,
     RISCVOp,
 )
-from xdsl.dialects.utils import AbstractYieldOperation
+from xdsl.dialects.utils import (
+    AbstractYieldOperation,
+    parse_assignment,
+    print_assignment,
+)
 from xdsl.ir import (
     Attribute,
     Block,
@@ -36,8 +38,10 @@ from xdsl.irdl import (
     region_def,
     result_def,
     traits_def,
+    var_operand_def,
+    var_result_def,
 )
-from xdsl.parser import Parser
+from xdsl.parser import Parser, UnresolvedOperand
 from xdsl.pattern_rewriter import RewritePattern
 from xdsl.printer import Printer
 from xdsl.traits import (
@@ -124,6 +128,10 @@ class FRepOperation(IRDLOperation, RISCVInstruction):
     """
     Instructions to repeat, containing maximum 15 instructions, with no side effects.
     """
+    iter_args = var_operand_def(riscv.RISCVRegisterType)
+    """
+    Loop-carried variable initial values.
+    """
     stagger_mask = attr_def(IntAttr)
     """
     4 bits for each operand (rs1 rs2 rs3 rd). If the bit is set, the corresponding operand
@@ -134,6 +142,10 @@ class FRepOperation(IRDLOperation, RISCVInstruction):
     3 bits, indicating for how many iterations the stagger should increment before it
     wraps again (up to 23 = 8).
     """
+    res = var_result_def(riscv.RISCVRegisterType)
+    """
+    Loop-carried variable initial values.
+    """
 
     traits = traits_def(
         lambda: frozenset((SingleBlockImplicitTerminator(FrepYieldOp),))
@@ -143,6 +155,7 @@ class FRepOperation(IRDLOperation, RISCVInstruction):
         self,
         max_rep: SSAValue | Operation,
         body: Sequence[Operation] | Sequence[Block] | Region,
+        iter_args: Sequence[SSAValue | Operation] = (),
         stagger_mask: IntAttr | None = None,
         stagger_count: IntAttr | None = None,
     ):
@@ -151,7 +164,8 @@ class FRepOperation(IRDLOperation, RISCVInstruction):
         if stagger_count is None:
             stagger_count = IntAttr(0)
         super().__init__(
-            operands=(max_rep,),
+            operands=(max_rep, iter_args),
+            result_types=[[SSAValue.get(a).type for a in iter_args]],
             regions=(body,),
             attributes={
                 "stagger_mask": stagger_mask,
@@ -187,9 +201,40 @@ class FRepOperation(IRDLOperation, RISCVInstruction):
 
         remaining_attributes = parser.parse_optional_attr_dict_with_keyword()
 
-        body = parser.parse_region()
+        # Parse iteration arguments
+        pos = parser.pos
+        unresolved_iter_args: list[Parser.UnresolvedArgument] = []
+        iter_arg_unresolved_operands: list[UnresolvedOperand] = []
+        iter_arg_types: list[Attribute] = []
+        if parser.parse_optional_characters("iter_args"):
+            for iter_arg, iter_arg_operand in parser.parse_comma_separated_list(
+                Parser.Delimiter.PAREN, lambda: parse_assignment(parser)
+            ):
+                unresolved_iter_args.append(iter_arg)
+                iter_arg_unresolved_operands.append(iter_arg_operand)
+            parser.parse_characters("->")
+            iter_arg_types = parser.parse_comma_separated_list(
+                Parser.Delimiter.PAREN, parser.parse_attribute
+            )
 
-        frep = cls(max_rep, body, IntAttr(stagger_mask), IntAttr(stagger_count))
+        iter_arg_operands = parser.resolve_operands(
+            iter_arg_unresolved_operands, iter_arg_types, pos
+        )
+
+        # Set block argument types
+        iter_args = [
+            u_arg.resolve(t) for u_arg, t in zip(unresolved_iter_args, iter_arg_types)
+        ]
+
+        body = parser.parse_region(iter_args)
+
+        frep = cls(
+            max_rep,
+            body,
+            iter_arg_operands,
+            IntAttr(stagger_mask),
+            IntAttr(stagger_count),
+        )
         if remaining_attributes is not None:
             frep.attributes |= remaining_attributes.data
 
@@ -212,12 +257,28 @@ class FRepOperation(IRDLOperation, RISCVInstruction):
         )
         printer.print_string(" ")
 
-        yield_op = self.body.block.last_op
+        block = self.body.block
+
+        yield_op = block.last_op
         print_block_terminators = not isinstance(yield_op, FrepYieldOp) or bool(
             yield_op.operands
         )
 
-        printer.print_region(self.body, print_block_terminators=print_block_terminators)
+        if iter_args := block.args:
+            printer.print_string("iter_args(")
+            printer.print_list(
+                zip(iter_args, self.iter_args),
+                lambda pair: print_assignment(printer, *pair),
+            )
+            printer.print_string(") -> (")
+            printer.print_list((a.type for a in iter_args), printer.print_attribute)
+            printer.print_string(") ")
+
+        printer.print_region(
+            self.body,
+            print_entry_block_args=False,
+            print_block_terminators=print_block_terminators,
+        )
 
     def verify_(self) -> None:
         if self.stagger_count.data:
@@ -226,12 +287,42 @@ class FRepOperation(IRDLOperation, RISCVInstruction):
             raise VerifyException("Non-zero stagger mask currently unsupported")
         for instruction in self.body.ops:
             if not instruction.has_trait(Pure) and not isinstance(
-                instruction, FrepYieldOp | ReadOp | WriteOp
+                instruction, FrepYieldOp | ReadOp | WriteOp | UnrealizedConversionCastOp
             ):
                 raise VerifyException(
                     "Frep operation body may not contain instructions "
                     f"with side-effects, found {instruction.name}"
                 )
+        if len(self.iter_args) != len(self.body.block.args):
+            raise VerifyException(
+                f"Wrong number of block arguments, expected {len(self.iter_args)}, got "
+                f"{len(self.body.block.args)}. The body must have the induction "
+                f"variable and loop-carried variables as arguments."
+            )
+        for idx, (arg, block_arg) in enumerate(
+            zip(self.iter_args, self.body.block.args)
+        ):
+            if block_arg.type != arg.type:
+                raise VerifyException(
+                    f"Block argument {idx} has wrong type, expected {arg.type}, "
+                    f"got {block_arg.type}. Arguments after the "
+                    f"induction variable must match the carried variables."
+                )
+        if len(self.body.ops) > 0 and isinstance(
+            yieldop := self.body.block.last_op, FrepYieldOp
+        ):
+            if len(yieldop.arguments) != len(self.iter_args):
+                raise VerifyException(
+                    f"Expected {len(self.iter_args)} args, got {len(yieldop.arguments)}. "
+                    f"The riscv_scf.frep must yield its carried variables."
+                )
+            for iter_arg, yield_arg in zip(self.iter_args, yieldop.arguments):
+                if iter_arg.type != yield_arg.type:
+                    raise VerifyException(
+                        f"Expected {iter_arg.type}, got {yield_arg.type}. The "
+                        f"riscv_snitch.frep's riscv_snitch.frep_yield must match carried"
+                        f"variables types."
+                    )
 
 
 @irdl_op_definition
