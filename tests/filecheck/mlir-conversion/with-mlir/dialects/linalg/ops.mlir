15c66690052a88bf46a9c88d596439ee809a8b85
Sasha Lopoukhine <superlopuh@gmail.com>
2024-02-07 15:18:21 +0000

    add results to linalg.generic custom format

--- tests/filecheck/mlir-conversion/with-mlir/dialects/linalg/ops.mlir
+++ tests/filecheck/mlir-conversion/with-mlir/dialects/linalg/ops.mlir
@@ -17,6 +17,12 @@ linalg.generic {indexing_maps = [affine_map<(d0, d1) -> ()>, affine_map<(d0, d1)
 
 %sum = linalg.add ins(%2, %2 : tensor<2x3xf32>, tensor<2x3xf32>) outs(%3 : tensor<2x3xf32>) -> tensor<2x3xf32>
 
+%sum_2 = linalg.generic {indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>], iterator_types = ["parallel", "parallel"]} ins(%2, %2 : tensor<2x3xf32>, tensor<2x3xf32>) outs(%2 : tensor<2x3xf32>) {
+^bb0(%in: f32, %in_0: f32, %out: f32):
+    %acc = arith.addf %in, %in_0 : f32
+    linalg.yield %acc : f32
+} -> tensor<2x3xf32>
+
 // CHECK-NEXT:  #map = affine_map<(d0, d1) -> ()>
 // CHECK-NEXT:  #map1 = affine_map<(d0, d1) -> (d0, d1)>
 // CHECK-NEXT:  module {
@@ -31,4 +37,9 @@ linalg.generic {indexing_maps = [affine_map<(d0, d1) -> ()>, affine_map<(d0, d1)
 // CHECK-NEXT:    }
 // CHECK-NEXT:    %1:2 = "test.op"() : () -> (tensor<2x3xf32>, tensor<2x3xf32>)
 // CHECK-NEXT:    %2 = linalg.add ins(%1#0, %1#0 : tensor<2x3xf32>, tensor<2x3xf32>) outs(%1#1 : tensor<2x3xf32>) -> tensor<2x3xf32>
+// CHECK-NEXT:    %3 = linalg.generic {indexing_maps = [#map1, #map1, #map1], iterator_types = ["parallel", "parallel"]} ins(%1#0, %1#0 : tensor<2x3xf32>, tensor<2x3xf32>) outs(%1#0 : tensor<2x3xf32>) {
+// CHECK-NEXT:    ^bb0(%in: f32, %in_0: f32, %out: f32):
+// CHECK-NEXT:      %4 = arith.addf %in, %in_0 : f32
+// CHECK-NEXT:      linalg.yield %4 : f32
+// CHECK-NEXT:    } -> tensor<2x3xf32>
 // CHECK-NEXT:  }
--- xdsl/dialects/linalg.py
+++ xdsl/dialects/linalg.py
@@ -111,12 +111,13 @@ class Generic(IRDLOperation):
         body: Region,
         indexing_maps: Sequence[AffineMapAttr] | ArrayAttr[AffineMapAttr],
         iterator_types: Sequence[Attribute] | ArrayAttr[Attribute],
+        result_types: Sequence[Attribute] = (),
         doc: StringAttr | None = None,
         library_call: StringAttr | None = None,
     ) -> None:
         super().__init__(
             operands=[inputs, outputs],
-            result_types=[[]],
+            result_types=[result_types],
             properties={
                 "indexing_maps": ArrayAttr(indexing_maps),
                 "iterator_types": ArrayAttr(iterator_types),
@@ -240,6 +241,10 @@ class Generic(IRDLOperation):
         printer.print_string(" ")
         printer.print_region(self.body)
 
+        if self.res:
+            printer.print_string(" -> ")
+            printer.print_list(self.res, lambda res: printer.print_attribute(res.type))
+
     @classmethod
     def parse(cls, parser: Parser) -> Self:
         attrs_start_pos = parser.pos
@@ -343,7 +348,16 @@ class Generic(IRDLOperation):
 
         body = parser.parse_region()
 
-        generic = cls(ins, outs, body, indexing_maps, iterator_types, doc, library_call)
+        if parser.parse_optional_punctuation("->"):
+            res_types = parser.parse_comma_separated_list(
+                parser.Delimiter.NONE, parser.parse_attribute
+            )
+        else:
+            res_types = ()
+
+        generic = cls(
+            ins, outs, body, indexing_maps, iterator_types, res_types, doc, library_call
+        )
         generic.attributes |= attrs
         generic.attributes |= extra_attrs
 
@@ -372,6 +386,21 @@ class AddOp(IRDLOperation):
     )
 
     irdl_options = [AttrSizedOperandSegments(as_property=True), ParsePropInAttrDict()]
+
+    def __init__(
+        self,
+        inputs: Sequence[SSAValue],
+        outputs: Sequence[SSAValue] = (),
+        res: Sequence[Attribute] | None = None,
+    ):
+        if res is None:
+            result_types = tuple(output.type for output in outputs)
+        else:
+            result_types = res
+        super().__init__(
+            operands=(inputs, outputs),
+            result_types=result_types,
+        )
 
 
 Linalg = Dialect(
