8061a3e69ad95922b7e23d0e54e4abe2c9338c55
Dalia Shaaban <dalia.shaaban08@gmail.com>
2024-02-07 13:41:36 +0000

    zcvx

--- xdsl/interactive/app.py
+++ xdsl/interactive/app.py
@@ -12,7 +12,7 @@ import os
 from collections.abc import Callable
 from dataclasses import fields
 from io import StringIO
-from typing import Any, ClassVar
+from typing import Any, ClassVar, NamedTuple
 
 from textual import events, on
 from textual.app import App, ComposeResult
@@ -40,16 +40,82 @@ from xdsl.interactive.pass_metrics import (
 from xdsl.ir import MLContext
 from xdsl.parser import Parser
 from xdsl.passes import ModulePass, PipelinePass, get_pass_argument_names_and_types
+from xdsl.pattern_rewriter import PatternRewriter
 from xdsl.printer import Printer
 from xdsl.tools.command_line_tool import get_all_dialects, get_all_passes
+from xdsl.transforms import individual_rewrite
 from xdsl.transforms.mlir_opt import MLIROptPass
 from xdsl.utils.exceptions import PassPipelineParseError
 from xdsl.utils.parse_pipeline import PipelinePassSpec, parse_pipeline
 
 from ._pasteboard import pyclip_copy
 
+
+class IndividualRewrite(NamedTuple):
+    """
+    Type alias for a possible rewrite, described by an operation and pattern name.
+    """
+
+    operation: str
+    pattern: str
+
+
+class IndexedIndividualRewrite(NamedTuple):
+    """
+    Type alias for a specific rewrite pattern, additionally consisting of its operation index.
+    """
+
+    operation_index: int
+    rewrite: IndividualRewrite
+
+
 ALL_PASSES = tuple(sorted((p_name, p()) for (p_name, p) in get_all_passes().items()))
 """Contains the list of xDSL passes."""
+
+ALL_PATTERNS: tuple[IndividualRewrite, ...] = tuple(
+    IndividualRewrite(op_name, pattern_name)
+    for (op_name, pattern_by_name) in individual_rewrite.REWRITE_BY_NAMES.items()
+    for (pattern_name, _) in pattern_by_name.items()
+)
+"""Contains all the rewrite patterns."""
+
+
+def get_all_possible_rewrites(
+    patterns: tuple[IndividualRewrite, ...], op: ModuleOp
+) -> tuple[IndexedIndividualRewrite, ...]:
+    """
+    Function that takes a sequence of IndividualRewrite Patterns and a ModuleOp, and
+    returns the possible rewrites.
+    """
+    old_module = op.clone()
+    num_ops = len(list(old_module.walk()))
+
+    current_module = old_module.clone()
+
+    res: tuple[IndexedIndividualRewrite, ...] = ()
+
+    for op_idx in range(num_ops):
+        matched_op = list(current_module.walk())[op_idx]
+        if matched_op.name not in individual_rewrite.REWRITE_BY_NAMES:
+            continue
+        pattern_by_name = individual_rewrite.REWRITE_BY_NAMES[matched_op.name]
+
+        for pattern_name, pattern in pattern_by_name.items():
+            rewriter = PatternRewriter(matched_op)
+            pattern.match_and_rewrite(matched_op, rewriter)
+            if rewriter.has_done_action:
+                res = (
+                    *res,
+                    (
+                        IndexedIndividualRewrite(
+                            op_idx, IndividualRewrite(matched_op.name, pattern_name)
+                        )
+                    ),
+                )
+                current_module = old_module.clone()
+                matched_op = list(current_module.walk())[op_idx]
+
+    return res
 
 
 def condensed_pass_list(input: builtin.ModuleOp) -> tuple[type[ModulePass], ...]:
