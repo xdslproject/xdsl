7bac644b9161aed4bbf5911093b7a6fe9faa591a
alecerio <alessandrocerioliphd@gmail.com>
2024-02-08 18:24:30 +0100

    [frontend]: re-arrange elem type

---
+++ tests/frontend/onnx/test_elem_type.py
@@ -0,0 +1,12 @@
+from xdsl.frontend.onnx.elem_type import f32, f64, get_elem_type
+
+
+def test_get_elem_type():
+    # test case 1: check if 1 corresponds to f32
+    assert get_elem_type(1) == f32
+
+    # test case 11: check if 11 corresponds to f64
+    assert get_elem_type(11) == f64
+
+    # test case -1: check if -1 (or other illegal values) corresponds to None
+    assert get_elem_type(-1) is None
