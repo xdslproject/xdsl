6de666b18d9137d723e70c36342ea07c09c1007c
kayode-gif <kayodeoke417@gmail.com>
2024-02-09 16:55:27 +0000

    custom parse/print for tensor op

--- xdsl/dialects/tensor.py
+++ xdsl/dialects/tensor.py
@@ -29,32 +29,44 @@ class EmptyOp(IRDLOperation):
         if self.dynamic_sizes:
             printer.print_string("(")
             printer.print_list(self.dynamic_sizes, printer.print_ssa_value)
-            printer.print_string(")")
-
-        if self.tensor:
             printer.print_string(" : ")
             printer.print_list(
-                self.tensor, lambda tensor: printer.print_attribute(tensor.type)
+                (i.type for i in self.dynamic_sizes), printer.print_attribute
             )
+            printer.print_string(")")
+
+        printer.print_string(" : ")
+        printer.print_string("(")
+        printer.print_string(")")
+        printer.print_string(" -> ")
+        printer.print_attribute(self.tensor.type)
 
     @classmethod
     def parse(cls, parser: Parser) -> Self:
         pos = parser.pos
-        if parser.parse_optional_punctuation("("):
-            unresolved_dynamic_sizes = parser.parse_comma_separated_list(
-                Parser.Delimiter.NONE, parser.parse_optional_unresolved_operand
-            )
-            dynamic_sizes_types = parser.parse_comma_separated_list(
-                Parser.Delimiter.NONE, parser.parse_optional_type
-            )
-            parser.parse_punctuation(")")
+
+        if parser.parse_punctuation("("):
+            if parser.parse_punctuation(")"):
+                unresolved_dynamic_sizes = ()
+                unresolved_types = ()
+            else:
+                unresolved_dynamic_sizes = parser.parse_comma_separated_list(
+                    Parser.Delimiter.NONE, parser.parse_unresolved_operand
+                )
+                parser.parse_punctuation(":")
+                unresolved_types = parser.parse_comma_separated_list(
+                    Parser.Delimiter.NONE, parser.parse_type
+                )
+                parser.parse_punctuation(")")
             dynamic_sizes = parser.resolve_operands(
-                unresolved_dynamic_sizes, dynamic_sizes_types, pos
+                unresolved_dynamic_sizes, unresolved_types, pos
             )
         else:
             dynamic_sizes = ()
 
-        if parser.parse_optional_punctuation(" : "):
+        parser.parse_punctuation(":")
+
+        if parser.parse_punctuation("->"):
             tensor = parser.parse_comma_separated_list(
                 parser.Delimiter.NONE, parser.parse_attribute
             )
