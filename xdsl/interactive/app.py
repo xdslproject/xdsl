369cc25c8bb441d6d6856aeae45ea243a8b4fea8
Dalia Shaaban <dalia.shaaban08@gmail.com>
2024-02-10 17:27:56 +0000

    adf

--- xdsl/interactive/app.py
+++ xdsl/interactive/app.py
@@ -12,7 +12,7 @@ import os
 from collections.abc import Callable
 from dataclasses import fields
 from io import StringIO
-from typing import Any, ClassVar, NamedTuple
+from typing import Any, ClassVar
 
 from textual import events, on
 from textual.app import App, ComposeResult
@@ -28,9 +28,9 @@ from textual.widgets import (
     TextArea,
 )
 
-from xdsl.dialects import builtin
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.interactive.add_arguments_screen import AddArguments
+from xdsl.interactive.get_rewrites_and_passes import condensed_pass_list
 from xdsl.interactive.load_file_screen import LoadFile
 from xdsl.interactive.pass_list_item import PassListItem
 from xdsl.interactive.pass_metrics import (
@@ -40,102 +40,15 @@ from xdsl.interactive.pass_metrics import (
 from xdsl.ir import MLContext
 from xdsl.parser import Parser
 from xdsl.passes import ModulePass, PipelinePass, get_pass_argument_names_and_types
-from xdsl.pattern_rewriter import PatternRewriter
 from xdsl.printer import Printer
 from xdsl.tools.command_line_tool import get_all_dialects, get_all_passes
-from xdsl.transforms import individual_rewrite
-from xdsl.transforms.mlir_opt import MLIROptPass
 from xdsl.utils.exceptions import PassPipelineParseError
 from xdsl.utils.parse_pipeline import PipelinePassSpec, parse_pipeline
 
 from ._pasteboard import pyclip_copy
 
-
-class IndividualRewrite(NamedTuple):
-    """
-    Type alias for a possible rewrite, described by an operation and pattern name.
-    """
-
-    operation: str
-    pattern: str
-
-
-class IndexedIndividualRewrite(NamedTuple):
-    """
-    Type alias for a specific rewrite pattern, additionally consisting of its operation index.
-    """
-
-    operation_index: int
-    rewrite: IndividualRewrite
-
-
 ALL_PASSES = tuple(sorted((p_name, p()) for (p_name, p) in get_all_passes().items()))
 """Contains the list of xDSL passes."""
-
-
-def get_all_possible_rewrites(
-    patterns: tuple[IndividualRewrite, ...], op: ModuleOp
-) -> tuple[IndexedIndividualRewrite, ...]:
-    """
-    Function that takes a sequence of IndividualRewrite Patterns and a ModuleOp, and
-    returns the possible rewrites.
-    """
-    old_module = op.clone()
-    num_ops = len(list(old_module.walk()))
-
-    current_module = old_module.clone()
-
-    res: tuple[IndexedIndividualRewrite, ...] = ()
-
-    for op_idx in range(num_ops):
-        matched_op = list(current_module.walk())[op_idx]
-        if matched_op.name not in individual_rewrite.REWRITE_BY_NAMES:
-            continue
-        pattern_by_name = individual_rewrite.REWRITE_BY_NAMES[matched_op.name]
-
-        for pattern_name, pattern in pattern_by_name.items():
-            rewriter = PatternRewriter(matched_op)
-            pattern.match_and_rewrite(matched_op, rewriter)
-            if rewriter.has_done_action:
-                res = (
-                    *res,
-                    (
-                        IndexedIndividualRewrite(
-                            op_idx, IndividualRewrite(matched_op.name, pattern_name)
-                        )
-                    ),
-                )
-                current_module = old_module.clone()
-                matched_op = list(current_module.walk())[op_idx]
-
-    return res
-
-
-def condensed_pass_list(input: builtin.ModuleOp) -> tuple[type[ModulePass], ...]:
-    """Returns a tuple of passes (pass name and pass instance) that modify the IR."""
-
-    ctx = MLContext(True)
-
-    for dialect_name, dialect_factory in get_all_dialects().items():
-        ctx.register_dialect(dialect_name, dialect_factory)
-
-    selections: list[type[ModulePass]] = []
-    for _, value in ALL_PASSES:
-        if value is MLIROptPass:
-            # Always keep MLIROptPass as an option in condensed list
-            selections.append(value)
-            continue
-        try:
-            cloned_module = input.clone()
-            cloned_ctx = ctx.clone()
-            value().apply(cloned_ctx, cloned_module)
-            if input.is_structurally_equivalent(cloned_module):
-                continue
-        except Exception:
-            pass
-        selections.append(value)
-
-    return tuple(selections)
 
 
 class OutputTextArea(TextArea):
