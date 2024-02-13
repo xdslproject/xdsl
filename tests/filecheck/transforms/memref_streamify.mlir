71ec3b40aa6993d702d4f65be30e671918446e2e
Sasha Lopoukhine <superlopuh@gmail.com>
2024-02-13 09:38:16 +0200

    transformations: add memref-streamify

---
+++ tests/filecheck/transforms/memref_streamify.mlir
@@ -0,0 +1,57 @@
+// RUN: xdsl-opt -p memref-streamify %s | filecheck %s
+
+// CHECK:       builtin.module {
+
+func.func public @dsum(%arg0 : memref<8x16xf64>, %arg1 : memref<8x16xf64>, %arg2 : memref<8x16xf64>) -> memref<8x16xf64> {
+    memref_stream.generic {
+        bounds = [#builtin.int<8>, #builtin.int<16>],
+        indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>],
+        iterator_types = ["parallel", "parallel"]
+    } ins(%arg0, %arg1 : memref<8x16xf64>, memref<8x16xf64>) outs(%arg2 : memref<8x16xf64>) {
+    ^0(%in : f64, %in_0 : f64, %out : f64):
+        %0 = arith.addf %in, %in_0 : f64
+        linalg.yield %0 : f64
+    }
+    func.return %arg2 : memref<8x16xf64>
+}
+
+// CHECK-NEXT:    func.func public @dsum(%arg0 : memref<8x16xf64>, %arg1 : memref<8x16xf64>, %arg2 : memref<8x16xf64>) -> memref<8x16xf64> {
+// CHECK-NEXT:      memref_stream.streaming_region {bounds = [8, 16], indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>]} ins(%arg0, %arg1 : memref<8x16xf64>, memref<8x16xf64>) outs(%arg2 : memref<8x16xf64>) {
+// CHECK-NEXT:      ^0(%0 : !stream.readable<f64>, %1 : !stream.readable<f64>, %2 : !stream.writable<f64>):
+// CHECK-NEXT:        memref_stream.generic {bounds = [#builtin.int<8>, #builtin.int<16>], indexing_maps = [[affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>], iterator_types = ["parallel", "parallel"]} ins(%0, %1 : !stream.readable<f64>, !stream.readable<f64>) outs(%2 : !stream.writable<f64>) {
+// CHECK-NEXT:        ^1(%in : f64, %in_0 : f64, %out : f64):
+// CHECK-NEXT:          %3 = arith.addf %in, %in_0 : f64
+// CHECK-NEXT:          linalg.yield %3 : f64
+// CHECK-NEXT:        }
+// CHECK-NEXT:      }
+// CHECK-NEXT:      func.return %arg2 : memref<8x16xf64>
+// CHECK-NEXT:    }
+
+func.func public @relu(%arg0_1 : memref<16x16xf64>, %arg1_1 : memref<16x16xf64>) -> memref<16x16xf64> {
+    %cst = arith.constant 0.000000e+00 : f64
+    memref_stream.generic {
+        bounds = [#builtin.int<16>, #builtin.int<16>],
+        indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>],
+        iterator_types = ["parallel", "parallel"]
+    } ins(%arg0_1 : memref<16x16xf64>) outs(%arg1_1 : memref<16x16xf64>) {
+    ^1(%in_1 : f64, %out_1 : f64):
+        %1 = arith.maximumf %in_1, %cst : f64
+        linalg.yield %1 : f64
+    }
+    func.return %arg1_1 : memref<16x16xf64>
+}
+
+// CHECK-NEXT:    func.func public @relu(%arg0_1 : memref<16x16xf64>, %arg1_1 : memref<16x16xf64>) -> memref<16x16xf64> {
+// CHECK-NEXT:      %cst = arith.constant 0.000000e+00 : f64
+// CHECK-NEXT:      memref_stream.streaming_region {bounds = [16, 16], indexing_maps = [affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>]} ins(%arg0_1 : memref<16x16xf64>) outs(%arg1_1 : memref<16x16xf64>) {
+// CHECK-NEXT:      ^2(%4 : !stream.readable<f64>, %5 : !stream.writable<f64>):
+// CHECK-NEXT:        memref_stream.generic {bounds = [#builtin.int<16>, #builtin.int<16>], indexing_maps = [[affine_map<(d0, d1) -> (d0, d1)>, affine_map<(d0, d1) -> (d0, d1)>], iterator_types = ["parallel", "parallel"]} ins(%4 : !stream.readable<f64>) outs(%5 : !stream.writable<f64>) {
+// CHECK-NEXT:        ^3(%in_1 : f64, %out_1 : f64):
+// CHECK-NEXT:          %6 = arith.maximumf %in_1, %cst : f64
+// CHECK-NEXT:          linalg.yield %6 : f64
+// CHECK-NEXT:        }
+// CHECK-NEXT:      }
+// CHECK-NEXT:      func.return %arg1_1 : memref<16x16xf64>
+// CHECK-NEXT:    }
+
+// CHECK-NEXT:  }
---
+++ xdsl/transforms/memref_streamify.py
@@ -0,0 +1,104 @@
+from dataclasses import dataclass, field
+from typing import cast
+
+from xdsl.dialects import memref, memref_stream, stream
+from xdsl.dialects.builtin import (
+    ArrayAttr,
+    ModuleOp,
+)
+from xdsl.ir import Attribute, Block, MLContext, Region
+from xdsl.passes import ModulePass
+from xdsl.pattern_rewriter import (
+    PatternRewriter,
+    PatternRewriteWalker,
+    RewritePattern,
+    op_type_rewrite_pattern,
+)
+
+
+@dataclass
+class StreamifyGenericOpPattern(RewritePattern):
+    streams: int = field()
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(
+        self, op: memref_stream.GenericOp, rewriter: PatternRewriter
+    ) -> None:
+        # Currently can only stream memrefs that are not inout
+        streamable_input_indices = tuple(
+            (index, cast(memref.MemRefType[Attribute], value_type).element_type)
+            for index, value in enumerate(op.inputs)
+            if isinstance(value_type := value.type, memref.MemRefType)
+        )
+        input_count = len(op.inputs)
+        streamable_output_indices = tuple(
+            (index, cast(memref.MemRefType[Attribute], value_type).element_type)
+            for index, value in enumerate(op.outputs)
+            if isinstance(value.type, memref.MemRefType)
+            if not op.body.block.args[index + input_count].uses
+        )
+        # We might want to pick which memref to stream by iteration count in the future
+        streamed_input_indices = streamable_input_indices[: self.streams]
+        streamed_output_indices = streamable_output_indices[
+            : self.streams - len(streamed_input_indices)
+        ]
+        streamed_operand_indices = streamed_input_indices + tuple(
+            (index + len(streamed_input_indices), el_type)
+            for index, el_type in streamed_output_indices
+        )
+        input_el_types = tuple(el_type for _, el_type in streamed_input_indices)
+        output_el_types = tuple(el_type for _, el_type in streamed_output_indices)
+        input_stream_types = tuple(
+            stream.ReadableStreamType(el_type) for el_type in input_el_types
+        )
+        output_stream_types = tuple(
+            stream.WritableStreamType(el_type) for el_type in output_el_types
+        )
+        rewriter.insert_op_before_matched_op(
+            streaming_region_op := memref_stream.StreamingRegionOp(
+                tuple(op.inputs[index] for index, _ in streamed_input_indices),
+                tuple(op.outputs[index] for index, _ in streamable_output_indices),
+                ArrayAttr(
+                    tuple(
+                        op.indexing_maps.data[index]
+                        for index, _ in streamed_operand_indices
+                    )
+                ),
+                op.bounds,
+                Region(Block(arg_types=input_stream_types + output_stream_types)),
+            )
+        )
+        new_body = streaming_region_op.body.block
+        new_operands = list(op.operands)
+        for stream_index, (index, _) in enumerate(streamed_operand_indices):
+            new_operands[index] = new_body.args[stream_index]
+
+        rewriter.insert_op_at_end(
+            memref_stream.GenericOp(
+                new_operands[:input_count],
+                new_operands[input_count:],
+                rewriter.move_region_contents_to_new_regions(op.body),
+                op.indexing_maps,
+                op.iterator_types,
+                op.bounds,
+            ),
+            new_body,
+        )
+        rewriter.erase_matched_op()
+
+
+@dataclass
+class MemrefStreamifyPass(ModulePass):
+    """
+    Converts a linalg generic to perfectly nested loops.
+    """
+
+    name = "memref-streamify"
+
+    streams: int = field(default=3)
+
+    def apply(self, ctx: MLContext, op: ModuleOp) -> None:
+        PatternRewriteWalker(
+            StreamifyGenericOpPattern(self.streams),
+            apply_recursively=False,
+        ).rewrite_module(op)
