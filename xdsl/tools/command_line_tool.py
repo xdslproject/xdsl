4cf45a1302526c3b929f9c0d7a7a43804821c657
Sasha Lopoukhine <superlopuh@gmail.com>
2024-02-16 10:54:57 +0200

    transformations: add lowering from memref_stream generic to loops (#2151)
    Note stacked PR
    
    This PR refactors the linalg generic lowering to be usable for the
    memref_stream generic also, with a custom function to lower sources and
    destinations when given indices

--- xdsl/tools/command_line_tool.py
+++ xdsl/tools/command_line_tool.py
@@ -441,6 +441,11 @@ def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
 
         return convert_memref_to_riscv.ConvertMemrefToRiscvPass
 
+    def get_convert_memref_stream_to_loops():
+        from xdsl.transforms import convert_memref_stream_to_loops
+
+        return convert_memref_stream_to_loops.ConvertMemrefStreamToLoopsPass
+
     def get_convert_onnx_to_linalg():
         from xdsl.transforms import convert_onnx_to_linalg
 
@@ -521,6 +526,7 @@ def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
         "convert-func-to-riscv-func": get_convert_func_to_riscv_func,
         "convert-linalg-to-memref-stream": get_convert_linalg_to_memref_stream,
         "convert-linalg-to-loops": get_convert_linalg_to_loops,
+        "convert-memref-stream-to-loops": get_convert_memref_stream_to_loops,
         "convert-memref-to-riscv": get_convert_memref_to_riscv,
         "convert-onnx-to-linalg": get_convert_onnx_to_linalg,
         "convert-memref-stream-to-snitch": get_convert_memref_stream_to_snitch,
