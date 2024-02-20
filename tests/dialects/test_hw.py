d5cc68783dcf1f932c234b0617318b27affd4fe2
Luc Jaulmes <ljaulmes@ed.ac.uk>
2024-02-20 16:27:44 +0000

    dialects: (hw) Add InnerSymbolTable and InnerRefNamespace to HW dialect (#1780)
    This is another standalone part of implementing HW.  It introduces verification
    of inner symbols.
    
    For more, refer to the [Symbol and Inner Symbol Rationale
    document](https://circt.llvm.org/docs/RationaleSymbols/#innerrefnamespace):
    
    > Inner symbols are more costly than normal symbols […].  Since nested regions
    > are allowed, finding all operations defining an inner_sym
    requires a recursive IR scan.
    > Verification is likewise trickier, partly due the significant increase
    in non-local references.
      >
    > For this reason, verification is driven as a trait verifier on
    InnerRefNamespace which constructs and verifies InnerSymbolTables in
    parallel, and uses these to conduct a per-InnerSymbolTable parallelized
    walk to verify references by calling the verifyInnerRefs hook on
    InnerRefUserOpInterface operations.
    
    Additionally all traits in HW now have the `Trait` suffix,
    `InnerSymbolTableTrait` etc., to distinguish upstream traits and the
    state-holding classes which are used for lookups.

--- tests/dialects/test_hw.py
+++ tests/dialects/test_hw.py
@@ -2,11 +2,36 @@
 Test HW traits and interfaces.
 """
 
+from unittest.mock import ANY, patch
 
+import pytest
+
+from xdsl.dialects.builtin import StringAttr
 from xdsl.dialects.hw import (
+    InnerRefAttr,
+    InnerRefNamespaceTrait,
+    InnerRefUserOpInterfaceTrait,
+    InnerSymbolTableCollection,
+    InnerSymbolTableTrait,
     InnerSymTarget,
 )
 from xdsl.dialects.test import TestOp
+from xdsl.irdl import (
+    IRDLOperation,
+    Region,
+    attr_def,
+    irdl_op_definition,
+    opt_region_def,
+    region_def,
+)
+from xdsl.traits import (
+    IsTerminator,
+    SingleBlockImplicitTerminator,
+    SymbolOpInterface,
+    SymbolTable,
+    ensure_terminator,
+)
+from xdsl.utils.exceptions import VerifyException
 
 
 def test_inner_sym_target():
@@ -25,3 +50,231 @@ def test_inner_sym_target():
     assert sub_target
     assert not sub_target.is_op_only()
     assert sub_target.is_field()
+
+
+# Module / Circuit / Wire / Output naming taken from FIRRTL classes using these interfaces.
+@irdl_op_definition
+class ModuleOp(IRDLOperation):
+    name = "module"
+    region = region_def()
+    sym_name = attr_def(StringAttr)
+    traits = frozenset({InnerSymbolTableTrait(), SymbolOpInterface()})
+
+
+@irdl_op_definition
+class OutputOp(IRDLOperation):
+    name = "output"
+    traits = frozenset({IsTerminator()})
+
+
+@irdl_op_definition
+class CircuitOp(IRDLOperation):
+    name = "circuit"
+    region: Region | None = opt_region_def()
+    sym_name = attr_def(StringAttr)
+    traits = frozenset(
+        {
+            InnerRefNamespaceTrait(),
+            SymbolTable(),
+            SingleBlockImplicitTerminator(OutputOp),
+        }
+    )
+
+    def __post_init__(self):
+        for trait in self.get_traits_of_type(SingleBlockImplicitTerminator):
+            ensure_terminator(self, trait)
+
+
+@irdl_op_definition
+class WireOp(IRDLOperation):
+    name = "wire"
+    sym_name = attr_def(StringAttr)
+    traits = frozenset({InnerRefUserOpInterfaceTrait()})
+
+
+def test_inner_symbol_table_interface():
+    """
+    Test operations that conform to InnerSymbolTableTrait
+    """
+    mod = ModuleOp(
+        attributes={"sym_name": StringAttr("symbol_name")}, regions=[[OutputOp()]]
+    )
+
+    circ = CircuitOp(attributes={"sym_name": StringAttr("other_name")}, regions=[[mod]])
+    circ.verify()
+
+    mod_no_parent = ModuleOp(
+        attributes={"sym_name": StringAttr("symbol_name")}, regions=[[OutputOp()]]
+    )
+    with pytest.raises(
+        VerifyException,
+        match="Operation module with trait InnerSymbolTableTrait must have a parent with trait SymbolOpInterface",
+    ):
+        mod_no_parent.verify()
+
+    mod_no_trait_circ = ModuleOp(
+        attributes={"sym_name": StringAttr("symbol_name")}, regions=[[OutputOp()]]
+    )
+    no_trait_circ = TestOp(regions=[[mod_no_trait_circ, OutputOp()]])
+    with pytest.raises(
+        VerifyException,
+        match="Operation module with trait InnerSymbolTableTrait must have a parent with trait InnerRefNamespaceTrait",
+    ):
+        mod_no_trait_circ.verify()
+    with pytest.raises(
+        VerifyException,
+        match="Operation module with trait InnerSymbolTableTrait must have a parent with trait InnerRefNamespaceTrait",
+    ):
+        no_trait_circ.verify()
+
+    @irdl_op_definition
+    class MissingTraitModuleOp(IRDLOperation):
+        name = "module"
+        region = region_def()
+        sym_name = attr_def(StringAttr)
+        traits = frozenset({InnerSymbolTableTrait()})
+
+    mod_missing_trait = MissingTraitModuleOp(
+        attributes={"sym_name": StringAttr("symbol_name")}, regions=[[OutputOp()]]
+    )
+    circ_mod_missing_trait = CircuitOp(regions=[[mod_missing_trait, OutputOp()]])
+    with pytest.raises(
+        VerifyException, match="Operation module must have trait SymbolOpInterface"
+    ):
+        mod_missing_trait.verify()
+    with pytest.raises(
+        VerifyException, match="Operation module must have trait SymbolOpInterface"
+    ):
+        circ_mod_missing_trait.verify()
+
+    @irdl_op_definition
+    class MissingAttrModuleOp(IRDLOperation):
+        name = "module"
+        region = region_def()
+        traits = frozenset({InnerSymbolTableTrait(), SymbolOpInterface()})
+
+    mod_missing_trait_parent = ModuleOp(regions=[[OutputOp()]])
+    MissingAttrModuleOp(regions=[[mod_missing_trait_parent, OutputOp()]])
+    with pytest.raises(
+        VerifyException,
+        match="attribute sym_name expected",
+    ):
+        mod_missing_trait_parent.verify()
+
+
+def test_inner_ref_namespace_interface():
+    """
+    Test operations that conform to InnerRefNamespaceTrait
+    """
+
+    @irdl_op_definition
+    class MissingTraitCircuitOp(IRDLOperation):
+        name = "circuit"
+        region: Region | None = opt_region_def()
+        sym_name = attr_def(StringAttr)
+        traits = frozenset(
+            {InnerRefNamespaceTrait(), SingleBlockImplicitTerminator(OutputOp)}
+        )
+
+    wire0 = WireOp(attributes={"sym_name": StringAttr("wire0")})
+    mod0 = MissingTraitCircuitOp(
+        attributes={"sym_name": StringAttr("mod0")}, regions=[[wire0, OutputOp()]]
+    )
+    circuit_no_symboltable = CircuitOp(
+        attributes={"sym_name": StringAttr("circuit")}, regions=[[mod0]]
+    )
+
+    with pytest.raises(
+        VerifyException, match="Operation circuit must have trait SymbolTable"
+    ):
+        circuit_no_symboltable.verify()
+
+    wire1 = WireOp(attributes={"sym_name": StringAttr("wire1")})
+    wire2 = WireOp(attributes={"sym_name": StringAttr("wire2")})
+    mod1 = ModuleOp(
+        attributes={"sym_name": StringAttr("mod1")}, regions=[[wire1, OutputOp()]]
+    )
+    mod2 = ModuleOp(
+        attributes={"sym_name": StringAttr("mod2")}, regions=[[wire2, OutputOp()]]
+    )
+    circuit = CircuitOp(
+        attributes={"sym_name": StringAttr("circuit")}, regions=[[mod1, mod2]]
+    )
+
+    # InnerRefUserOpInterfaceTrait.verify_inner_refs() does not do anything, so just mock
+    # to check it is called
+    with patch.object(
+        InnerRefUserOpInterfaceTrait, "verify_inner_refs"
+    ) as inner_ref_verif:
+        circuit.verify()
+
+    inner_ref_verif.assert_any_call(wire1, ANY)
+    inner_ref_verif.assert_any_call(wire2, ANY)
+    assert inner_ref_verif.call_count == 2
+
+
+def test_inner_symbol_table_collection():
+    """
+    Test operations that pertain to InnerSymbolTableCollection
+    """
+    wire1 = WireOp(attributes={"sym_name": StringAttr("wire1")})
+    wire2 = WireOp(attributes={"sym_name": StringAttr("wire2")})
+    mod1 = ModuleOp(
+        attributes={"sym_name": StringAttr("mod1")}, regions=[[wire1, OutputOp()]]
+    )
+    mod2 = ModuleOp(
+        attributes={"sym_name": StringAttr("mod2")}, regions=[[wire2, OutputOp()]]
+    )
+    circuit = CircuitOp(
+        attributes={"sym_name": StringAttr("circuit")}, regions=[[mod1, mod2]]
+    )
+
+    with pytest.raises(
+        VerifyException, match="Operation wire should have InnerRefNamespaceTrait trait"
+    ):
+        inner_sym_tables = InnerSymbolTableCollection(wire1)
+
+    inner_sym_tables = InnerSymbolTableCollection(circuit)
+
+    with pytest.raises(
+        VerifyException, match=r"Trying to insert the same op twice in symbol tables:"
+    ):
+        inner_sym_tables.populate_and_verify_tables(circuit)
+
+    with pytest.raises(
+        VerifyException, match="Operation wire should have InnerSymbolTableTrait trait"
+    ):
+        inner_sym_tables.get_inner_symbol_table(wire1)
+
+    sym_table1 = inner_sym_tables.get_inner_symbol_table(mod1)
+    sym_table2 = inner_sym_tables.get_inner_symbol_table(mod2)
+    assert (
+        sym_table1 is not sym_table2
+    ), "Different InnerSymbolTableTrait objects must return different instances of inner symbol tables"
+
+    unpopulated_inner_sym_tables = InnerSymbolTableCollection()
+    sym_table3 = unpopulated_inner_sym_tables.get_inner_symbol_table(mod1)
+    sym_table4 = unpopulated_inner_sym_tables.get_inner_symbol_table(mod2)
+    assert (
+        sym_table3 is not sym_table4
+    ), "InnerSymbolTableTrait still behave as expected when created on the fly"
+
+
+def test_inner_ref_attr():
+    """
+    Test inner reference attributes
+    """
+    wire1 = WireOp(attributes={"sym_name": StringAttr("wire1")})
+    wire2 = WireOp(attributes={"sym_name": StringAttr("wire2")})
+    mod1 = ModuleOp(
+        attributes={"sym_name": StringAttr("mod1")}, regions=[[wire1, OutputOp()]]
+    )
+    mod2 = ModuleOp(
+        attributes={"sym_name": StringAttr("mod2")}, regions=[[wire2, OutputOp()]]
+    )
+    CircuitOp(attributes={"sym_name": StringAttr("circuit")}, regions=[[mod1, mod2]])
+
+    ref = InnerRefAttr("mod2", "wire2")
+    assert (
+        ref.get_module().data == "mod2"
+    ), "Name of the referenced module should be returned correctly"
