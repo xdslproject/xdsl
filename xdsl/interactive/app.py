efed40bec0625225df91b5b2d47bb3bb53e2fb53
Dalia Shaaban <dalia.shaaban08@gmail.com>
2024-02-21 22:55:19 +0000

    sasha strikes again

--- xdsl/interactive/app.py
+++ xdsl/interactive/app.py
@@ -30,6 +30,7 @@ from textual.widgets import (
 
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.interactive.add_arguments_screen import AddArguments
+from xdsl.interactive.get_all_available_passes import get_available_pass_list
 from xdsl.interactive.load_file_screen import LoadFile
 from xdsl.interactive.pass_list_item import PassListItem
 from xdsl.interactive.pass_metrics import (
@@ -40,12 +41,7 @@ from xdsl.interactive.passes import (
     ALL_PASSES,
     AvailablePass,
     apply_passes_to_module,
-    get_condensed_pass_list,
     get_new_registered_context,
-)
-from xdsl.interactive.rewrites import (
-    convert_indexed_individual_rewrites_to_available_pass,
-    get_all_possible_rewrites,
 )
 from xdsl.parser import Parser
 from xdsl.passes import ModulePass, PipelinePass, get_pass_argument_names_and_types
@@ -255,26 +251,12 @@ class InputApp(App[None]):
             case Exception():
                 return ()
             case ModuleOp():
-                # get all rewrites
-                rewrites = get_all_possible_rewrites(
-                    self.current_module,
+                return get_available_pass_list(
+                    self.input_text_area.text,
+                    self.pass_pipeline,
+                    self.condense_mode,
                     individual_rewrite.REWRITE_BY_NAMES,
                 )
-                # transform rewrites into passes
-                rewrites_as_pass_list = (
-                    convert_indexed_individual_rewrites_to_available_pass(
-                        rewrites, self.current_module
-                    )
-                )
-                # merge rewrite passes with "other" pass list
-                if self.condense_mode:
-                    pass_list = get_condensed_pass_list(self.current_module)
-                    return pass_list + rewrites_as_pass_list
-                else:
-                    pass_list = tuple(
-                        AvailablePass(p.name, p, None) for _, p in ALL_PASSES
-                    )
-                    return pass_list + rewrites_as_pass_list
 
     def watch_available_pass_list(
         self,
