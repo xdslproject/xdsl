3193b2964becbd5fdcac8306b2ca05fbd12cd520
Sasha Lopoukhine <superlopuh@gmail.com>
2024-02-21 21:05:40 +0000

    wip dfs

--- dag.py
+++ dag.py
@@ -55,31 +55,36 @@ def __(mo):
     import networkx as nx
 
     G = nx.random_geometric_graph(200, 0.125)
+    while not nx.is_connected(G):
+        G = nx.random_geometric_graph(200, 0.125)
 
-    edge_x = []
-    edge_y = []
-    for edge in G.edges():
-        x0, y0 = G.nodes[edge[0]]['pos']
-        x1, y1 = G.nodes[edge[1]]['pos']
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
 
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(width=0.5, color='#888'),
-        hoverinfo='none',
-        mode='lines')
-
-    node_x = []
-    node_y = []
-    for node in G.nodes():
+    node_x = [-1] * len(G.nodes())
+    node_y = [-1] * len(G.nodes())
+    for i, node in enumerate(G.nodes()):
         x, y = G.nodes[node]['pos']
-        node_x.append(x)
-        node_y.append(y)
+        node_x[i] = (x)
+        node_y[i] = (y)
+
+        
+    node_index_by_node = {n:i for i, n in enumerate(G.nodes())}
+
+    # g_root = G.nodes()[0]
+
+    node_x[node_index_by_node[0]] = 0.0
+    node_y[node_index_by_node[0]] = 0.5
+
+    layers = dict(enumerate(nx.bfs_layers(G, 0)))
+    x_scale = len(layers) + 2.0
+    y_scales = tuple(len(layers[i]) + 2.0 for i in range(len(layers)))
+
+    for layer_index, nodes in layers.items():
+        x = (layer_index + 1.0) / x_scale
+        y_scale = y_scales[layer_index]
+        for node_index, node in enumerate(nodes):
+            y = (node_index + 1.0) / y_scale
+            node_x[node] = x
+            node_y[node] = y
 
     node_trace = go.Scatter(
         x=node_x, y=node_y,
@@ -102,6 +107,27 @@ def __(mo):
                 titleside='right'
             ),
             line_width=2))
+
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        n0 = node_index_by_node[edge[0]]
+        n1 = node_index_by_node[edge[1]]
+        x0, y0 = node_x[n0], node_y[n0]
+        x1, y1 = node_x[n1], node_y[n1]
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
 
     node_adjacencies = []
     node_text = []
@@ -138,19 +164,30 @@ def __(mo):
         edge_y,
         fig,
         go,
+        i,
+        layer_index,
+        layers,
+        n0,
+        n1,
         node,
         node_adjacencies,
+        node_index,
+        node_index_by_node,
         node_text,
         node_trace,
         node_x,
         node_y,
+        nodes,
         nx,
         x,
         x0,
         x1,
+        x_scale,
         y,
         y0,
         y1,
+        y_scale,
+        y_scales,
     )
 
 
@@ -175,23 +212,11 @@ def __(input_module, nx):
             bla.add_edge(source, target, available_pass.display_name)
             if target not in visited:
                 queue.append(target)
-
-
-    for n in bla.nodes:
-        print(n.module)
-        paths = nx.all_simple_edge_paths(bla, root, n)
-
-        for path in paths:
-           print("Path :: " + ','.join(e[2] for e in path))
-    str(bla)
     return (
         HashableModule,
         available_pass,
         bla,
         iter_condensed_passes,
-        n,
-        path,
-        paths,
         queue,
         root,
         source,
@@ -199,6 +224,31 @@ def __(input_module, nx):
         target,
         visited,
     )
+
+
+@app.cell
+def __(bla):
+    node_index_by_module = {n: i for i, n in enumerate(bla.nodes())}
+
+
+    return node_index_by_module,
+
+
+@app.cell
+def __(bla, nx, root):
+    for n in bla.nodes:
+        print(n.module)
+        paths = nx.all_simple_edge_paths(bla, root, n)
+
+        for path in paths:
+           print("Path :: " + ','.join(e[2] for e in path))
+    str(bla)
+    return n, path, paths
+
+
+@app.cell
+def __():
+    return
 
 
 if __name__ == "__main__":
