66873f903e34ae57c738a3618a49d636f652a913
kayode-gif <kayodeoke417@gmail.com>
2024-02-21 19:29:42 +0000

    wip ml_program global

--- xdsl/dialects/ml_program.py
+++ xdsl/dialects/ml_program.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from xdsl.dialects.builtin import (
     StringAttr,
-    TypeAttribute,
+    SymbolRefAttr,
     UnitAttr,
 )
 from xdsl.ir import (
@@ -13,6 +13,7 @@ from xdsl.irdl import (
     IRDLOperation,
     attr_def,
     irdl_op_definition,
+    result_def,
 )
 from xdsl.parser import Parser
 from xdsl.printer import Printer
@@ -30,7 +31,7 @@ class Global(IRDLOperation):
     name = "ml_program.global"
 
     sym_name = attr_def(StringAttr)
-    type = attr_def(TypeAttribute)
+    type = attr_def()
     is_mutuable = attr_def(UnitAttr)
     value = attr_def(Attribute)
     sym_visibility = attr_def(StringAttr)
@@ -63,9 +64,43 @@ class Global(IRDLOperation):
             raise VerifyException("Immutable global must have an initial value")
 
 
+@irdl_op_definition
+class GlobalLoadConstant(IRDLOperation):
+    """
+    Direct load a constant value from a global
+
+    See https://mlir.llvm.org/docs/Dialects/MLProgramOps/#ml_programglobal_load_const-ml_programgloballoadconstop
+    """
+
+    name = "ml_program.global_load_const"
+
+    global_attr = attr_def(SymbolRefAttr)
+    result = result_def()
+
+    def __init__(
+        self,
+        global_attr: Attribute,
+        result_type: Attribute | None,
+    ):
+        super().__init__(
+            attributes={
+                "global_attr": global_attr,
+            },
+            result_types=[result_type],
+        )
+
+    def print(self, printer: Printer):
+        pass
+
+    @classmethod
+    def parse(cls, parser: Parser) -> None:
+        pass
+
+
 MLProgram = Dialect(
     "ml_program",
     [
         Global,
+        GlobalLoadConstant,
     ],
 )
