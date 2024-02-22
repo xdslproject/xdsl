46f1f793592f2da78370d7e8829bc13172c763d9
Dalia Shaaban <dalia.shaaban08@gmail.com>
2024-02-22 16:35:57 +0000

    draft1

--- xdsl/interactive/app.py
+++ xdsl/interactive/app.py
@@ -135,6 +135,7 @@ class InputApp(App[None]):
     DataTable displaying the diff of operation names and counts of the input and output
     text areas.
     """
+    current_argument_pass: tuple[tuple[type[ModulePass], PipelinePassSpec]] = ()
 
     pre_loaded_input_text: str
     current_file_path: str
@@ -377,16 +378,20 @@ class InputApp(App[None]):
                         f"{selected_pass_value.name}{{{concatenated_arg_val}}}"
                     )
                 )[0]
-                self.pass_pipeline = (
-                    *self.pass_pipeline,
+                # self.pass_pipeline = (
+                #     *self.pass_pipeline,
+                #     (selected_pass_value, new_pass_with_arguments),
+                # )
+                self.current_argument_pass = (
                     (selected_pass_value, new_pass_with_arguments),
                 )
-
             except PassPipelineParseError as e:
                 res = f"PassPipelineParseError: {e}"
                 screen = AddArguments(TextArea(res, id="argument_text_area"))
                 self.push_screen(screen, add_pass_with_arguments_to_pass_pipeline)
 
+        # clear global temporary variable
+        self.current_argument_pass = ()
         # generates a string containing the concatenated_arg_val and types of the selected pass and initializes the AddArguments Screen to contain the string
         self.push_screen(
             AddArguments(
@@ -413,9 +418,17 @@ class InputApp(App[None]):
         # get instance
         selected_pass_value, selected_pass_spec = selected_pass.data
 
+        # get root to child passes due to tree traversal possibility
+        root_to_child_pass_list = self.get_root_to_child_pass_list(selected_pass)
+
         # if selected_pass_value has arguments, call get_arguments_function to push screen for user input
         if fields(selected_pass_value) and selected_pass_spec is None:
             self.get_pass_arguments(selected_pass_value, selected_pass_spec)
+            self.pass_pipeline = (
+                *self.pass_pipeline,
+                *root_to_child_pass_list,
+                *self.current_argument_pass,
+            )
         else:
             # if selected_pass_value contains no arguments add the selected pass to pass_pipeline
             if selected_pass_spec is None:
@@ -423,8 +436,59 @@ class InputApp(App[None]):
             # selected_pass_value is an "individual_rewrite", add the selected pass to pass_pipeline
             self.pass_pipeline = (
                 *self.pass_pipeline,
+                *root_to_child_pass_list,
                 (selected_pass_value, selected_pass_spec),
             )
+
+    @on(Tree.NodeExpanded, "#passes_tree")
+    def expand_tree_node(
+        self, event: Tree.NodeExpanded[tuple[type[ModulePass], PipelinePassSpec | None]]
+    ) -> None:
+        """
+        Function called when a user expands a node (i.e. a pass) and adds another level
+        to the pass selection tree. Allow's multi-level tree traversal.
+        """
+        expanded_node = event.node
+        assert expanded_node.data is not None
+        assert not expanded_node.is_root
+
+        # get instance
+        selected_pass_value, selected_pass_spec = expanded_node.data
+
+        # if expanded_pass has arguments, call get_arguments_function to push screen for user input
+        if fields(selected_pass_value) and selected_pass_spec is None:
+            self.get_pass_arguments(selected_pass_value, selected_pass_spec)
+            # if arguments were not successfully entered, do not continue with tree expansion
+            if self.current_argument_pass == ():
+                return
+
+            # remove current event node from root_to_child_pass_list
+            root_to_child_pass_list = self.get_root_to_child_pass_list(expanded_node)[
+                :-1
+            ]
+            child_pass_pipeline = (
+                *self.pass_pipeline,
+                *root_to_child_pass_list,
+                *self.current_argument_pass,
+            )
+
+        else:
+            # if selected_pass_value contains no arguments add the selected pass to pass_pipeline
+            root_to_child_pass_list = self.get_root_to_child_pass_list(expanded_node)
+
+            child_pass_pipeline = (
+                *self.pass_pipeline,
+                *root_to_child_pass_list,
+            )
+
+        child_pass_list = get_available_pass_list(
+            self.input_text_area.text,
+            child_pass_pipeline,
+            self.condense_mode,
+            individual_rewrite.REWRITE_BY_NAMES,
+        )
+
+        self.expand_node(expanded_node, child_pass_list)
 
     def watch_pass_pipeline(self) -> None:
         """
