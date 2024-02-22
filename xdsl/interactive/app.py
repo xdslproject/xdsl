d436726c3462dc9b7db24c525f21c3f237fed0e2
Dalia Shaaban <dalia.shaaban08@gmail.com>
2024-02-22 15:30:49 +0000

    dfasdf

--- xdsl/interactive/app.py
+++ xdsl/interactive/app.py
@@ -270,8 +270,7 @@ class InputApp(App[None]):
         """
         This function facilitates user input of pass concatenated_arg_val by navigating
         to the AddArguments screen, and subsequently parses the returned string upon
-        screen dismissal and updates the reactive variable current_argument_pass to hold
-        the constructed pass in a form ready to append to the pass_pipeline.
+        screen dismissal and appends the pass to the pass_pipeline variable.
         """
 
         def add_pass_with_arguments_to_pass_pipeline(concatenated_arg_val: str) -> None:
