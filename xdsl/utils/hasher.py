c6dcd636fe9b3664f3fb4c0b3c82ac9b5b768b3b
Sasha Lopoukhine <superlopuh@gmail.com>
2024-02-22 09:24:48 +0000

    hash of tuple

--- xdsl/utils/hasher.py
+++ xdsl/utils/hasher.py
@@ -11,8 +11,5 @@ class Hasher:
     def __init__(self, *, seed: int = 0):
         self.hash = seed
 
-    def combine_hash(self, other_hash: int) -> None:
-        self.hash ^= other_hash
-
     def combine(self, other: Hashable) -> None:
-        self.combine_hash(hash(other))
+        self.hash = hash((self.hash, other))
