d37b5a584bb7a4705bb15c79fdfb13c6e1aab778
Sasha Lopoukhine <superlopuh@gmail.com>
2024-03-03 13:58:08 +0000

    interactive: don't include the child in the root to child list

--- xdsl/interactive/app.py
+++ xdsl/interactive/app.py
@@ -273,13 +273,13 @@ class InputApp(App[None]):
     ) -> tuple[tuple[type[ModulePass], PipelinePassSpec], ...]:
         """
         Helper function that returns a pass_pipeline consisiting of the list of nodes
-        from the root of the tree to and including the expanded_node child.
+        from the root of the tree, not including the expanded_node child.
         """
         assert expanded_node.data is not None
 
         pass_list_items: list[tuple[type[ModulePass], PipelinePassSpec | None]] = []
 
-        current = expanded_node
+        current = expanded_node.parent
 
         # traverse the path starting from the child node until we reach the root
         while current is not None and current.data is not None and not current.is_root:
@@ -415,7 +415,7 @@ class InputApp(App[None]):
         selected_pass_value, selected_pass_spec = selected_pass.data
 
         # get root to child passes due to tree traversal possibility
-        root_to_child_pass_list = self.get_root_to_child_pass_list(selected_pass)[:-1]
+        root_to_child_pass_list = self.get_root_to_child_pass_list(selected_pass)
 
         # if selected_pass_value has arguments, call get_arguments_function to push screen for user input
         if fields(selected_pass_value) and selected_pass_spec is None:
@@ -449,9 +449,13 @@ class InputApp(App[None]):
         # get instance
         selected_pass_value, selected_pass_spec = expanded_node.data
 
-        # if expanded_pass requires arguments, do not allow node expansion
-        if fields(selected_pass_value) and selected_pass_spec is None:
-            return
+        if selected_pass_spec is None:
+            if fields(selected_pass_value):
+                # if expanded_pass requires arguments, do not allow node expansion
+                return
+            else:
+                # Get the default/empty pass spec
+                selected_pass_spec = selected_pass_value().pipeline_pass_spec()
 
         # if selected_pass_value requires no arguments add the selected pass to pass_pipeline
         root_to_child_pass_list = self.get_root_to_child_pass_list(expanded_node)
@@ -459,6 +463,7 @@ class InputApp(App[None]):
         child_pass_pipeline = (
             *self.pass_pipeline,
             *root_to_child_pass_list,
+            (selected_pass_value, selected_pass_spec),
         )
 
         child_pass_list = get_available_pass_list(
