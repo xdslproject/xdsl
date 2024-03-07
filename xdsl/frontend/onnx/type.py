a747c99e06aa18ae8f84c2a8d0b74673b0cf2d2d
alecerio <alessandrocerioliphd@gmail.com>
2024-03-07 18:31:27 +0100

    reorganise in two files

--- xdsl/frontend/onnx/type.py
+++ xdsl/frontend/onnx/type.py
@@ -1,8 +1,24 @@
 from onnx import TensorShapeProto, TypeProto
 
-from xdsl.dialects.builtin import TensorType
-from xdsl.frontend.onnx.elem_type import get_elem_type
+from xdsl.dialects.builtin import TensorType, f32, f64
 from xdsl.ir import Attribute
+
+ELEM_TYPE = {
+    1: f32,
+    11: f64,
+}
+
+
+def get_elem_type(code: int) -> Attribute:
+    if code in ELEM_TYPE:
+        return ELEM_TYPE[code]
+    else:
+        raise ValueError(f"Unknown elem_type: {code}")
+
+
+def get_type(type: TypeProto) -> Attribute:
+    tt = get_tensor_type(type.tensor_type)
+    return tt
 
 
 def get_shape(shape: TensorShapeProto) -> tuple[int, ...]:
