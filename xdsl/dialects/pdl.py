043818a0543c0c09c6986aeab1f280296fc0d440
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2024-03-10 16:43:30 +0100

    satisfy code style

--- xdsl/dialects/pdl.py
+++ xdsl/dialects/pdl.py
@@ -43,8 +43,7 @@ from xdsl.irdl import (
     var_operand_def,
     var_result_def,
 )
-from xdsl.parser import Parser
-from xdsl.parser.attribute_parser import AttrParser
+from xdsl.parser import Parser, AttrParser
 from xdsl.printer import Printer
 from xdsl.traits import HasParent, IsTerminator, NoTerminator, OptionalSymbolOpInterface
 from xdsl.utils.exceptions import VerifyException
