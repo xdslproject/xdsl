edf933162fc6a32fb61bd1961811ade675f4dd6f
alecerio <alessandrocerioliphd@gmail.com>
2024-03-12 13:14:26 +0100

    test get_shape

---
+++ tests/frontend/onnx/test_type.py
@@ -0,0 +1,40 @@
+import onnx
+import pytest
+
+from xdsl.frontend.onnx.type import f32, f64, get_elem_type, get_shape
+
+
+def test_get_elem_type():
+    # test case 1: check if 1 corresponds to f32
+    assert get_elem_type(1) == f32
+
+    # test case 11: check if 11 corresponds to f64
+    assert get_elem_type(11) == f64
+
+    # test case -1: check if -1 (or other illegal values) corresponds to None
+    with pytest.raises(ValueError, match="Unknown elem_type: -1"):
+        get_elem_type(-1)
+
+
+def test_get_type():
+    pass
+
+
+def test_get_shape():
+    tensor_shape = onnx.TensorShapeProto()
+    tensor_shape.dim.extend(
+        [
+            onnx.TensorShapeProto.Dimension(dim_value=3),
+            onnx.TensorShapeProto.Dimension(dim_value=4),
+            onnx.TensorShapeProto.Dimension(dim_value=5),
+        ]
+    )
+    shape = get_shape(tensor_shape)
+    assert len(shape) == 3
+    assert shape[0] == 3
+    assert shape[1] == 4
+    assert shape[2] == 5
+
+
+def test_get_tensor_type():
+    pass
