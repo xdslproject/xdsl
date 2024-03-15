b9b267fc0aaf14a072af13670b02b419e1fe8557
kayode-gif <kayodeoke417@gmail.com>
2024-03-15 11:16:05 +0000

    impl onnx and numpy updates

--- tests/interpreters/test_wgpu.py
+++ tests/interpreters/test_wgpu.py
@@ -11,6 +11,7 @@ from xdsl.ir import MLContext
 from xdsl.parser import Parser
 
 pytest.importorskip("wgpu", reason="wgpu is an optional dependency")
+pytest.importorskip("numpy", reason="numpy is an optional dependency in xDSL")
 
 from xdsl.interpreters.experimental.wgpu import WGPUFunctions  # noqa: E402
 
