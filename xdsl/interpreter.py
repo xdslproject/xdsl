239d48c94071334e85328c6f91c8925c89fbbb82
kayode-gif <kayodeoke417@gmail.com>
2024-03-24 14:17:27 +0000

    add run op func

--- xdsl/interpreter.py
+++ xdsl/interpreter.py
@@ -614,6 +614,7 @@ class Interpreter:
         """
         if isinstance(op, str):
             op = self.get_op_for_symbol(op)
+
         return self._run_op(op, inputs).values
 
     def call_op(self, op: Operation | str, inputs: PythonValues) -> PythonValues:
