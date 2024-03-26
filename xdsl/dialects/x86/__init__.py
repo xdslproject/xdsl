891a373b56e18904301380ac9993e3181263b7a3
ChrisG <krzgrykiel@gmail.com>
2024-03-26 14:18:23 +0000

    Added sub, imul, and, or, xor and mov

--- xdsl/dialects/x86/__init__.py
+++ xdsl/dialects/x86/__init__.py
@@ -1,12 +1,18 @@
 from xdsl.ir import Dialect
 
-from .ops import AddOp, GetRegisterOp
+from .ops import AddOp, AndOp, GetRegisterOp, ImulOp, MovOp, OrOp, SubOp, XorOp
 from .register import GeneralRegisterType
 
 X86 = Dialect(
     "x86",
     [
         AddOp,
+        SubOp,
+        ImulOp,
+        AndOp,
+        OrOp,
+        XorOp,
+        MovOp,
         GetRegisterOp,
     ],
     [
