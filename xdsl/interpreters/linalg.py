d4a49634ccfc8ca63d67c5e1d70e7b121121ab64
kayode-gif <s2020476@ed.ac.uk>
2024-03-27 18:52:26 +0000

    interpreter: (linalg) Add linalg operations for ONNX model MNIST targeting (#2355)
    resolves #2354

--- xdsl/interpreters/linalg.py
+++ xdsl/interpreters/linalg.py
@@ -61,3 +61,104 @@ class LinalgFunctions(InterpreterFunctions):
         self, interpreter: Interpreter, op: linalg.YieldOp, args: tuple[Any, ...]
     ):
         return ReturnedValues(args), ()
+
+    @impl(linalg.AddOp)
+    def run_add(
+        self, interpreter: Interpreter, op: linalg.AddOp, args: tuple[Any, ...]
+    ) -> tuple[Any, ...]:
+        (lhs, rhs, res) = (args[0], args[1], args[2])
+        assert isinstance(lhs, ShapedArray)
+        assert isinstance(rhs, ShapedArray)
+        assert isinstance(res, ShapedArray)
+        lhs = cast(ShapedArray[float], lhs)
+        rhs = cast(ShapedArray[float], rhs)
+        res = cast(ShapedArray[float], res)
+        assert all(res.data_ptr[i] == 0.0 for i in range(len(res.data)))
+        assert lhs.shape == rhs.shape == res.shape
+        for i in range(len(lhs.data)):
+            res.data_ptr[i] = lhs.data_ptr[i] + rhs.data_ptr[i]
+        if len(op.results) > 0:
+            return (res,)
+        return ()
+
+    @impl(linalg.FillOp)
+    def run_fill(
+        self, interpreter: Interpreter, op: linalg.FillOp, args: tuple[Any, ...]
+    ) -> tuple[Any, ...]:
+        operand, res = args[0], args[1]
+        assert isinstance(operand, ShapedArray)
+        assert isinstance(res, ShapedArray)
+        operand = cast(ShapedArray[float], operand)
+        res = cast(ShapedArray[float], res)
+        assert all(res.data_ptr[i] == 0.0 for i in range(len(res.data)))
+        for i in range(len(res.data)):
+            res.data_ptr[i] = operand.data_ptr[0]
+        if len(op.results) > 0:
+            return (res,)
+        return ()
+
+    @impl(linalg.MulOp)
+    def run_mul(
+        self, interpreter: Interpreter, op: linalg.MulOp, args: tuple[Any, ...]
+    ) -> tuple[Any, ...]:
+        lhs, rhs, res = args[0], args[1], args[2]
+        assert isinstance(lhs, ShapedArray)
+        assert isinstance(rhs, ShapedArray)
+        assert isinstance(res, ShapedArray)
+        lhs = cast(ShapedArray[float], lhs)
+        rhs = cast(ShapedArray[float], rhs)
+        res = cast(ShapedArray[float], res)
+        assert all(res.data_ptr[i] == 0.0 for i in range(len(res.data)))
+        assert lhs.shape == rhs.shape == res.shape
+        for i in range(len(lhs.data)):
+            res.data_ptr[i] = lhs.data_ptr[i] * rhs.data_ptr[i]
+        if len(op.results) > 0:
+            return (res,)
+        return ()
+
+    @impl(linalg.TransposeOp)
+    def run_transpose(
+        self, interpreter: Interpreter, op: linalg.TransposeOp, args: tuple[Any, ...]
+    ) -> tuple[Any, ...]:
+        operand, res = args[0], args[1]
+        assert isinstance(operand, ShapedArray)
+        assert isinstance(res, ShapedArray)
+        operand = cast(ShapedArray[float], operand)
+        res = cast(ShapedArray[float], res)
+        assert all(res.data_ptr[i] == 0.0 for i in range(len(res.data)))
+        assert len(operand.shape) == 2
+        assert len(res.shape) == 2
+        rows, cols = operand.shape
+        for i in range(rows):
+            for j in range(cols):
+                res.data_ptr[j * rows + i] = operand.data_ptr[i * cols + j]
+        if len(op.results) > 0:
+            return (res,)
+        return ()
+
+    @impl(linalg.MatmulOp)
+    def run_mat_mul(
+        self, interpreter: Interpreter, op: linalg.MatmulOp, args: tuple[Any, ...]
+    ) -> tuple[Any, ...]:
+        lhs, rhs, res = args[0], args[1], args[2]
+        assert isinstance(lhs, ShapedArray)
+        assert isinstance(rhs, ShapedArray)
+        assert isinstance(res, ShapedArray)
+        lhs = cast(ShapedArray[float], lhs)
+        rhs = cast(ShapedArray[float], rhs)
+        res = cast(ShapedArray[float], res)
+        assert all(res.data_ptr[i] == 0.0 for i in range(len(res.data)))
+        rows = lhs.shape[0]
+        cols = rhs.shape[1]
+        assert rows == cols
+        for i in range(rows):
+            for j in range(cols):
+                res.data_ptr[i * cols + j] = sum(
+                    lhs.data_ptr[i * lhs.shape[1] + k]
+                    * rhs.data_ptr[k * rhs.shape[1] + j]
+                    for k in range(lhs.shape[1])
+                )
+
+        if len(op.results) > 0:
+            return (res,)
+        return ()
