fe3c829b7676111b9929dcc3399f0ac8d3826775
kayode-gif <kayodeoke417@gmail.com>
2024-04-01 17:48:06 +0100

    we can suport strides of two

--- xdsl/interpreters/onnx.py
+++ xdsl/interpreters/onnx.py
@@ -386,7 +386,7 @@ class OnnxFunctions(InterpreterFunctions):
             dtype=matrix.dtype,
         )
 
-        # do convolution
+        # do maxpool
         for k in range(matrix.shape[0]):
             for l in range(matrix.shape[1]):
                 for i in range(0, m_height - ky + 1, strides[0]):
