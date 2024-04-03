d53fbfe9a4456d4c61eb75c0990ce751924fa315
Christos Lamprakos <cplamprakos@microlab.ntua.gr>
2024-04-03 09:55:42 +0100

    Add lowering, tests for dma_start_1d_wideptr

--- xdsl/dialects/snitch_runtime.py
+++ xdsl/dialects/snitch_runtime.py
@@ -312,7 +312,9 @@ class DmaStart1DBaseOperation(SnitchRuntimeBaseOperation, Generic[_T], ABC):
     T = Annotated[Attribute, ConstraintVar("T"), _T]
     dst: Operand = operand_def(_T)
     src: Operand = operand_def(_T)
-    size: Operand = operand_def(IndexType)
+    # Pylance was complaining about the below.
+    # size: Operand = operand_def(Annotated[Attribute, i32])
+    size: Operand = operand_def(i32)
     transfer_id: OpResult = result_def(tx_id)
 
     def __init__(
