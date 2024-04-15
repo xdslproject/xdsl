01e8e73c8139980dad286a6ca677785e1ffc9547
Alessandro Cerioli <137900456+alecerio@users.noreply.github.com>
2024-04-15 14:50:34 +0200

    frontend: (onnx) visit_node implementation (#2441)
    Implementation of visit_node and related tests.

--- tests/frontend/onnx/test_build_ir.py
+++ tests/frontend/onnx/test_build_ir.py
@@ -1,13 +1,17 @@
 import onnx
 import pytest
 
+from xdsl.dialects.builtin import TensorType, f32
+from xdsl.dialects.onnx import Add, Sub
 from xdsl.ir import Attribute
+from xdsl.utils.test_value import TestSSAValue
 
 try:
-    from onnx import TensorProto, ValueInfoProto
+    from onnx import TensorProto, ValueInfoProto, helper
 
     from xdsl.frontend.onnx.ir_builder import (
         OnnxXdslMapping,
+        visit_node,
         visit_value_info,
     )
 except ImportError as exc:
@@ -59,3 +63,113 @@ def test_visit_value_info():
 
     # check it is returned the same reference
     assert t1 is t2
+
+
+def test_visit_node_unknown_op_name():
+    """
+    Test for unknown expected onnx op
+    """
+    node_attributes = {
+        "name": "dummy_name",
+        "op_type": "dummy_op",
+    }
+
+    node = helper.make_node(
+        **node_attributes, inputs=["input1", "input2"], outputs=["output"]
+    )
+    ctx = OnnxXdslMapping()
+    with pytest.raises(ValueError, match="Unknown ONNX op name dummy_op"):
+        visit_node(node=node, ctx=ctx)
+
+
+def test_visit_node_add():
+    # initialize context
+    ctx = OnnxXdslMapping()
+
+    # create graph composed only of one Add operation
+    _, add_node = _create_graph_binary_op("Add", "add_graph")
+
+    lhs = TestSSAValue(TensorType(f32, [64]))
+    rhs = TestSSAValue(TensorType(f32, [64]))
+    ctx.value_by_name["input1"] = lhs
+    ctx.value_by_name["input2"] = rhs
+
+    lhs_type = TensorType(f32, [64])
+    rhs_type = TensorType(f32, [64])
+    out_type = TensorType(f32, [64])
+    ctx.type_by_name["input1"] = lhs_type
+    ctx.type_by_name["input2"] = rhs_type
+    ctx.type_by_name["output"] = out_type
+
+    # visit node
+    op = visit_node(add_node, ctx)
+
+    assert isinstance(op, Add)
+    assert op.lhs is lhs
+    assert op.rhs is rhs
+    assert op.res is ctx.value_by_name["output"]
+    assert not op.attributes
+    assert not op.regions
+
+
+def test_visit_node_sub():
+    # initialize context
+    ctx = OnnxXdslMapping()
+
+    # create graph composed only of one Sub operation
+    _, sub_node = _create_graph_binary_op("Sub", "sub_graph")
+
+    lhs = TestSSAValue(TensorType(f32, [64]))
+    rhs = TestSSAValue(TensorType(f32, [64]))
+    ctx.value_by_name["input1"] = lhs
+    ctx.value_by_name["input2"] = rhs
+
+    lhs_type = TensorType(f32, [64])
+    rhs_type = TensorType(f32, [64])
+    out_type = TensorType(f32, [64])
+    ctx.type_by_name["input1"] = lhs_type
+    ctx.type_by_name["input2"] = rhs_type
+    ctx.type_by_name["output"] = out_type
+
+    op = visit_node(sub_node, ctx)
+
+    assert isinstance(op, Sub)
+    assert op.lhs is lhs
+    assert op.rhs is rhs
+    assert op.res is ctx.value_by_name["output"]
+    assert not op.attributes
+    assert not op.regions
+
+
+def _create_graph_binary_op(op_name: str, graph_name: str):
+    # define input and output names
+    input1_name = "input1"
+    input2_name = "input2"
+    output_name = "output"
+
+    # define input shapes
+    input1_shape = [64]
+    input2_shape = [64]
+    output_shape = [64]
+
+    # define op node
+    op_node = helper.make_node(
+        op_type=op_name,
+        inputs=[input1_name, input2_name],
+        outputs=[output_name],
+    )
+
+    # create graph (composed of just one operation)
+    graph = helper.make_graph(
+        nodes=[op_node],
+        name=graph_name,
+        inputs=[
+            helper.make_tensor_value_info(input1_name, TensorProto.FLOAT, input1_shape),
+            helper.make_tensor_value_info(input2_name, TensorProto.FLOAT, input2_shape),
+        ],
+        outputs=[
+            helper.make_tensor_value_info(output_name, TensorProto.FLOAT, output_shape),
+        ],
+    )
+
+    return graph, op_node
--- xdsl/frontend/onnx/ir_builder.py
+++ xdsl/frontend/onnx/ir_builder.py
@@ -1,16 +1,45 @@
-from onnx import ValueInfoProto
+from onnx import NodeProto, ValueInfoProto
 
+from xdsl.dialects import onnx
 from xdsl.frontend.onnx.type import get_type
-from xdsl.ir import Attribute
+from xdsl.ir import Attribute, SSAValue
+from xdsl.irdl import IRDLOperation
+
+OP_BY_OP_TYPE: dict[str, type[IRDLOperation]] = {
+    "Add": onnx.Add,
+    "Sub": onnx.Sub,
+}
+"""Associate the name of the operations with the respective operation in ONNX dialect."""
 
 
 class OnnxXdslMapping:
     """The representation of the onnx context."""
 
     type_by_name: dict[str, Attribute]
+    value_by_name: dict[str, SSAValue]
 
     def __init__(self):
         self.type_by_name = {}
+        self.value_by_name = {}
+
+
+def visit_node(node: NodeProto, ctx: OnnxXdslMapping) -> IRDLOperation:
+    """Update the onnx context with the current node of the onnx graph."""
+    if node.op_type not in OP_BY_OP_TYPE:
+        raise ValueError(f"Unknown ONNX op name {node.op_type}")
+
+    op_class = OP_BY_OP_TYPE[node.op_type]
+
+    operands = tuple(ctx.value_by_name[name] for name in node.input)
+    result_types = tuple(ctx.type_by_name[name] for name in node.output)
+
+    op = op_class.build(operands=operands, result_types=result_types)
+    results = op.results
+
+    for output_name, result in zip(node.output, results, strict=True):
+        ctx.value_by_name[output_name] = result
+
+    return op
 
 
 def visit_value_info(i: ValueInfoProto, ctx: OnnxXdslMapping) -> Attribute:
