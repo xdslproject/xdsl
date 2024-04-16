e869b8a86e43d5dfff56b147612be277058fde7a
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2024-04-16 15:28:43 +0200

    revert CI error testing

--- docs/book/src/starting.py
+++ docs/book/src/starting.py
@@ -7,5 +7,3 @@ from xdsl.irdl import IRDLOperation, irdl_op_definition
 class SomeTestOp(IRDLOperation):
     name = "some.test"
 # ANCHOR_END: test_op
-
-raise ValueError("oh no this is not working")
