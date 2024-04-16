b76a8196a03fbab59c37172f718fa183d576727c
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2024-04-16 15:09:51 +0200

    introduce CI

--- docs/book/src/starting.md
+++ docs/book/src/starting.md
@@ -1,3 +1,7 @@
 # Starting an xDSL project
 
 TODO: installation steps, environment setup
+
+```py
+{{#include starting.py:test_op}}
+```
---
+++ docs/book/src/starting.py
@@ -0,0 +1,9 @@
+from xdsl.irdl import IRDLOperation, irdl_op_definition
+
+# fmt: off
+
+# ANCHOR: test_op
+@irdl_op_definition
+class SomeTestOp(IRDLOperation):
+    name = "some.test"
+# ANCHOR_END: test_op
