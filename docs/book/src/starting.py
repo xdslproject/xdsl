5353f7fcd2b563577f764f510336446593aa0081
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2024-04-16 15:23:45 +0200

    test book breakage detection

--- docs/book/src/starting.py
+++ docs/book/src/starting.py
@@ -7,3 +7,5 @@ from xdsl.irdl import IRDLOperation, irdl_op_definition
 class SomeTestOp(IRDLOperation):
     name = "some.test"
 # ANCHOR_END: test_op
+
+raise ValueError("oh no this is not working")
