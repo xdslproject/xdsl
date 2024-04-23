5289045c0d466d5b64ff35fc434e904788bdfee5
Nick Brown <nick@mesham.com>
2024-04-23 21:24:26 +0100

    Test for floating point number parsing and printing in array values

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -718,7 +718,7 @@ def test_densearray_attr():
     """Test that a DenseArrayAttr can be parsed and then printed."""
 
     prog = """
-"func.func"() <{"sym_name" = "test", "function_type" = i64, "sym_visibility" = "private", "unit_attr"}> {"arg_attrs" = array<i1: false, true>, "other_attr" = array<i32: 19, 23, 55>} : () -> ()
+"func.func"() <{"sym_name" = "test", "function_type" = i64, "sym_visibility" = "private", "unit_attr"}> {"bool_attrs" = array<i1: false, true>, "int_attr" = array<i32: 19, 23, 55>, "float_attr" = array<f32: 0.34>} : () -> ()
     """
 
     ctx = MLContext()
