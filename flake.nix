b35517f4749da2963f1b88ceb79673075ccd13b4
Alex Rice <alexrice999@hotmail.co.uk>
2024-05-10 16:47:31 +0100

    installation: Add nix flake (#2554)
    Makes the following two changes to make building easier on nixos:
    - Adds direnv files to gitignore
    - Adds a basic flake that allows `make tests` to be run
    
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "xDSL devshell";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+          {
+            devShells.default = with pkgs; mkShell {
+              LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib";
+              buildInputs = [
+                nodejs_22
+              ];
+            };
+          }
+    );
+}
