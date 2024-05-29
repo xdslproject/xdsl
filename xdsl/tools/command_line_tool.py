8fbb3b2befb45ebfef5319252043ce5f7e9060a0
Sasha Lopoukhine <superlopuh@gmail.com>
2024-05-29 09:33:44 +0100

    rewriting: Add convert-ml-program-to-memref (#2580)
    I'm not 100% sure that this is the intended way that ml_program should
    be lowered but it seems to work for our ONNX kernels.

--- xdsl/tools/command_line_tool.py
+++ xdsl/tools/command_line_tool.py
@@ -352,10 +352,10 @@ def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
 
         return convert_linalg_to_loops.ConvertLinalgToLoopsPass
 
-    def get_stencil_tensorize_z_dimension():
-        from xdsl.transforms.experimental import stencil_tensorize_z_dimension
+    def get_convert_ml_program_to_memref():
+        from xdsl.transforms import convert_ml_program_to_memref
 
-        return stencil_tensorize_z_dimension.StencilTensorizeZDimension
+        return convert_ml_program_to_memref.ConvertMlProgramToMemrefPass
 
     def get_convert_riscv_scf_for_to_frep():
         from xdsl.transforms import convert_riscv_scf_for_to_frep
@@ -577,6 +577,11 @@ def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
 
         return replace_incompatible_fpga.ReplaceIncompatibleFPGA
 
+    def get_stencil_tensorize_z_dimension():
+        from xdsl.transforms.experimental import stencil_tensorize_z_dimension
+
+        return stencil_tensorize_z_dimension.StencilTensorizeZDimension
+
     def get_stencil_unroll():
         from xdsl.transforms import stencil_unroll
 
@@ -597,9 +602,9 @@ def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
         "convert-func-to-riscv-func": get_convert_func_to_riscv_func,
         "convert-linalg-to-memref-stream": get_convert_linalg_to_memref_stream,
         "convert-linalg-to-loops": get_convert_linalg_to_loops,
-        "stencil-tensorize-z-dimension": get_stencil_tensorize_z_dimension,
         "convert-memref-stream-to-loops": get_convert_memref_stream_to_loops,
         "convert-memref-to-riscv": get_convert_memref_to_riscv,
+        "convert-ml-program-to-memref": get_convert_ml_program_to_memref,
         "convert-onnx-to-linalg": get_convert_onnx_to_linalg,
         "convert-memref-stream-to-snitch": get_convert_memref_stream_to_snitch,
         "convert-print-format-to-riscv-debug": get_convert_print_format_to_riscv_debug,
@@ -639,6 +644,7 @@ def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
         "snitch-allocate-registers": get_snitch_register_allocation,
         "stencil-shape-inference": get_stencil_shape_inference,
         "stencil-storage-materialization": get_stencil_storage_materialization,
+        "stencil-tensorize-z-dimension": get_stencil_tensorize_z_dimension,
         "stencil-unroll": get_stencil_unroll,
         "test-lower-snitch-stream-to-asm": get_test_lower_snitch_stream_to_asm,
     }
