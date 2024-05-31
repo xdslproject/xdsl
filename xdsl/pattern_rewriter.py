450507f0ccededb955e5642e35d2fa176a2c3a7c
Sasha Lopoukhine <superlopuh@gmail.com>
2024-05-31 17:00:13 +0100

    core: add `inline_block_at_location` to `PatternRewriter` (#2664)
    Splitting out an independent change from #2660

--- xdsl/pattern_rewriter.py
+++ xdsl/pattern_rewriter.py
@@ -251,6 +251,18 @@ class PatternRewriter(PatternRewriterListener):
         self._replace_all_uses_with(arg, None, safe_erase=safe_erase)
         arg.block.erase_arg(arg, safe_erase)
 
+    def inline_block_at_location(
+        self,
+        block: Block,
+        insertion_point: InsertPoint,
+        arg_values: Sequence[SSAValue] = (),
+    ):
+        """
+        Move the block operations to the specified insertion point.
+        """
+        self.has_done_action = True
+        Rewriter.inline_block_at_location(block, insertion_point, arg_values=arg_values)
+
     def inline_block_at_end(
         self, block: Block, target_block: Block, arg_values: Sequence[SSAValue] = ()
     ):
@@ -258,8 +270,9 @@ class PatternRewriter(PatternRewriterListener):
         Move the block operations to the end of another block.
         This block should not be a parent of the block to move to.
         """
-        self.has_done_action = True
-        Rewriter.inline_block_at_end(block, target_block, arg_values=arg_values)
+        self.inline_block_at_location(
+            block, InsertPoint.at_end(target_block), arg_values=arg_values
+        )
 
     def inline_block_at_start(
         self, block: Block, target_block: Block, arg_values: Sequence[SSAValue] = ()
@@ -268,8 +281,9 @@ class PatternRewriter(PatternRewriterListener):
         Move the block operations to the start of another block.
         This block should not be a parent of the block to move to.
         """
-        self.has_done_action = True
-        Rewriter.inline_block_at_start(block, target_block, arg_values)
+        self.inline_block_at_location(
+            block, InsertPoint.at_start(target_block), arg_values=arg_values
+        )
 
     def inline_block_before_matched_op(
         self, block: Block, arg_values: Sequence[SSAValue] = ()
@@ -287,8 +301,9 @@ class PatternRewriter(PatternRewriterListener):
         Move the block operations before the given operation.
         The block should not be a parent of the operation.
         """
-        self.has_done_action = True
-        Rewriter.inline_block_before(block, op, arg_values=arg_values)
+        self.inline_block_at_location(
+            block, InsertPoint.before(op), arg_values=arg_values
+        )
 
     def inline_block_after_matched_op(
         self, block: Block, arg_values: Sequence[SSAValue] = ()
@@ -306,8 +321,9 @@ class PatternRewriter(PatternRewriterListener):
         Move the block operations after the given operation.
         The block should not be a parent of the operation.
         """
-        self.has_done_action = True
-        Rewriter.inline_block_after(block, op, arg_values=arg_values)
+        self.inline_block_at_location(
+            block, InsertPoint.after(op), arg_values=arg_values
+        )
 
     def move_region_contents_to_new_regions(self, region: Region) -> Region:
         """Move the region blocks to a new region."""
