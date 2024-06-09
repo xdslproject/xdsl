19e90098bb4794249d75c36ea442861905984c94
Emilien Bauer <bauer.emilien@gmail.com>
2024-06-09 10:04:28 +0100

    Implement range variables.

--- xdsl/dialects/stencil.py
+++ xdsl/dialects/stencil.py
@@ -1142,6 +1142,9 @@ class TensorIgnoreSizeConstraint(VarConstraint):
                 and attr.get_element_type() == other.get_element_type()
             ):
                 return
+        import pdb
+
+        pdb.set_trace()
         super().verify(attr, constraint_context)
 
 
