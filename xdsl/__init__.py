900ba4e7ceb954a8f1ec8ae7f09f49c1855edab8
Emilien Bauer <bauer.emilien@gmail.com>
2024-06-17 20:19:08 +0100

    comments

--- xdsl/__init__.py
+++ xdsl/__init__.py
@@ -16,12 +16,19 @@ import sys
 
 
 class IRDLDialectLoader(importlib.abc.Loader):
+    """
+    Custom module loader for IRDL files.
+
+    When loading an irdl file as a module:
+    - parse the dialect
+    - generate its PyRDL implementation
+    - generate type stubs for its implmentation and write to disk
+    - populate the Python module
+    """
+
     def __init__(self, module_name: str, path: str):
         self.module_name = module_name
         self.path = path
-
-    def create_module(self, spec: Any):
-        return None
 
     def exec_module(self, module: Any):
         from xdsl.context import MLContext
@@ -41,39 +48,49 @@ class IRDLDialectLoader(importlib.abc.Loader):
             dialect_op = SymbolTable.lookup_symbol(irdl_module, dialect_name)
             assert isinstance(dialect_op, DialectOp)
             dialect = make_dialect(dialect_op)
+
+            # Generate and write type stubs
             print(
                 DialectStub(dialect).dialect_stubs(),
                 file=open(f"{self.path[:-5]}.pyi", "w"),
             )
 
+            # Populate the module
             for obj in chain(dialect.attributes, dialect.operations):
                 setattr(module, obj.__name__, obj)
             setattr(module, dialect.name.capitalize(), dialect)
 
 
 class IRDLDialectFinder(importlib.abc.MetaPathFinder):
+    """
+    Custom module finder for IRDL files.
+
+    Look for a <name>.irdl file instead of a <name>.py file.
+
+    """
 
     def find_spec(self, fullname: str, path: Sequence[str] | None, target: Any = None):
-        # Check if module is already loaded
+        # Check if module is already loaded and return it if so
         if fullname in sys.modules:
             return sys.modules[fullname].__spec__
 
+        # Look for the file
         filename = fullname.split(".")[-1] + ".irdl"
         if path is None:
             path = [os.getcwd()]
-
         for entry in path:
             potential_path = os.path.join(entry, filename)
             if os.path.isfile(potential_path):
+                # If found, create the right loader and return it
                 loader = IRDLDialectLoader(fullname, potential_path)
                 return importlib.util.spec_from_file_location(
                     fullname, potential_path, loader=loader
                 )
+
+        # Return None if not found to let importlib do its thing.
         return None
 
 
-def parse_custom_file(content: str):
-    pass
-
-
+# Add the IRDLDialectFinder to the meta path as last resort, i.e, it will look for a
+# .irdl implementation if no .py implementation is found.
 sys.meta_path.append(IRDLDialectFinder())
