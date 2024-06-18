ec32ac5e293e60d9edc6f0f226d95c28c01ab739
Anton Lydike <me@AntonLydike.de>
2024-06-18 10:12:57 +0100

    backend: (csl) Slightly rework printer to promote certain values to inline expressions (#2728)
    This patch makes the printer promote certain values to inlined
    expressions (e.g. not creating a new `const x = 5;` variable for an
    `arith.constant` operation, or for arithmetic expressions.
    
    This is for now applied very carefully to a select few operations. Some
    checks are performed, such as that the value is not referenced at any
    point using the `AddressOf`operation, that the operation that produced
    the value is pure, and more.

--- xdsl/dialects/csl.py
+++ xdsl/dialects/csl.py
@@ -69,6 +69,7 @@ from xdsl.traits import (
     HasParent,
     IsolatedFromAbove,
     IsTerminator,
+    NoMemoryEffect,
     NoTerminator,
     OpTrait,
     SymbolOpInterface,
@@ -388,6 +389,8 @@ class ImportModuleConstOp(IRDLOperation):
 class ConstStructOp(IRDLOperation):
     name = "csl.const_struct"
 
+    traits = frozenset([NoMemoryEffect()])
+
     items = opt_prop_def(DictionaryAttr)
     ssa_fields = opt_prop_def(ArrayAttr[StringAttr])
     ssa_values = var_operand_def()
@@ -422,6 +425,8 @@ class ConstStructOp(IRDLOperation):
 class GetColorOp(IRDLOperation):
     name = "csl.get_color"
 
+    traits = frozenset([NoMemoryEffect()])
+
     id = operand_def(IntegerType)
     res = result_def(ColorType)
 
@@ -436,6 +441,8 @@ class MemberAccessOp(IRDLOperation):
     """
 
     name = "csl.member_access"
+
+    traits = frozenset([NoMemoryEffect()])
 
     struct = operand_def(StructLike)
 
@@ -1444,6 +1451,8 @@ class AddressOfOp(IRDLOperation):
     value = operand_def()
     res = result_def(PtrType)
 
+    traits = frozenset([NoMemoryEffect()])
+
     def _verify_memref_addr(self, val_ty: MemRefType[Attribute], res_ty: PtrType):
         """
         Verify that if the address of a memref is taken, the resulting pointer is either:
@@ -1546,6 +1555,8 @@ class SignednessCastOp(IRDLOperation):
     """
     Cast that throws away signedness attributes
     """
+
+    traits = frozenset([NoMemoryEffect()])
 
     name = "csl.mlir.signedness_cast"
 
