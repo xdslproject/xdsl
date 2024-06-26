2ab91f526baf4a0fd53888cac7d6f8338e92a10e
Sasha Lopoukhine <superlopuh@gmail.com>
2024-06-26 11:09:18 +0100

    dependencies: (wasm) Add wasmtime as an optional dependency

--- tests/dialects/wasm/test_wat_encoding.py
+++ tests/dialects/wasm/test_wat_encoding.py
@@ -1,3 +1,5 @@
+import pytest
+
 from xdsl.dialects import wasm
 
 
@@ -5,3 +7,14 @@ def test_empty_module():
     module = wasm.WasmModule()
 
     assert module.wat() == "(module)"
+
+
+pytest.importorskip("wasmtime", reason="wasmtime is an optional dependency")
+
+from wasmtime import wat2wasm  # noqa: E402
+
+
+def test_wasmtime_coherency():
+    empty = wasm.WasmModule()
+
+    assert wat2wasm(empty.wat()) == empty.wasm()
