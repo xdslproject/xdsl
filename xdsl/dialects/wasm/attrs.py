a59fe03f79d0117039ff0f8ec1d1324f3917f76d
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2024-06-28 12:08:10 +0200

    refactor wasm dialect for new file layout

---
+++ xdsl/dialects/wasm/attrs.py
@@ -0,0 +1,274 @@
+"""
+This file contains the definition of the attributes of the WebAssembly dialect.
+
+The paragraphs prefixed by `wasm>` in the documentation of this dialect are
+excerpts from the WebAssembly Specification, which is licensed under the terms
+described in the WASM-SPEC-LICENSE file.
+"""
+
+from enum import auto
+from typing import Annotated
+
+from xdsl.dialects.builtin import (
+    ArrayAttr,
+    IntegerAttr,
+    IntegerType,
+    StringAttr,
+    f32,
+    f64,
+    i32,
+    i64,
+)
+from xdsl.ir import (
+    EnumAttribute,
+    ParametrizedAttribute,
+    SpacedOpaqueSyntaxAttribute,
+)
+from xdsl.irdl import (
+    ParameterDef,
+    irdl_attr_definition,
+)
+from xdsl.utils.str_enum import StrEnum
+
+i128 = IntegerType(128)
+
+I32Type = Annotated[IntegerType, i32]
+I64Type = Annotated[IntegerType, i64]
+I128Type = Annotated[IntegerType, i128]
+F32Type = Annotated[IntegerType, f32]
+F64Type = Annotated[IntegerType, f64]
+
+I32Attr = IntegerAttr[I32Type]
+
+TypeIdx = I32Attr
+FuncIdx = I32Attr
+TableIdx = I32Attr
+MemIdx = I32Attr
+GlobalIdx = I32Attr
+ElemIdx = I32Attr
+DataIdx = I32Attr
+LocalIdx = I32Attr
+LabelIdx = I32Attr
+
+##==------------------------------------------------------------------------==##
+# WebAssembly value types
+##==------------------------------------------------------------------------==##
+
+
+class WasmRefTypeEnum(StrEnum):
+    FuncRef = auto()
+    """
+    wasm> The type funcref denotes the infinite union of all references to
+    functions, regardless of their function types.
+    """
+
+    ExternRef = auto()
+    """
+    wasm> The type externref denotes the infinite union of all references to
+    objects owned. by the embedder and that can be passed into WebAssembly
+    under this type.
+    """
+
+
+class WasmRefType(EnumAttribute[WasmRefTypeEnum], SpacedOpaqueSyntaxAttribute):
+    name = "wasm.reftype"
+
+
+WasmNumType = I32Type | I64Type | F32Type | F64Type
+WasmVecType = I128Type
+WasmValueType = WasmNumType | WasmVecType | WasmRefType
+
+
+@irdl_attr_definition
+class WasmFuncType(ParametrizedAttribute):
+    """
+    wasm> Function types classify the signature of functions, mapping a vector
+    of parameters to a vector of results. They are also used to classify the
+    inputs and outputs of instructions.
+    """
+
+    name = "wasm.functype"
+
+    args: ParameterDef[ArrayAttr[WasmValueType]]
+    res: ParameterDef[ArrayAttr[WasmValueType]]
+
+
+##==------------------------------------------------------------------------==##
+# WebAssembly tables and memories
+##==------------------------------------------------------------------------==##
+
+
+@irdl_attr_definition
+class WasmLimits(ParametrizedAttribute):
+    """
+    wasm> Limits classify the size range of resizable storage associated with
+    memory types and table types. If no maximum is given, the respective storage
+    can grow to any size.
+    """
+
+    name = "wasm.limits"
+
+    min: ParameterDef[I32Attr]
+    max: ParameterDef[I32Attr] | None
+
+
+@irdl_attr_definition
+class WasmMemoryType(ParametrizedAttribute):
+    """
+    wasm> Memory types classify linear memories and their size range. The limits
+    constrain the minimum and optionally the maximum size of a memory. The limits
+    are given in units of page size.
+    """
+
+    name = "wasm.mem"
+
+    limits: ParameterDef[WasmLimits]
+
+
+@irdl_attr_definition
+class WasmTableType(ParametrizedAttribute):
+    """
+    wasm> Table types classify tables over elements of reference type within a
+    size range. Like memories, tables are constrained by limits for their
+    minimum and optionally maximum size. The limits are given in numbers of
+    entries.
+    """
+
+    name = "wasm.table"
+
+    elements: ParameterDef[WasmRefType]
+    limits: ParameterDef[WasmLimits]
+
+
+class WasmMutEnum(StrEnum):
+    Const = auto()
+    Var = auto()
+
+
+class WasmMut(EnumAttribute[WasmMutEnum], SpacedOpaqueSyntaxAttribute):
+    name = "wasm.mut"
+
+
+@irdl_attr_definition
+class WasmGlobalType(ParametrizedAttribute):
+    """
+    wasm> Global types classify global variables, which hold a value and can
+    either be mutable or immutable.
+    """
+
+    name = "wasm.globaltype"
+
+    mutability: ParameterDef[WasmMut]
+    type: ParameterDef[WasmValueType]
+
+
+##==------------------------------------------------------------------------==##
+# WebAssembly exports
+##==------------------------------------------------------------------------==##
+
+
+@irdl_attr_definition
+class WasmExportDescFunc(ParametrizedAttribute):
+    name = "wasm.export_desc_func"
+    id: ParameterDef[FuncIdx]
+
+
+@irdl_attr_definition
+class WasmExportDescTable(ParametrizedAttribute):
+    name = "wasm.export_desc_table"
+    id: ParameterDef[TableIdx]
+
+
+@irdl_attr_definition
+class WasmExportDescMem(ParametrizedAttribute):
+    name = "wasm.export_desc_mem"
+    id: ParameterDef[MemIdx]
+
+
+@irdl_attr_definition
+class WasmExportDescGlobal(ParametrizedAttribute):
+    name = "wasm.export_desc_global"
+    id: ParameterDef[GlobalIdx]
+
+
+WasmExportDesc = (
+    WasmExportDescFunc | WasmExportDescTable | WasmExportDescMem | WasmExportDescGlobal
+)
+
+
+@irdl_attr_definition
+class WasmExport(ParametrizedAttribute):
+    """
+    wasm> The exports component of a module defines a set of exports that become
+    accessible to the host environment once the module has been instantiated.
+    Each export is labeled by a unique name. Exportable definitions are
+    functions, tables, memories, and globals, which are referenced through a
+    respective descriptor.
+    """
+
+    name = "wasm.export"
+
+    export_name: ParameterDef[StringAttr]
+    desc: ParameterDef[WasmExportDesc]
+
+
+##==------------------------------------------------------------------------==##
+# WebAssembly imports
+##==------------------------------------------------------------------------==##
+
+
+@irdl_attr_definition
+class WasmImportDescFunc(ParametrizedAttribute):
+    name = "wasm.import_desc_func"
+    id: ParameterDef[FuncIdx]
+
+
+@irdl_attr_definition
+class WasmImportDescTable(ParametrizedAttribute):
+    name = "wasm.import_desc_table"
+    id: ParameterDef[TableIdx]
+
+
+@irdl_attr_definition
+class WasmImportDescMem(ParametrizedAttribute):
+    name = "wasm.import_desc_mem"
+    id: ParameterDef[MemIdx]
+
+
+@irdl_attr_definition
+class WasmImportDescGlobal(ParametrizedAttribute):
+    name = "wasm.import_desc_global"
+    id: ParameterDef[GlobalIdx]
+
+
+WasmImportDesc = (
+    WasmImportDescFunc | WasmImportDescTable | WasmImportDescMem | WasmImportDescGlobal
+)
+
+
+@irdl_attr_definition
+class WasmImport(ParametrizedAttribute):
+    """
+    wasm> The component of a module defines a set of imports that are required
+    for instantiation. Each import is labeled by a two-level name space,
+    consisting of a module name and a name for an entity within that module.
+    Importable definitions are functions, tables, memories, and globals. Each
+    import is specified by a descriptor with a respective type that a definition
+    provided during instantiation is required to match. Every import defines an
+    index in the respective index space. In each index space, the indices of
+    imports go before the first index of any definition contained in the module
+    itself.
+
+    wasm> Unlike export names, import names are not necessarily unique. It is
+    possible to import the same module/name pair multiple times; such imports
+    may even have different type descriptions, including different kinds of
+    entities. A module with such imports can still be instantiated depending
+    on the specifics of how an embedder allows resolving and supplying imports.
+    However, embedders are not required to support such overloading, and a
+    WebAssembly module itself cannot implement an overloaded name.
+    """
+
+    name = "wasm.import"
+
+    import_name: ParameterDef[StringAttr]
+    desc: ParameterDef[WasmImportDesc]
--- xdsl/dialects/wasm/ops.py
+++ xdsl/dialects/wasm/ops.py
@@ -6,7 +6,7 @@ WebAssembly Specification 2.0.
 
 The paragraphs prefixed by `wasm>` in the documentation of this dialect are
 excerpts from the WebAssembly Specification, which is licensed under the terms
-at the bottom of this file.
+described in the WASM-SPEC-LICENSE file.
 """
 
 from io import BytesIO, StringIO
@@ -90,61 +90,3 @@ class WasmModule(IRDLOperation, WasmBinaryEncodable, WatPrintable):
         self.print_wat(printer)
         res = io.getvalue()
         return res
-
-
-"""
---- Licensing Terms for the WebAssembly Specification
---- Only applies to the parts of the documentation specified in the header
---- of this file.
-
-WebAssembly Specification: https://webassembly.github.io/spec/core/index.html
-
-    Copyright © 2024 World Wide Web Consortium.
-    All Rights Reserved. This work is distributed under the
-    W3C® Software and Document License [1] in the hope that it
-    will be useful, but WITHOUT ANY WARRANTY; without even the
-    implied warranty of MERCHANTABILITY or FITNESS FOR A
-    PARTICULAR PURPOSE.
-
-    [1] https://www.w3.org/Consortium/Legal/copyright-software
-
-
-This work is being provided by the copyright holders under the following
-license.
-
-License
-
-By obtaining and/or copying this work, you (the licensee) agree that you
-have read, understood, and will comply with the following terms and conditions.
-
-Permission to copy, modify, and distribute this work, with or without
-modification, for any purpose and without fee or royalty is hereby granted,
-provided that you include the following on ALL copies of the work or portions
-thereof, including modifications:
-
-    - The full text of this NOTICE in a location viewable to users of the
-      redistributed or derivative work.
-    - Any pre-existing intellectual property disclaimers, notices, or terms and
-      conditions. If none exist, the W3C software and document short notice
-      should be included.
-    - Notice of any changes or modifications, through a copyright statement on
-      the new code or document such as "This software or document includes
-      material copied from or derived from [title and URI of the W3C document].
-      Copyright © [$year-of-document] World Wide Web Consortium.
-      https://www.w3.org/copyright/software-license-2023/"
-
-Disclaimers
-
-THIS WORK IS PROVIDED "AS IS," AND COPYRIGHT HOLDERS MAKE NO REPRESENTATIONS
-OR WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO, WARRANTIES OF
-MERCHANTABILITY OR FITNESS FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF THE
-SOFTWARE OR DOCUMENT WILL NOT INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS,
-TRADEMARKS OR OTHER RIGHTS.
-
-COPYRIGHT HOLDERS WILL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL OR
-CONSEQUENTIAL DAMAGES ARISING OUT OF ANY USE OF THE SOFTWARE OR DOCUMENT.
-
-The name and trademarks of copyright holders may NOT be used in advertising or
-publicity pertaining to the work without specific, written prior permission.
-Title to copyright in this work will at all times remain with copyright holders.
-"""
