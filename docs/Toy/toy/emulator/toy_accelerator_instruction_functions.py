620d7fb6b4052e0217dab352b11cd632ac4519ab
Sasha Lopoukhine <superlopuh@gmail.com>
2024-07-08 09:49:44 +0100

    docs: (toy) remove all custom instructions from Toy (#2863)
    I added a bunch of hacks to Toy to get it to work, but we now have all
    the functionality we need to remove the custom behaviour.

--- docs/Toy/toy/emulator/toy_accelerator_instruction_functions.py
+++ docs/Toy/toy/emulator/toy_accelerator_instruction_functions.py
@@ -1,20 +1,12 @@
 from typing import Any
 
 from xdsl.dialects import riscv
-from xdsl.interpreter import Interpreter, PythonValues, impl, register_impls
-from xdsl.interpreters.ptr import RawPtr
+from xdsl.interpreter import Interpreter, impl, register_impls
 from xdsl.interpreters.riscv import RiscvFunctions
 
 
 @register_impls
 class ToyAcceleratorInstructionFunctions(RiscvFunctions):
-    def __init__(self):
-        super().__init__(
-            custom_instructions={
-                "buffer.alloc": accelerator_buffer_alloc,
-                "buffer.copy": accelerator_buffer_copy,
-            },
-        )
 
     @impl(riscv.EcallOp)
     def run_ecall(
@@ -25,21 +17,3 @@ class ToyAcceleratorInstructionFunctions(RiscvFunctions):
     ):
         # In Toy, ecall is always exit
         return ()
-
-
-def accelerator_buffer_copy(
-    interpreter: Interpreter, op: riscv.CustomAssemblyInstructionOp, args: PythonValues
-) -> PythonValues:
-    size, dest_buffer, source_buffer = args
-
-    for i in range(size):
-        dest_buffer[i] = source_buffer[i]
-
-    return ()
-
-
-def accelerator_buffer_alloc(
-    interpreter: Interpreter, op: riscv.CustomAssemblyInstructionOp, args: PythonValues
-) -> PythonValues:
-    (size,) = args
-    return (RawPtr.zeros(size * 8),)
