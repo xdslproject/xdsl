9c767b2e8bf210ccddb2cc95b65ae4a08e2e00bf
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2024-07-15 03:43:13 +0200

    Add test for preallocated regs on 2-address ops

--- tests/backend/riscv/test_register_allocation.py
+++ tests/backend/riscv/test_register_allocation.py
@@ -1,5 +1,7 @@
 from typing import Annotated, TypeAlias
 
+import pytest
+
 from xdsl.backend.riscv.register_allocation import RegisterAllocatorLivenessBlockNaive
 from xdsl.builder import Builder
 from xdsl.dialects import builtin, riscv_func, riscv
@@ -16,32 +18,33 @@ from xdsl.irdl import (
     result_def,
 )
 from xdsl.irdl.operations import irdl_op_definition
+from xdsl.utils.exceptions import VerifyException
+
+
+# We use the infamous post-increment load instruction
+# provided by several architectures (ARM, PULP, ...) that presents
+# the classical form of a 2-address instruction: the first operand
+# is incremented and in a brand new SSA value as the second result.
+# Both SSA values must be allocated on the same register, otherwise the
+# generated code is broken.
+@irdl_op_definition
+class PostIncrementLoad(RISCVInstruction):
+    name = "postload"
+
+    SameIntRegisterType: TypeAlias = Annotated[IntRegisterType, ConstraintVar("T")]
+
+    rd: OpResult = result_def(IntRegisterType)  # loaded value
+    off: OpResult = result_def(
+        SameIntRegisterType
+    )  # new value of the incremented offset
+    rs1: Operand = operand_def(SameIntRegisterType)  # offset
+    rs2: Operand = operand_def(IntRegisterType)  # base adddress
+
+    def assembly_line_args(self) -> tuple[AssemblyInstructionArg, ...]:
+        return self.rd, self.rs1, self.rs2
 
 
 def test_allocate_2address():
-    # We use the infamous post-increment load instruction
-    # provided by several architectures (ARM, PULP, ...) that presents
-    # the classical form of a 2-address instruction: the first operand
-    # is incremented and in a brand new SSA value as the second result.
-    # Both SSA values must be allocated on the same register, otherwise the
-    # generated code is broken.
-
-    @irdl_op_definition
-    class PostIncrementLoad(RISCVInstruction):
-        name = "postload"
-
-        SameIntRegisterType: TypeAlias = Annotated[IntRegisterType, ConstraintVar("T")]
-
-        rd: OpResult = result_def(IntRegisterType)  # loaded value
-        off: OpResult = result_def(
-            SameIntRegisterType
-        )  # new value of the incremented offset
-        rs1: Operand = operand_def(SameIntRegisterType)  # offset
-        rs2: Operand = operand_def(IntRegisterType)  # base adddress
-
-        def assembly_line_args(self) -> tuple[AssemblyInstructionArg, ...]:
-            return self.rd, self.rs1, self.rs2
-
     # riscv_func.func @function() {
     #     %0 = riscv.li 1 : !riscv.reg<t1>
     #     %1 = riscv.li 2 : !riscv.reg<t2>
@@ -77,6 +80,41 @@ def test_allocate_2address():
     body = list(func.body.block.ops)
     # load.rs1 == load.off
     assert body[2].operands[0].type == body[2].results[1].type
+    # load.rs1 != load.rd
+    assert body[2].operands[0].type != body[2].results[0].type
+    # load.rs1 != load.rs2
+    assert body[2].operands[0].type != body[2].operands[1].type
+
+
+def test_allocate_2address_preallocated():
+    @Builder.implicit_region
+    def region():
+        base = riscv.LiOp(1)
+        offset = riscv.LiOp(2)
+        load = PostIncrementLoad(
+            operands=[base.rd, offset.rd],
+            result_types=[
+                IntRegisterType.unallocated(),
+                riscv.Registers.A7,
+            ],
+        )
+        builtin.UnrealizedConversionCastOp.get((load.rd,), (builtin.i32,))
+        builtin.UnrealizedConversionCastOp.get((load.off,), (builtin.i32,))
+        riscv_func.ReturnOp()
+
+    func = riscv_func.FuncOp("function", region, ((), ()))
+    with pytest.raises(VerifyException):
+        func.verify()
+
+    allocator = RegisterAllocatorLivenessBlockNaive()
+    allocator.exclude_preallocated = True
+    allocator.allocate_func(func)
+    func.verify()
+
+    body = list(func.body.block.ops)
+    # load.rs1 == load.off
+    assert body[2].operands[0].type == riscv.Registers.A7
+    assert body[2].results[1].type == riscv.Registers.A7
     # load.rs1 != load.rd
     assert body[2].operands[0].type != body[2].results[0].type
     # load.rs1 != load.rs2
