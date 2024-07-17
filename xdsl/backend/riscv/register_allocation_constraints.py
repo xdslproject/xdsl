2126dae56b578622d442e242514b2e3c6d02df00
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2024-07-17 15:24:57 +0200

    Cleanup gathering of constraint vars

--- xdsl/backend/riscv/register_allocation_constraints.py
+++ xdsl/backend/riscv/register_allocation_constraints.py
@@ -49,26 +49,65 @@ class RegisterAllocationConstraints(abc.ABC):
 
 
 @dataclass(frozen=True)
-class OpOperand:
+class OpOperandIdx:
     idx: int
 
 
 @dataclass(frozen=True)
-class OpResult:
+class OpResultIdx:
     idx: int
 
 
 @dataclass
 class Tie:
-    elements: frozenset[OpResult | OpOperand]
+    elements: frozenset[OpResultIdx | OpOperandIdx]
     register: RegisterType | None
 
 
 class OpTieConstraints(RegisterAllocationConstraints):
 
-    ConstraintsDict: TypeAlias = dict[OpOperand | OpResult, Tie]
+    ConstraintsDict: TypeAlias = dict[OpOperandIdx | OpResultIdx, Tie]
 
     _constraints: ConstraintsDict
+
+    @staticmethod
+    def _gather_constraint_vars(
+        vars: dict[str, list[OpOperandIdx | OpResultIdx]],
+        defs: Sequence[tuple[str, OperandDef | ResultDef]],
+    ) -> None:
+        for idx, (_, reg_def) in enumerate(defs):
+            constr = reg_def.constr
+            if isinstance(constr, RangeOf | SingleOf) and isinstance(
+                constr.constr, VarConstraint
+            ):
+                # We use the VarConstraint name as a dict key just like
+                # ConstraintContext
+                var_name: str = constr.constr.name
+                if isinstance(reg_def, OperandDef):
+                    tag = OpOperandIdx(idx)
+                else:
+                    assert isinstance(reg_def, ResultDef)
+                    tag = OpResultIdx(idx)
+                vars.setdefault(var_name, []).append(tag)
+
+    @staticmethod
+    def _gather_preallocated(constr: OpTieConstraints, op: IRDLOperation):
+        for idx, arg in enumerate(op.operands):
+            if not (
+                isinstance(arg.type, RegisterType)
+                and arg.type.is_allocated
+                and constr.operand_has_constraints(idx)
+            ):
+                continue
+            constr.operand_satisfy_constraint(idx, arg.type)
+        for idx, res in enumerate(op.results):
+            if not (
+                isinstance(res.type, RegisterType)
+                and res.type.is_allocated
+                and constr.result_has_constraints(idx)
+            ):
+                continue
+            constr.result_satisfy_constraint(idx, res.type)
 
     @staticmethod
     def from_op(op: IRDLOperation) -> OpTieConstraints:
@@ -81,49 +120,20 @@ class OpTieConstraints(RegisterAllocationConstraints):
             Sequence[tuple[str, OperandDef]],
             get_construct_defs(op_def, VarIRConstruct.OPERAND),
         )
-        var_constraints: dict[str, list[OpOperand | OpResult]] = {}
-        for idx, (_, reg_def) in enumerate(arg_defs):
-            constr = reg_def.constr
-            if isinstance(constr, RangeOf | SingleOf) and isinstance(
-                constr.constr, VarConstraint
-            ):
-                # We use the VarConstraint name as a dict key just like
-                # ConstraintContext
-                var_name: str = constr.constr.name
-                var_constraints.setdefault(var_name, []).append(OpOperand(idx))
-        for idx, (_, reg_def) in enumerate(result_defs):
-            constr = reg_def.constr
-            if isinstance(constr, RangeOf | SingleOf) and isinstance(
-                constr.constr, VarConstraint
-            ):
-                # We use the VarConstraint name as a dict key just like
-                # ConstraintContext
-                var_name: str = constr.constr.name
-                var_constraints.setdefault(var_name, []).append(OpResult(idx))
+        # Gather all ConstraintVars associated to operands/results
+        constraint_vars: dict[str, list[OpOperandIdx | OpResultIdx]] = {}
+        OpTieConstraints._gather_constraint_vars(constraint_vars, result_defs)
+        OpTieConstraints._gather_constraint_vars(constraint_vars, arg_defs)
         tie_constraints = [
             Tie(elements=frozenset(value), register=None)
-            for value in var_constraints.values()
+            for value in constraint_vars.values()
         ]
+        # Init the constraints dict
         ret = OpTieConstraints(
             {idx: tie for tie in tie_constraints for idx in tie.elements}
         )
         # Take into account any pre-allocated result/operand
-        for idx, arg in enumerate(op.operands):
-            if not (
-                isinstance(arg.type, RegisterType)
-                and arg.type.is_allocated
-                and ret.operand_has_constraints(idx)
-            ):
-                continue
-            ret.operand_satisfy_constraint(idx, arg.type)
-        for idx, res in enumerate(op.results):
-            if not (
-                isinstance(res.type, RegisterType)
-                and res.type.is_allocated
-                and ret.result_has_constraints(idx)
-            ):
-                continue
-            ret.result_satisfy_constraint(idx, res.type)
+        OpTieConstraints._gather_preallocated(ret, op)
         return ret
 
     def __init__(self, constr: ConstraintsDict | None = None) -> None:
@@ -133,16 +143,18 @@ class OpTieConstraints(RegisterAllocationConstraints):
             self._constraints = {}
         super().__init__()
 
-    def _has_constraints(self, element: OpOperand | OpResult) -> bool:
+    def _has_constraints(self, element: OpOperandIdx | OpResultIdx) -> bool:
         return element in self._constraints
 
-    def _is_constrained_to(self, element: OpOperand | OpResult) -> RegisterType | None:
+    def _is_constrained_to(
+        self, element: OpOperandIdx | OpResultIdx
+    ) -> RegisterType | None:
         if element not in self._constraints:
             return None
         return self._constraints[element].register
 
     def _satisfy_constraint(
-        self, element: OpOperand | OpResult, reg: RegisterType
+        self, element: OpOperandIdx | OpResultIdx, reg: RegisterType
     ) -> None:
         if element not in self._constraints:
             raise KeyError()
@@ -152,19 +164,19 @@ class OpTieConstraints(RegisterAllocationConstraints):
         self._constraints[element].register = reg
 
     def operand_has_constraints(self, idx: int) -> bool:
-        return self._has_constraints(OpOperand(idx))
+        return self._has_constraints(OpOperandIdx(idx))
 
     def operand_is_constrained_to(self, idx: int) -> RegisterType | None:
-        return self._is_constrained_to(OpOperand(idx))
+        return self._is_constrained_to(OpOperandIdx(idx))
 
     def operand_satisfy_constraint(self, idx: int, reg: RegisterType) -> None:
-        self._satisfy_constraint(OpOperand(idx), reg)
+        self._satisfy_constraint(OpOperandIdx(idx), reg)
 
     def result_has_constraints(self, idx: int) -> bool:
-        return self._has_constraints(OpResult(idx))
+        return self._has_constraints(OpResultIdx(idx))
 
     def result_is_constrained_to(self, idx: int) -> RegisterType | None:
-        return self._is_constrained_to(OpResult(idx))
+        return self._is_constrained_to(OpResultIdx(idx))
 
     def result_satisfy_constraint(self, idx: int, reg: RegisterType) -> None:
-        self._satisfy_constraint(OpResult(idx), reg)
+        self._satisfy_constraint(OpResultIdx(idx), reg)
