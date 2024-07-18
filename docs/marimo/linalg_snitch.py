70e83695a2fa4e92233c13d61fd012a92be63f13
Sasha Lopoukhine <superlopuh@gmail.com>
2024-07-18 10:00:11 +0100

    move around xdsl imports and repeat sliders

--- docs/marimo/linalg_snitch.py
+++ docs/marimo/linalg_snitch.py
@@ -25,23 +25,94 @@ def __(mo):
 
 @app.cell
 def __():
+    # Import all the necessary functionality from xDSL for this notebook
+    # If you see an error about xdsl not being defined run this cell manually
+
+    from xdsl.backend.riscv.lowering import (
+        convert_arith_to_riscv,
+        convert_func_to_riscv_func,
+        convert_memref_to_riscv,
+        convert_scf_to_riscv_scf,
+    )
+    from xdsl.backend.riscv.lowering.convert_riscv_scf_to_riscv_cf import (
+        ConvertRiscvScfToRiscvCfPass,
+    )
+    from xdsl.backend.riscv.lowering.convert_snitch_stream_to_snitch import (
+        ConvertSnitchStreamToSnitch,
+    )
     from xdsl.builder import ImplicitBuilder
+    from xdsl.context import MLContext
     from xdsl.dialects import arith, func, linalg, llvm
     from xdsl.dialects.builtin import AffineMap, AffineMapAttr, MemRefType, ModuleOp, f64
-    from xdsl.ir import Block, Region
+    from xdsl.dialects.riscv import riscv_code
+    from xdsl.ir import Attribute, Block, Region, SSAValue
+    from xdsl.passes import PipelinePass
+    from xdsl.tools.command_line_tool import get_all_dialects
+    from xdsl.transforms import (
+        arith_add_fastmath,
+        convert_linalg_to_loops,
+        convert_linalg_to_memref_stream,
+        convert_memref_stream_to_loops,
+        convert_memref_stream_to_snitch_stream,
+        convert_riscv_scf_for_to_frep,
+        dead_code_elimination,
+        loop_hoist_memref,
+        lower_affine,
+        memref_streamify,
+        reconcile_unrealized_casts,
+        test_lower_snitch_stream_to_asm,
+    )
+    from xdsl.transforms.canonicalize import CanonicalizePass
+    from xdsl.transforms.lower_snitch import LowerSnitchPass
+    from xdsl.transforms.mlir_opt import MLIROptPass
+    from xdsl.transforms.riscv_register_allocation import RISCVRegisterAllocation
+    from xdsl.transforms.riscv_scf_loop_range_folding import (
+        RiscvScfLoopRangeFoldingPass,
+    )
+    from xdsl.transforms.snitch_register_allocation import SnitchRegisterAllocation
     return (
         AffineMap,
         AffineMapAttr,
+        Attribute,
         Block,
+        CanonicalizePass,
+        ConvertRiscvScfToRiscvCfPass,
+        ConvertSnitchStreamToSnitch,
         ImplicitBuilder,
+        LowerSnitchPass,
+        MLContext,
+        MLIROptPass,
         MemRefType,
         ModuleOp,
+        PipelinePass,
+        RISCVRegisterAllocation,
         Region,
+        RiscvScfLoopRangeFoldingPass,
+        SSAValue,
+        SnitchRegisterAllocation,
         arith,
+        arith_add_fastmath,
+        convert_arith_to_riscv,
+        convert_func_to_riscv_func,
+        convert_linalg_to_loops,
+        convert_linalg_to_memref_stream,
+        convert_memref_stream_to_loops,
+        convert_memref_stream_to_snitch_stream,
+        convert_memref_to_riscv,
+        convert_riscv_scf_for_to_frep,
+        convert_scf_to_riscv_scf,
+        dead_code_elimination,
         f64,
         func,
+        get_all_dialects,
         linalg,
         llvm,
+        loop_hoist_memref,
+        lower_affine,
+        memref_streamify,
+        reconcile_unrealized_casts,
+        riscv_code,
+        test_lower_snitch_stream_to_asm,
     )
 
 
@@ -63,6 +134,7 @@ def __(
     html,
     linalg,
     llvm,
+    mo,
 ):
     a_type = MemRefType(f64, a_shape)
     b_type = MemRefType(f64, b_shape)
@@ -104,7 +176,7 @@ def __(
 
     linalg_module = ModuleOp((kernel_op,))
 
-    html(linalg_module)
+    mo.md(html(linalg_module))
     return (
         a,
         a_type,
@@ -130,19 +202,23 @@ def __(mo):
     m = mo.ui.slider(min_val, max_val, value=2, label="M")
     n = mo.ui.slider(min_val, max_val, value=2, label="N")
     k = mo.ui.slider(min_val, max_val, value=2, label="K")
+    return k, m, max_val, min_val, n
 
+
+@app.cell
+def __(k, m, max_val, min_val, mo, n):
     mo.md(
         f"""
         For example, here are some sliders, which can take on values from {min_val} to {max_val}.
 
-        {m}
+        {m}{m.value}
 
-        {n}
+        {n}{n.value}
 
-        {k}
+        {k}{k.value}
         """
     )
-    return k, m, max_val, min_val, n
+    return
 
 
 @app.cell
@@ -163,12 +239,6 @@ def __(k, m, mo, n):
         """
     )
     return a_shape, b_shape, c_shape
-
-
-@app.cell
-def __(linalg_module):
-    linalg_module
-    return
 
 
 @app.cell
@@ -376,6 +446,22 @@ def __(
 
 
 @app.cell
+def __(k, m, mo, n):
+    mo.md(
+        f"""
+        We can see how changing our input sizes affects the assembly produced:
+
+        {m}{m.value}
+
+        {n}{n.value}
+
+        {k}{k.value}
+        """
+    )
+    return
+
+
+@app.cell
 def __(mo, riscv_code, snitch_asm_module):
     snitch_asm = riscv_code(snitch_asm_module)
 
@@ -387,296 +473,6 @@ def __(mo, riscv_code, snitch_asm_module):
         }
     )
     return snitch_asm,
-
-
-@app.cell(hide_code=True)
-def __(mo):
-    mo.md(
-        """
-        ### Interpreting the assembly using xDSL
-
-        One of the useful features of xDSL is its interpreter. Here we've implemented all the necessary functions to interpret the code at a low level, to check that our compilation is correct. Here's the slider modifying the shape variable defined above, we can slide it to see the result of the code compiled with different input shapes, and interpreted at the RISC-V level.
-        """
-    )
-    return
-
-
-@app.cell
-def __(a_shape, b_shape, c_shape):
-    from math import prod as product
-
-    a_len = product(a_shape)
-    b_len = product(b_shape)
-    c_len = product(c_shape)
-    return a_len, b_len, c_len, product
-
-
-@app.cell
-def __(
-    Interpreter,
-    OpCounter,
-    TypedPtr,
-    a_len,
-    a_shape,
-    b_len,
-    b_shape,
-    c_len,
-    c_shape,
-    ctx,
-    register_implementations,
-    riscv_module,
-):
-    from xdsl.interpreters.shaped_array import ShapedArray
-
-    a_ptr = TypedPtr.new_float64([i + 1 for i in range(a_len)]).raw
-    b_ptr = TypedPtr.new_float64([(i + 1) / 100 for i in range(b_len)]).raw
-    c_ptr = TypedPtr.new_float64([0.0 for i in range(c_len)]).raw
-
-    a_shaped = ShapedArray(a_ptr.float64, a_shape)
-    b_shaped = ShapedArray(b_ptr.float64, b_shape)
-    c_shaped = ShapedArray(c_ptr.float64, c_shape)
-
-    riscv_op_counter = OpCounter()
-    riscv_interpreter = Interpreter(riscv_module, listener=riscv_op_counter)
-
-    register_implementations(riscv_interpreter, ctx, include_wgpu=False)
-
-    _ = riscv_interpreter.call_op("matmul", (a_ptr, b_ptr, c_ptr))
-    return (
-        ShapedArray,
-        a_ptr,
-        a_shaped,
-        b_ptr,
-        b_shaped,
-        c_ptr,
-        c_shaped,
-        riscv_interpreter,
-        riscv_op_counter,
-    )
-
-
-@app.cell
-def __(a_shaped, b_shaped, c_shaped, mo):
-    mo.md(
-        f"""
-        ```
-        A:    {a_shaped}
-        B:    {b_shaped}
-        C:    {c_shaped}
-        ```
-        """
-    )
-    return
-
-
-@app.cell
-def __(
-    Interpreter,
-    OpCounter,
-    ShapedArray,
-    TypedPtr,
-    a_ptr,
-    b_ptr,
-    c_len,
-    c_shape,
-    ctx,
-    register_implementations,
-    snitch_stream_module,
-):
-    snitch_op_counter = OpCounter()
-    snitch_interpreter = Interpreter(snitch_stream_module, listener=snitch_op_counter)
-
-    register_implementations(snitch_interpreter, ctx, include_wgpu=False)
-
-    snitch_c_ptr = TypedPtr.new_float64([0.0 for i in range(c_len)]).raw
-    snitch_c_shaped = ShapedArray(snitch_c_ptr.float64, c_shape)
-
-    _ = snitch_interpreter.call_op("matmul", (a_ptr, b_ptr, snitch_c_ptr))
-    return (
-        snitch_c_ptr,
-        snitch_c_shaped,
-        snitch_interpreter,
-        snitch_op_counter,
-    )
-
-
-@app.cell
-def __(mo, snitch_c_shaped):
-    mo.md(
-        f"""
-        We can also interpret the Snitch version of the code to compare the results:
-
-        ```
-        Snitch result: {snitch_c_shaped}
-        ```
-        """
-    )
-    return
-
-
-@app.cell
-def __(k, m, mo, n, riscv_op_counter, snitch_op_counter):
-    rv_dict = dict(riscv_op_counter.ops)
-    sn_dict = dict(snitch_op_counter.ops)
-
-    all_keys = sorted(set(rv_dict) | set(sn_dict))
-    max_len_key = max(len(k) for k in all_keys)
-    max_len_value = max(len(str(val)) for val in (*rv_dict.values(), *sn_dict.values()))
-
-    def format_row(key: str, *values: str):
-        paddings = tuple(" " * (max_len_value - len(val)) for val in values)
-        vals = "".join(f"\t{padding}{val}" for padding, val in zip(paddings, values))
-        return f"{key}{' ' * (max_len_key - len(key))}{vals}\n"
-
-    rows = " " * max_len_key + "\trv\tsn\tdiff\n"
-
-    ZERO_VAL = "."
-
-    for key in all_keys:
-        rv_val = rv_dict.get(key, 0)
-        sn_val = sn_dict.get(key, 0)
-        diff_val = sn_val - rv_val
-
-        rv_str = str(rv_val) if rv_val else ZERO_VAL
-        sn_str = str(sn_val) if sn_val else ZERO_VAL
-        diff_str = (
-            (f"+{diff_val}" if diff_val > 0 else f"{diff_val}") if diff_val else "="
-        )
-
-        rows += key
-        rows += " " * (max_len_key - len(key))
-        rows += "\t"
-        rows += " " * (max_len_value - len(rv_str))
-        rows += rv_str
-        rows += "\t"
-        rows += " " * (max_len_value - len(sn_str))
-        rows += sn_str
-        rows += "\t"
-        rows += " " * (max_len_value - len(diff_str))
-        rows += diff_str
-        rows += "\n"
-
-    rv_sum = sum(rv_dict.values())
-    sn_sum = sum(sn_dict.values())
-    total_diff = sn_sum - rv_sum
-
-    rows += format_row("total", str(rv_sum), str(sn_sum), str(total_diff))
-
-    mo.md(
-        f"""
-    The interpreter kept track of the number of times an operation was executed, which we can use as a proxy for performance.
-
-    For example, we can see that one version has many more instructions executed overall ({rv_sum} vs {sn_sum}), and that one version uses explicit load and store instructions, while the other uses the streaming equivalents:
-
-    {m}{n}{k}
-
-    ```
-    {rows}
-    ```
-    """
-    )
-    return (
-        ZERO_VAL,
-        all_keys,
-        diff_str,
-        diff_val,
-        format_row,
-        key,
-        max_len_key,
-        max_len_value,
-        rows,
-        rv_dict,
-        rv_str,
-        rv_sum,
-        rv_val,
-        sn_dict,
-        sn_str,
-        sn_sum,
-        sn_val,
-        total_diff,
-    )
-
-
-@app.cell
-def __():
-    from xdsl.backend.riscv.lowering import (
-        convert_arith_to_riscv,
-        convert_func_to_riscv_func,
-        convert_memref_to_riscv,
-        convert_scf_to_riscv_scf,
-    )
-    from xdsl.backend.riscv.lowering.convert_riscv_scf_to_riscv_cf import (
-        ConvertRiscvScfToRiscvCfPass,
-    )
-    from xdsl.backend.riscv.lowering.convert_snitch_stream_to_snitch import (
-        ConvertSnitchStreamToSnitch,
-    )
-    from xdsl.context import MLContext
-    from xdsl.dialects.riscv import riscv_code
-    from xdsl.interpreter import Interpreter, OpCounter
-    from xdsl.interpreters import register_implementations
-    from xdsl.interpreters.ptr import TypedPtr
-    from xdsl.ir import Attribute, SSAValue
-    from xdsl.passes import PipelinePass
-    from xdsl.tools.command_line_tool import get_all_dialects
-    from xdsl.transforms import (
-        arith_add_fastmath,
-        convert_linalg_to_loops,
-        convert_linalg_to_memref_stream,
-        convert_memref_stream_to_loops,
-        convert_memref_stream_to_snitch_stream,
-        convert_riscv_scf_for_to_frep,
-        dead_code_elimination,
-        loop_hoist_memref,
-        lower_affine,
-        memref_streamify,
-        reconcile_unrealized_casts,
-        test_lower_snitch_stream_to_asm,
-    )
-    from xdsl.transforms.canonicalize import CanonicalizePass
-    from xdsl.transforms.lower_snitch import LowerSnitchPass
-    from xdsl.transforms.mlir_opt import MLIROptPass
-    from xdsl.transforms.riscv_register_allocation import RISCVRegisterAllocation
-    from xdsl.transforms.riscv_scf_loop_range_folding import (
-        RiscvScfLoopRangeFoldingPass,
-    )
-    from xdsl.transforms.snitch_register_allocation import SnitchRegisterAllocation
-    return (
-        Attribute,
-        CanonicalizePass,
-        ConvertRiscvScfToRiscvCfPass,
-        ConvertSnitchStreamToSnitch,
-        Interpreter,
-        LowerSnitchPass,
-        MLContext,
-        MLIROptPass,
-        OpCounter,
-        PipelinePass,
-        RISCVRegisterAllocation,
-        RiscvScfLoopRangeFoldingPass,
-        SSAValue,
-        SnitchRegisterAllocation,
-        TypedPtr,
-        arith_add_fastmath,
-        convert_arith_to_riscv,
-        convert_func_to_riscv_func,
-        convert_linalg_to_loops,
-        convert_linalg_to_memref_stream,
-        convert_memref_stream_to_loops,
-        convert_memref_stream_to_snitch_stream,
-        convert_memref_to_riscv,
-        convert_riscv_scf_for_to_frep,
-        convert_scf_to_riscv_scf,
-        dead_code_elimination,
-        get_all_dialects,
-        loop_hoist_memref,
-        lower_affine,
-        memref_streamify,
-        reconcile_unrealized_casts,
-        register_implementations,
-        riscv_code,
-        test_lower_snitch_stream_to_asm,
-    )
 
 
 @app.cell
