18bbe09335b6c5b2ec1dbe819f3219d4eac6c086
Sasha Lopoukhine <superlopuh@gmail.com>
2024-08-17 11:10:51 +0100

    jax.py -> jax_jit.py

--- tests/backend/test_jax_jit.py
+++ tests/backend/test_jax_jit.py
@@ -7,14 +7,7 @@ from xdsl.dialects.builtin import ModuleOp, TensorType, i32
 
 pytest.importorskip("jax")
 
-from xdsl.backend.jax import jax_jit  # noqa: E402
-
-
-def test_no_main():
-    module = ModuleOp([])
-
-    with pytest.raises(ValueError, match="No `main` function in module"):
-        jax_jit(module)
+from xdsl.backend.jax_jit import jax_jit  # noqa: E402
 
 
 def test_abs():
@@ -54,6 +47,13 @@ def test_abs():
     assert abs_one(jax.numpy.array(2, dtype=jax.numpy.int32)) == jax.numpy.array(
         2, dtype=jax.numpy.int32
     )
+
+
+def test_no_main():
+    module = ModuleOp([])
+
+    with pytest.raises(ValueError, match="No `main` function in module"):
+        jax_jit(module)
 
 
 def test_fail():
