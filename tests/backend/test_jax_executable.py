ee1c02b47f5d1d191e5c1c10030994f7c561e5ea
Sasha Lopoukhine <superlopuh@gmail.com>
2024-08-17 19:00:49 +0100

    cover in tests

--- tests/backend/test_jax_executable.py
+++ tests/backend/test_jax_executable.py
@@ -3,7 +3,9 @@ import pytest
 
 from xdsl.builder import ImplicitBuilder
 from xdsl.dialects import func, stablehlo
-from xdsl.dialects.builtin import ModuleOp, TensorType, i32
+from xdsl.dialects.builtin import ModuleOp, StringAttr, TensorType, i32
+from xdsl.irdl import IRDLOperation, attr_def, irdl_op_definition
+from xdsl.traits import SymbolOpInterface
 
 pytest.importorskip("jax")
 
@@ -63,3 +65,134 @@ def test_fail():
 
         @JaxExecutable.compile(module)
         def abs(a: jax.Array) -> tuple[jax.Array]: ...  # pyright: ignore[reportUnusedFunction]
+
+
+def test_main_not_func():
+    @irdl_op_definition
+    class SymNameOp(IRDLOperation):
+        name = "sym_name"
+
+        sym_name = attr_def(StringAttr)
+        traits = frozenset((SymbolOpInterface(),))
+
+    module = ModuleOp([SymNameOp(attributes={"sym_name": StringAttr("main")})])
+
+    with pytest.raises(ValueError, match="`main` operation is not a `func.func`"):
+        JaxExecutable.compile(module)
+
+
+def test_parameter_count_mismatch():
+    TI32 = TensorType(i32, ())
+
+    main_op = func.FuncOp("main", ((TI32,), (TI32,)))
+    with ImplicitBuilder(main_op.body) as (arg,):
+        res = stablehlo.AbsOp(arg).result
+        func.Return(res)
+
+    module = ModuleOp([main_op])
+    executable = JaxExecutable.compile(module)
+
+    with pytest.raises(
+        ValueError,
+        match="Number of parameters .* does not match the number of operand types",
+    ):
+
+        @executable
+        def abs_two_params(a: jax.Array, b: jax.Array) -> jax.Array: ...  # pyright: ignore[reportUnusedFunction]
+
+
+def test_parameter_annotation():
+    TI32 = TensorType(i32, ())
+
+    main_op = func.FuncOp("main", ((TI32,), (TI32,)))
+    with ImplicitBuilder(main_op.body) as (arg,):
+        res = stablehlo.AbsOp(arg).result
+        func.Return(res)
+
+    module = ModuleOp([main_op])
+    executable = JaxExecutable.compile(module)
+
+    with pytest.raises(
+        NotImplementedError, match="Parameter .* is not annotated as jnp.ndarray"
+    ):
+
+        @executable
+        def abs_wrong_annotation(a: int) -> jax.Array: ...  # pyright: ignore[reportUnusedFunction]
+
+
+def test_return_annotation_tuple_length():
+    TI32 = TensorType(i32, ())
+
+    main_op = func.FuncOp("main", ((TI32,), (TI32,)))
+    with ImplicitBuilder(main_op.body) as (arg,):
+        res = stablehlo.AbsOp(arg).result
+        func.Return(res)
+
+    module = ModuleOp([main_op])
+    executable = JaxExecutable.compile(module)
+
+    with pytest.raises(
+        NotImplementedError, match="Only return values of length 1 supported"
+    ):
+
+        @executable
+        def abs_wrong_tuple_length(a: jax.Array) -> tuple[jax.Array, jax.Array]: ...  # pyright: ignore[reportUnusedFunction]
+
+
+def test_return_annotation_tuple_type():
+    TI32 = TensorType(i32, ())
+
+    main_op = func.FuncOp("main", ((TI32,), (TI32,)))
+    with ImplicitBuilder(main_op.body) as (arg,):
+        res = stablehlo.AbsOp(arg).result
+        func.Return(res)
+
+    module = ModuleOp([main_op])
+    executable = JaxExecutable.compile(module)
+
+    with pytest.raises(
+        NotImplementedError, match="Return annotation .* is not jnp.ndarray"
+    ):
+
+        @executable  # pyright: ignore[reportArgumentType]
+        def abs_wrong_tuple_type(a: jax.Array) -> tuple[int]: ...  # pyright: ignore[reportUnusedFunction]
+
+
+def test_return_annotation_single():
+    TI32 = TensorType(i32, ())
+
+    main_op = func.FuncOp("main", ((TI32,), (TI32,)))
+    with ImplicitBuilder(main_op.body) as (arg,):
+        res = stablehlo.AbsOp(arg).result
+        func.Return(res)
+
+    module = ModuleOp([main_op])
+    executable = JaxExecutable.compile(module)
+
+    with pytest.raises(
+        NotImplementedError,
+        match="Return annotation is must be jnp.ndarray or a tuple of jnp.ndarray",
+    ):
+
+        @executable  # pyright: ignore[reportArgumentType]
+        def abs_wrong_single_type(a: jax.Array) -> int: ...  # pyright: ignore[reportUnusedFunction]
+
+
+def test_return_value_count_mismatch():
+    TI32 = TensorType(i32, ())
+
+    main_op = func.FuncOp("main", ((TI32,), (TI32, TI32)))
+    with ImplicitBuilder(main_op.body) as (arg,):
+        res = stablehlo.AbsOp(arg).result
+        func.Return(res, res)
+
+    module = ModuleOp([main_op])
+    executable = JaxExecutable.compile(module)
+
+    with pytest.raises(
+        ValueError,
+        match="Number of return values .* does not match the stub's return annotation",
+    ):
+
+        @executable
+        def abs_return_count_mismatch(a: jax.Array) -> jax.Array: ...  # pyright: ignore[reportUnusedFunction]
