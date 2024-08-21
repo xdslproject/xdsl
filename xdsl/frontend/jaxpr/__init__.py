ffd7991ed97447e14531155f2dddc40fb48cfeba
Shreya A S <shreya.a.s@um.es>
2024-08-21 17:43:35 +0100

    assert i/p and o/p to fp32

--- xdsl/frontend/jaxpr/__init__.py
+++ xdsl/frontend/jaxpr/__init__.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from jax._src.core import ClosedJaxpr
 
 from xdsl.builder import Builder
-from xdsl.dialects.builtin import Float32Type, FunctionType, ModuleOp, TensorType
+from xdsl.dialects.builtin import FunctionType, ModuleOp, TensorType, f32
 from xdsl.dialects.func import FuncOp, Return
 from xdsl.ir import Block, Region
 
@@ -38,25 +38,27 @@ class IRGen:
         self.module = ModuleOp([])
         self.builder = Builder.at_end(self.module.body.blocks[0])
 
-    def ir_gen_module(self, module_ast: ClosedJaxpr) -> ModuleOp:
+    def ir_gen_module(self, jaxpr: ClosedJaxpr) -> ModuleOp:
         """
         Public API: convert the jaxpr (source file) to an MLIR
         Module operation."""
         parent_builder = self.builder
 
-        assert len(module_ast.jaxpr.invars) == 1
-        assert len(module_ast.jaxpr.outvars) == 1
+        assert len(jaxpr.jaxpr.invars) == 1
+        assert len(jaxpr.jaxpr.outvars) == 1
+        for arg in jaxpr.jaxpr.invars:
+            assert arg.aval.dtype == "float32"
 
-        inputVars = module_ast.jaxpr.invars[0]
-        outputVars = module_ast.jaxpr.outvars[0]
+        inputVars = jaxpr.jaxpr.invars[0]
+        outputVars = jaxpr.jaxpr.outvars[0]
 
         input_types = [
-            TensorType(Float32Type(), [inputVars.aval.size])
-            for _ in range(len(module_ast.jaxpr.invars))
+            TensorType(f32, [inputVars.aval.size])
+            for _ in range(len(jaxpr.jaxpr.invars))
         ]
         output_types = [
-            TensorType(Float32Type(), [outputVars.aval.size])
-            for _ in range(len(module_ast.jaxpr.outvars))
+            TensorType(f32, [outputVars.aval.size])
+            for _ in range(len(jaxpr.jaxpr.outvars))
         ]
 
         block = Block(arg_types=input_types)
@@ -64,7 +66,7 @@ class IRGen:
 
         func_type = FunctionType.from_lists(input_types, output_types)
 
-        for _ in module_ast.jaxpr.eqns:
+        for _ in jaxpr.jaxpr.eqns:
             raise NotImplementedError("jax equation not implemented")
 
         if inputVars == outputVars:
