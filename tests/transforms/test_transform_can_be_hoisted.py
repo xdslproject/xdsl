8891e6cd898f98c7fff03d1e61a0b8f55c1fb720
Ravikiran <rravikiran1729@gmail.com>
2024-08-23 13:27:07 +0100

    Precise testcases for Can_be_hoisted helper

--- tests/transforms/test_transform_can_be_hoisted.py
+++ tests/transforms/test_transform_can_be_hoisted.py
@@ -1,11 +1,10 @@
-from xdsl.builder import Builder
+from xdsl.builder import Builder, ImplicitBuilder
 from xdsl.dialects import builtin, func, scf, test
-from xdsl.dialects.arith import Addf, Addi, Constant, DivUI, Muli
+from xdsl.dialects.arith import Addf, Constant
 from xdsl.dialects.builtin import (
     FloatAttr,
     FunctionType,
     IndexType,
-    IntegerAttr,
     ModuleOp,
     f32,
     i32,
@@ -17,20 +16,6 @@ from xdsl.transforms.loop_invariant_code_motion import can_be_hoisted
 index = IndexType()
 
 
-def test_is_loop_dependent_no_dep():
-    lb = Constant.from_int_and_width(0, i32)
-    ub = Constant.from_int_and_width(42, i32)
-    step = Constant.from_int_and_width(3, i32)
-
-    op1 = test.TestOp(result_types=[i32])
-    op2 = test.TestOp(result_types=[i32])
-
-    for_op = For(lb, ub, step, [], Block([op1, op2], arg_types=[i32]))
-
-    assert can_be_hoisted(op1, for_op.body)
-    assert can_be_hoisted(op2, for_op.body)
-
-
 def test_for_with_loop_invariant_verify():
     """Test for with loop-variant variables"""
 
@@ -39,26 +24,28 @@ def test_for_with_loop_invariant_verify():
     step = Constant.from_int_and_width(3, IndexType())
     carried = Constant.from_int_and_width(1, IndexType())
 
-    @Builder.implicit_region((IndexType(), IndexType()))
-    def body(_: tuple[BlockArgument, ...]) -> None:
-        a = Constant(IntegerAttr.from_int_and_width(1, 32), i32)
-        b = Constant(IntegerAttr.from_int_and_width(2, 32), i32)
-        # Operations on these constants
-        c = Addi(a, b)
-        d = Muli(a, b)
-        e = DivUI(c, d)
-        Yield(e)
+    a = test.TestOp(result_types=[i32])
+    b = test.TestOp(result_types=[i32])
+    block = Block(
+        arg_types=(
+            index,
+            index,
+        )
+    )
+    region = Region(block)
+    with ImplicitBuilder(block) as (arg0, _):
+        op1 = test.TestOp(result_types=[i32])
+        op2 = test.TestOp(result_types=[i32])
+        op3 = test.TestOp([op1, op2, arg0], result_types=[i32])
+        _f = Yield(op3)
+    for_op = For(lower, upper, step, [carried], region)
+    block0 = Block([a, b, for_op])
+    _region_outer = Region(block0)
 
-    for_op = For(lower, upper, step, [carried], body)
-
-    for op in for_op.body.walk():
-        for regoin in for_op.regions:
-            # MLIR iterates the for loop's body and hoists operations that
-            # can_be_hoisted. However, the DivUI operation cannot be hoisted immediately
-            # because its operands are still within the loop's body. It can only be
-            # hoisted once the instructions generating those operands have been hoisted.
-            if not (isinstance(op, Yield) or isinstance(op, DivUI)):
-                assert can_be_hoisted(op, regoin)
+    for region in for_op.regions:
+        for op in region.block.walk():
+            if not isinstance(op, scf.Yield):
+                assert can_be_hoisted(op3, region)
 
 
 def test_invariant_loop_dialect():
@@ -83,15 +70,12 @@ def test_invariant_loop_dialect():
         _v0 = Addf(cf7, cf8)
 
     @Builder.implicit_region((builtin.IndexType(),))
-    # @Builder.implicit_region((IndexType(),))
     def outer_loop_body(args: tuple[BlockArgument, ...]):
         scf.For(ci0, ci10, ci1, [], inner_loop_body)
 
     for_op = For(co0, co1, coi, [], outer_loop_body)
     body0 = Block([cf7, cf8, for_op])
     region0 = Region(body0)
-    # regoin1 = Region([body0, outer_loop_body.block])
-    # # Create the function
     func_type = FunctionType.from_lists([], [])
     function = func.FuncOp("invariant_loop_dialect", func_type, region0)
 
@@ -99,40 +83,10 @@ def test_invariant_loop_dialect():
     mod = ModuleOp([function])
     # Assertions to check loop invariant and hoisting
     for op in mod.body.walk():
-        for regoin in mod.regions:
+        for region in mod.regions:
             if isinstance(op, test.TestOp):
                 assert not can_be_hoisted(op, mod.body)
             elif isinstance(op, Addf):
-                regoin = op.parent_region()
-                assert regoin is not None
-                assert can_be_hoisted(op, regoin)
-
-
-def test_for_with_loop_invariant_verify1():
-    """Test for with loop-invariant variables"""
-    lb = Constant.from_int_and_width(0, i32)
-    ub = Constant.from_int_and_width(42, i32)
-    step = Constant.from_int_and_width(3, i32)
-
-    op1 = test.TestOp(result_types=[i32])
-    op2 = test.TestOp(result_types=[i32])
-    op3 = test.TestOp([op2, op1], result_types=[i32])
-    op4 = test.TestOp([op3, op2], result_types=[i32])
-    bb0 = Block([op1, op2], arg_types=[i32])
-    bb1 = Block([op3, op4], arg_types=[i32])
-
-    for_op = For(lb, ub, step, [], bb1)
-    bb3 = Block([for_op])
-    regions = Region([bb0, bb3])
-
-    # # Create the function
-    func_type = FunctionType.from_lists([], [])
-    function = func.FuncOp("invariant_loop_dialect", func_type, regions)
-
-    for region in function.regions:
-        assert can_be_hoisted(op3, region)
-        # MLIR iterates the for loop's body and hoists operations that can_be_hoisted.
-        # However, the DivUI operation cannot be hoisted immediately because its
-        # operands are still within the loop's body. It can only be hoisted once the
-        # instructions generating those operands have been hoisted.
-        assert not can_be_hoisted(op4, region)
+                region = op.parent_region()
+                assert region is not None
+                assert can_be_hoisted(op, region)
