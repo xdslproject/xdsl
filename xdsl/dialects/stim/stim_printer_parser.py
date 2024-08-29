123328ab884d162425060558e82bdc2e6f607921
Kim Worrall <kimsworrall@gmail.com>
2024-08-29 02:05:07 +0100

    Add qubit coordinates annotation and attributes.

--- xdsl/dialects/stim/stim_printer_parser.py
+++ xdsl/dialects/stim/stim_printer_parser.py
@@ -1,7 +1,11 @@
 import abc
+from collections.abc import Callable, Iterable
 from contextlib import contextmanager
 from dataclasses import dataclass, field
-from typing import Any
+from typing import Any, TypeVar, cast
+
+from xdsl.dialects.builtin import ArrayAttr
+from xdsl.ir import Attribute
 
 
 @dataclass(eq=False, repr=False)
@@ -18,6 +22,32 @@ class StimPrinter:
             yield
         finally:
             self.print_string("}")
+
+    @contextmanager
+    def in_parens(self):
+        self.print_string("(")
+        try:
+            yield
+        finally:
+            self.print_string(")")
+
+    T = TypeVar("T")
+
+    def print_list(
+        self, elems: Iterable[T], print_fn: Callable[[T], Any], delimiter: str = ", "
+    ) -> None:
+        for i, elem in enumerate(elems):
+            if i:
+                self.print_string(delimiter)
+            print_fn(elem)
+
+    def print_attribute(self, attribute: Attribute) -> None:
+        if isinstance(attribute, ArrayAttr):
+            attribute = cast(ArrayAttr[Attribute], attribute)
+            self.print_string("(")
+            self.print_list(attribute.data, self.print_attribute)
+            self.print_string(") ")
+            return
 
 
 class StimPrintable(abc.ABC):
