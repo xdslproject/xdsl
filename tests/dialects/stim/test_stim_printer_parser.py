bf565137123582a87763eb79a8c1f1ff9fe7c28c
Kim Worrall <kimsworrall@gmail.com>
2024-08-29 02:05:31 +0100

    add tests for stim printer

--- tests/dialects/stim/test_stim_printer_parser.py
+++ tests/dialects/stim/test_stim_printer_parser.py
@@ -1,4 +1,10 @@
+from io import StringIO
+
+import pytest
+
 from xdsl.dialects import stim
+from xdsl.dialects.stim.stim_printer_parser import StimPrinter
+from xdsl.dialects.test import TestOp
 from xdsl.ir import Block, Region
 
 
@@ -8,3 +14,16 @@ def test_empty_circuit():
     module = stim.StimCircuitOp(empty_region)
 
     assert module.stim() == ""
+
+
+def test_stim_circuit_ops_stim_printable():
+    op = TestOp()
+    block = Block([op])
+    region = Region(block)
+    module = stim.StimCircuitOp(region)
+
+    with pytest.raises(ValueError, match="Cannot print in stim format:"):
+        res_io = StringIO()
+        printer = StimPrinter(stream=res_io)
+
+        module.print_stim(printer)
