f155aa7de6009dcce568751e34b99e1591de68ec
manainen <manainen@yandex-team.ru>
2024-09-29 13:17:51 +0100

    pyright fixes

--- xdsl/transforms/experimental/convert_jax_to_linalg.py
+++ xdsl/transforms/experimental/convert_jax_to_linalg.py
@@ -4,7 +4,8 @@ from xdsl.context import MLContext
 from xdsl.dialects import builtin
 from xdsl.dialects.builtin import TensorType
 from xdsl.dialects.func import FuncOp
-from xdsl.dialects.linalg import NamedOpBase
+from xdsl.ir import BlockArgument, SSAValue
+from xdsl.irdl import VarOperand
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     GreedyRewritePatternApplier,
@@ -20,25 +21,26 @@ from xdsl.transforms.mlir_opt import MLIROptPass
 class SubstituteDonatedTensors(RewritePattern):
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: FuncOp, rewriter: PatternRewriter, /):
-        donated_inputs = {}
-        for inp, attr in zip(op.regions[0].block._args, op.arg_attrs):
-            if (
-                type(inp.type) is not TensorType
-                or "tf.aliasing_output" not in attr.data
-            ):
-                continue
-            donated_inputs[inp.name_hint] = inp
+        if op.arg_attrs is None:
+            return
 
-        for child_op in op.regions[0].ops:
-            if issubclass(type(child_op), NamedOpBase):
-                value_mapper = {}
-                for output in child_op.outputs:
-                    for arg_name, arg in list(donated_inputs.items()):
-                        if type(
-                            output.type
-                        ) is TensorType and arg.type.is_same_type_with(output.type):
-                            value_mapper[output] = arg
-                            del donated_inputs[arg_name]
+        donated_inputs: list[BlockArgument] = []
+        for inp, attr in zip(op.args, op.arg_attrs):
+            if type(inp.type) is TensorType and "tf.aliasing_output" in attr.data:
+                donated_inputs.append(inp)
+
+        for child_op in op.body.ops:
+            if (
+                hasattr(child_op, "outputs")
+                and type(getattr(child_op, "outputs")) is VarOperand
+            ):
+                value_mapper: dict[SSAValue, SSAValue] = {}
+                for output in getattr(child_op, "outputs"):
+                    for i, arg in enumerate(donated_inputs):
+                        if type(getattr(output, "type")) is TensorType and getattr(
+                            arg, "type"
+                        ).is_same_type_with(output.type):
+                            value_mapper[output] = donated_inputs.pop(i)
                             break
                 new_op = child_op.clone(value_mapper)
                 rewriter.replace_op(child_op, [new_op])
@@ -56,4 +58,4 @@ class ConvertJaxToLinalgPass(ModulePass):
             walk_regions_first=True,
         )
         the_one_pass.rewrite_module(op)
-        MLIROptPass(arguments=["--linalg-fuse-elementwise-ops"]).apply(ctx, op)
+        MLIROptPass(arguments=("--linalg-fuse-elementwise-ops",)).apply(ctx, op)
