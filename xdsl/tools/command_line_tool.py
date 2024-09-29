1319404a3b63608964bfd8fccfab8baeb83d48b5
manainen <manainen@yandex-team.ru>
2024-09-29 13:37:34 +0100

    change names

--- xdsl/tools/command_line_tool.py
+++ xdsl/tools/command_line_tool.py
@@ -86,10 +86,10 @@ def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
 
         return convert_stencil_to_ll_mlir.ConvertStencilToLLMLIRPass
 
-    def get_convert_jax_to_linalg():
-        from xdsl.transforms.experimental import convert_jax_to_linalg
+    def get_jax_use_donated_arguments():
+        from xdsl.transforms.experimental import jax_use_donated_arguments
 
-        return convert_jax_to_linalg.ConvertJaxToLinalgPass
+        return jax_use_donated_arguments.JaxUseDonatedArguments
 
     def get_convert_riscv_scf_to_riscv_cf():
         from xdsl.backend.riscv.lowering import convert_riscv_scf_to_riscv_cf
@@ -460,7 +460,7 @@ def get_all_passes() -> dict[str, Callable[[], type[ModulePass]]]:
         "convert-stencil-to-csl-stencil": get_convert_stencil_to_csl_stencil,
         "inline-snrt": get_convert_snrt_to_riscv,
         "convert-stencil-to-ll-mlir": get_convert_stencil_to_ll_mlir,
-        "convert-jax-to-linalg": get_convert_jax_to_linalg,
+        "jax-use-donated-arguments": get_jax_use_donated_arguments,
         "cse": get_cse,
         "csl-stencil-bufferize": get_csl_stencil_bufferize,
         "csl-stencil-materialize-stores": get_csl_stencil_materialize_stores,
