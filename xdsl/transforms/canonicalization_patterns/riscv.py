bc67b83a2967d53d65fed8c59a954dbbd88bf5e2
Leon Frenot <leon.frenot@ens-lyon.fr>
2024-10-01 15:52:37 +0100

    dialects: (riscv) canonicalization pattern added (x|0=x)

--- xdsl/transforms/canonicalization_patterns/riscv.py
+++ xdsl/transforms/canonicalization_patterns/riscv.py
@@ -403,6 +403,27 @@ class BitwiseAndByZero(RewritePattern):
             rewriter.replace_matched_op(riscv.MVOp(op.rs2, rd=rd))
 
 
+class BitwiseOrByZero(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: riscv.OrOp, rewriter: PatternRewriter):
+        """
+        rewrite pattern to optimize bitwise or by 0
+        x | 0 = 0
+        """
+
+        # check if the first operand is 0
+        if (rs1 := get_constant_value(op.rs1)) is not None and rs1.value.data == 0:
+            # if the first operand is 0, set the destination to the second operand
+            rd = cast(riscv.IntRegisterType, op.rd.type)
+            rewriter.replace_matched_op(riscv.MVOp(op.rs2, rd=rd))
+
+        # check if the second operand is 0
+        elif (rs2 := get_constant_value(op.rs2)) is not None and rs2.value.data == 0:
+            # if the second operand is 0, set the destination to first operand
+            rd = cast(riscv.IntRegisterType, op.rd.type)
+            rewriter.replace_matched_op(riscv.MVOp(op.rs1, rd=rd))
+
+
 class XorBySelf(RewritePattern):
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: riscv.XorOp, rewriter: PatternRewriter):
