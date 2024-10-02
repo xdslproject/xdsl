9ef1062eb1dd9bc55cb80e8f5ae8de0e0f2c2866
Alex Rice <alexrice999@hotmail.co.uk>
2024-10-02 13:22:53 +0100

    core: Block predecessors

--- tests/test_use.py
+++ tests/test_use.py
@@ -1,5 +1,7 @@
 from xdsl.context import MLContext
 from xdsl.dialects.builtin import Builtin, ModuleOp
+from xdsl.dialects.cf import Cf
+from xdsl.dialects.func import Func, FuncOp
 from xdsl.dialects.test import Test
 from xdsl.ir import Use
 from xdsl.parser import Parser
@@ -28,3 +30,37 @@ def test_main():
     assert op2.results[0].uses == set()
 
     print("Done")
+
+
+test_prog_blocks = """
+func.func @blocks() {
+  "test.termop"() [^0, ^1] : () -> ()
+^0:
+  cf.br ^2
+^1:
+  cf.br ^2
+^2:
+  func.return
+}
+"""
+
+
+def test_predecessor():
+    ctx = MLContext()
+    ctx.load_dialect(Builtin)
+    ctx.load_dialect(Test)
+    ctx.load_dialect(Func)
+    ctx.load_dialect(Cf)
+
+    parser = Parser(ctx, test_prog_blocks)
+    func = parser.parse_op()
+
+    func.verify()
+    assert isinstance(func, FuncOp)
+
+    block1, block2, block3, block4 = func.body.blocks
+
+    assert block1.predecessors() == ()
+    assert block2.predecessors() == (block1,)
+    assert block3.predecessors() == (block1,)
+    assert set(block4.predecessors()) == set((block2, block3))
