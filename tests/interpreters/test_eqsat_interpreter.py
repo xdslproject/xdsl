eef7c82ed450d6cff8415fa47c3e84d8f35e3d00
Sasha Lopoukhine <superlopuh@gmail.com>
2024-10-07 17:40:41 +0100

    wip wip wip

--- tests/interpreters/test_eqsat_interpreter.py
+++ tests/interpreters/test_eqsat_interpreter.py
@@ -7,6 +7,7 @@ from xdsl.dialects.builtin import (
     ModuleOp,
     StringAttr,
 )
+from xdsl.interpreters.experimental.eqsat_pdl import EqsatPDLRewritePattern
 from xdsl.interpreters.experimental.pdl import (
     PDLRewritePattern,
 )
@@ -16,6 +17,7 @@ from xdsl.pattern_rewriter import (
     RewritePattern,
     op_type_rewrite_pattern,
 )
+from xdsl.transforms.eqsat_create_eclasses import insert_eclass_ops
 
 """
     ======================================================
@@ -136,3 +138,27 @@ def swap_arguments_pdl():
                 pdl.ReplaceOp(x_y_op, y_x_op)
 
     return pdl_module
+
+
+def test_hello():
+    input_module = swap_arguments_input()
+    output_module = swap_arguments_output()
+    rewrite_module = swap_arguments_pdl()
+
+    insert_eclass_ops(next(iter(input_module.body.block.ops)).regions[0].block)
+
+    pdl_rewrite_op = next(
+        op for op in rewrite_module.walk() if isinstance(op, pdl.RewriteOp)
+    )
+
+    ctx = MLContext()
+    ctx.load_dialect(arith.Arith)
+
+    PatternRewriteWalker(
+        EqsatPDLRewritePattern(pdl_rewrite_op, ctx),
+        apply_recursively=False,
+    ).rewrite_module(input_module)
+
+    assert str(input_module) == ""
+
+    assert input_module.is_structurally_equivalent(output_module)
