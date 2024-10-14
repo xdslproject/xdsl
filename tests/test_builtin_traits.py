3a6918c80bc6766a63491749aa26d57c075e5179
Alex Rice <alexrice999@hotmail.co.uk>
2024-10-14 12:07:08 +0100

    core: (HasParent) allow no parent for testing

--- tests/test_builtin_traits.py
+++ tests/test_builtin_traits.py
@@ -68,26 +68,6 @@ class HasMultipleParentOp(IRDLOperation):
     traits = frozenset([HasParent(ParentOp, Parent2Op)])
 
 
-def test_has_parent_no_parent():
-    """
-    Test that an operation with an HasParentOp trait
-    fails with no parents.
-    """
-    has_parent_op = HasParentOp()
-    with pytest.raises(
-        VerifyException, match="'test.has_parent' expects parent op 'test.parent'"
-    ):
-        has_parent_op.verify()
-
-    has_multiple_parent_op = HasMultipleParentOp()
-    message = (
-        "'test.has_multiple_parent' expects parent op to "
-        "be one of 'test.parent', 'test.parent2'"
-    )
-    with pytest.raises(VerifyException, match=message):
-        has_multiple_parent_op.verify()
-
-
 def test_has_parent_wrong_parent():
     """
     Test that an operation with an HasParentOp trait
