dfc70a5b7a2ae64b8250b072a67bcac6d04167ca
Max Manainen <manainen@Maxs-MacBook-Pro.local>
2024-10-29 15:35:21 +0000

    store operation conversion

--- tests/filecheck/transforms/convert_memref_to_ptr.mlir
+++ tests/filecheck/transforms/convert_memref_to_ptr.mlir
@@ -1,21 +1,29 @@
+// RUN: xdsl-opt -p convert-memref-to-ptr  --split-input-file --verify-diagnostics %s | filecheck %s
+
+
+// CHECK:      builtin.module {
+
 %v, %idx, %arr = "test.op"() : () -> (i32, index, memref<10xi32>)
 memref.store %v, %arr[%idx] {"nontemporal" = false} : memref<10xi32>
 
-// CHECK-NEXT:    %arr_ptr  = memref.to_ptr %arr : !ptr.ptr
-// CHECK-NEXT:    %type_offset = ptr.type_offset i32 : index
-// CHECK-NEXT:    %offset = index.mul %type_offset, %idx : index
-// CHECK-NEXT:    %element_ptr = ptr.ptradd %arr_ptr, %offset : !ptr.ptr
-// CHECK-NEXT:    ptr.store %v, %element_ptr : i32
+// CHECK-NEXT:  %v, %idx, %arr = "test.op"() : () -> (i32, index, memref<10xi32>)
+// CHECK-NEXT:  %0 = ptr.type_offset %v : i32 -> index
+// CHECK-NEXT:  %1 = arith.muli %idx, %0 : index
+// CHECK-NEXT:  %2 = memref.to_ptr %arr : memref<10xi32> -> !ptr.ptr
+// CHECK-NEXT:  %3 = ptr.ptradd %2, %1 : (!ptr.ptr, index) -> !ptr.ptr
+// CHECK-NEXT:  ptr.store %v, %3 : i32, !ptr.ptr
 
 %idx1, %idx2, %arr2 = "test.op"() : () -> (index, index, memref<10x10xi32>)
 memref.store %v, %arr2[%idx1, %idx2] {"nontemporal" = false} : memref<10x10xi32>
 
-// CHECK-NEXT:    %arr2_ptr  = memref.to_ptr %arr_2 : !ptr.ptr
-// CHECK-NEXT:    %type_offset = ptr.type_offset i32 : index
-// CHECK-NEXT:    %pointer_dim_stride = index.casts 10 : i32 to index
-// CHECK-NEXT:    %pointer_dim_offset = index.mul %pointer_dim_stride, %idx1 : index
-// CHECK-NEXT:    %scaled_dim_offset = index.mul %pointer_dim_offset, %type_offset : index
-// CHECK-NEXT:    %second_dim_offset = index.mul %idx2, %type_offset : index
-// CHECK-NEXT:    %offset = index.add %scaled_dim_offset, %second_dim_offset : index
-// CHECK-NEXT:    %element_ptr = ptr.ptradd %arr2_ptr, %offset : !ptr.ptr
-// CHECK-NEXT:    ptr.store %v, %element_ptr : i32
+// CHECK-NEXT:  %idx1, %idx2, %arr2 = "test.op"() : () -> (index, index, memref<10x10xi32>)
+// CHECK-NEXT:  %4 = arith.constant 10 : index
+// CHECK-NEXT:  %5 = arith.muli %idx1, %4 : index
+// CHECK-NEXT:  %6 = arith.addi %5, %idx2 : index
+// CHECK-NEXT:  %7 = ptr.type_offset %v : i32 -> index
+// CHECK-NEXT:  %8 = arith.muli %6, %7 : index
+// CHECK-NEXT:  %9 = memref.to_ptr %arr2 : memref<10x10xi32> -> !ptr.ptr
+// CHECK-NEXT:  %10 = ptr.ptradd %9, %8 : (!ptr.ptr, index) -> !ptr.ptr
+// CHECK-NEXT:  ptr.store %v, %10 : i32, !ptr.ptr
+
+// CHECK-NEXT: }
---
+++ xdsl/transforms/convert_memref_to_ptr.py
@@ -0,0 +1,128 @@
+from collections.abc import Iterable
+from dataclasses import dataclass
+from typing import cast
+
+from xdsl.context import MLContext
+from xdsl.dialects import arith, builtin, memref, ptr
+from xdsl.dialects.builtin import i32
+from xdsl.dialects.ptr import TypeOffsetOp
+from xdsl.ir import Operation, SSAValue
+from xdsl.irdl import Any
+from xdsl.passes import ModulePass
+from xdsl.pattern_rewriter import (
+    GreedyRewritePatternApplier,
+    PatternRewriter,
+    PatternRewriteWalker,
+    RewritePattern,
+    op_type_rewrite_pattern,
+)
+from xdsl.utils.exceptions import DiagnosticException
+
+
+# I think we also need to pass the adress width.
+def offset_calculations(
+    memref_type: memref.MemRefType[Any], indices: Iterable[SSAValue], val: SSAValue
+) -> tuple[list[Operation], SSAValue]:
+    assert isinstance(memref_type.element_type, builtin.FixedBitwidthType)
+
+    match memref_type.layout:
+        case builtin.NoneAttr():
+            strides = builtin.ShapedType.strides_for_shape(memref_type.get_shape())
+        case builtin.StridedLayoutAttr():
+            strides = memref_type.layout.get_strides()
+        case _:
+            raise DiagnosticException(f"Unsupported layout type {memref_type.layout}")
+
+    ops: list[Operation] = []
+
+    head: SSAValue | None = None
+
+    for index, stride in zip(indices, strides, strict=True):
+        # Calculate the offset that needs to be added through the index of the current
+        # dimension.
+        increment = index
+        match stride:
+            case None:
+                raise DiagnosticException(
+                    f"MemRef {memref} with dynamic stride is not yet implemented"
+                )
+            case 1:
+                # Stride 1 is a noop making the index equal to the offset.
+                pass
+            case _:
+                # Otherwise, multiply the stride (which by definition is the number of
+                # elements required to be skipped when incrementing that dimension).
+                ops.extend(
+                    (
+                        stride_op := arith.Constant.from_int_and_width(
+                            stride, builtin.IndexType()
+                        ),
+                        offset_op := arith.Muli(increment, stride_op),
+                    )
+                )
+                increment = offset_op.result
+
+        if head is None:
+            # First iteration.
+            head = increment
+            continue
+
+        # Otherwise sum up the products.
+        add_op = arith.Addi(head, increment)
+        ops.append(add_op)
+        head = add_op.result
+
+    if head is None:
+        ops.append(const_op := arith.Constant.from_int_and_width(0, i32))
+        return ops, const_op.result
+
+    ops.extend(
+        [
+            bytes_per_element_op := TypeOffsetOp(
+                operands=[val], result_types=[builtin.IndexType()]
+            ),
+            final_offset := arith.Muli(head, bytes_per_element_op),
+        ]
+    )
+
+    return ops, final_offset.result
+
+
+@dataclass
+class ConvertStoreOp(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: memref.Store, rewriter: PatternRewriter, /):
+        assert isinstance(op_memref_type := op.memref.type, memref.MemRefType)
+        memref_type = cast(memref.MemRefType[Any], op_memref_type)
+
+        ops, offset = offset_calculations(memref_type, op.indices, op.value)
+
+        ops.extend(
+            [
+                memref_ptr := memref.ToPtrOp(
+                    operands=[op.memref], result_types=[ptr.PtrType()]
+                ),
+                target_ptr := ptr.PtrAddOp(
+                    operands=[memref_ptr, offset], result_types=[ptr.PtrType()]
+                ),
+                ptr.StoreOp(operands=[target_ptr, op.value]),
+            ]
+        )
+
+        rewriter.replace_matched_op(ops)
+
+
+dataclass(frozen=True)
+
+
+class ConvertMemrefToPtr(ModulePass):
+    name = "convert-memref-to-ptr"
+
+    def apply(self, ctx: MLContext, op: builtin.ModuleOp) -> None:
+        the_one_pass = PatternRewriteWalker(
+            GreedyRewritePatternApplier([ConvertStoreOp()]),
+            apply_recursively=False,
+            walk_reverse=True,
+            walk_regions_first=True,
+        )
+        the_one_pass.rewrite_module(op)
