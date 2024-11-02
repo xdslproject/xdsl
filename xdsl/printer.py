eba721cd31715dd3b8d6109c03f7ac4a4da8589a
n-io <n-io@users.noreply.github.com>
2024-11-02 23:05:48 +0100

    same printing logic for dense elements

--- xdsl/printer.py
+++ xdsl/printer.py
@@ -603,7 +603,11 @@ class Printer:
                 if isinstance(val, IntegerAttr):
                     self.print_string(f"{val.value.data}")
                 elif isinstance(val, FloatAttr):
-                    self.print_string(f"{val.value.data:.6e}")
+                    float_str = f"{val.value.data:.6e}"
+                    if float(float_str) == val.value.data:
+                        self.print_string(float_str)
+                    else:
+                        self.print_string(f"{repr(val.value.data)}")
                 else:
                     raise Exception(
                         "unexpected attribute type "
