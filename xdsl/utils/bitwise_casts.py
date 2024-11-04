335582868713829a902b759fc18f4e498f66a364
n-io <n-io@users.noreply.github.com>
2024-11-04 12:50:56 +0100

    simplifying bitcasts

--- xdsl/utils/bitwise_casts.py
+++ xdsl/utils/bitwise_casts.py
@@ -11,7 +11,7 @@ def convert_f32_to_u32(value: float) -> int:
     Convert an IEEE 754 float to a raw unsigned integer representation.
     """
     raw_float = ctypes.c_float(value)
-    raw_int = ctypes.c_uint32.from_address(ctypes.addressof(raw_float)).value
+    raw_int = ctypes.c_uint32.from_buffer(raw_float).value
     return raw_int
 
 
@@ -20,7 +20,7 @@ def convert_u32_to_f32(value: int) -> float:
     Convert a raw 32-bit unsigned integer to IEEE 754 float representation.
     """
     raw_int = ctypes.c_uint32(value)
-    raw_float = ctypes.c_float.from_address(ctypes.addressof(raw_int)).value
+    raw_float = ctypes.c_float.from_buffer(raw_int).value
     return raw_float
 
 
