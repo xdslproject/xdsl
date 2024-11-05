abb9ef3145e4db1ccf24e19df73e1ab0bf9e51aa
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2024-11-05 20:56:03 +0000

    Import not accessed

--- xdsl/interpreters/pdl.py
+++ xdsl/interpreters/pdl.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from collections.abc import Callable
 from dataclasses import dataclass, field
-from typing import IO, Any, ClassVar, cast
+from typing import IO, Any, ClassVar
 
 from xdsl.context import MLContext
 from xdsl.dialects import pdl
@@ -109,8 +109,7 @@ class PDLMatcher:
         if ssa_val in self.matching_context:
             return self.matching_context[ssa_val] == xdsl_attr
 
-        type_op = cast(pdl.TypeOp, ssa_val.owner)
-        if type_op.constantType is None or type_op.constantType == xdsl_attr:
+        if pdl_op.constantType is None or pdl_op.constantType == xdsl_attr:
             self.matching_context[ssa_val] = xdsl_attr
             return True
         else:
