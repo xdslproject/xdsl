f7520ecc85d34c2e390f5622e865e21c023dd462
Sasha Lopoukhine <superlopuh@gmail.com>
2024-11-08 11:02:41 +0000

    testing: use test dialect and custom passes for test_get_all_available_passes

--- tests/interactive/test_get_all_available_passes.py
+++ tests/interactive/test_get_all_available_passes.py
@@ -1,57 +1,103 @@
-from xdsl.backend.riscv.lowering import (
-    convert_arith_to_riscv,
-    convert_func_to_riscv_func,
-)
-from xdsl.dialects import get_all_dialects
+from dataclasses import dataclass
+from typing import ClassVar
+
+from xdsl.context import MLContext
+from xdsl.dialects.builtin import ModuleOp, StringAttr
+from xdsl.dialects.test import Test, TestOp
 from xdsl.interactive.get_all_available_passes import get_available_pass_list
 from xdsl.interactive.passes import AvailablePass
-from xdsl.interactive.rewrites import individual_rewrite
-from xdsl.transforms import (
-    get_all_passes,
-    reconcile_unrealized_casts,
-    test_lower_linalg_to_snitch,
+from xdsl.passes import ModulePass
+from xdsl.pattern_rewriter import (
+    PatternRewriter,
+    PatternRewriteWalker,
+    RewritePattern,
+    op_type_rewrite_pattern,
 )
+from xdsl.transforms.individual_rewrite import IndividualRewrite
 from xdsl.utils.parse_pipeline import PipelinePassSpec
+
+
+@dataclass
+class ReplacePattern(RewritePattern):
+    before: str
+    after: str
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: TestOp, rewriter: PatternRewriter):
+        if op.attributes["key"] == StringAttr(self.before):
+            rewriter.replace_matched_op(
+                TestOp(attributes={"key": StringAttr(self.after)})
+            )
+
+
+class ReplacePass(ModulePass):
+    before: ClassVar[str]
+    after: ClassVar[str]
+
+    def apply(self, ctx: MLContext, op: ModuleOp) -> None:
+        PatternRewriteWalker(ReplacePattern(self.before, self.after)).rewrite_module(op)
+
+
+class ABPass(ReplacePass):
+    name = "ab"
+    before = "a"
+    after = "b"
+
+
+class ACPass(ReplacePass):
+    name = "ac"
+    before = "a"
+    after = "c"
+
+
+class BCPass(ReplacePass):
+    name = "bc"
+    before = "b"
+    after = "c"
 
 
 def test_get_all_available_passes():
     input_text = """
-        func.func @hello(%n : index) -> index {
-          %two = arith.constant 2 : index
-          %res = arith.muli %two, %n : index
-          func.return %res : index
-        }
-        """
+    "test.op"() {key="a"} : () -> ()
+    """
 
-    # Select two passes
     pass_pipeline = (
         (
-            convert_arith_to_riscv.ConvertArithToRiscvPass,
-            PipelinePassSpec(name="convert-arith-to-riscv", args={}),
-        ),
-        (
-            convert_func_to_riscv_func.ConvertFuncToRiscvFuncPass,
-            PipelinePassSpec(name="convert-func-to-riscv-func", args={}),
+            ABPass,
+            PipelinePassSpec(name="ab", args={}),
         ),
     )
 
     expected_res = tuple(
         (
             AvailablePass(
-                display_name="reconcile-unrealized-casts",
-                module_pass=reconcile_unrealized_casts.ReconcileUnrealizedCastsPass,
+                display_name="bc",
+                module_pass=BCPass,
                 pass_spec=None,
             ),
             AvailablePass(
-                display_name="test-lower-linalg-to-snitch",
-                module_pass=test_lower_linalg_to_snitch.TestLowerLinalgToSnitchPass,
-                pass_spec=None,
+                display_name='TestOp("test.op"() {"key" = "b"} : () -> ()):test.op:bd',
+                module_pass=IndividualRewrite,
+                pass_spec=PipelinePassSpec(
+                    "apply-individual-rewrite",
+                    {
+                        "matched_operation_index": (1,),
+                        "operation_name": ("test.op",),
+                        "pattern_name": ("bd",),
+                    },
+                ),
             ),
         )
     )
 
-    all_dialects = tuple((d_name, d) for d_name, d in get_all_dialects().items())
-    all_passes = tuple((p_name, p()) for p_name, p in sorted(get_all_passes().items()))
+    all_dialects = ((Test.name, lambda: Test),)
+    all_passes = tuple((p.name, p) for p in (ABPass, ACPass, BCPass))
+
+    rewrite_by_names_dict: dict[str, dict[str, RewritePattern]] = {
+        "test.op": {
+            "bd": ReplacePattern("b", "d"),
+        }
+    }
 
     res = get_available_pass_list(
         all_dialects,
@@ -59,7 +105,7 @@ def test_get_all_available_passes():
         input_text,
         pass_pipeline,
         condense_mode=True,
-        rewrite_by_names_dict=individual_rewrite.REWRITE_BY_NAMES,
+        rewrite_by_names_dict=rewrite_by_names_dict,
     )
 
     assert res == expected_res
