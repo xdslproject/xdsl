c5a1c4e19e7bf751027acaec10f8148868b4decc
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2024-11-12 00:26:34 +0100

    transformations: (pdl) Different type matching between polymorphic and fixed case (#3405)
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

--- tests/interpreters/test_pdl_interpreter.py
+++ tests/interpreters/test_pdl_interpreter.py
@@ -482,6 +482,48 @@ def test_native_constraint():
     assert new_input_module_true.is_structurally_equivalent(input_module_true)
 
 
+def test_match_type():
+    matcher = PDLMatcher()
+
+    pdl_op = pdl.TypeOp()
+    ssa_value = pdl_op.result
+    xdsl_value = StringAttr("a")
+
+    # New value
+    assert matcher.match_type(ssa_value, pdl_op, xdsl_value)
+    assert matcher.matching_context == {ssa_value: xdsl_value}
+
+    # Same value
+    assert matcher.match_type(ssa_value, pdl_op, xdsl_value)
+    assert matcher.matching_context == {ssa_value: xdsl_value}
+
+    # Other value
+    assert not matcher.match_type(ssa_value, pdl_op, StringAttr("b"))
+    assert matcher.matching_context == {ssa_value: xdsl_value}
+
+
+def test_match_fixed_type():
+    matcher = PDLMatcher()
+
+    pdl_op = pdl.TypeOp(IntegerType(32))
+    xdsl_value = IntegerType(32)
+    ssa_value = pdl_op.result
+
+    assert matcher.match_type(ssa_value, pdl_op, xdsl_value)
+    assert matcher.matching_context == {ssa_value: xdsl_value}
+
+
+def test_not_match_fixed_type():
+    matcher = PDLMatcher()
+
+    pdl_op = pdl.TypeOp(IntegerType(64))
+    xdsl_value = IntegerType(32)
+    ssa_value = pdl_op.result
+
+    assert not matcher.match_type(ssa_value, pdl_op, xdsl_value)
+    assert matcher.matching_context == {}
+
+
 def test_native_constraint_constant_parameter():
     """
     Check that `pdl.apply_native_constraint` can take constant attribute parameters
