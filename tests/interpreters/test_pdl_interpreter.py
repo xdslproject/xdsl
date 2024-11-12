36f47f3df90d503deca37e5910a34f98df55b4de
Sasha Lopoukhine <superlopuh@gmail.com>
2024-11-12 22:35:14 +0000

    move apply pdl tests

--- tests/interpreters/test_pdl_interpreter.py
+++ tests/interpreters/test_pdl_interpreter.py
@@ -2,7 +2,7 @@ from io import StringIO
 
 from xdsl.builder import Builder, ImplicitBuilder
 from xdsl.context import MLContext
-from xdsl.dialects import arith, func, pdl, test
+from xdsl.dialects import arith, pdl, test
 from xdsl.dialects.builtin import (
     ArrayAttr,
     IntegerAttr,
@@ -13,131 +13,11 @@ from xdsl.dialects.builtin import (
     i64,
 )
 from xdsl.interpreter import Interpreter
-from xdsl.interpreters.pdl import (
-    PDLMatcher,
-    PDLRewriteFunctions,
-    PDLRewritePattern,
-)
-from xdsl.ir import Attribute, OpResult
+from xdsl.interpreters.pdl import PDLMatcher, PDLRewriteFunctions, PDLRewritePattern
+from xdsl.ir import Attribute
 from xdsl.irdl import IRDLOperation, irdl_op_definition, prop_def
-from xdsl.pattern_rewriter import (
-    PatternRewriter,
-    PatternRewriteWalker,
-    RewritePattern,
-    op_type_rewrite_pattern,
-)
+from xdsl.pattern_rewriter import PatternRewriteWalker
 from xdsl.utils.test_value import TestSSAValue
-
-
-class AddZero(RewritePattern):
-    @op_type_rewrite_pattern
-    def match_and_rewrite(self, op: arith.Addi, rewriter: PatternRewriter, /):
-        if not isinstance(op.rhs, OpResult):
-            return
-        if not isinstance(op.rhs.op, arith.Constant):
-            return
-        rhs = op.rhs.op
-        if not isinstance(rhs_value := rhs.value, IntegerAttr):
-            return
-        if rhs_value.value.data != 0:
-            return
-        rewriter.replace_matched_op([], new_results=[op.lhs])
-
-
-def test_rewrite_add_zero_python():
-    input_module = add_zero_input()
-    output_module = add_zero_output()
-
-    PatternRewriteWalker(AddZero(), apply_recursively=False).rewrite_module(
-        input_module
-    )
-
-    assert input_module.is_structurally_equivalent(output_module)
-
-
-def test_rewrite_add_zero_pdl():
-    input_module = add_zero_input()
-    output_module = add_zero_output()
-    rewrite_module = add_zero_pdl()
-    # input_module.verify()
-    # output_module.verify()
-    rewrite_module.verify()
-
-    pdl_rewrite_op = next(
-        op for op in rewrite_module.walk() if isinstance(op, pdl.RewriteOp)
-    )
-
-    stream = StringIO()
-
-    ctx = MLContext()
-    ctx.load_dialect(arith.Arith)
-
-    PatternRewriteWalker(
-        PDLRewritePattern(pdl_rewrite_op, ctx, file=stream),
-        apply_recursively=False,
-    ).rewrite_module(input_module)
-
-    assert input_module.is_structurally_equivalent(output_module)
-
-
-def add_zero_input():
-    @ModuleOp
-    @Builder.implicit_region
-    def ir_module():
-        with ImplicitBuilder(func.FuncOp("impl", ((), ())).body):
-            x = arith.Constant.from_int_and_width(4, 32)
-            y = arith.Constant.from_int_and_width(0, 32)
-            z = arith.Addi(x, y)
-            func.Return(z)
-
-    return ir_module
-
-
-def add_zero_output():
-    @ModuleOp
-    @Builder.implicit_region
-    def ir_module():
-        with ImplicitBuilder(func.FuncOp("impl", ((), ())).body):
-            x = arith.Constant.from_int_and_width(4, 32)
-            _y = arith.Constant.from_int_and_width(0, 32)
-            func.Return(x)
-
-    return ir_module
-
-
-def add_zero_pdl():
-    # The rewrite below matches the second addition as root op
-    @ModuleOp
-    @Builder.implicit_region
-    def pdl_module():
-        with ImplicitBuilder(pdl.PatternOp(2, None).body):
-            # Type i32
-            pdl_i32 = pdl.TypeOp().result
-
-            # LHS: i32
-            lhs = pdl.OperandOp().results[0]
-
-            # Constant 0: i32
-            zero = pdl.AttributeOp(value=IntegerAttr(0, 32)).results[0]
-            rhs_op = pdl.OperationOp(
-                op_name=StringAttr("arith.constant"),
-                attribute_value_names=ArrayAttr([StringAttr("value")]),
-                attribute_values=[zero],
-                type_values=[pdl_i32],
-            ).op
-            rhs = pdl.ResultOp(IntegerAttr(0, 32), parent=rhs_op).val
-
-            # LHS + 0
-            sum = pdl.OperationOp(
-                StringAttr("arith.addi"),
-                operand_values=[lhs, rhs],
-                type_values=[pdl_i32],
-            ).op
-
-            with ImplicitBuilder(pdl.RewriteOp(sum).body):
-                pdl.ReplaceOp(sum, repl_values=[lhs])
-
-    return pdl_module
 
 
 def test_interpreter_functions():
