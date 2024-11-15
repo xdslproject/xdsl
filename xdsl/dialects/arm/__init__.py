e0b8472ece7f274a0bfbf4ad8fcd832704c89a65
emmau678 <eu233@Emma-laptop>
2024-11-15 15:19:36 +0000

    move docstring to top

--- xdsl/dialects/arm/__init__.py
+++ xdsl/dialects/arm/__init__.py
@@ -1,11 +1,11 @@
-from xdsl.ir import Dialect
-
-from .register import IntRegisterType
-
 """
 ARM dialect, based on the ISA specification in:
 https://developer.arm.com/documentation/102374/0101/Overview
 """
+
+from xdsl.ir import Dialect
+
+from .register import IntRegisterType
 
 ARM = Dialect(
     "arm",
