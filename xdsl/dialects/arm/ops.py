4ab62b16f935b3f63b4a8b4984bc950aa05c4fd9
emmau678 <eu233@Emma-laptop>
2024-11-19 11:34:43 +0000

    remove get any reg op

--- xdsl/dialects/arm/ops.py
+++ xdsl/dialects/arm/ops.py
@@ -1,38 +1,28 @@
-from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from abc import ABC
 
 from xdsl.irdl import IRDLOperation, irdl_op_definition, result_def
 
-from .register import ARMRegisterType
-
-R1InvT = TypeVar("R1InvT", bound=ARMRegisterType)
+from .register import IntRegisterType
 
 
-class ARMOp(IRDLOperation, ABC):
+class ARMOperation(IRDLOperation, ABC):
     """
     Base class for operations that can be a part of ARM assembly printing.
     """
 
-    @abstractmethod
-    def assembly_line(self) -> str | None:
-        raise NotImplementedError()
+    ...
 
 
-class GetAnyRegisterOperation(Generic[R1InvT], ARMOp, ABC):
+@irdl_op_definition
+class GetRegisterOp(ARMOperation):
     """
     This instruction allows us to create an SSAValue for a given register name.
     """
 
-    result = result_def(R1InvT)
-
-    def __init__(self, register_type: R1InvT):
-        super().__init__(result_types=[register_type])
-
-    def assembly_line(self) -> str | None:
-        return None
-
-
-@irdl_op_definition
-class GetRegisterOp(GetAnyRegisterOperation[ARMRegisterType]):
     name = "arm.get_register"
+
+    result = result_def(IntRegisterType)
     assembly_format = "attr-dict `:` type($result)"
+
+    def __init__(self, register_type: IntRegisterType):
+        super().__init__(result_types=[register_type])
