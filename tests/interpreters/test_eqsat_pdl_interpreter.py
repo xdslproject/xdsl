88647a1a9b1434a6903520a2b7868e491650cc33
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2024-12-05 15:16:39 +0000

    corrected types...

--- tests/interpreters/test_eqsat_pdl_interpreter.py
+++ tests/interpreters/test_eqsat_pdl_interpreter.py
@@ -11,7 +11,7 @@ def test_match_type():
 
     pdl_op = pdl.TypeOp()
     ssa_value = pdl_op.result
-    ssa_value = eqsat.EClassOp(ssa_value)
+    ssa_value = eqsat.EClassOp(ssa_value).result
     xdsl_value = StringAttr("a")
 
     # New value
@@ -33,7 +33,7 @@ def test_match_fixed_type():
     pdl_op = pdl.TypeOp(IntegerType(32))
     xdsl_value = IntegerType(32)
     ssa_value = pdl_op.result
-    ssa_value = eqsat.EClassOp(ssa_value)
+    ssa_value = eqsat.EClassOp(ssa_value).result
 
     assert matcher.match_type(ssa_value, pdl_op, xdsl_value)
     assert matcher.matching_context == {ssa_value: xdsl_value}
@@ -45,7 +45,7 @@ def test_not_match_fixed_type():
     pdl_op = pdl.TypeOp(IntegerType(64))
     xdsl_value = IntegerType(32)
     ssa_value = pdl_op.result
-    ssa_value = eqsat.EClassOp(ssa_value)
+    ssa_value = eqsat.EClassOp(ssa_value).result
 
     assert not matcher.match_type(ssa_value, pdl_op, xdsl_value)
     assert matcher.matching_context == {}
