ed2f6e30b0b6f105a07492c8cb13eb6d637f7e17
Sasha Lopoukhine <superlopuh@gmail.com>
2024-12-13 19:09:03 +0000

    dynamic version

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from collections.abc import Mapping
 from typing import cast
 
@@ -5,8 +6,14 @@ from setuptools import Command, find_packages, setup
 
 import versioneer
 
+if "XDSL_VERSION_OVERRIDE" in os.environ:
+    version = os.environ["XDSL_VERSION_OVERRIDE"]
+else:
+    version = versioneer.get_version()
+
+
 setup(
-    version=versioneer.get_version(),
+    version=version,
     cmdclass=cast(Mapping[str, type[Command]], versioneer.get_cmdclass()),
     packages=find_packages(),
 )
