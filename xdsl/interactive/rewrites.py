d43ac2fcfb301dd132bc267507eebb6a73cdee0a
Sasha Lopoukhine <superlopuh@gmail.com>
2024-12-16 10:30:31 +0000

    interactive: use individual rewrite pass directly instead of helpers (#3642)
    It doesn't seem that this abstraction currently adds anything, both the
    available pass and pass itself are already immutable and can be reasoned
    about directly.

--- xdsl/interactive/rewrites.py
+++ xdsl/interactive/rewrites.py
@@ -1,68 +1,23 @@
 from collections.abc import Sequence
-from typing import NamedTuple
 
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.interactive.passes import AvailablePass
 from xdsl.pattern_rewriter import PatternRewriter, RewritePattern
 from xdsl.traits import HasCanonicalizationPatternsTrait
 from xdsl.transforms import individual_rewrite
-from xdsl.utils.parse_pipeline import PipelinePassSpec
-
-
-class IndividualRewrite(NamedTuple):
-    """
-    Type alias for a possible rewrite, described by an operation and pattern name.
-    """
-
-    operation: str
-    pattern: str
-
-
-class IndexedIndividualRewrite(NamedTuple):
-    """
-    Type alias for a specific rewrite pattern, additionally consisting of its operation index.
-    """
-
-    operation_index: int
-    rewrite: IndividualRewrite
-
-
-def convert_indexed_individual_rewrites_to_available_pass(
-    rewrites: Sequence[IndexedIndividualRewrite], current_module: ModuleOp
-) -> tuple[AvailablePass, ...]:
-    """
-    Function that takes a tuple of rewrites, converts each rewrite into an IndividualRewrite pass and returns the tuple of AvailablePass.
-    """
-    rewrites_as_pass_list: tuple[AvailablePass, ...] = ()
-    for op_idx, (op_name, pat_name) in rewrites:
-        rewrite_pass = individual_rewrite.ApplyIndividualRewritePass
-        rewrite_spec = PipelinePassSpec(
-            name=rewrite_pass.name,
-            args={
-                "matched_operation_index": (op_idx,),
-                "operation_name": (op_name,),
-                "pattern_name": (pat_name,),
-            },
-        )
-        op = list(current_module.walk())[op_idx]
-        rewrites_as_pass_list = (
-            *rewrites_as_pass_list,
-            (AvailablePass(f"{op}:{op_name}:{pat_name}", rewrite_pass, rewrite_spec)),
-        )
-    return rewrites_as_pass_list
 
 
 def get_all_possible_rewrites(
     module: ModuleOp,
     rewrite_by_name: dict[str, dict[str, RewritePattern]],
-) -> Sequence[IndexedIndividualRewrite]:
+) -> Sequence[AvailablePass]:
     """
     Function that takes a sequence of IndividualRewrite Patterns and a ModuleOp, and
     returns the possible rewrites.
     Issue filed: https://github.com/xdslproject/xdsl/issues/2162
     """
 
-    res: list[IndexedIndividualRewrite] = []
+    res: list[AvailablePass] = []
 
     for op_idx, matched_op in enumerate(module.walk()):
         pattern_by_name = rewrite_by_name.get(matched_op.name, {}).copy()
@@ -78,9 +33,14 @@ def get_all_possible_rewrites(
             rewriter = PatternRewriter(cloned_op)
             pattern.match_and_rewrite(cloned_op, rewriter)
             if rewriter.has_done_action:
+                p = individual_rewrite.ApplyIndividualRewritePass(
+                    op_idx, cloned_op.name, pattern_name
+                )
                 res.append(
-                    IndexedIndividualRewrite(
-                        op_idx, IndividualRewrite(cloned_op.name, pattern_name)
+                    AvailablePass(
+                        f"{cloned_op}:{cloned_op.name}:{pattern_name}",
+                        individual_rewrite.ApplyIndividualRewritePass,
+                        p.pipeline_pass_spec(),
                     )
                 )
 
