949e6d44ea37ab8654c99ea902bcef640b1b0fcd
Sasha Lopoukhine <superlopuh@gmail.com>
2024-12-16 09:21:22 +0000

    interactive: use individual rewrite pass directly instead of helpers

--- xdsl/interactive/get_all_available_passes.py
+++ xdsl/interactive/get_all_available_passes.py
@@ -6,10 +6,7 @@ from xdsl.interactive.passes import (
     get_condensed_pass_list,
     get_new_registered_context,
 )
-from xdsl.interactive.rewrites import (
-    convert_indexed_individual_rewrites_to_available_pass,
-    get_all_possible_rewrites,
-)
+from xdsl.interactive.rewrites import get_all_possible_rewrites
 from xdsl.ir import Dialect
 from xdsl.parser import Parser
 from xdsl.passes import ModulePass
@@ -34,19 +31,14 @@ def get_available_pass_list(
 
     current_module = apply_passes_to_module(current_module, ctx, pass_pipeline)
 
-    # get all rewrites
-    rewrites = get_all_possible_rewrites(
+    # get all individual rewrites
+    individual_rewrites = get_all_possible_rewrites(
         current_module,
         rewrite_by_names_dict,
-    )
-    # transform rewrites into passes
-    rewrites_as_pass_list = convert_indexed_individual_rewrites_to_available_pass(
-        rewrites, current_module
     )
     # merge rewrite passes with "other" pass list
     if condense_mode:
         pass_list = get_condensed_pass_list(current_module, all_passes)
-        return pass_list + rewrites_as_pass_list
     else:
         pass_list = tuple(AvailablePass(p.name, p, None) for _, p in all_passes)
-        return pass_list + rewrites_as_pass_list
+    return pass_list + tuple(individual_rewrites)
