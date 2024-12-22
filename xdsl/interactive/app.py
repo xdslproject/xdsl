d63f14bcb00249d69c205cd05ae013e53edb4e72
Dalia Shaaban <144673861+dshaaban01@users.noreply.github.com>
2024-12-22 15:29:47 +0100

    formatting

--- xdsl/interactive/app.py
+++ xdsl/interactive/app.py
@@ -455,9 +455,7 @@ class InputApp(App[None]):
 
         # if selected_pass_value has arguments, call get_arguments_function to push screen for user input
         if fields(selected_pass_value) and selected_pass_spec is None:
-            self.get_pass_arguments(
-                selected_pass_value, root_to_child_pass_list
-            )
+            self.get_pass_arguments(selected_pass_value, root_to_child_pass_list)
         else:
             # if selected_pass_value contains no arguments add the selected pass to pass_pipeline
             if selected_pass_spec is None:
