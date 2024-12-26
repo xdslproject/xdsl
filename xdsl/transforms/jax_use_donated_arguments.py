548df83ec7572be4d85cb17a55d70c6a2a753ecd
Max Manainen <maximmanainen@gmail.com>
2024-12-26 13:14:44 +0100

    simplify

--- xdsl/transforms/jax_use_donated_arguments.py
+++ xdsl/transforms/jax_use_donated_arguments.py
@@ -1,14 +1,14 @@
+import itertools
 from dataclasses import dataclass
 
 from xdsl.context import MLContext
 from xdsl.dialects import builtin
 from xdsl.dialects.bufferization import MaterializeInDestinationOp
-from xdsl.dialects.builtin import Attribute, FunctionType, TensorType
+from xdsl.dialects.builtin import FunctionType, TensorType
 from xdsl.dialects.func import FuncOp, ReturnOp
 from xdsl.ir import Operation, SSAValue
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
-    GreedyRewritePatternApplier,
     PatternRewriter,
     PatternRewriteWalker,
     RewritePattern,
@@ -39,10 +39,11 @@ class SubstituteDonatedTensors(RewritePattern):
 
         new_ops: list[Operation] = []
         new_outputs: list[SSAValue] = []
-        matched_output_idxes: set[int] = set()
+        keep_output_param: list[bool] = []
 
-        for output_idx, output in enumerate(op.arguments):
+        for output in op.arguments:
             final_output = output
+            keep_output_param.append(True)
             for i, arg in enumerate(donated_inputs):
                 if arg.type == output.type:
                     new_ops.append(
@@ -52,20 +53,15 @@ class SubstituteDonatedTensors(RewritePattern):
                         )
                     )
                     final_output = new_ops[-1].results[0]
-                    matched_output_idxes.add(output_idx)
+                    keep_output_param[-1] = False
                     break
             new_outputs.append(final_output)
 
         output_types = list(func_op.function_type.outputs.data)
 
         if self.remove_matched_outputs:
-            new_outputs_trimmed: list[SSAValue] = []
-            output_types_trimmed: list[Attribute] = []
-            for i in range(len(new_outputs)):
-                if i not in matched_output_idxes:
-                    new_outputs_trimmed.append(new_outputs[i])
-                    output_types_trimmed.append(output_types[i])
-            new_outputs, output_types = new_outputs_trimmed, output_types_trimmed
+            output_types = list(itertools.compress(output_types, keep_output_param))
+            new_outputs = list(itertools.compress(new_outputs, keep_output_param))
 
         func_op.function_type = FunctionType.from_lists(
             func_op.function_type.inputs.data, output_types
@@ -81,12 +77,9 @@ class JaxUseDonatedArguments(ModulePass):
     remove_matched_outputs: bool = False
 
     def apply(self, ctx: MLContext, op: builtin.ModuleOp) -> None:
-        the_one_pass = PatternRewriteWalker(
-            GreedyRewritePatternApplier(
-                [SubstituteDonatedTensors(self.remove_matched_outputs)]
-            ),
+        PatternRewriteWalker(
+            SubstituteDonatedTensors(self.remove_matched_outputs),
             apply_recursively=False,
             walk_reverse=True,
             walk_regions_first=True,
-        )
-        the_one_pass.rewrite_module(op)
+        ).rewrite_module(op)
