a32e6d5a2a9aad8dd14b2b09181996d067a47054
Sasha Lopoukhine <superlopuh@gmail.com>
2025-01-06 20:26:16 +0000

    interpreter: add type property to ShapedArray

--- tests/interpreters/test_shaped_array.py
+++ tests/interpreters/test_shaped_array.py
@@ -1,5 +1,11 @@
+from xdsl.dialects.builtin import i32
 from xdsl.interpreters.shaped_array import ShapedArray
 from xdsl.interpreters.utils.ptr import TypedPtr
+
+
+def test_shaped_array_type():
+    array = ShapedArray(TypedPtr.new_int32([1, 2, 3]), [3])
+    assert array.type == i32
 
 
 def test_shaped_array_offset():
