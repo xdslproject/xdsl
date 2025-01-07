906155769284028ab17b748be9fac7ca0e0cb0a5
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-01-07 16:43:59 +0100

    backend: (csl) use iter_attrs method for DenseIntOrFPElementsArray
    to become unaffected by the representation changes

--- xdsl/backend/csl/print_csl.py
+++ xdsl/backend/csl/print_csl.py
@@ -452,8 +452,8 @@ class CslPrintContext:
                 return str(val.data)
             case StringAttr() as s:
                 return f'"{s.data}"'
-            case DenseIntOrFPElementsAttr(data=data, type=typ):
-                return f"{self.mlir_type_to_csl_type(typ)} {{ {', '.join(self.attribute_value_to_str(d) for d in data)} }}"
+            case DenseIntOrFPElementsAttr(type=typ):
+                return f"{self.mlir_type_to_csl_type(typ)} {{ {', '.join(self.attribute_value_to_str(a) for a in attr.iter_attrs())} }}"
             case _:
                 return f"<!unknown value {attr}>"
 
