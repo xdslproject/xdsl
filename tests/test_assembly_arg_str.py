b2a43788613fe31c4331d07ee663e30fe970fceb
emmau678 <emmau678@gmail.com>
2025-01-13 14:09:09 +0000

    Add unit tests for other valid and invalid SSAValues not already being tested

--- tests/test_assembly_arg_str.py
+++ tests/test_assembly_arg_str.py
@@ -1,6 +1,10 @@
+import pytest
+
 from xdsl.dialects.arm.assembly import assembly_arg_str
 from xdsl.dialects.arm.attrs import LabelAttr
 from xdsl.dialects.arm.register import IntRegisterType
+from xdsl.dialects.test import TestType
+from xdsl.utils.test_value import TestSSAValue
 
 
 def test_assembly_arg_str_ARMRegister():
@@ -11,3 +15,14 @@ def test_assembly_arg_str_ARMRegister():
 def test_assembly_arg_str_LabelAttr():
     arg = LabelAttr("main")
     assert assembly_arg_str(arg) == "main"
+
+
+def test_assembly_arg_str_SSAValue_valid():
+    arg = TestSSAValue(IntRegisterType("x1"))
+    assert assembly_arg_str(arg) == "x1"
+
+
+def test_assembly_arg_str_SSAValue_invalid():
+    arg = TestSSAValue(TestType("foo"))
+    with pytest.raises(ValueError, match="Unexpected argument type"):
+        assembly_arg_str(arg)
