2d3e5c0202c24343fbf4627cd097f7ad927248dc
Mathieu Fehr <mathieu.fehr@gmail.com>
2025-01-22 03:00:06 +0000

    Some basic notebooks

---
+++ docs/mlir_winter_school/attribute.py
@@ -0,0 +1,92 @@
+import marimo
+
+__generated_with = "0.10.15"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    from xdsl.dialects.builtin import IndexType, IntegerType
+    from xdsl.ir import Attribute
+    from xdsl.utils.exceptions import VerifyException
+
+    return Attribute, IndexType, IntegerType, VerifyException, mo
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        # Attributes (and types)
+
+        ## What is an attribute
+
+        Attributes store static information that can be used in MLIR IR.
+        Attributes are present in both the operation properties and attribute dictionary, and encode the type for each SSA Value.
+
+        Examples of attributes are:
+
+        * `i32`, a signless integer type of bitwidth 32
+        * `!llvm.void`, representing `void` in the LLVM dialect
+        * `4 : i32`, the constant 4 encoded on 32 bits
+        * `memref<5xi32>`, a memory reference to 5 32-bits integers
+
+        In particular, types are a subset of attributes, and only types are allowed to encode SSA value types.
+
+        ## Instantiating attributes
+
+        Each attribute is an instance of a given attribute definition. Each attribute definition is represented as a single Python type, which decides how the type parameters are stored in memory. For instance, `i32` is an instance of an `IntegerType` with `32` for its bitwidth parameter.
+        """
+    )
+    return
+
+
+@app.cell
+def _(IntegerType):
+    print(IntegerType(32))
+    print(IntegerType(64))
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""Each attribute definition defines a set of accepted parameters. `IntegerType` for instance has only a single bitwidth parameter that should be a positive integer. When a set of parameters is invalid, the attribute should return a `VerifyException` upon instantiation."""
+    )
+    return
+
+
+@app.cell
+def _(IntegerType, VerifyException):
+    try:
+        print(IntegerType(-1))
+    except VerifyException() as e:
+        print(e)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""Given an `Attribute` variable, the python `isinstance` function allows to match a specific attribute:"""
+    )
+    return
+
+
+@app.cell
+def _(Attribute, IndexType, IntegerType):
+    def print_if_integer_type(attr: Attribute):
+        if isinstance(attr, IntegerType):
+            print(attr)
+        else:
+            print("Not an integer type")
+
+    print_if_integer_type(IntegerType(32))
+    print_if_integer_type(IndexType())
+    return (print_if_integer_type,)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ docs/mlir_winter_school/operation.py
@@ -0,0 +1,83 @@
+import marimo
+
+__generated_with = "0.10.15"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    from xdsl.dialects.arith import AddiOp
+    from xdsl.dialects.builtin import IntegerType
+    from xdsl.printer import Printer
+    from xdsl.utils.test_value import TestSSAValue
+
+    return AddiOp, IntegerType, Printer, TestSSAValue, mo
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        # Operations
+
+        ## What is an operation
+
+        An operation is the basic unit of execution. It usually reprents a computation that can be arbitrarily complex, or can represent information. An operation is an instantiation of an op, otherwise known as an operation definition.
+
+        Example of ops are:
+
+        * `arith.addi`, which adds two integer values together
+        * `linalg.matmul`, which multiplies two matrices together
+        * `func.func`, which defines a function
+
+        Operations are composed of:
+
+        * A name,
+        * A list of operands, which are dynamic values taken as input
+        * A list of results, which are dynamic values outputted by the operation
+        * Properties, which are encoded as an attribute dictionary, which correspond to additional information operations may need (such as flags)
+        * An attribute dictionary, which correspond to additional discardable information that users can attach to operations.
+        * A list of regions, which correspond to code blocks that may be executed by the operation
+        * A list of successors (basic block arguments), which are the possible block that the operation may give control flow to. Only terminator operations may have successors.
+
+        ## Instantiating operations
+
+        Each op is encoded as a Python class, and each operation is an instantiation of the class.
+
+        """
+    )
+    return
+
+
+@app.cell
+def _(AddiOp, IntegerType, Printer, TestSSAValue):
+    # Create two SSA values of type i32 for testing
+    lhs = TestSSAValue(IntegerType(32))
+    rhs = TestSSAValue(IntegerType(32))
+
+    # Create an operation
+    addi_op = AddiOp(lhs, rhs)
+
+    # Print it
+    print(addi_op)
+
+    # Print it with the generic format
+    Printer(print_generic_format=True).print(addi_op)
+    return addi_op, lhs, rhs
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        Note that here, a default attribute was created for the `overflowFlags` property.
+
+        """
+    )
+    return
+
+
+if __name__ == "__main__":
+    app.run()
