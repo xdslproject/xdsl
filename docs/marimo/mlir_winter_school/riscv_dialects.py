7d2e31c7a91128ab7929c0996db1d649c04940c2
Sasha Lopoukhine <superlopuh@gmail.com>
2025-01-31 09:43:46 +0100

    wip riscv notebook

--- docs/marimo/mlir_winter_school/riscv_dialects.py
+++ docs/marimo/mlir_winter_school/riscv_dialects.py
@@ -48,7 +48,7 @@ def _(mo):
         Here are some resources on the RISC-V ISA and assembly format:
 
         * [Cheat Sheet](https://www.cl.cam.ac.uk/teaching/1617/ECAD+Arch/files/docs/RISCVGreenCardv8-20151013.pdf)
-        * [Assembly Manual](https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/notebooks/RISCV/RISCV_ASM.pdf))
+        * [Assembly Manual](https://github.com/riscv-non-isa/riscv-asm-manual/blob/main/src/asm-manual.adoc)
         * [ISA Manual](https://github.com/riscv/riscv-isa-manual)
         * [Detailed Instruction Definition](https://msyksphinz-self.github.io/riscv-isadoc/html/rvi.html)
         * [ABI](https://d3s.mff.cuni.cz/files/teaching/nswi200/202324/doc/riscv-abi.pdf)
@@ -60,7 +60,7 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## Some Example Functions""")
+    mo.md(r"""## Simple Arithmetic in RISC-V""")
     return
 
 
@@ -68,86 +68,43 @@ def _(mo):
 def _(mo):
     mo.md(
         r"""
-        Here are some functions in C:
+        Here is a simple C function and its corresponding assembly:
 
         ```C
-        int square(int num) {
-            return num * num;
-        }
-        // Assume n non-negative
-        int fib(int n) {
-            int a = 1;
-            int b = 1;
-            for (int i = 0; i < n; ++i) {
-                int c = a + b;
-                a = b;
-                b = c;
-            }
-            return a;
+        int fused_multiply_add(int a, int b, int c) {
+            return a * b + c;
         }
         ```
 
-        ([Compiler Explorer](https://godbolt.org/z/bedsKMWMc))
+        ```asm
+        # Label corresponding to the function name
+        # Arguments passed in a0, a1, a2 registers
+        # Result expected to be stored in a0 register at the end of execution
+        fused_multiply_add:
+            # a0 <- a0 * a1
+            mul     a0, a1, a0
+            # a0 <- a0 + a2
+            add     a0, a0, a2
+            # Assembly pseudo-operation that jumps to the caller-passed return address
+            ret
+        ```
+
+        ([Compiler Explorer](https://godbolt.org/z/veWo8rbnK))
         """
     )
     return
-
-
-@app.cell(hide_code=True)
-def _():
-    fib_text = """
-    # The label corresponding to the fib function
-    fib:
-            ble     zero, a0, .LBB1_4
-            li      a2, 1
-            li      a3, 1
-    # Label inserted by Clang corresponding to the loop body
-    .LBB1_2:
-            add     a4, a2, a3
-            addi    a0, a0, -1
-            mv      a1, a3
-            mv      a2, a3
-            mv      a3, a4
-            # Jump to beginning of loop body if need to do more work
-            bne     zero, a0, .LBB1_2
-            # Moves the result value to the first integer return register, as specified by ABI
-            mv      a0, a1
-            ret
-    # Label inserted by Clang corresponding to the case where the input is <= 0, can just return 1
-    .LBB1_4:
-            li      a0, 1
-            ret
-    """
-    return (fib_text,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
-        And their corresponding RISC-V assembly:
-
-        ```asm
-        # The label corresponding to the square function
-        square:
-                # Results and arguments are stored in a0-a7
-                mul     a0, a0, a0
-                # Assembly pseudo-operation that jumps to the caller-passed return address
-                ret
-        ```
+        A few aspects of assembly make it difficult to reason about directly.
+        It doesn't track dependencies explicitly, making it difficult to know what the values in the inputs of an operation are, and whether its results will be used.
+        Control flow is done via jumps to labels, such as the `fused_multiply_add` above.
+        In order to represent and reason about assembly-level code, we introduce a set of backend dialects: `riscv`, `riscv_func`, and `riscv_cf`.
         """
     )
-    return
-
-
-@app.cell(hide_code=True)
-def _(fib_text, mo):
-    mo.md(fr"""
-
-    ```asm
-    {fib_text}
-    ```
-    """)
     return
 
 
@@ -205,7 +162,7 @@ def _(Block, Builder, InsertPoint, Printer, riscv):
     builder = Builder(InsertPoint.at_end(block))
 
     # Explicitly specify result registers
-    addi_op = builder.insert(riscv.AddiOp(a0, a1, rd=riscv.Registers.A2))
+    addi_op = builder.insert(riscv.AddiOp(a0, 42, rd=riscv.Registers.A2))
     sub_op = builder.insert(riscv.SubOp(a0, a1, rd=riscv.Registers.A3))
 
     # Don't specify result registers
@@ -217,7 +174,26 @@ def _(Block, Builder, InsertPoint, Printer, riscv):
 
 @app.cell(hide_code=True)
 def _(mo):
+    mo.md(
+        r"""
+        Almost all the operations in the riscv dialect correspond to the instructions in the RISC-V ISA.
+        RISC-V assembly has operations that correspond 1:1 with instructions, as well as pseudo-operations that are syntactic sugar for other operations, such as the `nop` instruction that desugars to `addi x0, x0, 0`.
+
+        [The RISC-V Assembly Manual](https://github.com/riscv-non-isa/riscv-asm-manual/blob/main/src/asm-manual.adoc#a-listing-of-standard-risc-v-pseudoinstructions) has the list of pseudo-instructions in RISC-V.
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
     mo.md(r"""## Printing Assembly""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""All operations in the `riscv` dialect implement an `assembly_line` function that communicates how they should be displayed in assembly:""")
     return
 
 
@@ -230,7 +206,7 @@ def _(block):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## Functions and the `riscv_cf` Dialect""")
+    mo.md(r"""## Functions and the `riscv_func` Dialect""")
     return
 
 
@@ -271,6 +247,34 @@ def _(Parser, ctx, mul_ir, riscv, xmo):
 def _(mo):
     mo.md(r"""## Unstructured Control Flow and the `riscv_cf` Dialect""")
     return
+
+
+@app.cell(hide_code=True)
+def _():
+    fib_text = """\
+    # The label corresponding to the fib function
+    fib:
+        bge zero, a0, .LBB1_4
+        li a2, 1
+        li a3, 1
+    # Label inserted by Clang corresponding to the loop body
+    .LBB1_2:
+        add a4, a2, a3
+        addi a0, a0, -1
+        mv a1, a3
+        mv a2, a3
+        mv a3, a4
+        # Jump to beginning of loop body if need to do more work
+        bne zero, a0, .LBB1_2
+        # Moves the result value to the first integer return register, as specified by ABI
+        mv a0, a1
+        ret
+    # Label inserted by Clang corresponding to the case where the input is <= 0, can just return 1
+    .LBB1_4:
+        li a0, 1
+        ret
+    """
+    return (fib_text,)
 
 
 @app.cell(hide_code=True)
@@ -343,20 +347,42 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""As an exercise, write the MLIR IR corresponding to the fib function above:""")
+    mo.md(
+        r"""
+        In this exercise, we will rewrite the following function to assembly-level IR:
+
+        ```C
+        // Assume n non-negative
+        int fib(int n) {
+            int a = 1;
+            int b = 1;
+            for (int i = 0; i < n; ++i) {
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+            return a;
+        }
+        ```
+        """
+    )
     return
 
 
 @app.cell(hide_code=True)
-def _(comment_only_line, fib_text, mo):
-    fib_cleaned = "\n".join(line for line in fib_text.splitlines() if not comment_only_line.findall(line))
-    mo.md(f"""
-    Goal:
+def _(fib_text, mo):
+    mo.md(fr"""
 
     ```asm
-    {fib_cleaned}
+    {fib_text}
     ```
     """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(comment_only_line, fib_text):
+    fib_cleaned = "\n".join(line for line in fib_text.splitlines() if not comment_only_line.findall(line))
     return (fib_cleaned,)
 
 
@@ -369,38 +395,51 @@ def _(mo):
     return (fib_editor,)
 
 
-@app.cell
-def _(Parser, ctx, fib_editor, mo, riscv):
+@app.cell(hide_code=True)
+def _(fib_editor, mo):
+    mo.md(f"""
+    Modify the following IR to reduce the diff below:
+
+    {fib_editor}
+
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(Parser, ctx, difflib, fib_cleaned, fib_editor, mo, riscv):
     try:
         _module = Parser(ctx, fib_editor.value).parse_module()
         _module.verify()
+        _asm = riscv.riscv_code(_module)
+        _diff = "\n".join(difflib.ndiff(_asm.splitlines(), fib_cleaned.splitlines()))
         _res = f"""
-    Result:
-
     ```asm
-    {riscv.riscv_code(_module)}
+    {_diff}
     ```
     """
     except Exception as e:
         _res = f"{type(e).__name__}: {e}"
-        # raise e from e
 
     mo.md(f"""
-    IR:
-
-    {fib_editor}
+    Diff:
 
     {_res}
-
     """)
     return
 
 
 @app.cell
 def _():
+    import difflib
+    return (difflib,)
+
+
+@app.cell
+def _():
     # Solution
 
-    _ = """
+    _ = """\
     riscv_func.func @fib(%num : !riscv.reg<a0>) -> !riscv.reg<a0> {
       %zero = riscv.get_register : !riscv.reg<zero>
       riscv_cf.bge %zero: !riscv.reg<zero>, %num :!riscv.reg<a0>, ^4(), ^1()
@@ -410,15 +449,19 @@ def _():
       riscv_cf.branch ^2 (%num : !riscv.reg<a0>, %a_init : !riscv.reg<a2>, %b_init : !riscv.reg<a3>)
     ^2(%i : !riscv.reg<a0>, %a_in : !riscv.reg<a2>, %b_in : !riscv.reg<a3>):
       riscv.label ".LBB1_2"
-      %c = riscv.add %a_in, %b_in : (!riscv.reg<a2>, !riscv.reg<a3>) -> !riscv.reg<a4>
+      %sum = riscv.add %a_in, %b_in : (!riscv.reg<a2>, !riscv.reg<a3>) -> !riscv.reg<a4>
       %i_next = riscv.addi %i, -1 : (!riscv.reg<a0>) -> !riscv.reg<a0>
-      // TODO
+      %temp = riscv.mv %b_in : (!riscv.reg<a3>) -> !riscv.reg<a1>
+      %a_next = riscv.mv %b_in : (!riscv.reg<a3>) -> !riscv.reg<a2>
+      %b_next = riscv.mv %sum : (!riscv.reg<a4>) -> !riscv.reg<a3>
       riscv_cf.bne %zero: !riscv.reg<zero>, %num :!riscv.reg<a0>, ^2(), ^3()
     ^3():
+      %res = riscv.mv %temp : (!riscv.reg<a1>) -> !riscv.reg<a0>
       riscv_func.return %num : !riscv.reg<a0>
     ^4():
       riscv.label ".LBB1_4"
-      riscv_func.return %num : !riscv.reg<a0>
+      %res_early = riscv.li 1 : !riscv.reg<a0>
+      riscv_func.return %res_early : !riscv.reg<a0>
     }
     """
     return
