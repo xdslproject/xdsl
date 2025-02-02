3ba4612af7bf8388405c6cf9b0812a130517ff67
kaylendog <actuallyori@gmail.com>
2025-02-02 13:46:35 +0000

    feat: ptr add, reconciliation

--- xdsl/transforms/convert_ptr_to_llvm.py
+++ xdsl/transforms/convert_ptr_to_llvm.py
@@ -69,7 +69,7 @@ class ConvertFuncOp(RewritePattern):
             if not isinstance(arg_type := arg.type, ptr.PtrType):
                 continue
 
-            arg.type = ptr.PtrType()
+            arg.type = llvm.LLVMPointerType.opaque()
 
             if not arg.uses:
                 continue
@@ -107,12 +107,116 @@ class ConvertReturnOp(RewritePattern):
         rewriter.replace_matched_op(func.ReturnOp(*new_arguments))
 
 
+@dataclass
+class ConvertCallOp(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: func.CallOp, rewriter: PatternRewriter, /):
+        if not any(
+            isinstance(arg.type, ptr.PtrType) for arg in op.arguments
+        ) and not any(isinstance(type, ptr.PtrType) for type in op.result_types):
+            return
+
+        # rewrite arguments
+        insert_point = InsertPoint.before(op)
+        new_arguments: list[SSAValue] = []
+
+        # insert `memref -> ptr` casts for memref arguments values
+        for argument in op.arguments:
+            if isinstance(argument.type, ptr.PtrType):
+                rewriter.insert_op(
+                    cast_op := builtin.UnrealizedConversionCastOp.get(
+                        [argument], [llvm.LLVMPointerType.opaque()]
+                    ),
+                    insert_point,
+                )
+                new_arguments.append(cast_op.results[0])
+            else:
+                new_arguments.append(argument)
+
+        insert_point = InsertPoint.after(op)
+        new_results: list[SSAValue] = []
+
+        #  insert `ptr -> memref` casts for return values
+        for result in op.results:
+            if isinstance(result.type, ptr.PtrType):
+                rewriter.insert_op(
+                    cast_op := builtin.UnrealizedConversionCastOp.get(
+                        [result],
+                        [result.type],
+                    ),
+                    insert_point,
+                )
+                new_results.append(cast_op.results[0])
+            else:
+                new_results.append(result)
+
+        new_return_types = [
+            llvm.LLVMPointerType.opaque() if isinstance(type, ptr.PtrType) else type
+            for type in op.result_types
+        ]
+
+        rewriter.replace_matched_op(
+            func.CallOp(op.callee, new_arguments, new_return_types)
+        )
+
+
+class ConvertPtrAddOp(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: ptr.PtrAddOp, rewriter: PatternRewriter, /):
+        rewriter.replace_matched_op(llvm.AddOp(op.addr, op.offset))
+
+
+class ReconcileUnrealizedPtrCasts(RewritePattern):
+    """
+    Eliminates two variants of unrealized ptr casts:
+    - `ptr_xdsl.ptr -> memref.MemRef -> ptr_xdsl.ptr`;
+    - `ptr_xdsl.ptr -> memref.memref` where all uses are `ToPtrOp` operations.
+    """
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(
+        self, op: builtin.UnrealizedConversionCastOp, rewriter: PatternRewriter, /
+    ):
+        # preconditions
+        if (
+            len(op.inputs) != 1
+            or len(op.outputs) != 1
+            or not isinstance(op.inputs[0].type, llvm.LLVMPointerType)
+            or not isinstance(op.outputs[0].type, ptr.PtrType)
+        ):
+            return
+
+        # erase ptr -> memref -> ptr cast pairs
+        uses = tuple(use for use in op.outputs[0].uses)
+        for use in uses:
+            if (
+                isinstance(use.operation, builtin.UnrealizedConversionCastOp)
+                and isinstance(use.operation.inputs[0].type, ptr.PtrType)
+                and isinstance(use.operation.outputs[0].type, llvm.LLVMPointerType)
+            ):
+                use.operation.outputs[0].replace_by(op.inputs[0])
+                rewriter.erase_op(use.operation)
+
+        # erase this cast entirely if no uses are remaining
+        if op.outputs[0].uses:
+            return
+
+        rewriter.erase_op(op)
+
+
 class ConvertPtrToLLVMPass(ModulePass):
     name = "convert-ptr-to-llvm"
 
     def apply(self, ctx: MLContext, op: builtin.ModuleOp) -> None:
         PatternRewriteWalker(
             GreedyRewritePatternApplier(
-                [ConvertStoreOp(), ConvertLoadOp(), ConvertFuncOp(), ConvertReturnOp()]
+                [
+                    ConvertStoreOp(),
+                    ConvertLoadOp(),
+                    ConvertFuncOp(),
+                    ConvertReturnOp(),
+                    ConvertCallOp(),
+                    ReconcileUnrealizedPtrCasts(),
+                ]
             )
         ).rewrite_module(op)
