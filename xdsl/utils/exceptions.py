34ab330e9f4ab7229b7ee9329bddec83040b4e95
Emma Urquhart <77412390+emmau678@users.noreply.github.com>
2025-02-06 10:05:34 +0000

    core: Fix multiline error printing (#3849)
    Fix the MultipleSpanParseError to print multiple lines in the error
    output
    
    ---------
    
    Co-authored-by: Alex Rice <alexrice999@hotmail.co.uk>

--- xdsl/utils/exceptions.py
+++ xdsl/utils/exceptions.py
@@ -87,9 +87,10 @@ class MultipleSpansParseError(ParseError):
     ref_text: str | None
     refs: list[tuple[Span, str | None]]
 
-    def __repr__(self) -> str:
-        res = super().__repr__() + "\n"
-        res += self.ref_text or "With respect to:\n"
+    def __str__(self) -> str:
+        res = self.span.print_with_context(self.msg)
+        if self.ref_text is not None:
+            res += self.ref_text + "\n"
         for span, msg in self.refs:
             res += span.print_with_context(msg) + "\n"
         return res
