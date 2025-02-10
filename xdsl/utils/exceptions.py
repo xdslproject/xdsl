249b04412d56b88e4fdb91048e390448ce40d081
Sasha Lopoukhine <superlopuh@gmail.com>
2025-02-10 10:25:20 +0000

    reflow ShrinkException doc comment

--- xdsl/utils/exceptions.py
+++ xdsl/utils/exceptions.py
@@ -50,7 +50,8 @@ class InvalidIRException(Exception):
 
 class ShrinkException(Exception):
     """
-    Exception for test case reduction when used in conjunction with the [Shrink Ray](https://github.com/DRMacIver/shrinkray) reducer.
+    Exception for test case reduction when used in conjunction with the [Shrink Ray](https://github.com/DRMacIver/shrinkray)
+    reducer.
 
     To find a reduced version of a test case, raise this exception on the line of code you want to hit,
     and pass the `--shrink` argument to `xdsl-opt`, by changing its invocation from:
