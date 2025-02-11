9cf7ddbc65f393bd778ce1181a540fd391bc49a6
Emma Urquhart <77412390+emmau678@users.noreply.github.com>
2025-02-11 13:50:47 +0000

    Update xdsl/parser/core.py
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

--- xdsl/parser/core.py
+++ xdsl/parser/core.py
@@ -145,7 +145,7 @@ class Parser(AttrParser):
             value_names = ", ".join(
                 "%" + name for name in self.forward_ssa_references.keys()
             )
-            if len(self.forward_ssa_references.keys()) > 1:
+            self.raise_error(f"value used but not defined: [{value_names}]")
                 self.raise_error(f"values used but not defined: [{value_names}]")
             else:
                 self.raise_error(f"value used but not defined: [{value_names}]")
