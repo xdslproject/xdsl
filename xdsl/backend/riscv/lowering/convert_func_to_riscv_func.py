704ba133c38cefeccab864bef083971960677c71
Timi Adeniran <oluwatimilehinadeniran@gmail.com>
2025-02-14 22:52:23 +0000

    Test new behaviour

--- xdsl/backend/riscv/lowering/convert_func_to_riscv_func.py
+++ xdsl/backend/riscv/lowering/convert_func_to_riscv_func.py
@@ -77,10 +77,13 @@ class LowerFuncCallOp(RewritePattern):
         move_operand_ops, moved_operands = move_to_a_regs(
             register_operands, operand_types
         )
+
+        new_result_value_types = [result.type for result in op.results]
         new_result_types = list(a_regs(op.results))
         new_op = riscv_func.CallOp(op.callee, moved_operands, new_result_types)
+
         move_result_ops, moved_results = move_to_unallocated_regs(
-            new_op.results, new_result_types
+            new_op.results, new_result_value_types
         )
         cast_result_ops = [
             UnrealizedConversionCastOp.get((moved_result,), (old_result.type,))
