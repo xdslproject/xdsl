9e6222c0bee8f255e70d829837291b7503587e0a
emmau678 <emmau678@gmail.com>
2025-03-07 11:13:51 +0000

    dialects [arm]: Add FP/SIMD register type

--- xdsl/dialects/arm/register.py
+++ xdsl/dialects/arm/register.py
@@ -14,6 +14,8 @@ class ARMRegisterType(RegisterType, abc.ABC):
 
 ARM_INDEX_BY_NAME = {f"x{i}": i for i in range(0, 31)}
 
+ARM_FPSIMD_INDEX_BY_NAME = {f"v{i}": i for i in range(0, 32)}
+
 
 @irdl_attr_definition
 class IntRegisterType(ARMRegisterType):
@@ -30,6 +32,27 @@ class IntRegisterType(ARMRegisterType):
     @classmethod
     def index_by_name(cls) -> dict[str, int]:
         return ARM_INDEX_BY_NAME
+
+    @classmethod
+    def infinite_register_prefix(cls):
+        return "inf_"
+
+
+@irdl_attr_definition
+class FPSIMDRegisterType(ARMRegisterType):
+    """
+    A floating-point/SIMD ARM register type.
+    """
+
+    name = "arm.fpsimdreg"
+
+    @classmethod
+    def instruction_set_name(cls) -> str:
+        return "arm_simd"
+
+    @classmethod
+    def abi_index_by_name(cls) -> dict[str, int]:
+        return ARM_FPSIMD_INDEX_BY_NAME
 
     @classmethod
     def infinite_register_prefix(cls):
@@ -68,3 +91,37 @@ X27 = IntRegisterType.from_name("x27")
 X28 = IntRegisterType.from_name("x28")
 X29 = IntRegisterType.from_name("x29")
 X30 = IntRegisterType.from_name("x30")
+
+UNALLOCATED_FPSIMD = FPSIMDRegisterType.unallocated()
+V0 = FPSIMDRegisterType.from_name("v0")
+V1 = FPSIMDRegisterType.from_name("v1")
+V2 = FPSIMDRegisterType.from_name("v2")
+V3 = FPSIMDRegisterType.from_name("v3")
+V4 = FPSIMDRegisterType.from_name("v4")
+V5 = FPSIMDRegisterType.from_name("v5")
+V6 = FPSIMDRegisterType.from_name("v6")
+V7 = FPSIMDRegisterType.from_name("v7")
+V8 = FPSIMDRegisterType.from_name("v8")
+V9 = FPSIMDRegisterType.from_name("v9")
+V10 = FPSIMDRegisterType.from_name("v10")
+V11 = FPSIMDRegisterType.from_name("v11")
+V12 = FPSIMDRegisterType.from_name("v12")
+V13 = FPSIMDRegisterType.from_name("v13")
+V14 = FPSIMDRegisterType.from_name("v14")
+V15 = FPSIMDRegisterType.from_name("v15")
+V16 = FPSIMDRegisterType.from_name("v16")
+V17 = FPSIMDRegisterType.from_name("v17")
+V18 = FPSIMDRegisterType.from_name("v18")
+V19 = FPSIMDRegisterType.from_name("v19")
+V20 = FPSIMDRegisterType.from_name("v20")
+V21 = FPSIMDRegisterType.from_name("v21")
+V22 = FPSIMDRegisterType.from_name("v22")
+V23 = FPSIMDRegisterType.from_name("v23")
+V24 = FPSIMDRegisterType.from_name("v24")
+V25 = FPSIMDRegisterType.from_name("v25")
+V26 = FPSIMDRegisterType.from_name("v26")
+V27 = FPSIMDRegisterType.from_name("v27")
+V28 = FPSIMDRegisterType.from_name("v28")
+V29 = FPSIMDRegisterType.from_name("v29")
+V30 = FPSIMDRegisterType.from_name("v30")
+V31 = FPSIMDRegisterType.from_name("v31")
