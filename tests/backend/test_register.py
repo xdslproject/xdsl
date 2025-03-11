9f467f41c3b8e487c43a9f4f803bbdfdbf2d5620
Sasha Lopoukhine <superlopuh@gmail.com>
2025-03-11 21:38:07 +0000

    backend: add abi_name_by_index helper to RegisterType (#4042)
    This will let us create registers from the index directly.

--- tests/backend/test_register.py
+++ tests/backend/test_register.py
@@ -18,7 +18,7 @@ def test_register_clashes():
             return "TEST"
 
         @classmethod
-        def abi_index_by_name(cls) -> dict[str, int]:
+        def index_by_name(cls) -> dict[str, int]:
             return {"x0": 0}
 
         @classmethod
@@ -41,8 +41,8 @@ class TestRegister(RegisterType):
         return "TEST"
 
     @classmethod
-    def abi_index_by_name(cls) -> dict[str, int]:
-        return {"x0": 0, "x1": 1}
+    def index_by_name(cls) -> dict[str, int]:
+        return {"x0": 0, "x1": 1, "a0": 0, "a1": 1}
 
     @classmethod
     def infinite_register_prefix(cls):
@@ -108,3 +108,7 @@ def test_invalid_index():
         VerifyException, match="Invalid index 2 for register x1, expected 1."
     ):
         TestRegister(IntAttr(2), StringAttr("x1"))
+
+
+def test_name_by_index():
+    assert TestRegister.abi_name_by_index() == {0: "a0", 1: "a1"}
