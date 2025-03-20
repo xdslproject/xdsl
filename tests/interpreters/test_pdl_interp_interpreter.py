1c4fc84ec0711dd6cecf48c5dfebf6585ca991d6
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-03-20 16:30:18 +0100

    first test of terminator op

--- tests/interpreters/test_pdl_interp_interpreter.py
+++ tests/interpreters/test_pdl_interp_interpreter.py
@@ -6,12 +6,13 @@ from xdsl.dialects.builtin import (
     i32,
     i64,
 )
-from xdsl.interpreter import Interpreter
+from xdsl.interpreter import Interpreter, Successor
 from xdsl.interpreters.pdl_interp import PDLInterpFunctions
+from xdsl.ir import Block
 from xdsl.utils.test_value import TestSSAValue
 
 
-def test_getoperand():
+def test_getters():
     interpreter = Interpreter(ModuleOp([]))
     interpreter.register_implementations(PDLInterpFunctions(Context()))
 
@@ -40,3 +41,37 @@ def test_getoperand():
     assert interpreter.run_op(
         pdl_interp.GetDefiningOpOp(TestSSAValue(pdl.OperationType())), (op_res,)
     ) == (op,)
+
+
+def test_checks():
+    interpreter = Interpreter(ModuleOp([]))
+    interpreter.register_implementations(PDLInterpFunctions(Context()))
+
+    truedest = Block()
+    falsedest = Block()
+
+    c0 = TestSSAValue(i32)
+    c1 = TestSSAValue(i32)
+
+    myattr = StringAttr("hello")
+    op = test.TestOp((c0, c1), (i32, i64), {"myattr": myattr})
+
+    trueresult = interpreter._run_op(  # pyright: ignore[reportPrivateUsage]
+        pdl_interp.CheckOperationNameOp(
+            "test.op", TestSSAValue(pdl.OperationType()), truedest, falsedest
+        ),
+        (op,),
+    )
+    assert isinstance(trueresult.terminator_value, Successor)
+    assert isinstance(trueresult.terminator_value.block, Block)
+    assert trueresult.terminator_value.block == truedest
+
+    falseresult = interpreter._run_op(  # pyright: ignore[reportPrivateUsage]
+        pdl_interp.CheckOperationNameOp(
+            "test.other", TestSSAValue(pdl.OperationType()), truedest, falsedest
+        ),
+        (op,),
+    )
+    assert isinstance(falseresult.terminator_value, Successor)
+    assert isinstance(falseresult.terminator_value.block, Block)
+    assert falseresult.terminator_value.block == falsedest
