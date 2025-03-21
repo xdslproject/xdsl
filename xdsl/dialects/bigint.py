851d80302d90a3a62d73ffeca423c5464bb4627c
EdmundGoodman <egoodman3141@gmail.com>
2025-03-21 15:48:27 +0000

    Add documentation

--- xdsl/dialects/bigint.py
+++ xdsl/dialects/bigint.py
@@ -1,9 +1,15 @@
+"""Dialect for arbitrary-precision integers."""
+
 from xdsl.ir import Dialect, ParametrizedAttribute, TypeAttribute
 from xdsl.irdl import irdl_attr_definition
 
 
 @irdl_attr_definition
 class BigIntegerType(ParametrizedAttribute, TypeAttribute):
+    """
+    Type for arbitrary-precision integers (bigints), such as those in Python.
+    """
+
     name = "bigint.bigint"
 
 
