5afb5e1ea9641b10a5a8fc121e1008ec5b5076f6
Erick Ochoa <erick@ceci-nest-pas.me>
2025-03-23 12:37:44 -0400

    move stablehlo.return

--- xdsl/dialects/stablehlo.py
+++ xdsl/dialects/stablehlo.py
@@ -445,6 +445,26 @@ class PopcntOp(IntegerTensorLikeElementwiseUnaryOperation):
 
 
 @irdl_op_definition
+class ReturnOp(IRDLOperation):
+    """This op is un-documented.
+
+    StableHLO's return is used inside of the bodies of StableHLO ops.
+    It behaves like func.return but for StableHLO ops.
+    The func.return op is used inside of func.func op.
+
+    https://discord.com/channels/999073994483433573/1259494021269688360/1259992088565645312
+    """
+
+    name = "stablehlo.return"
+
+    input = var_operand_def(AnyTensorType)
+    traits = traits_def(IsTerminator())
+
+    def __init__(self, input: list[SSAValue]):
+        super().__init__(operands=(input,))
+
+
+@irdl_op_definition
 class XorOp(IntegerTensorLikeElementwiseBinaryOperation):
     """
     Performs element-wise XOR of two tensors lhs and rhs and produces a result tensor.
@@ -514,26 +534,6 @@ class SubtractOp(ElementwiseBinaryOperation):
     """
 
     name = "stablehlo.subtract"
-
-
-@irdl_op_definition
-class ReturnOp(IRDLOperation):
-    """This op is un-documented.
-
-    StableHLO's return is used inside of the bodies of StableHLO ops.
-    It behaves like func.return but for StableHLO ops.
-    The func.return op is used inside of func.func op.
-
-    https://discord.com/channels/999073994483433573/1259494021269688360/1259992088565645312
-    """
-
-    name = "stablehlo.return"
-
-    input = var_operand_def(AnyTensorType)
-    traits = traits_def(IsTerminator())
-
-    def __init__(self, input: list[SSAValue]):
-        super().__init__(operands=(input,))
 
 
 @irdl_op_definition
