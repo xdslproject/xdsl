c9d1ef4f4ba2dfd0c31e75ff0896dd5a6b86207a
Sasha Lopoukhine <superlopuh@gmail.com>
2025-03-27 10:52:55 +0000

    dialects: (arm) add helper to print register SSAValues in ARM assembly

--- xdsl/dialects/arm/assembly.py
+++ xdsl/dialects/arm/assembly.py
@@ -1,5 +1,8 @@
 import abc
 
+from xdsl.backend.register_type import RegisterType
+from xdsl.ir import SSAValue
+
 
 class AssemblyInstructionArg(abc.ABC):
     """
@@ -9,3 +12,19 @@ class AssemblyInstructionArg(abc.ABC):
     @abc.abstractmethod
     def assembly_str(self) -> str:
         raise NotImplementedError()
+
+
+class reg(AssemblyInstructionArg):
+    """
+    A wrapper around SSAValue to be printed in assembly.
+    Only valid if the type of the value is a RegisterType.
+    """
+
+    value: SSAValue
+
+    def __init__(self, value: SSAValue) -> None:
+        self.value = value
+
+    def assembly_str(self) -> str:
+        assert isinstance(self.value.type, RegisterType)
+        return self.value.type.register_name.data
