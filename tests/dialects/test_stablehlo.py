db35a8635fcdade9b9238fdcd68be31c125b007f
Erick Ochoa <erick@ceci-nest-pas.me>
2025-03-30 14:26:29 -0400

    Test AddOp which uses abstract factory

--- tests/dialects/test_stablehlo.py
+++ tests/dialects/test_stablehlo.py
@@ -1,8 +1,10 @@
-from xdsl.dialects.builtin import AnyTensorType
+from xdsl.dialects.builtin import AnyTensorType, IntegerType, TensorType
 from xdsl.dialects.stablehlo import (
+    AddOp,
     _abstract_operation_class_factory,  # pyright: ignore[reportPrivateUsage]
 )
 from xdsl.irdl import VarConstraint, base
+from xdsl.utils.test_value import TestSSAValue
 
 
 def test_abstract_factory():
@@ -12,3 +14,10 @@ def test_abstract_factory():
     unop.operand
     binop.lhs
     binop.rhs
+
+
+def test_impact_of_abstract_factory_in_ops_that_uses_base_classes():
+    a = TestSSAValue(TensorType(IntegerType(32), []))
+    addop = AddOp(a, a)
+    addop.lhs
+    addop.rhs
