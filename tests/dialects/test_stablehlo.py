b7c72fa7a5769f341da36614751747ec62de9efc
Erick Ochoa <erick@ceci-nest-pas.me>
2025-03-31 17:28:02 -0400

    add tests

--- tests/dialects/test_stablehlo.py
+++ tests/dialects/test_stablehlo.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING, reveal_type
+
 from xdsl.dialects.builtin import AnyTensorType, IntegerType, TensorType
 from xdsl.dialects.stablehlo import (
     AddOp,
@@ -12,12 +14,18 @@ def test_abstract_factory():
         "", VarConstraint("T", base(AnyTensorType))
     )
     unop.operand
-    binop.lhs
-    binop.rhs
+    if TYPE_CHECKING:
+        reveal_type(unop.T, expected_text="VarConstraint[Attribute]")
+        reveal_type(unop.operand, expected_text="SSAValue[Attribute]")
+        reveal_type(binop.T, expected_text="VarConstraint[Attribute]")
+        reveal_type(binop.lhs, expected_text="SSAValue[Attribute]")
+        reveal_type(binop.rhs, expected_text="SSAValue[Attribute]")
 
 
 def test_impact_of_abstract_factory_in_ops_that_uses_base_classes():
     a = TestSSAValue(TensorType(IntegerType(32), []))
     addop = AddOp(a, a)
-    addop.lhs
-    addop.rhs
+    if TYPE_CHECKING:
+        reveal_type(addop.T, expected_text="VarConstraint[Attribute]")
+        reveal_type(addop.lhs, expected_text="SSAValue[Attribute]")
+        reveal_type(addop.rhs, expected_text="SSAValue[Attribute]")
