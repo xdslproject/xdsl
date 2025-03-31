4d1a6a0d6a9641f8e87b3a4fb54c67f46b7f04f8
EdmundGoodman <egoodman3141@gmail.com>
2025-04-01 00:44:19 +0100

    Add test case for floats

--- tests/filecheck/frontend/refactor/operations.py
+++ tests/filecheck/frontend/refactor/operations.py
@@ -1,15 +1,17 @@
 # RUN: python %s | filecheck %s
 
+from xdsl.dialects.arith import AddfOp, MulfOp, SubfOp
 from xdsl.dialects.bigint import AddBigIntOp, BigIntegerType, MulBigIntOp, SubBigIntOp
+from xdsl.dialects.builtin import Float64Type
 from xdsl.frontend.pyast.context import CodeContext
 from xdsl.frontend.pyast.program import FrontendProgram
 
-p = FrontendProgram()
-p.register_type(int, BigIntegerType)
-p.register_method(int, "__add__", AddBigIntOp)
-p.register_method(int, "__sub__", SubBigIntOp)
-p.register_method(int, "__mul__", MulBigIntOp)
-with CodeContext(p):
+p1 = FrontendProgram()
+p1.register_type(int, BigIntegerType)
+p1.register_method(int, "__add__", AddBigIntOp)
+p1.register_method(int, "__sub__", SubBigIntOp)
+p1.register_method(int, "__mul__", MulBigIntOp)
+with CodeContext(p1):
     # CHECK:      builtin.module {
     # CHECK-NEXT:     func.func @foo(%0 : !bigint.bigint, %1 : !bigint.bigint, %2 : !bigint.bigint) -> !bigint.bigint {
     # CHECK-NEXT:       %3 = bigint.mul %1, %2 : !bigint.bigint
@@ -21,5 +23,26 @@ with CodeContext(p):
         return x - y * z
 
 
-p.compile()
-print(p.textual_format())
+p1.compile()
+print(p1.textual_format())
+
+
+p2 = FrontendProgram()
+p2.register_type(float, Float64Type)
+p2.register_method(float, "__add__", AddfOp)
+p2.register_method(float, "__sub__", SubfOp)
+p2.register_method(float, "__mul__", MulfOp)
+with CodeContext(p2):
+    # CHECK:      builtin.module {
+    # CHECK-NEXT:       func.func @bar(%0 : f64, %1 : f64, %2 : f64) -> f64 {
+    # CHECK-NEXT:         %3 = arith.mulf %1, %2 : f64
+    # CHECK-NEXT:         %4 = arith.subf %0, %3 : f64
+    # CHECK-NEXT:         func.return %4 : f64
+    # CHECK-NEXT:       }
+    # CHECK-NEXT:     }
+    def bar(x: float, y: float, z: float) -> float:  # pyright: ignore[reportUnusedFunction]
+        return x - y * z
+
+
+p2.compile()
+print(p2.textual_format())
