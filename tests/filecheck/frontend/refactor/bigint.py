dd681090634e4a06abdc27d2b12e0de6e320461d
EdmundGoodman <egoodman3141@gmail.com>
2025-03-31 15:03:30 +0100

    Add failing test for refactor

---
+++ tests/filecheck/frontend/refactor/bigint.py
@@ -0,0 +1,20 @@
+# RUN: python %s | filecheck %s
+
+from xdsl.dialects.bigint import BigIntegerType
+from xdsl.frontend.pyast.context import CodeContext
+from xdsl.frontend.pyast.program import FrontendProgram
+
+p = FrontendProgram()
+p.register_type(int, BigIntegerType)
+with CodeContext(p):
+    # CHECK:      builtin.module {
+    # CHECK-NEXT:   func.func @foo(%0 : !bigint.bigint) -> !bigint.bigint {
+    # CHECK-NEXT:     func.return %0 : !bigint.bigint
+    # CHECK-NEXT:   }
+    # CHECK-NEXT: }
+    def foo(x: int) -> int:  # pyright: ignore[reportUnusedFunction]
+        return x
+
+
+p.compile()
+print(p.textual_format())
