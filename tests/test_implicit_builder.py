24e5110fbe4282ef94787b78c04189331ed31b0d
EdmundGoodman <egoodman3141@gmail.com>
2025-04-02 16:48:58 +0100

    Respond to review

--- tests/test_implicit_builder.py
+++ tests/test_implicit_builder.py
@@ -1,29 +1,15 @@
 from __future__ import annotations
 
-import pytest
-
 from xdsl.builder import ImplicitBuilder
-from xdsl.context import Context
-from xdsl.dialects import arith, builtin, test
+from xdsl.dialects import builtin, test
 
 
-@pytest.fixture
-def dialect_context() -> Context:
-    """Fixture that creates a Context with Arith and Builtin dialects loaded.
-
-    Returns:
-        Context: An MLIR Context with required dialects.
-    """
-    ctx = Context()
-    ctx.load_dialect(arith.Arith)
-    ctx.load_dialect(builtin.Builtin)
-    return ctx
-
-
-def test_no_implicit_builder(dialect_context: Context) -> None:
+def test_no_implicit_builder() -> None:
     """Test creating an operation without an implict builder."""
-    assert dialect_context
-    expected = 'builtin.module {\n  %i = "test.op"() : () -> i32\n}'
+    expected = """\
+builtin.module {
+  %i = "test.op"() : () -> i32
+}"""
 
     module = builtin.ModuleOp([])
     i = test.TestOp.create(result_types=[builtin.i32])
@@ -32,10 +18,12 @@ def test_no_implicit_builder(dialect_context: Context) -> None:
     assert str(module) == expected
 
 
-def test_single_implicit_builder(dialect_context: Context):
+def test_single_implicit_builder() -> None:
     """Test creating an operation with a single implict builder."""
-    assert dialect_context
-    expected = 'builtin.module {\n  %i = "test.op"() : () -> i32\n}'
+    expected = """\
+builtin.module {
+  %i = "test.op"() : () -> i32
+}"""
 
     with ImplicitBuilder((module := builtin.ModuleOp([])).body):
         assert len(ImplicitBuilder._stack.stack) == 1  # pyright: ignore[reportPrivateUsage]
@@ -44,12 +32,14 @@ def test_single_implicit_builder(dialect_context: Context):
     assert str(module) == expected
 
 
-def test_nested_implicit_builders(dialect_context: Context):
+def test_nested_implicit_builders() -> None:
     """Test creating an operation with a single implict builder."""
-    assert dialect_context
-    expected = (
-        'builtin.module {\n  builtin.module {\n    %i = "test.op"() : () -> i32\n  }\n}'
-    )
+    expected = """\
+builtin.module {
+  builtin.module {
+    %i = "test.op"() : () -> i32
+  }
+}"""
 
     with ImplicitBuilder((module_outer := builtin.ModuleOp([])).body):
         assert len(ImplicitBuilder._stack.stack) == 1  # pyright: ignore[reportPrivateUsage]
