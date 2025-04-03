b34acae20c20437da7abc84b5abbb8b1064827f4
EdmundGoodman <egoodman3141@gmail.com>
2025-04-03 22:16:20 +0100

    Fix broken tests

--- tests/filecheck/frontend/refactor/operations.py
+++ tests/filecheck/frontend/refactor/operations.py
@@ -13,10 +13,10 @@ p1.register_method(int, "__sub__", SubOp)
 p1.register_method(int, "__mul__", MulOp)
 with CodeContext(p1):
     # CHECK:      builtin.module {
-    # CHECK-NEXT:     func.func @foo(%0 : !bigint.bigint, %1 : !bigint.bigint, %2 : !bigint.bigint) -> !bigint.bigint {
-    # CHECK-NEXT:       %3 = bigint.mul %1, %2 : !bigint.bigint
-    # CHECK-NEXT:       %4 = bigint.sub %0, %3 : !bigint.bigint
-    # CHECK-NEXT:       func.return %4 : !bigint.bigint
+    # CHECK-NEXT:     func.func @foo(%x : !bigint.bigint, %y : !bigint.bigint, %z : !bigint.bigint) -> !bigint.bigint {
+    # CHECK-NEXT:       %0 = bigint.mul %y, %z : !bigint.bigint
+    # CHECK-NEXT:       %1 = bigint.sub %x, %0 : !bigint.bigint
+    # CHECK-NEXT:       func.return %1 : !bigint.bigint
     # CHECK-NEXT:     }
     # CHECK-NEXT:   }
     def foo(x: int, y: int, z: int) -> int:
@@ -34,10 +34,10 @@ p2.register_method(float, "__sub__", SubfOp)
 p2.register_method(float, "__mul__", MulfOp)
 with CodeContext(p2):
     # CHECK:      builtin.module {
-    # CHECK-NEXT:       func.func @bar(%0 : f64, %1 : f64, %2 : f64) -> f64 {
-    # CHECK-NEXT:         %3 = arith.mulf %1, %2 : f64
-    # CHECK-NEXT:         %4 = arith.subf %0, %3 : f64
-    # CHECK-NEXT:         func.return %4 : f64
+    # CHECK-NEXT:       func.func @bar(%x : f64, %y : f64, %z : f64) -> f64 {
+    # CHECK-NEXT:         %0 = arith.mulf %y, %z : f64
+    # CHECK-NEXT:         %1 = arith.subf %x, %0 : f64
+    # CHECK-NEXT:         func.return %1 : f64
     # CHECK-NEXT:       }
     # CHECK-NEXT:     }
     def bar(x: float, y: float, z: float) -> float:
