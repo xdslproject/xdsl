307e3faed836d92a0a9407ddf7756cd816ba7626
Sasha Lopoukhine <superlopuh@gmail.com>
2025-04-08 23:30:02 +0100

    misc: raise ValueError when getting owner of TestSSAValue

--- xdsl/utils/test_value.py
+++ xdsl/utils/test_value.py
@@ -6,6 +6,4 @@ from xdsl.ir import AttributeCovT, Block, Operation, SSAValue
 class TestSSAValue(Generic[AttributeCovT], SSAValue[AttributeCovT]):
     @property
     def owner(self) -> Operation | Block:
-        import pytest
-
-        pytest.fail("Attempting to get the owner of a `TestSSAValue`")
+        raise ValueError("Attempting to get the owner of a `TestSSAValue`")
