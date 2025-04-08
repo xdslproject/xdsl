ad7c3daf85772f516490ec1fd4e0560d20469130
Edmund Goodman <37504168+EdmundGoodman@users.noreply.github.com>
2025-04-08 21:41:57 +0100

    core: TestSSAValue depends on pytest, which is an optional depedency (#4204)
    Resolves #4203.
    
    I'm not sure if this is a good permanent solution, but at the very least
    this makes it possible to `import xdsl.dialects.arith` with only the
    core dependencies.

--- xdsl/utils/test_value.py
+++ xdsl/utils/test_value.py
@@ -1,11 +1,11 @@
 from typing import Generic
 
-import pytest
-
 from xdsl.ir import AttributeCovT, Block, Operation, SSAValue
 
 
 class TestSSAValue(Generic[AttributeCovT], SSAValue[AttributeCovT]):
     @property
     def owner(self) -> Operation | Block:
+        import pytest
+
         pytest.fail("Attempting to get the owner of a `TestSSAValue`")
