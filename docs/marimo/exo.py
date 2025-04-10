49aa7992acc1df3ac470ba627ad51f15d46c0968
emmau678 <emmau678@gmail.com>
2025-04-10 14:28:19 +0100

    add input and expected output

--- docs/marimo/exo.py
+++ docs/marimo/exo.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.12.6"
+__generated_with = "0.10.13"
 app = marimo.App(width="medium")
 
 
@@ -15,6 +15,30 @@ def _():
 def _(mo):
     mo.md(r"""# Exo-Style Scheduling in xDSL""")
     return
+
+
+@app.cell
+def _(test):
+    def hello():
+        for i in range(100):
+            for j in range(200):
+                test.op(i,j)
+        return
+    return (hello,)
+
+
+@app.cell
+def _(test):
+    def hello_2():
+        for io in range(0, 100, 2):
+            for jo in range(0, 200, 5):
+                for il in range(4):
+                    for jl in range(5):
+                        i = io + il
+                        j = jo + jl
+                        test.op(i,j)
+        return
+    return (hello_2,)
 
 
 if __name__ == "__main__":
