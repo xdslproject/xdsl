d5ef2df8986ef127c45a5186c7e0f1c98663bf6c
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-04-15 14:43:31 +0200

    add getters and creator operations

---
+++ tests/interpreters/test_pdl_interp_interpreter.py
@@ -0,0 +1,116 @@
+from xdsl.builder import Builder
+from xdsl.context import Context
+from xdsl.dialects import pdl, pdl_interp, test
+from xdsl.dialects.builtin import (
+    ModuleOp,
+    StringAttr,
+    UnitAttr,
+    i32,
+    i64,
+)
+from xdsl.interpreter import Interpreter
+from xdsl.interpreters.pdl_interp import PDLInterpFunctions
+from xdsl.pattern_rewriter import PatternRewriter
+from xdsl.utils.test_value import create_ssa_value
+
+
+def test_getters():
+    # TODO: test negative cases returning None
+    interpreter = Interpreter(ModuleOp([]))
+    interpreter.register_implementations(PDLInterpFunctions(Context()))
+
+    c0 = create_ssa_value(i32)
+    c1 = create_ssa_value(i32)
+    myattr = StringAttr("hello")
+    op = test.TestOp((c0, c1), (i32, i64), {"myattr": myattr})
+    op_res = op.results[0]
+
+    assert interpreter.run_op(
+        pdl_interp.GetOperandOp(1, create_ssa_value(pdl.OperationType())), (op,)
+    ) == (c1,)
+
+    assert interpreter.run_op(
+        pdl_interp.GetResultOp(1, create_ssa_value(pdl.OperationType())), (op,)
+    ) == (op.results[1],)
+
+    assert (
+        interpreter.run_op(
+            pdl_interp.GetResultsOp(
+                None,
+                create_ssa_value(pdl.OperationType()),
+                pdl.RangeType(pdl.ValueType()),
+            ),
+            (op,),
+        )[0]
+        == op.results
+    )
+
+    assert interpreter.run_op(
+        pdl_interp.GetAttributeOp("myattr", create_ssa_value(pdl.OperationType())),
+        (op,),
+    ) == (myattr,)
+
+    assert interpreter.run_op(
+        pdl_interp.GetValueTypeOp(create_ssa_value(pdl.ValueType())), (c0,)
+    ) == (i32,)
+
+    assert interpreter.run_op(
+        pdl_interp.GetDefiningOpOp(create_ssa_value(pdl.OperationType())), (op_res,)
+    ) == (op,)
+
+
+def test_create_attribute():
+    interpreter = Interpreter(ModuleOp([]))
+    interpreter.register_implementations(PDLInterpFunctions(Context()))
+
+    # Create test attribute
+    test_attr = StringAttr("test")
+
+    # Test create attribute operation
+    create_attr_op = pdl_interp.CreateAttributeOp(test_attr)
+    result = interpreter.run_op(create_attr_op, ())
+
+    assert len(result) == 1
+    assert result[0] == test_attr
+
+
+def test_create_operation():
+    interpreter = Interpreter(ModuleOp([]))
+    ctx = Context()
+    ctx.register_dialect("test", lambda: test.Test)
+    implementations = PDLInterpFunctions(ctx)
+    interpreter.register_implementations(implementations)
+
+    @ModuleOp
+    @Builder.implicit_region
+    def testmodule():
+        root = test.TestOp()
+        implementations.rewriter = PatternRewriter(root)
+
+    # Create test values
+    c0 = create_ssa_value(i32)
+    c1 = create_ssa_value(i32)
+    attr = StringAttr("test")
+
+    # Test create operation
+    create_op = pdl_interp.CreateOperationOp(
+        name="test.op",
+        inferred_result_types=UnitAttr(),
+        input_attribute_names=[StringAttr("attr")],
+        input_operands=[c0, c1],
+        input_attributes=[create_ssa_value(pdl.AttributeType())],
+        input_result_types=[create_ssa_value(pdl.TypeType())],
+    )
+
+    result = interpreter.run_op(create_op, (c0, c1, attr, i32))
+
+    assert len(result) == 1
+    assert isinstance(result[0], test.TestOp)
+    created_op = result[0]
+    assert len(created_op.operands) == 2
+    assert created_op.ops == (c0, c1)
+    assert created_op.attributes["attr"] == attr
+    assert len(created_op.results) == 1
+    assert created_op.results[0].type == i32
+    # Verify that the operation was inserted:
+    assert created_op.parent == testmodule.body.first_block
---
+++ xdsl/interpreters/pdl_interp.py
@@ -0,0 +1,189 @@
+from dataclasses import dataclass, field
+from typing import Any, cast
+
+from xdsl.context import Context
+from xdsl.dialects import pdl_interp
+from xdsl.dialects.builtin import StringAttr
+from xdsl.dialects.pdl import ValueType
+from xdsl.interpreter import (
+    Interpreter,
+    InterpreterFunctions,
+    impl,
+    register_impls,
+)
+from xdsl.ir import Attribute, Operation, OpResult, SSAValue, TypeAttribute
+from xdsl.pattern_rewriter import PatternRewriter
+from xdsl.utils.exceptions import InterpretationError
+
+
+@register_impls
+@dataclass
+class PDLInterpFunctions(InterpreterFunctions):
+    ctx: Context
+
+    _rewriter: PatternRewriter | None = field(default=None)
+
+    @property
+    def rewriter(self) -> PatternRewriter:
+        assert self._rewriter is not None
+        return self._rewriter
+
+    @rewriter.setter
+    def rewriter(self, rewriter: PatternRewriter):
+        self._rewriter = rewriter
+
+    def clear_rewriter(self):
+        self._rewriter = None
+
+    @impl(pdl_interp.GetOperandOp)
+    def run_getoperand(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.GetOperandOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        assert len(args) > 0
+        assert isinstance(args[0], Operation)
+        if op.index.value.data >= len(args[0].operands):
+            return (None,)
+        else:
+            return (args[0].operands[op.index.value.data],)
+
+    @impl(pdl_interp.GetResultOp)
+    def run_getresult(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.GetResultOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        assert len(args) > 0
+        assert isinstance(args[0], Operation)
+        if len(args[0].results) <= op.index.value.data:
+            return (None,)
+        return (args[0].results[op.index.value.data],)
+
+    @impl(pdl_interp.GetResultsOp)
+    def run_getresults(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.GetResultsOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        assert len(args) > 0
+        assert isinstance(args[0], Operation)
+        src_op = args[0]
+        assert op.index is None, (
+            "TODO: No support yet for getting a specific result group"
+        )
+        if isinstance(op.result_types[0], ValueType) and len(src_op.results) != 1:
+            return (None,)
+        return (src_op.results,)
+
+    @impl(pdl_interp.GetAttributeOp)
+    def run_getattribute(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.GetAttributeOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        assert len(args) > 0
+        assert isinstance(args[0], Operation)
+        attrname = op.constraint_name.data
+        if attrname in args[0].attributes:
+            return (args[0].attributes[attrname],)
+        elif attrname in args[0].properties:
+            return (args[0].properties[attrname],)
+        else:
+            return (None,)
+
+    @impl(pdl_interp.GetValueTypeOp)
+    def run_getvaluetype(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.GetValueTypeOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        assert len(args) > 0
+        assert len(args) == 1, "TODO: Implement this"
+        assert isinstance(args[0], SSAValue)
+        value = cast(SSAValue, args[0])
+        return (value.type,)
+
+    @impl(pdl_interp.GetDefiningOpOp)
+    def run_getdefiningop(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.GetDefiningOpOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        assert len(args) > 0
+        if args[0] is None:
+            return (None,)
+        assert isinstance(args[0], SSAValue)
+        if not isinstance(args[0], OpResult):
+            return (None,)
+        assert isinstance(args[0].owner, Operation), (
+            "Cannot get defining op of a Block argument"
+        )
+        return (args[0].owner,)
+
+    @impl(pdl_interp.CreateAttributeOp)
+    def run_createattribute(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.CreateAttributeOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        # Simply return the attribute value
+        return (op.value,)
+
+    @impl(pdl_interp.CreateOperationOp)
+    def run_createoperation(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.CreateOperationOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        # Get operation name
+        op_name = op.constraint_name.data
+        op_type = self.ctx.get_optional_op(op_name)
+        if op_type is None:
+            raise InterpretationError(
+                f"Could not find op type for name {op_name} in context"
+            )
+
+        # Split args into operands, attributes and result types based on operand segments
+        operands = list(args[0 : len(op.input_operands)])
+        attributes = list(
+            args[
+                len(op.input_operands) : len(op.input_operands)
+                + len(op.input_attributes)
+            ]
+        )
+        result_types = list(args[len(op.input_operands) + len(op.input_attributes) :])
+
+        # Verify all arguments have correct types
+        for operand in operands:
+            assert isinstance(operand, SSAValue)
+        for attr in attributes:
+            assert isinstance(attr, Attribute)
+        for res_type in result_types:
+            assert isinstance(res_type, TypeAttribute)
+
+        # Create attribute dictionary using input_attribute_names
+        attr_names: list[str] = [
+            cast(StringAttr, name).data for name in op.input_attribute_names.data
+        ]
+        attr_dict = dict(zip(attr_names, attributes))
+
+        # Create the new operation
+        result_op = op_type.create(
+            operands=operands,
+            result_types=result_types,
+            attributes=attr_dict,
+        )
+
+        self.rewriter.insert_op_before_matched_op(result_op)
+
+        # Return the created operation
+        return (result_op,)
