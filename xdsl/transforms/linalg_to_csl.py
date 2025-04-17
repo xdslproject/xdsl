dc77871bb4cfe41a34ab2eccddde4f807ebf0faa
n-io <n-io@users.noreply.github.com>
2025-04-17 22:29:24 +0200

    linalg to csl min and max translations

--- xdsl/transforms/linalg_to_csl.py
+++ xdsl/transforms/linalg_to_csl.py
@@ -157,6 +157,18 @@ class ConvertLinalgMulPass(ConvertBinaryLinalgOp):
         self.transform_op(op, rewriter, f16=csl.FmulhOp, f32=csl.FmulsOp)
 
 
+class ConvertLinalgMaxPass(ConvertBinaryLinalgOp):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: linalg.MaxOp, rewriter: PatternRewriter, /):
+        self.transform_op(op, rewriter, f16=csl.FmaxhOp, f32=csl.FmaxsOp)
+
+
+class ConvertLinalgMinPass(ConvertBinaryLinalgOp):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: linalg.MinOp, rewriter: PatternRewriter, /):
+        self.transform_op(op, rewriter, f16=csl.FminhOp, f32=csl.FminsOp)
+
+
 @dataclass(frozen=True)
 class LinalgToCsl(ModulePass):
     """
@@ -175,6 +187,8 @@ class LinalgToCsl(ModulePass):
                     ConvertLinalgAddPass(),
                     ConvertLinalgSubPass(),
                     ConvertLinalgMulPass(),
+                    ConvertLinalgMaxPass(),
+                    ConvertLinalgMinPass(),
                 ]
             ),
         )
