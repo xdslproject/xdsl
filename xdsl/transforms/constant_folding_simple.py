3dffa88e8cd365af8fedcf4954ffefebd3d825c7
EdmundGoodman <egoodman3141@gmail.com>
2025-04-23 21:17:55 +0100

    Eliminate development stubs

--- xdsl/transforms/constant_folding_simple.py
+++ xdsl/transforms/constant_folding_simple.py
@@ -12,8 +12,6 @@ from xdsl.ir import Operation
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     PatternRewriter,
-    PatternRewriterListener,
-    PatternRewriteWalker,
     RewritePattern,
     Worklist,
 )
@@ -73,29 +71,13 @@ class ConstantFoldingSimplePass(ModulePass):
         op_was_modified = True
         walker_worklist = Worklist()
 
-        ### Stubs for development
-        pattern = ConstantFoldingIntegerAdditionPattern()
-        walker = PatternRewriteWalker(pattern)
-
         ### The function implementation
-
-        ## Inline `listener = walker._get_rewriter_listener()`
-        rewriter_listener = PatternRewriterListener(
-            # In non-recursive rewriting case these do nothing
-            operation_insertion_handler=[],
-            operation_modification_handler=[],
-            operation_replacement_handler=[],
-            block_creation_handler=[],
-            # In constant folding specialisation, no operations are removed
-            operation_removal_handler=[],
-        )
-
         while op_was_modified:
             ## Inline `walker._populate_worklist(region)`
             for sub_op in region.walk(reverse=True, region_first=True):
                 walker_worklist.push(sub_op)
 
-            ## Inline `walker._process_worklist(rewriter_listener)`
+            ## Inline `walker._process_worklist(listener)`
             rewriter_has_done_action = False
 
             # Handle empty worklist
@@ -106,7 +88,9 @@ class ConstantFoldingSimplePass(ModulePass):
 
             # Create a rewriter on the first operation
             rewriter = PatternRewriter(rewrite_op)
-            rewriter.extend_from_listener(rewriter_listener)
+            # No custom listeners have any effect, as we are operating in the
+            # non-recursive mode and no operations are removed in constant folding.
+            # As a result of this, we elide `rewriter.extend_from_listener(listener)`.
 
             # do/while loop
             while True:
@@ -116,18 +100,19 @@ class ConstantFoldingSimplePass(ModulePass):
                 rewriter.insertion_point = InsertPoint.before(rewrite_op)
 
                 # Apply the pattern on the operation
-                try:
-                    walker.pattern.match_and_rewrite(rewrite_op, rewriter)
-                except Exception as err:
-                    rewrite_op.emit_error(
-                        f"Error while applying pattern: {str(err)}",
-                        exception_type=type(err),
-                        underlying_error=err,
+                # Inline `walker.pattern.match_and_rewrite(rewrite_op, rewriter)`
+                if isinstance(rewrite_op, AddiOp):
+                    lhs: int = rewrite_op.operands[0].owner.value.value.data  # pyright: ignore
+                    rhs: int = rewrite_op.operands[1].owner.value.value.data  # pyright: ignore
+                    folded_op = ConstantOp(
+                        IntegerAttr(lhs + rhs, rewrite_op.result.type)  # pyright: ignore
                     )
+                    rewriter.replace_matched_op(folded_op, [folded_op.results[0]])
+
                 rewriter_has_done_action |= rewriter.has_done_action
 
                 # If the worklist is empty, we are done
                 rewrite_op = walker_worklist.pop()
                 if rewrite_op is None:
-                    op_was_modified = False
+                    op_was_modified = rewriter_has_done_action
                     break
