07a09026c312a27aacfb25ac6de778df05ef5de5
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-04-25 13:38:00 +0000

    add test for yield op

--- tests/interpreters/test_transform_interpreter.py
+++ tests/interpreters/test_transform_interpreter.py
@@ -6,9 +6,6 @@ from xdsl.interpreters.transform import TransformFunctions
 from xdsl.ir import Block, Region
 from xdsl.parser import Parser
 
-interpreter = Interpreter(builtin.ModuleOp([]))
-interpreter.register_implementations(TransformFunctions())
-
 
 def test_empty_transform_module():
     payload = """
@@ -22,7 +19,7 @@ def test_empty_transform_module():
     ty = transform.OperationType("builtin.module")
     block = Block(arg_types=[ty])
     with ImplicitBuilder(block):
-        transform.YieldOp()
+        transform.YieldOp(block.args[0])
 
     module = builtin.ModuleOp(
         [], attributes={"transform.with_named_sequence": builtin.UnitAttr()}
@@ -30,17 +27,17 @@ def test_empty_transform_module():
     with ImplicitBuilder(module.body):
         body = Region(block)
         sym_name = "__transform_main"
-        function_type = builtin.FunctionType.from_lists([ty], [])
+        function_type = builtin.FunctionType.from_lists([ty], [ty])
         named_sequence = transform.NamedSequenceOp(sym_name, function_type, body)
+
+    interpreter = Interpreter(module)
+    interpreter.register_implementations(TransformFunctions())
 
     ctx = Context()
     ctx.load_dialect(builtin.Builtin)
     ctx.load_dialect(func.Func)
     ctx.load_dialect(transform.Transform)
 
-    module = Parser(ctx, payload).parse_module()
     expected = Parser(ctx, payload).parse_module()
-    interpreter.call_op(named_sequence, (module,))
-    # No changes because named sequence just contains yield
-    # Compare string as proxy for structure equality.
-    assert str(expected) == str(module)
+    (observed,) = interpreter.call_op(named_sequence, (expected,))
+    assert expected is observed
