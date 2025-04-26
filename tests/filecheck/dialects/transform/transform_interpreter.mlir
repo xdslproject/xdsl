2d021433608a8ed31b83e9b25922cd1753daefa0
Erick Ochoa <erick@ceci-nest-pas.me>
2025-04-26 17:05:14 -0400

    Add transform-interpreter pass

--- tests/filecheck/dialects/transform/transform_interpreter.mlir
+++ tests/filecheck/dialects/transform/transform_interpreter.mlir
@@ -1,6 +1,5 @@
 // RUN: xdsl-opt %s -p transform-interpreter | filecheck %s
 
-// CHECK: builtin.module
 
 module {
 
@@ -10,5 +9,9 @@ module {
       "transform.yield"() : () -> ()
     }) : () -> ()
   }) {transform.with_named_sequence} : () -> ()
-
 }
+
+// CHECK: "transform.named_sequence"() <{arg_attrs = [{transform.readonly}], function_type = (!transform.any_op) -> (), sym_name = "__transform_main"}> ({
+// CHECK-NEXT: ^0(%arg0 : !transform.any_op):
+// CHECK-NEXT:   transform.yield
+// CHECK-NEXT: }) : () -> ()
--- xdsl/transforms/transform_interpreter.py
+++ xdsl/transforms/transform_interpreter.py
@@ -2,7 +2,9 @@
 
 from dataclasses import dataclass
 
-from xdsl.dialects import builtin
+from xdsl.dialects import builtin, transform
+from xdsl.interpreter import Interpreter
+from xdsl.interpreters.transform import TransformFunctions
 from xdsl.passes import Context, ModulePass
 
 
@@ -10,6 +12,28 @@ from xdsl.passes import Context, ModulePass
 class TransformInterpreterPass(ModulePass):
     """Transform dialect interpreter"""
 
+    # TODO: Add the rest of options to the transform interpreter
+    # and add semantics for all of the other transform operations
+
     name = "transform-interpreter"
 
-    def apply(self, ctx: Context, op: builtin.ModuleOp) -> None: ...
+    entry_point: str = "__transform_main"
+
+    @staticmethod
+    def find_transform_entry_point(
+        root: builtin.ModuleOp, entry_point: str
+    ) -> transform.NamedSequenceOp:
+        for op in root.walk():
+            if (
+                isinstance(op, transform.NamedSequenceOp)
+                and op.sym_name.data == entry_point
+            ):
+                return op
+
+    def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
+        schedule = TransformInterpreterPass.find_transform_entry_point(
+            op, self.entry_point
+        )
+        interpreter = Interpreter(op)
+        interpreter.register_implementations(TransformFunctions())
+        interpreter.call_op(schedule, (op,))
