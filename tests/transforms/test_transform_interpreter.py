d00bb653a1d773e21e7423c1cba4742f6138bb25
Erick Ochoa <erick@ceci-nest-pas.me>
2025-04-27 19:47:19 -0400

    inline match with pytest.raises call

--- tests/transforms/test_transform_interpreter.py
+++ tests/transforms/test_transform_interpreter.py
@@ -7,7 +7,8 @@ from xdsl.utils.exceptions import PassFailedException
 
 def test_entry_point_not_found():
     root = builtin.ModuleOp(ops=[])
-    entry_point = "symbol-name"
-    msg = f"could not find a nested named sequence with name: {entry_point}"
-    with pytest.raises(PassFailedException, match=msg):
+    with pytest.raises(
+        PassFailedException,
+        match="could not find a nested named sequence with name: symbol-name",
+    ):
         TransformInterpreterPass.find_transform_entry_point(root, "symbol-name")
