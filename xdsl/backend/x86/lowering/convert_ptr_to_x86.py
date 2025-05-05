f4e5f9b7c3451b514fb9a4f4d7f359e216d5f2f3
hpompougnac <hugo.pompougnac@inria.fr>
2025-05-05 15:33:38 +0200

    After review

--- xdsl/backend/x86/lowering/convert_ptr_to_x86.py
+++ xdsl/backend/x86/lowering/convert_ptr_to_x86.py
@@ -1,13 +1,10 @@
 from dataclasses import dataclass
-from functools import reduce
 from typing import cast
 
 from xdsl.context import Context
 from xdsl.dialects import builtin, ptr, x86
 from xdsl.dialects.builtin import (
-    ArrayAttr,
     FixedBitwidthType,
-    IntAttr,
     UnrealizedConversionCastOp,
     VectorType,
 )
@@ -31,52 +28,51 @@ class PtrLoadToX86(RewritePattern):
         source = op.addr
         value = op.res
         value_type = value.type
-        if isinstance(value_type, VectorType):
-            # Pointer cast
-            x86_reg_type = x86.register.UNALLOCATED_GENERAL
-            cast_op = UnrealizedConversionCastOp.get((source,), (x86_reg_type,))
-            # Output vector description
-            vector_int_shape = [i.data for i in value_type.shape.data]
-            vector_shape = ArrayAttr(
-                [IntAttr(reduce(lambda x, y: x * y, vector_int_shape))]
+        if not isinstance(value_type, VectorType):
+            raise DiagnosticException(
+                "The lowering of ptr.load is not yet implemented for non-vector types."
             )
-            vector_num_elements = vector_shape.data[0].data
-            element_type = cast(FixedBitwidthType, value_type.get_element_type())
-            element_size = element_type.bitwidth
-            vector_size = vector_num_elements * element_size
-            # Choose the x86 vector register according to the
-            # target architecture and the abstract vector size
-            if vector_size == 128:
-                vect_reg_type = x86.register.UNALLOCATED_SSE
-            elif vector_size == 256 and (self.arch == "avx2" or self.arch == "avx512"):
-                vect_reg_type = x86.register.UNALLOCATED_AVX2
-            elif vector_size == 512 and self.arch == "avx512":
-                vect_reg_type = x86.register.UNALLOCATED_AVX512
-            else:
-                vect_reg_type = None
-            assert vect_reg_type is not None
-            # Choose the x86 vector instruction according to the
-            # abstract vector element size
-            match element_size:
-                case 16:
-                    raise DiagnosticException(
-                        "Half-precision vector load is not implemented yet."
-                    )
-                case 32:
-                    mov = x86.ops.RM_VmovupsOp
-                case 64:
-                    # mov = x86.ops.RM_VmovapdOp
-                    raise DiagnosticException(
-                        "Double precision vector load is not implemented yet."
-                    )
-                case _:
-                    mov = None
-
-            assert mov is not None
-            mov_op = mov(cast_op, offset=0, result=vect_reg_type)
-            rewriter.replace_matched_op([cast_op, mov_op])
+        # Pointer cast
+        x86_reg_type = x86.register.UNALLOCATED_GENERAL
+        cast_op = UnrealizedConversionCastOp.get((source,), (x86_reg_type,))
+        # Output vector description
+        vector_num_elements = value_type.element_count()
+        element_type = cast(FixedBitwidthType, value_type.get_element_type())
+        element_size = element_type.bitwidth
+        vector_size = vector_num_elements * element_size
+        # Choose the x86 vector register according to the
+        # target architecture and the abstract vector size
+        if vector_size == 128:
+            vect_reg_type = x86.register.UNALLOCATED_SSE
+        elif vector_size == 256 and (self.arch == "avx2" or self.arch == "avx512"):
+            vect_reg_type = x86.register.UNALLOCATED_AVX2
+        elif vector_size == 512 and self.arch == "avx512":
+            vect_reg_type = x86.register.UNALLOCATED_AVX512
         else:
-            raise DiagnosticException("Not implemented")
+            raise DiagnosticException(
+                "The vector size and target architecture are inconsistent."
+            )
+        # Choose the x86 vector instruction according to the
+        # abstract vector element size
+        match element_size:
+            case 16:
+                raise DiagnosticException(
+                    "Half-precision vector load is not implemented yet."
+                )
+            case 32:
+                mov = x86.ops.RM_VmovupsOp
+            case 64:
+                # mov = x86.ops.RM_VmovapdOp
+                raise DiagnosticException(
+                    "Double precision vector load is not implemented yet."
+                )
+            case _:
+                raise DiagnosticException(
+                    "Float precision must be half, single or double."
+                )
+
+        mov_op = mov(cast_op, offset=0, result=vect_reg_type)
+        rewriter.replace_matched_op([cast_op, mov_op])
 
 
 @dataclass(frozen=True)
@@ -87,6 +83,10 @@ class ConvertPtrToX86Pass(ModulePass):
 
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
         PatternRewriteWalker(
-            GreedyRewritePatternApplier([PtrLoadToX86(self.arch)]),
+            GreedyRewritePatternApplier(
+                [
+                    PtrLoadToX86(self.arch),
+                ]
+            ),
             apply_recursively=False,
         ).rewrite_module(op)
