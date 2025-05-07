5d33420a81e834732c119047e49ca10a76a32e13
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-05-07 15:12:52 +0000

    Make complex type struct packable type

--- xdsl/dialects/builtin.py
+++ xdsl/dialects/builtin.py
@@ -1005,12 +1005,44 @@ class FloatAttr(Generic[_FloatAttrType], BuiltinAttribute, TypedAttribute):
 
 
 @irdl_attr_definition
-class ComplexType(ParametrizedAttribute, BuiltinAttribute, TypeAttribute):
+class ComplexType(StructPackableType[tuple[float, float]], ParametrizedAttribute, BuiltinAttribute, TypeAttribute):
     name = "complex"
     element_type: ParameterDef[IntegerType | AnyFloat]
 
     def __init__(self, element_type: IntegerType | AnyFloat) -> None:
         ParametrizedAttribute.__init__(self, [element_type])
+
+    @property
+    def size(self) -> int:
+        return 2 * self.element_type.size
+
+    @property
+    def format(self) -> str:
+        return f"<2{self.element_type.format[1]}"
+
+    def iter_unpack(self, buffer: ReadableBuffer, /) -> Iterator[tuple[float, float]]:
+        values = (values[0] for values in struct.iter_unpack(self.format, buffer))
+        return ((real, imag) for real, imag in zip(values, values))
+
+    def unpack(self, buffer: ReadableBuffer, num: int, /) -> tuple[tuple[float, float], ...]:
+        length = 2 * num
+        fmt = self.format[0] + str(length) + self.format[2:]
+        unpacked_floats = struct.unpack(fmt, buffer)
+        agg : list[tuple[float, float]] = []
+        for i in range(length // 2):
+            agg.append((unpacked_floats[i * 2], unpacked_floats[i * 2 + 1]))
+        return tuple(agg)
+
+    def pack_into(self, buffer: WriteableBuffer, offset: int, value: tuple[float, float]) -> None:
+        struct.pack_into(self.format, buffer, offset, value[0], value[1])
+
+    def pack(self, values: Sequence[tuple[float, float]]) -> bytes:
+        fmt = self.format[0] + str(2 * len(values)) + self.format[2:]
+        import itertools
+        floats : Iterator[tuple[float, ...]] = ((value[0], value[1]) for value in values)
+
+        flat_floats = itertools.chain.from_iterable(floats)
+        return struct.pack(fmt, *flat_floats)
 
 
 @irdl_attr_definition
