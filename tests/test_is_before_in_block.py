3005503cf3461f027607a8096ea89cd9b0bc0b0c
Gabriel Rodríguez Canal <gabriel.rodcanal@ed.ac.uk>
2025-05-13 15:15:48 +0100

    Simplify test for is_before_in_block
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

--- tests/test_is_before_in_block.py
+++ tests/test_is_before_in_block.py
@@ -2,25 +2,19 @@ from xdsl.dialects import test
 from xdsl.irdl import Block, Region
 
 
-def test_is_before_in_block_correct():
+def test_same_block():
     op1 = test.TestOp()
     op2 = test.TestOp()
     test.TestOp(regions=[Region(Block([op1, op2]))])
 
     assert op1.is_before_in_block(op2)
-
-
-def test_is_before_in_block_incorrect():
-    op1 = test.TestOp()
-    op2 = test.TestOp()
-    test.TestOp(regions=[Region(Block([op1, op2]))])
-
     assert not op2.is_before_in_block(op1)
-
-
-def test_is_before_in_block_same_op():
+    assert not op1.is_before_in_block(op1)
+    
+def test_different_blocks():
     op1 = test.TestOp()
     op2 = test.TestOp()
-    test.TestOp(regions=[Region(Block([op1, op2]))])
+    test.TestOp(regions=[Region(Block([op1])), Region(Block([op2]))])
 
-    assert not op1.is_before_in_block(op1)
+    assert not op1.is_before_in_block(op2)
+    assert not op2.is_before_in_block(op1)
