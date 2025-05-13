29d957d7b0ea9e42321b6ef12117ea4544244eb7
Gabriel Rodriguez-Canal <gabriel.rodriguezcanal@pnnl.gov>
2025-05-13 15:19:14 +0100

    Fix format

--- tests/test_is_before_in_block.py
+++ tests/test_is_before_in_block.py
@@ -10,7 +10,8 @@ def test_same_block():
     assert op1.is_before_in_block(op2)
     assert not op2.is_before_in_block(op1)
     assert not op1.is_before_in_block(op1)
-    
+
+
 def test_different_blocks():
     op1 = test.TestOp()
     op2 = test.TestOp()
