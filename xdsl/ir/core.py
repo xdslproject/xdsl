4411b621d137a8ffb41ca13a6bbf07db7bbe084d
Gabriel Rodríguez Canal <gabriel.rodcanal@ed.ac.uk>
2025-05-14 11:23:28 +0100

    core: Find the ancestor operation of an operation in a block (#4391)
    Efficient method to find the ancestor operation of an operation in a
    block. Ported from
    https://mlir.llvm.org/doxygen/Block_8cpp_source.html#l00076
    
    ---------
    
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

--- xdsl/ir/core.py
+++ xdsl/ir/core.py
@@ -1774,6 +1774,20 @@ class Block(IRNode, IRWithUses):
         for op in self.ops:
             op.drop_all_references()
 
+    def find_ancestor_op_in_block(self, op: Operation) -> Operation | None:
+        """
+        Traverse up the operation hierarchy starting from op to find the ancestor
+        operation that resides in the block.
+
+        Returns None if no ancestor is found.
+        """
+        curr_op = op
+        while curr_op.parent_block() != self:
+            if (curr_op := curr_op.parent_op()) is None:
+                return None
+
+        return curr_op
+
     def erase(self, safe_erase: bool = True) -> None:
         """
         Erase the block, and remove all its references to other operations.
