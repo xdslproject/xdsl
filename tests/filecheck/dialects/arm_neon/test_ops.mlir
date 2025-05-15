6aef2f449b44616c9416b07347b554bb838781d6
emmau678 <emmau678@gmail.com>
2025-05-15 14:12:23 +0100

    make scalar_idx optional

--- tests/filecheck/dialects/arm_neon/test_ops.mlir
+++ tests/filecheck/dialects/arm_neon/test_ops.mlir
@@ -12,13 +12,13 @@
 // CHECK: %v4 = arm_neon.get_register : !arm_neon.reg<v4>
 %v4 = arm_neon.get_register : !arm_neon.reg<v4>
 
-// CHECK: %dss_fmulvecscalar = arm_neon.dss.fmulvecscalar %v1, %v2[0] S {comment = "floating-point vector multiply (mixed) v1 by v2"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
+// CHECK: %dss_fmul_mixed = arm_neon.dss.fmul %v1, %v2[0] S {comment = "floating-point vector multiply (mixed) v1 by v2"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
 // CHECK-ASM: fmul v3.4S, v1.4S, v2.S[0] # floating-point vector multiply (mixed) v1 by v2
-%dss_fmulvecscalar = arm_neon.dss.fmulvecscalar %v1, %v2[0] S {"comment" = "floating-point vector multiply (mixed) v1 by v2"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
+%dss_fmul_mixed = arm_neon.dss.fmul %v1, %v2[0] S {"comment" = "floating-point vector multiply (mixed) v1 by v2"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
 
-// CHECK: %dss_fmulvec = arm_neon.dss.fmulvec %v1, %v2 S {comment = "floating-point vector multiply v1 by v2"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
+// CHECK: %dss_fmul_vec = arm_neon.dss.fmul %v1, %v2 S {comment = "floating-point vector multiply v1 by v2"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
 // CHECK-ASM: fmul v3.4S, v1.4S, v2.4S # floating-point vector multiply v1 by v2
-%dss_fmulvec = arm_neon.dss.fmulvec %v1, %v2 S {"comment" = "floating-point vector multiply v1 by v2"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
+%dss_fmul_vec = arm_neon.dss.fmul %v1, %v2 S {"comment" = "floating-point vector multiply v1 by v2"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
 
 // CHECK: %dss_fmla = arm_neon.dss.fmla %v3, %v1, %v2[0] S {comment = "Floating-point fused Multiply-Add to accumulator"} : (!arm_neon.reg<v1>, !arm_neon.reg<v2>) -> !arm_neon.reg<v3>
 // CHECK-ASM: fmla v3.4S, v1.4S, v2.S[0] # Floating-point fused Multiply-Add to accumulator
--- xdsl/dialects/arm_neon.py
+++ xdsl/dialects/arm_neon.py
@@ -7,6 +7,7 @@ from xdsl.dialects.arm.assembly import AssemblyInstructionArg, reg, square_brack
 from xdsl.dialects.arm.ops import ARMInstruction, ARMOperation
 from xdsl.dialects.arm.register import ARMRegisterType, IntRegisterType
 from xdsl.dialects.builtin import IntegerAttr, StringAttr, i8
+from xdsl.dialects.stencil import IndexAttr
 from xdsl.ir import (
     Attribute,
     Dialect,
@@ -23,6 +24,7 @@ from xdsl.irdl import (
     irdl_attr_definition,
     irdl_op_definition,
     operand_def,
+    opt_prop_def,
     prop_def,
     result_def,
     var_operand_def,
@@ -179,80 +181,33 @@ class GetRegisterOp(ARMOperation):
 
 
 @irdl_op_definition
-class DSSFMulVecScalarOp(ARMInstruction):
+class DSSFMulOp(ARMInstruction):
     """
-    Floating-point multiply (mixed: first source operand is a vector, second is a scalar. Destination is a vector)
-    This instruction multiplies each of the floating-point values in the first source operand by the
-    second source operand and writes the resulting values to the corresponding lanes of the destination.
+    Floating-point multiply. Different instruction types supported:
+
+    1. Vector:  multiplies corresponding floating-point values in the vectors in the two
+    source NEON registers, and writes the result vector to the destination.
+
+    See external [documentation](https://developer.arm.com/documentation/100069/0606/SIMD-Vector-Instructions/FMUL--vector-).
+
+    2. Mixed: (first source operand is a vector, second is a scalar. Destination is a vector)
+    This instruction multiplies each of the floating-point values in the first source
+    operand by the second source operand and writes the resulting values to the corresponding
+    lanes of the destination.
     Encoding: FMUL <Vd>.<T>, <Vn>.<T>, <Vm>.<idx>.
-    Vd, Vn, Vm specify the regs. The <T> specifier determines element arrangement (size and count).
-    The <idx> specifier determines the index of Vm at which the second source operand (scalar) can be found,
-    preceded by a size specifier.
 
     See external [documentation](https://developer.arm.com/documentation/ddi0602/2024-12/SIMD-FP-Instructions/FMUL--vector---Floating-point-multiply--vector--?lang=en#T_option__4).
     """
 
-    name = "arm_neon.dss.fmulvecscalar"
+    name = "arm_neon.dss.fmul"
     d = result_def(NEONRegisterType)
     s1 = operand_def(NEONRegisterType)
     s2 = operand_def(NEONRegisterType)
-    scalar_idx = attr_def(IntegerAttr[i8])
-    arrangement = attr_def(NeonArrangementAttr)
-
-    assembly_format = "$s1 `,` $s2 `[` $scalar_idx `]` $arrangement attr-dict `:` `(` type($s1) `,` type($s2) `)` `->` type($d)"
-
-    def __init__(
-        self,
-        s1: Operation | SSAValue,
-        s2: Operation | SSAValue,
-        *,
-        d: NEONRegisterType,
-        arrangement: NeonArrangement | NeonArrangementAttr,
-        comment: str | StringAttr | None = None,
-    ):
-        if isinstance(comment, str):
-            comment = StringAttr(comment)
-        if isinstance(arrangement, NeonArrangement):
-            arrangement = NeonArrangementAttr(arrangement)
-        super().__init__(
-            operands=(s1, s2),
-            attributes={
-                "comment": comment,
-                "arrangement": arrangement,
-            },
-            result_types=(d,),
-        )
-
-    def assembly_instruction_name(self) -> str:
-        return "fmul"
-
-    def assembly_line_args(self):
-        return (
-            VectorWithArrangement(self.d, self.arrangement),
-            VectorWithArrangement(self.s1, self.arrangement),
-            VectorWithArrangement(
-                self.s2, self.arrangement, index=self.scalar_idx.value.data
-            ),
-        )
-
-
-@irdl_op_definition
-class DSSFMulVecOp(ARMInstruction):
-    """
-    Floating-point Multiply (vector). This instruction multiplies corresponding
-    floating-point values in the vectors in the two source NEON registers,
-    places the result in a vector, and writes the vector to the destination.
-
-    See external [documentation](https://developer.arm.com/documentation/100069/0606/SIMD-Vector-Instructions/FMUL--vector-).
-    """
-
-    name = "arm_neon.dss.fmulvec"
-    d = result_def(NEONRegisterType)
-    s1 = operand_def(NEONRegisterType)
-    s2 = operand_def(NEONRegisterType)
+    scalar_idx = opt_prop_def(IntegerAttr[i8])
     arrangement = prop_def(NeonArrangementAttr)
 
-    assembly_format = "$s1 `,` $s2 $arrangement attr-dict `:` `(` type($s1) `,` type($s2) `)` `->` type($d)"
+    assembly_format = "$s1 `,` $s2 (`[` $scalar_idx^ `]`)? $arrangement attr-dict `:` \
+    `(` type($s1) `,` type($s2) `)` `->` type($d)"
 
     def __init__(
         self,
@@ -260,6 +215,7 @@ class DSSFMulVecOp(ARMInstruction):
         s2: Operation | SSAValue,
         *,
         d: NEONRegisterType,
+        scalar_idx: IndexAttr | None,
         arrangement: NeonArrangement | NeonArrangementAttr,
         comment: str | StringAttr | None = None,
     ):
@@ -273,6 +229,7 @@ class DSSFMulVecOp(ARMInstruction):
                 "comment": comment,
             },
             properties={
+                "scalar_idx": scalar_idx,
                 "arrangement": arrangement,
             },
             result_types=(d,),
@@ -285,7 +242,15 @@ class DSSFMulVecOp(ARMInstruction):
         return (
             VectorWithArrangement(self.d, self.arrangement),
             VectorWithArrangement(self.s1, self.arrangement),
-            VectorWithArrangement(self.s2, self.arrangement),
+            VectorWithArrangement(
+                self.s2,
+                self.arrangement,
+                **(
+                    {"index": self.scalar_idx.value.data}
+                    if self.scalar_idx is not None
+                    else {}
+                ),
+            ),
         )
 
 
@@ -559,8 +524,7 @@ ARM_NEON = Dialect(
     "arm_neon",
     [
         DSSFmlaVecScalarOp,
-        DSSFMulVecScalarOp,
-        DSSFMulVecOp,
+        DSSFMulOp,
         DSDupOp,
         DSVecMovOp,
         DVarSSt1Op,
