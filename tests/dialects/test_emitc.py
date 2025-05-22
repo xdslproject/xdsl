8d6806c7ebabc3aa7c9e926ca9d63559cc6e6f51
Hendrik Klug <hendrik.klug@gmail.com>
2025-05-22 19:31:57 +0200

    dialects: (emitc) add emitc array verify and parsing checks

--- tests/dialects/test_emitc.py
+++ tests/dialects/test_emitc.py
@@ -1,6 +1,6 @@
 import pytest
 
-from xdsl.dialects.builtin import i32
+from xdsl.dialects.builtin import MemRefType, i32
 from xdsl.dialects.emitc import EmitC_ArrayType
 from xdsl.utils.exceptions import VerifyException
 
@@ -8,3 +8,29 @@ from xdsl.utils.exceptions import VerifyException
 def test_emitc_array_empty_shape():
     with pytest.raises(VerifyException, match="EmitC array shape must not be empty"):
         EmitC_ArrayType([], i32)
+
+
+def test_emitc_array_negative_dimension():
+    with pytest.raises(
+        VerifyException, match="EmitC array dimensions must have non-negative size"
+    ):
+        EmitC_ArrayType([-1], i32)
+
+
+def test_emitc_array_nested_array_type():
+    nested_array = EmitC_ArrayType([2], i32)
+    with pytest.raises(
+        VerifyException,
+        match="EmitC array element type cannot be another EmitC_ArrayType.",
+    ):
+        EmitC_ArrayType([1], nested_array)
+
+
+def test_emitc_array_unsupported_element_type():
+    unsupported_type = MemRefType(element_type=i32, shape=[1])
+
+    with pytest.raises(
+        VerifyException,
+        match=f"EmitC array element type '{unsupported_type}' is not a supported EmitC type.",
+    ):
+        EmitC_ArrayType([1], unsupported_type)
