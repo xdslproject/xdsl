fdabac7889b2e41ce8e08dac249e9d9dde7af61b
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-05-22 00:40:59 +0000

    remove unnecessary test

--- tests/filecheck/dialects/transform/transform_named_sequence.mlir
+++ tests/filecheck/dialects/transform/transform_named_sequence.mlir
@@ -12,7 +12,7 @@ module {
   module attributes {transform.with_named_sequence} {
     "transform.named_sequence"() <{arg_attrs = [{transform.readonly}], function_type = (!transform.op<"builtin.module">) -> (), sym_name = "__transform_main"}> ({
     ^bb0(%arg0: !transform.op<"builtin.module">):
-      %0 = "transform.apply_registered_pass"(%arg0) <{options = "", pass_name = "canonicalize"}> : (!transform.op<"builtin.module">) -> !transform.op<"builtin.module">
+      %0 = transform.apply_registered_pass "canonicalize" to %arg0 : (!transform.op<"builtin.module">) -> !transform.op<"builtin.module">
       transform.yield
     }) : () -> ()
   }
@@ -25,6 +25,6 @@ module {
 
 // CHECK: "transform.named_sequence"() <{arg_attrs = [{transform.readonly}], function_type = (!transform.op<"builtin.module">) -> (), sym_name = "__transform_main"}> ({
 // CHECK-NEXT: ^0(%arg0 : !transform.op<"builtin.module">):
-// CHECK-NEXT:   %0 = "transform.apply_registered_pass"(%arg0) <{options = "", pass_name = "canonicalize"}> : (!transform.op<"builtin.module">) -> !transform.op<"builtin.module">
+// CHEKC-NEXT:   %0 = transform.apply_registered_pass "canonicalize" to %arg0 : (!transform.op<"builtin.module">) -> !transform.op<"builtin.module">
 // CHECK-NEXT:   transform.yield
 // CHECK-NEXT: }) : () -> ()
--- tests/interpreters/test_transform_interpreter.py
+++ tests/interpreters/test_transform_interpreter.py
@@ -1,6 +1,6 @@
 from xdsl.builder import ImplicitBuilder
 from xdsl.context import Context
-from xdsl.dialects import arith, builtin, func, transform
+from xdsl.dialects import builtin, func, transform
 from xdsl.interpreter import Interpreter
 from xdsl.interpreters.transform import TransformFunctions
 from xdsl.ir import Block, Region
@@ -42,54 +42,3 @@ def test_empty_transform_module():
     expected = Parser(ctx, payload).parse_module()
     (observed,) = interpreter.call_op(named_sequence, (expected,))
     assert expected is observed
-
-
-def test_simple_transform_module():
-    payload = """
-    module {
-        func.func @foo() -> i32 {
-            %c1 = arith.constant 1 : i32
-            %add = arith.addi %c1, %c1 : i32
-            func.return %add : i32
-        }
-    }
-    """
-
-    expected = """
-    module {
-        func.func @foo() -> i32 {
-            %add = arith.constant 2 : i32
-            func.return %add : i32
-        }
-    }
-    """
-
-    ty = transform.OperationType("builtin.module")
-    block = Block(arg_types=[ty])
-    with ImplicitBuilder(block):
-        param = block.args[0]
-        _ = transform.ApplyRegisteredPassOp("canonicalize", param)
-        transform.YieldOp()
-
-    module = builtin.ModuleOp(
-        [], attributes={"transform.with_named_sequence": builtin.UnitAttr()}
-    )
-    with ImplicitBuilder(module.body):
-        body = Region(block)
-        sym_name = "__transform_main"
-        function_type = builtin.FunctionType.from_lists([ty], [])
-        named_sequence = transform.NamedSequenceOp(sym_name, function_type, body)
-
-    ctx = Context()
-    ctx.load_dialect(builtin.Builtin)
-    ctx.load_dialect(arith.Arith)
-    ctx.load_dialect(func.Func)
-    ctx.load_dialect(transform.Transform)
-
-    module = Parser(ctx, payload).parse_module()
-    expected = Parser(ctx, expected).parse_module()
-
-    interpreter = Interpreter(builtin.ModuleOp([]))
-    interpreter.register_implementations(TransformFunctions(ctx, get_all_passes()))
-    interpreter.call_op(named_sequence, (module,))
-    assert str(expected) == str(module)
