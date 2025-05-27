5866d74ad3964397c40cc265c981368bc9315e85
Emma Urquhart <77412390+emmau678@users.noreply.github.com>
2025-05-27 22:44:38 +0100

    dialects: (arm_neon) add VectorWithArrangement.from_vec_type (#4345)

--- tests/dialects/test_arm_neon.py
+++ tests/dialects/test_arm_neon.py
@@ -1,9 +1,12 @@
+import pytest
+
 from xdsl.dialects.arm_neon import (
     NeonArrangement,
     NeonArrangementAttr,
     NEONRegisterType,
     VectorWithArrangement,
 )
+from xdsl.dialects.builtin import Float16Type, Float32Type, Float64Type, VectorType
 
 
 def test_assembly_str_without_index():
@@ -18,3 +21,23 @@ def test_assembly_str_with_index():
     arrangement = NeonArrangementAttr(NeonArrangement.D)
     vecreg = VectorWithArrangement(reg=reg, arrangement=arrangement, index=5)
     assert vecreg.assembly_str() == "v0.D[5]"
+
+
+def test_arr_from_vec():
+    assert (
+        NeonArrangement.from_vec_type(VectorType(Float16Type(), [8]))
+        == NeonArrangement.H
+    )
+    assert (
+        NeonArrangement.from_vec_type(VectorType(Float32Type(), [4]))
+        == NeonArrangement.S
+    )
+    assert (
+        NeonArrangement.from_vec_type(VectorType(Float64Type(), [2]))
+        == NeonArrangement.D
+    )
+
+    with pytest.raises(
+        ValueError, match="Invalid vector type for ARM NEON: vector<3xf16>"
+    ):
+        NeonArrangement.from_vec_type(VectorType(Float16Type(), [3]))
