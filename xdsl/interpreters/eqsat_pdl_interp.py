f975c7e12e0b621fe7729f40236ad94aaa0a5249
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-06-06 15:51:20 +0200

    GetDefiningOp and FinalizeOp with backtracking

--- xdsl/interpreters/eqsat_pdl_interp.py
+++ xdsl/interpreters/eqsat_pdl_interp.py
@@ -1,26 +1,42 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Any
+from typing import Any, cast
 
 from xdsl.dialects import eqsat, pdl_interp
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.dialects.pdl import ValueType
 from xdsl.interpreter import (
     Interpreter,
+    ReturnedValues,
+    Successor,
     impl,
+    impl_terminator,
     register_impls,
 )
 from xdsl.interpreters.pdl_interp import PDLInterpFunctions
-from xdsl.ir import Operation, OpResult
+from xdsl.ir import Block, Operation, OpResult, SSAValue
 from xdsl.transforms.common_subexpression_elimination import KnownOps
 from xdsl.utils.disjoint_set import DisjointSet
 from xdsl.utils.exceptions import InterpretationError
+from xdsl.utils.scoped_dict import ScopedDict
+
+
+@dataclass
+class BacktrackPoint:
+    block: Block
+    block_args: tuple[SSAValue, ...]
+    scope: ScopedDict[SSAValue, Any]
+    gdo_op: pdl_interp.GetDefiningOpOp
+    index: int
+    max_index: int
 
 
 @register_impls
 @dataclass
 class EqsatPDLInterpFunctions(PDLInterpFunctions):
+    backtrack_stack: list[BacktrackPoint] = field(default_factory=list[BacktrackPoint])
+    visited: bool = True
     known_ops: KnownOps = field(default_factory=KnownOps)
     eclass_union_find: DisjointSet[eqsat.EClassOp] = field(
         default_factory=lambda: DisjointSet[eqsat.EClassOp]()
@@ -104,3 +120,56 @@ class EqsatPDLInterpFunctions(PDLInterpFunctions):
                         )
             results.append(result)
         return (results,)
+
+    @impl(pdl_interp.GetDefiningOpOp)
+    def run_getdefiningop(
+        self,
+        interpreter: Interpreter,
+        op: pdl_interp.GetDefiningOpOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        defining_op = cast(
+            None | Operation,
+            PDLInterpFunctions.run_get_defining_op(self, interpreter, op, args).values[
+                0
+            ],
+        )
+        if isinstance(eclass_op := defining_op, eqsat.EClassOp):
+            if not self.visited:
+                if op != self.backtrack_stack[-1].gdo_op:
+                    raise InterpretationError(
+                        "TODO: handle the case where a block contains multiple pdl_interp.get_defining_op."
+                    )
+                index = self.backtrack_stack[-1].index
+                self.visited = True
+            else:
+                block = op.parent_block()
+                assert block
+                block_args = interpreter.get_values(block.args)
+                scope = interpreter._ctx.parent  # pyright: ignore[reportPrivateUsage]
+                assert scope
+                index = 0
+                self.backtrack_stack.append(
+                    BacktrackPoint(
+                        block, block_args, scope, op, index, len(eclass_op.operands) - 1
+                    )
+                )
+            return PDLInterpFunctions.run_get_defining_op(
+                self, interpreter, op, (eclass_op.operands[index],)
+            ).values
+
+        return (defining_op,)
+
+    @impl_terminator(pdl_interp.FinalizeOp)
+    def run_finalize(
+        self, interpreter: Interpreter, _: pdl_interp.FinalizeOp, args: tuple[Any, ...]
+    ):
+        for backtrack_point in reversed(self.backtrack_stack):
+            if backtrack_point.index >= backtrack_point.max_index:
+                self.backtrack_stack.pop()
+            else:
+                backtrack_point.index += 1
+                interpreter._ctx = backtrack_point.scope  # pyright: ignore[reportPrivateUsage]
+                self.visited = False
+                return Successor(backtrack_point.block, backtrack_point.block_args), ()
+        return ReturnedValues(()), ()
