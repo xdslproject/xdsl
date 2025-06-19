a5d8ba451403b0e41ad64beccae3e3fcad7bbedf
EdmundGoodman <egoodman3141@gmail.com>
2025-06-19 11:52:09 +0100

    Reduce number of operations to make micro-benchmarks shorter

--- benchmarks/microbenchmarks.py
+++ benchmarks/microbenchmarks.py
@@ -65,7 +65,7 @@ class HasManyTraitOp(IRDLOperation):
 class IRTraversal:
     """Benchmark the time to traverse xDSL IR."""
 
-    EXAMPLE_BLOCK_NUM_OPS = 32_768
+    EXAMPLE_BLOCK_NUM_OPS = 1_000
     EXAMPLE_OPS = [EmptyOp() for _ in range(EXAMPLE_BLOCK_NUM_OPS)]
     EXAMPLE_BLOCK = Block(ops=EXAMPLE_OPS)
 
