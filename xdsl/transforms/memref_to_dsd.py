ad0e90f50691cef1f5d13f1d7bd0b9c3600c4be2
Sasha Lopoukhine <superlopuh@gmail.com>
2025-06-20 10:55:14 +0100

    remove size = size

--- xdsl/transforms/memref_to_dsd.py
+++ xdsl/transforms/memref_to_dsd.py
@@ -135,7 +135,6 @@ class LowerSubviewOpPass(RewritePattern):
                 "1d access into nd memref must specify one size > 1"
             )
             size, size_count = counter_sizes.most_common()[0]
-            size = size
 
             assert size_count == 1, (
                 "1d access into nd memref can only specify one size > 1, which can occur only once"
