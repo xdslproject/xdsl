ab082369d41dc3107cd6c4f272cef2c8a28691c0
EdmundGoodman <egoodman3141@gmail.com>
2025-06-23 17:58:00 +0100

    Split out type and function tests

--- tests/filecheck/frontend/refactor/builtin.py
+++ tests/filecheck/frontend/refactor/builtin.py
@@ -5,21 +5,35 @@ from xdsl.dialects.builtin import I64, IntegerAttr, i64
 from xdsl.frontend.pyast.context import CodeContext
 from xdsl.frontend.pyast.program import FrontendProgram
 
-p = FrontendProgram()
-p.register_type(IntegerAttr[I64], i64, annotation_name="IntegerAttr[I64]")
-p.register_function(IntegerAttr.__add__, arith.AddiOp)
-with CodeContext(p):
+p1 = FrontendProgram()
+p1.register_type(IntegerAttr[I64], i64, annotation_name="IntegerAttr[I64]")
+with CodeContext(p1):
     # CHECK:      builtin.module {
-    # CHECK-NEXT:   func.func @test_addi_overload(%a : i64, %b : i64) -> i64 {
+    # CHECK-NEXT:   func.func @test_identity(%a : i64) -> i64 {
+    # CHECK-NEXT:     func.return %a : i64
+    # CHECK-NEXT:   }
+    # CHECK-NEXT: }
+    def test_identity(a: IntegerAttr[I64]) -> IntegerAttr[I64]:
+        return a
+
+
+p1.compile(desymref=True)
+print(p1.textual_format())
+
+
+p2 = FrontendProgram()
+p2.register_type(IntegerAttr[I64], i64, annotation_name="IntegerAttr[I64]")
+p2.register_function(IntegerAttr.__add__, arith.AddiOp)
+with CodeContext(p2):
+    # CHECK:      builtin.module {
+    # CHECK-NEXT:   func.func @test_addi(%a : i64, %b : i64) -> i64 {
     # CHECK-NEXT:     %0 = arith.addi %a, %b : i64
     # CHECK-NEXT:     func.return %0 : i64
     # CHECK-NEXT:   }
     # CHECK-NEXT: }
-    def test_addi_overload(
-        a: IntegerAttr[I64], b: IntegerAttr[I64]
-    ) -> IntegerAttr[I64]:
+    def test_addi(a: IntegerAttr[I64], b: IntegerAttr[I64]) -> IntegerAttr[I64]:
         return a + b
 
 
-p.compile(desymref=True)
-print(p.textual_format())
+p2.compile(desymref=True)
+print(p2.textual_format())
