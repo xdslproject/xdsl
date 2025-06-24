8696ce7d1269fb1248a6f9fd7efe92ee463158b4
EdmundGoodman <egoodman3141@gmail.com>
2025-06-24 16:38:29 +0100

    Revert "Remove additional parts of #4617 unrelated to PR"
    This reverts commit a40ee68ae99f9f578a444db9c229af0423638212.

--- xdsl/dialects/builtin.py
+++ xdsl/dialects/builtin.py
@@ -887,6 +887,12 @@ class IntegerAttr(
         """
         return tuple(IntegerAttr(value, type) for value in type.unpack(buffer, num))
 
+    def __add__(self, other: Self) -> Self:
+        """Add two integers."""
+        return type(self)(
+            self.value.data + other.value.data, self.type, truncate_bits=True
+        )
+
 
 BoolAttr: TypeAlias = IntegerAttr[Annotated[IntegerType, IntegerType(1)]]
 
