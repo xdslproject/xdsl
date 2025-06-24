421e0bed8a1c5f88373067a4a7682abd11b07d76
EdmundGoodman <egoodman3141@gmail.com>
2025-06-24 18:15:30 +0100

    Move cf dialect tests to not use pyast dialect

--- tests/filecheck/frontend/dialects/cf.py
+++ tests/filecheck/frontend/dialects/cf.py
@@ -1,19 +1,21 @@
 # RUN: python %s | filecheck %s
 
+from xdsl.dialects.builtin import I1, I32, IntegerAttr, i1, i32
 from xdsl.frontend.pyast.context import CodeContext
-from xdsl.frontend.pyast.dialects.builtin import i1, i32
 from xdsl.frontend.pyast.exception import CodeGenerationException
 from xdsl.frontend.pyast.program import FrontendProgram
 
 p = FrontendProgram()
+p.register_type(IntegerAttr[I1], i1)
+p.register_type(IntegerAttr[I32], i32)
 with CodeContext(p):
     # CHECK: cf.assert %{{.*}}, ""
-    def test_assert_I(cond: i1):
+    def test_assert_I(cond: IntegerAttr[I1]):
         assert cond
         return
 
     # CHECK: cf.assert %{{.*}}, "some message"
-    def test_assert_II(cond: i1):
+    def test_assert_II(cond: IntegerAttr[I1]):
         assert cond, "some message"
         return
 
@@ -24,7 +26,7 @@ print(p.textual_format())
 try:
     with CodeContext(p):
         # CHECK: Expected a string constant for assertion message, found 'ast.Name'
-        def test_assert_message_type(cond: i1, a: i32):
+        def test_assert_message_type(cond: IntegerAttr[I1], a: IntegerAttr[I32]):
             assert cond, a
             return
 
