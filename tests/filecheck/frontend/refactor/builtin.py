3f205a5fd968e6fb7e5dcdadf9fe2dd01c0d4ee2
EdmundGoodman <egoodman3141@gmail.com>
2025-06-25 13:27:55 +0100

    Change user API to use c_int rather than IntegerAttr

--- tests/filecheck/frontend/refactor/builtin.py
+++ tests/filecheck/frontend/refactor/builtin.py
@@ -1,18 +1,20 @@
 # RUN: python %s | filecheck %s
 
-from xdsl.dialects.builtin import I64, IntegerAttr, i64
+from ctypes import c_int64
+
+from xdsl.dialects.builtin import i64
 from xdsl.frontend.pyast.context import CodeContext
 from xdsl.frontend.pyast.program import FrontendProgram
 
 p1 = FrontendProgram()
-p1.register_type(IntegerAttr[I64], i64)
+p1.register_type(c_int64, i64)
 with CodeContext(p1):
     # CHECK:      builtin.module {
     # CHECK-NEXT:   func.func @test_identity(%a : i64) -> i64 {
     # CHECK-NEXT:     func.return %a : i64
     # CHECK-NEXT:   }
     # CHECK-NEXT: }
-    def test_identity(a: IntegerAttr[I64]) -> IntegerAttr[I64]:
+    def test_identity(a: c_int64) -> c_int64:
         return a
 
 
