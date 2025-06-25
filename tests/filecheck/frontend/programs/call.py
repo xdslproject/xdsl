249e5cd1de8107075cdb729262256a45f50a9d60
EdmundGoodman <egoodman3141@gmail.com>
2025-06-25 15:50:33 +0100

    Use ctypes to indicate i32

--- tests/filecheck/frontend/programs/call.py
+++ tests/filecheck/frontend/programs/call.py
@@ -1,16 +1,18 @@
 # RUN: python %s | filecheck %s
 
+from ctypes import c_int32
+
 from xdsl.dialects import arith, builtin
 from xdsl.frontend.pyast.context import CodeContext
 from xdsl.frontend.pyast.program import FrontendProgram
 
 
-def add_bigint(operand1: int, operand2: int) -> int: ...
+def add_i32(operand1: c_int32, operand2: c_int32) -> c_int32: ...
 
 
 p1 = FrontendProgram()
-p1.register_type(int, builtin.i32)
-p1.register_function(add_bigint, arith.AddiOp)
+p1.register_type(c_int32, builtin.i32)
+p1.register_function(add_i32, arith.AddiOp)
 with CodeContext(p1):
     # CHECK:      builtin.module {
     # CHECK-NEXT:   func.func @foo(%x : i32, %y : i32) -> i32 {
@@ -18,8 +20,8 @@ with CodeContext(p1):
     # CHECK-NEXT:     func.return %0 : i32
     # CHECK-NEXT:   }
     # CHECK-NEXT: }
-    def foo(x: int, y: int) -> int:
-        return add_bigint(x, operand2=y)
+    def foo(x: c_int32, y: c_int32) -> c_int32:
+        return add_i32(x, operand2=y)
 
 
 p1.compile()
