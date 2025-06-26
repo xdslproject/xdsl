a7dfbc1374153aafae9c9c8c158568a2a2e03fa3
Edmund Goodman <37504168+EdmundGoodman@users.noreply.github.com>
2025-06-26 13:53:15 +0100

    frontend: (pyast) remove custom pyast dialects (#4643)
    Remove the custom dialects from PyAST. #4640 swaps test cases to use the
    new API, allowing this PR to remove their implementation.

--- tests/filecheck/frontend/dialects/func.py
+++ tests/filecheck/frontend/dialects/func.py
@@ -1,17 +1,18 @@
 # RUN: python %s | filecheck %s
 
+from xdsl.dialects import bigint
 from xdsl.frontend.pyast.context import CodeContext
-from xdsl.frontend.pyast.dialects.builtin import i32
 from xdsl.frontend.pyast.program import FrontendProgram
 
 p = FrontendProgram()
+p.register_type(int, bigint.bigint)
 with CodeContext(p):
-    # CHECK:      func.func @f1(%{{.*}} : i32) {
+    # CHECK:      func.func @f1(%{{.*}} : !bigint.bigint) {
     # CHECK-NEXT:   symref.declare "x"
-    # CHECK-NEXT:   symref.update @x = %{{.*}} : i32
+    # CHECK-NEXT:   symref.update @x = %{{.*}} : !bigint.bigint
     # CHECK-NEXT:   func.return
     # CHECK-NEXT: }
-    def f1(x: i32):
+    def f1(x: int):
         return
 
     # CHECK:      func.func @f2() {
@@ -20,13 +21,13 @@ with CodeContext(p):
     def f2():
         return
 
-    # CHECK:      func.func @f3(%{{.*}} : i32) -> i32 {
+    # CHECK:      func.func @f3(%{{.*}} : !bigint.bigint) -> !bigint.bigint {
     # CHECK-NEXT:   symref.declare "x"
-    # CHECK-NEXT:   symref.update @x = %{{.*}} : i32
-    # CHECK-NEXT:   %{{.*}} = symref.fetch @x : i32
-    # CHECK-NEXT:   func.return %{{.*}} : i32
+    # CHECK-NEXT:   symref.update @x = %{{.*}} : !bigint.bigint
+    # CHECK-NEXT:   %{{.*}} = symref.fetch @x : !bigint.bigint
+    # CHECK-NEXT:   func.return %{{.*}} : !bigint.bigint
     # CHECK-NEXT: }
-    def f3(x: i32) -> i32:
+    def f3(x: int) -> int:
         return x
 
 
--- xdsl/frontend/pyast/type_conversion.py
+++ xdsl/frontend/pyast/type_conversion.py
@@ -1,20 +1,12 @@
-import ast
 import importlib
 from collections.abc import Callable
 from dataclasses import dataclass, field
 from typing import (
     Any,
-    _GenericAlias,  # pyright: ignore[reportUnknownVariableType, reportAttributeAccessIssue]
     cast,
 )
 
-import xdsl.dialects.builtin as xdsl_builtin
-import xdsl.frontend.pyast.dialects.builtin as frontend_builtin
-from xdsl.frontend.pyast.dialects.builtin import (
-    _FrontendType,  # pyright: ignore[reportPrivateUsage]
-)
 from xdsl.frontend.pyast.exception import (
-    CodeGenerationException,
     FrontendProgramException,
 )
 from xdsl.ir import Attribute, Operation, SSAValue, TypeAttribute
@@ -55,9 +47,8 @@ class FunctionRegistry:
         method = importlib.import_module(module_name)
         for attr in method_name.split("."):
             method = getattr(method, attr, None)
-        assert method is not None, (
-            f"'{module_name}.{method_name}' must exist to be passed as argument during registration"
-        )
+        if method is None:
+            return None
         assert callable(method), "Guaranteed by type signature of registration method"
         if (operation_type := self.get_operation_type(method)) is not None:
             return operation_type(*args, **kwargs)
@@ -83,7 +74,7 @@ class TypeRegistry:
             )
         if attribute in self._mapping.values():
             raise FrontendProgramException(
-                f"Cannot register multiple source types for IR type '{attribute.__name__}'"
+                f"Cannot register multiple source types for IR type '{attribute}'"
             )
         self._mapping[annotation] = attribute
 
@@ -125,130 +116,3 @@ class TypeConverter:
 
     function_registry: FunctionRegistry = field(default_factory=FunctionRegistry)
     """Mappings between methods on objects and their operations."""
-
-    name_to_xdsl_type_map: dict[str, Attribute] = field(
-        default_factory=dict[str, Attribute]
-    )
-    """
-    Map to cache xDSL types created so far to avoid repeated conversions.
-    """
-
-    xdsl_to_frontend_type_map: dict[type[Attribute], type[_FrontendType]] = field(
-        default_factory=dict[type[Attribute], type[_FrontendType]]
-    )
-    """
-    Map to lookup frontend types based on xDSL type. Useful if we want to see
-    what overloaded Python operations does this xDSL type support.
-    """
-
-    file: str | None = field(default=None)
-
-    def __post_init__(self) -> None:
-        # Cache index type because it is always used implicitly in loops and
-        # many other IR constructs.
-        index = frontend_builtin._Index  # pyright: ignore[reportPrivateUsage]
-        self._cache_type(index, xdsl_builtin.IndexType(), "index")
-
-    def _cache_type(
-        self,
-        frontend_type: type[_FrontendType],
-        xdsl_type: Attribute,
-        type_name: str,
-    ) -> None:
-        """Records frontend and corresponding xDSL types in cache."""
-        if type_name not in self.name_to_xdsl_type_map:
-            self.name_to_xdsl_type_map[type_name] = xdsl_type
-        if xdsl_type.__class__ not in self.xdsl_to_frontend_type_map:
-            self.xdsl_to_frontend_type_map[xdsl_type.__class__] = frontend_type
-
-    def _convert_name(self, type_hint: ast.Name) -> Attribute:
-        # First, check if we have already converted this type hint.
-        type_name = type_hint.id
-        if type_name in self.name_to_xdsl_type_map:
-            return self.name_to_xdsl_type_map[type_name]
-
-        # Otherwise, it must be some frontend type, and we can look up its class
-        # using the imports.
-        if type_name not in self.globals:
-            raise CodeGenerationException(
-                self.file,
-                type_hint.lineno,
-                type_hint.col_offset,
-                f"Unknown type hint '{type_name}'.",
-            )
-        type_class = self.globals[type_name]
-
-        # First, type can be generic, e.g. `class _Integer(Generic[_W, _S])`.
-        if isinstance(type_class, _GenericAlias):
-            generic_type_arguments = type_class.__args__
-            arguments_for_constructor: list[Any] = []
-            for type_argument in generic_type_arguments:
-                # Convert Literal[...] to concrete values.
-                materialized_arguments = type_argument.__args__
-                if len(materialized_arguments) != 1:
-                    raise CodeGenerationException(
-                        self.file,
-                        type_hint.lineno,
-                        type_hint.col_offset,
-                        f"Expected 1 type argument for generic type '{type_name}', got "
-                        f"{len(materialized_arguments)} type arguments instead.",
-                    )
-                arguments_for_constructor.append(materialized_arguments[0])
-                continue
-
-            # Finally, get the constructor of this type and build an xDSL type.
-            if issubclass(type_class.__origin__, _FrontendType):
-                xdsl_type = type_class.to_xdsl()(*arguments_for_constructor)
-                self._cache_type(type_class.__origin__, xdsl_type, type_name)
-                return xdsl_type
-
-            # If this is not a subclass of FrontendType, then abort.
-            raise CodeGenerationException(
-                self.file,
-                type_hint.lineno,
-                type_hint.col_offset,
-                f"'{type_name}' is not a frontend type.",
-            )
-
-        # Otherwise, type can be a simple non-generic frontend type, e.g. `class
-        # _Index(FrontendType)`.
-        if issubclass(type_class, _FrontendType):
-            xdsl_type = type_class.to_xdsl()()
-            self._cache_type(type_class, xdsl_type, type_name)
-            return xdsl_type
-
-        raise CodeGenerationException(
-            self.file,
-            type_hint.lineno,
-            type_hint.col_offset,
-            f"Unknown type hint for type '{type_name}' inside 'ast.Name' expression.",
-        )
-
-    def convert_type_hint(self, type_hint: ast.expr) -> Attribute:
-        """Convertes a Python/frontend type given as AST expression into xDSL type."""
-
-        # Type hint should always be provided if this function is called.
-        assert type_hint is not None
-
-        # TODO: Type hint can be a Subscript AST node, for example
-        # `Foo[Literal[2]]``. Support this in the future patches.
-        if isinstance(type_hint, ast.Subscript):
-            raise CodeGenerationException(
-                self.file,
-                type_hint.lineno,
-                type_hint.col_offset,
-                "Converting subscript type hints is not supported.",
-            )
-
-        # Type hint can also be a TypeAlias. For example, one can define
-        # `foo = Foo[Literal[2]]`. This case also handles standard Python types, like
-        # int, float, etc.
-        if isinstance(type_hint, ast.Name):
-            return self._convert_name(type_hint)
-
-        raise CodeGenerationException(
-            self.file,
-            type_hint.lineno,
-            type_hint.col_offset,
-            f"Unknown type hint AST node '{type_hint}'.",
-        )
