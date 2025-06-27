0876b9189257abc47b05e40d781c5f58c8476cfd
Sasha Lopoukhine <superlopuh@gmail.com>
2025-06-27 14:25:01 +0100

    backend: closed set of allocatable registers (#4633)

--- xdsl/backend/riscv/register_stack.py
+++ xdsl/backend/riscv/register_stack.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
+from typing_extensions import override
+
 from xdsl.backend.register_stack import RegisterStack
-from xdsl.dialects.riscv import FloatRegisterType, IntRegisterType, Registers
+from xdsl.dialects.riscv import FloatRegisterType, IntRegisterType
 
 
 @dataclass
@@ -10,24 +12,12 @@ class RiscvRegisterStack(RegisterStack):
     Available RISCV-specific registers.
     """
 
-    DEFAULT_RESERVED_REGISTERS = {
-        Registers.ZERO,
-        Registers.SP,
-        Registers.GP,
-        Registers.TP,
-        Registers.FP,
-        Registers.S0,  # Same register as FP
-    }
-
-    DEFAULT_AVAILABLE_REGISTERS = (
+    DEFAULT_ALLOCATABLE_REGISTERS = (
         *reversed(IntRegisterType.allocatable_registers()),
         *reversed(FloatRegisterType.allocatable_registers()),
     )
 
     @classmethod
-    def default_reserved_registers(cls):
-        return RiscvRegisterStack.DEFAULT_RESERVED_REGISTERS
-
-    @classmethod
-    def default_available_registers(cls):
-        return RiscvRegisterStack.DEFAULT_AVAILABLE_REGISTERS
+    @override
+    def default_allocatable_registers(cls):
+        return RiscvRegisterStack.DEFAULT_ALLOCATABLE_REGISTERS
