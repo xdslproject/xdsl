ef778b507448e20d6c209377bf4c7a3c3a4675b6
Sasha Lopoukhine <superlopuh@gmail.com>
2025-06-30 09:19:34 +0100

    core: add diagnostic info to original error __notes__ (#4683)

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -47,7 +47,7 @@ from xdsl.irdl import (
 from xdsl.parser import AttrParser, Parser
 from xdsl.printer import Printer
 from xdsl.utils.diagnostic import Diagnostic
-from xdsl.utils.exceptions import DiagnosticException, ParseError
+from xdsl.utils.exceptions import ParseError
 from xdsl.utils.test_value import create_ssa_value
 
 
@@ -306,10 +306,12 @@ def test_diagnostic():
     parser = Parser(ctx, prog)
     module = parser.parse_op()
 
+    class MyException(Exception): ...
+
     diag = Diagnostic()
     diag.add_message(module, "Test")
-    with pytest.raises(DiagnosticException):
-        diag.raise_exception("test message", module)
+    with pytest.raises(MyException, match="Test"):
+        diag.raise_exception(module, MyException("hello"))
 
 
 #  ____ ____    _    _   _
