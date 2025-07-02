a854fc2b75d73c4bb808d1e6d109d7baf4a3e8fc
Erick Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2025-07-02 18:40:03 +0000

    format

--- xdsl/dialects/stablehlo.py
+++ xdsl/dialects/stablehlo.py
@@ -323,7 +323,10 @@ class DotAttr(ParametrizedAttribute):
     def print_parameters(self, printer: Printer) -> None:
         with printer.in_angle_brackets():
             with printer.indented():
-                if (self.lhs_batching_dimensions.data and self.rhs_batching_dimensions.data):
+                if (
+                    self.lhs_batching_dimensions.data
+                    and self.rhs_batching_dimensions.data
+                ):
                     DotAttr._print_parameter(
                         "lhs_batching_dimensions", self.lhs_batching_dimensions, printer
                     )
