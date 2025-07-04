4c1107e393a26d41cc8a15fb1a15a3fa2f0eda3f
Sasha Lopoukhine <superlopuh@gmail.com>
2025-07-04 10:17:27 +0100

    move test

--- tests/transforms/test_memref_stream_interleave.py
+++ tests/transforms/test_memref_stream_interleave.py
@@ -1,6 +1,20 @@
 import pytest
 
-from xdsl.transforms.memref_stream_interleave import interleave_index_and_factor
+from xdsl.transforms.memref_stream_interleave import (
+    factors,
+    interleave_index_and_factor,
+)
+
+
+def test_factors():
+    assert factors(-1) == ()
+    assert factors(0) == ()
+    assert factors(1) == (1,)
+    assert factors(2) == (1, 2)
+    assert factors(3) == (1, 3)
+    assert factors(4) == (1, 2, 4)
+    assert factors(6) == (1, 2, 3, 6)
+    assert factors(24) == (1, 2, 3, 4, 6, 8, 12, 24)
 
 
 @pytest.mark.parametrize(
