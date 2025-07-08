540c2036390f6ecbcf84d1ef733134312cdfd1b1
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-08 12:01:09 +0100

    fixed operand name in assembly_format

--- xdsl/dialects/omp.py
+++ xdsl/dialects/omp.py
@@ -614,7 +614,7 @@ class ParallelOp(BlockArgOpenMPOperation):
 class YieldOp(AbstractYieldOperation[Attribute]):
     name = "omp.yield"
 
-    assembly_format = "( `(` $results^ `:` type($results) `)` )? attr-dict"
+    assembly_format = "( `(` $arguments^ `:` type($arguments) `)` )? attr-dict"
 
     traits = traits_def(
         IsTerminator(),
