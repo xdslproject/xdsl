d172b680994a87cfec29bc17880802e703bddbdb
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-08 12:11:52 +0100

    removed print_op_override

--- xdsl/backend/csl/print_csl.py
+++ xdsl/backend/csl/print_csl.py
@@ -503,8 +503,6 @@ class CslPrintContext:
         Walks over a block and prints every operation in the block.
         """
         for op in body.ops:
-            if self._print_op_override(op):
-                continue
             match op:
                 case (
                     arith.AndIOp(lhs=lhs, rhs=rhs, result=res)
@@ -853,14 +851,6 @@ class CslPrintContext:
                     )
                 case anyop:
                     self.print(f"unknown op {anyop}", prefix="//")
-
-    def _print_op_override(self, op: Operation) -> bool:
-        """
-        Extension point for classes inheriting `CslPrintContext`.
-
-        Returning `True` from this function indicates that the child class has handled the printing of `op`
-        """
-        return False
 
     @contextmanager
     def descend(self, block_start: str = ""):
