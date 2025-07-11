b624afc9240bc231a8fc3f20a1c02f6d94462b63
Sasha Lopoukhine <superlopuh@gmail.com>
2025-07-11 16:13:04 +0100

    add some documentation

--- xdsl/passes.py
+++ xdsl/passes.py
@@ -191,13 +191,21 @@ def get_pass_option_infos(
 
 @dataclass(frozen=True)
 class PassPipeline:
+    """
+    A representation of a pass pipeline, with an optional callback to be executed
+    between each of the passes.
+    """
+
     passes: tuple[ModulePass, ...]
+    """
+    These will be executed sequentially during the execution of the pipeline.
+    """
     callback: Callable[[ModulePass, builtin.ModuleOp, ModulePass], None] | None = field(
         default=None
     )
     """
-    Function called in between every pass, taking the pass that just ran, the module, and
-    the next pass.
+    Function called in between every pass, taking the pass that just ran, the module,
+    and the next pass.
     """
 
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
