ad133372e1a47e8df4c305ebf9f88a16f6cc040d
Sasha Lopoukhine <superlopuh@gmail.com>
2025-07-15 11:27:03 +0100

    dialects: (x86) add get_constant_value

--- xdsl/transforms/canonicalization_patterns/x86.py
+++ xdsl/transforms/canonicalization_patterns/x86.py
@@ -1,4 +1,5 @@
 from xdsl.dialects import x86
+from xdsl.ir import Attribute, OpResult, SSAValue
 from xdsl.pattern_rewriter import (
     PatternRewriter,
     RewritePattern,
@@ -11,3 +12,14 @@ class RemoveRedundantDS_Mov(RewritePattern):
     def match_and_rewrite(self, op: x86.DS_MovOp, rewriter: PatternRewriter) -> None:
         if op.destination.type == op.source.type and op.destination.type.is_allocated:
             rewriter.replace_matched_op((), (op.source,))
+
+
+def get_constant_value(value: SSAValue) -> Attribute | None:
+    if not isinstance(value, OpResult):
+        return
+
+    if isinstance(value.op, x86.DS_MovOp):
+        return get_constant_value(value.op.source)
+
+    if isinstance(value.op, x86.DI_MovOp):
+        return value.op.immediate
