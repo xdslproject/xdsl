xdslproject/xdsl

commit e5ae7c81f2479619238badaebecb5de929f19094

dialects: (tensor) add ExpandShapeOp (#4245)
Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

xdsl/dialects/tensor.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import math
 from collections.abc import Mapping, Sequence
-from typing import cast
+from typing import ClassVar, cast
 
 from typing_extensions import Self
 
@@ -10,14 +10,22 @@ from xdsl.dialects import memref
 from xdsl.dialects.builtin import (
     Annotated,
     AnySignlessIntegerOrIndexType,
+    ArrayAttr,
     DenseArrayBase,
     IndexType,
+    IntegerAttr,
+    ShapedType,
     TensorType,
     UnrankedTensorType,
     i64,
 )
+from xdsl.dialects.utils.dynamic_index_list import (
+    parse_dynamic_index_list_without_types,
+    print_dynamic_index_list,
+)
 from xdsl.dialects.utils.reshape_ops_utils import (
     ContiguousArrayOfIntArray,
+    verify_reshape_like_types,
 )
 from xdsl.ir import Attribute, Dialect, Operation, SSAValue
 from xdsl.irdl import (
@@ -289,6 +297,120 @@ class ReshapeOp(IRDLOperation):
 
 
 @irdl_op_definition
+class ExpandShapeOp(IRDLOperation):
+    """
+    Operation to produce a tensor with a higher rank
+    See external [documentation](https://mlir.llvm.org/docs/Dialects/TensorOps/#tensorexpand_shape-tensorexpandshapeop)
+    """
+
+    # Constant value used to denote dynamic indices in offsets, sizes, and strides.
+    # Same constant as in MLIR.
+    DYNAMIC_INDEX: ClassVar[int] = -9223372036854775808
+
+    name = "tensor.expand_shape"
+
+    src = operand_def(TensorType)
+    dynamic_output_shape = var_operand_def(IndexType)
+
+    reassociation = prop_def(ContiguousArrayOfIntArray())
+
+    static_output_shape = prop_def(DenseArrayBase.constr(i64))
+
+    result = result_def(TensorType[Attribute])
+
+    def __init__(
+        self,
+        src: SSAValue | Operation,
+        dynamic_output_shape: Sequence[SSAValue],
+        reassociation: ArrayAttr[ArrayAttr[IntegerAttr]],
+        static_output_shape: Sequence[int] | DenseArrayBase,
+        result_type: TensorType[Attribute],
+        attributes: dict[str, Attribute] | None = None,
+    ):
+        if not isinstance(static_output_shape, DenseArrayBase):
+            static_output_shape = DenseArrayBase.from_list(i64, static_output_shape)
+
+        super().__init__(
+            operands=[src, dynamic_output_shape],
+            result_types=[result_type],
+            properties={
+                "reassociation": reassociation,
+                "static_output_shape": static_output_shape,
+            },
+            attributes=attributes,
+        )
+
+    def verify_(self):
+        assert isinstance(self.src.type, ShapedType)
+        assert isinstance(self.result.type, ShapedType)
+
+        # make sure the static output shape matches the result type
+        if len(self.static_output_shape) != len(self.result.type.get_shape()):
+            raise VerifyException(
+                "expected number of static shape dims to be equal to the output rank "
+                f"({len(self.result.type.get_shape())}) but found {len(self.static_output_shape)} inputs instead"
+            )
+
+        verify_reshape_like_types(
+            collapsed_type=self.src.type,
+            expanded_type=self.result.type,
+            reassociation=self.reassociation,
+        )
+
+    @classmethod
+    def parse(cls, parser: Parser) -> Self:
+        src_operand = parser.parse_unresolved_operand()
+
+        reassociation = parser.parse_attribute()
+        parser.parse_characters("output_shape")
+        index = IndexType()
+
+        # Parse shape: mixture of ints and SSA values
+        dyn_shape, static_shape = parse_dynamic_index_list_without_types(
+            parser, dynamic_index=cls.DYNAMIC_INDEX
+        )
+
+        dyn_shape = parser.resolve_operands(
+            dyn_shape, (index,) * len(dyn_shape), parser.pos
+        )
+
+        attributes = parser.parse_optional_attr_dict()
+
+        parser.parse_punctuation(":")
+        src_type = parser.parse_type()
+        parser.parse_characters("into")
+        result_type = parser.parse_type()
+        src = parser.resolve_operand(src_operand, src_type)
+
+        shape_attr = DenseArrayBase.from_list(i64, static_shape)
+
+        reassociation = cast(ArrayAttr[ArrayAttr[IntegerAttr]], reassociation)
+        result_type = cast(TensorType[Attribute], result_type)
+
+        return cls(src, dyn_shape, reassociation, shape_attr, result_type, attributes)
+
+    def print(self, printer: Printer):
+        printer.print_string(" ")
+        printer.print_ssa_value(self.src)
+        printer.print_string(" ")
+        printer.print_attribute(self.reassociation)
+        printer.print_string(" static_output_shape ")
+        print_dynamic_index_list(
+            printer,
+            self.DYNAMIC_INDEX,
+            self.dynamic_output_shape,
+            self.static_output_shape.get_values(),
+        )
+
+        printer.print_op_attributes(attributes=self.attributes)
+
+        printer.print_string(" : ")
+        printer.print_attribute(self.src.type)
+        printer.print_string(" into ")
+        printer.print_attribute(self.result.type)
+
+
+@irdl_op_definition
 class ExtractSliceOp(IRDLOperation):
     name = "tensor.extract_slice"
 
@@ -529,6 +651,7 @@ Tensor = Dialect(
         CastOp,
         DimOp,
         EmptyOp,
+        ExpandShapeOp,
         ExtractSliceOp,
         InsertSliceOp,
         ReshapeOp,

tests/filecheck/dialects/tensor/ops.mlir
@@ -11,6 +11,9 @@
 %fromelements = tensor.from_elements %index, %index1 : tensor<2xindex>
 %res_collapse1 = tensor.collapse_shape %tensor [ [0, 1], [2] ] : tensor<?x?xf32> into tensor<4x1xf32>
 
+%expanded = tensor.expand_shape %tensor [[0, 1, 2], [3]] output_shape [%dim1, 1, 1, %dim2] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+%expanded_with_attr_dict = tensor.expand_shape %tensor [[0, 1, 2], [3]] output_shape [%dim1, 1, 1, %dim2] {test_attr = 42 : i8} : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+%expanded_generic = "tensor.expand_shape"(%tensor, %dim1, %dim2) <{reassociation = [[0 : i64, 1 : i64, 2 : i64], [3 : i64]], static_output_shape = array<i64: -9223372036854775808, 1, 1, -9223372036854775808>}> : (tensor<?x?xf32>, index, index) -> tensor<?x1x1x?xf32>
 
 // CHECK: builtin.module {
 // CHECK-NEXT:   %index, %index1, %tensor = "test.op"() : () -> (index, index, tensor<?x?xf32>)
@@ -22,6 +25,9 @@
 // CHECK-NEXT:   %insert1 = tensor.insert %extract1 into %tensor[%index, %index1] : tensor<?x?xf32>
 // CHECK-NEXT:   %fromelements = tensor.from_elements %index, %index1 : tensor<2xindex>
 // CHECK-NEXT:   %res_collapse1 = tensor.collapse_shape %tensor [[0 : i64, 1 : i64], [2 : i64]] : tensor<?x?xf32> into tensor<4x1xf32>
+// CHECK-NEXT:   %expanded = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] static_output_shape [%dim1, 1, 1, %dim2] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+// CHECK-NEXT:   %expanded_with_attr_dict = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] static_output_shape [%dim1, 1, 1, %dim2] {test_attr = 42 : i8} : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+// CHECK-NEXT:   %expanded_generic = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] static_output_shape [%dim1, 1, 1, %dim2] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
 // CHECK-NEXT: }
 
 // CHECK-GENERIC: "builtin.module"() ({
@@ -34,4 +40,7 @@
 // CHECK-GENERIC-NEXT:   %insert1 = "tensor.insert"(%extract1, %tensor, %index, %index1) : (f32, tensor<?x?xf32>, index, index) -> tensor<?x?xf32>
 // CHECK-GENERIC-NEXT:   %fromelements = "tensor.from_elements"(%index, %index1) : (index, index) -> tensor<2xindex>
 // CHECK-GENERIC-NEXT:   %res_collapse1 = "tensor.collapse_shape"(%tensor) <{reassociation = [[0 : i64, 1 : i64], [2 : i64]]}> : (tensor<?x?xf32>) -> tensor<4x1xf32>
+// CHECK-GENERIC-NEXT:   %expanded = "tensor.expand_shape"(%tensor, %dim1, %dim2) <{reassociation = [[0 : i64, 1 : i64, 2 : i64], [3 : i64]], static_output_shape = array<i64: -9223372036854775808, 1, 1, -9223372036854775808>}> : (tensor<?x?xf32>, index, index) -> tensor<?x1x1x?xf32>
+// CHECK-GENERIC-NEXT:   %expanded_with_attr_dict = "tensor.expand_shape"(%tensor, %dim1, %dim2) <{reassociation = [[0 : i64, 1 : i64, 2 : i64], [3 : i64]], static_output_shape = array<i64: -9223372036854775808, 1, 1, -9223372036854775808>}> {test_attr = 42 : i8} : (tensor<?x?xf32>, index, index) -> tensor<?x1x1x?xf32>
+// CHECK-GENERIC-NEXT:   %expanded_generic = "tensor.expand_shape"(%tensor, %dim1, %dim2) <{reassociation = [[0 : i64, 1 : i64, 2 : i64], [3 : i64]], static_output_shape = array<i64: -9223372036854775808, 1, 1, -9223372036854775808>}> : (tensor<?x?xf32>, index, index) -> tensor<?x1x1x?xf32>
 // CHECK-GENERIC-NEXT: }) : () -> ()

tests/filecheck/dialects/tensor/invalid_ops.mlir
@@ -87,3 +87,27 @@ builtin.module {
 
 // CHECK: expected integer >= 0, got -2
 %res_collapse2 = tensor.collapse_shape %t0 [ [-2, 3], [0, 1] ] : tensor<4x1xf32> into tensor<4x1xf32>
+
+// -----
+
+%0 = "test.op"() : () -> (tensor<2x3x20xf32>)
+// CHECK: expected dimension 2 of collapsed type to be static value of 40
+%illegal_expanding_reshape_static_tensor = tensor.expand_shape %0 [[0], [1], [2, 3, 4]] output_shape [2, 3, 2, 4, 5]
+      : tensor<2x3x20xf32> into tensor<2x3x2x4x5xf32>
+
+// -----
+
+%sz0 = "test.op"() : () -> (index)
+%0 = "test.op"() : () -> (tensor<?x?xf32>)
+// CHECK: expected dimension 1 of collapsed type to be static value of 5
+%illegal_expanding_reshape_mixed_tensor = tensor.expand_shape %0 [[0, 1], [2]] output_shape [%sz0, 4, 5]
+    : tensor<?x?xf32> into tensor<?x4x5xf32>
+
+
+// -----
+
+%sz0 = "test.op"() : () -> (index)
+%0 = "test.op"() : () -> (tensor<?x?xf32>)
+// CHECK: expected dimension 1 of collapsed type to be static value of 20
+%illegal_expanding_reshape_mixed_tensor_2 = tensor.expand_shape %0 [[0], [1, 2]] output_shape [%sz0, 4, 5]
+      : tensor<?x?xf32> into tensor<?x4x5xf32>

xdsl/dialects/utils/reshape_ops_utils.py
@@ -9,7 +9,7 @@ from typing import cast
 
 from typing_extensions import TypeVar
 
-from xdsl.dialects.builtin import I64, Annotated, ArrayAttr, IntegerAttr
+from xdsl.dialects.builtin import I64, Annotated, ArrayAttr, IntegerAttr, ShapedType
 from xdsl.ir import Attribute
 from xdsl.irdl import (
     AtLeast,
@@ -31,11 +31,11 @@ _CONTIGUOUS_ARRAY_TYPE_CONSTRAINT = irdl_to_attr_constraint(
     ]
 )
 
+ArrayOfIntArrayAttr = ArrayAttr[ArrayAttr[IntegerAttr]]
+
 
 @dataclass(frozen=True)
-class ContiguousArrayOfIntArray(
-    GenericAttrConstraint[ArrayAttr[ArrayAttr[IntegerAttr]]]
-):
+class ContiguousArrayOfIntArray(GenericAttrConstraint[ArrayOfIntArrayAttr]):
     """
     Enforce an ArrayAttr of ArrayAttr[IntegerAttr] to contain contiguous integer values across all inner arrays.
     For example: [[0, 1], [2, 3]] is valid, but [[3, 4], [0, 1]] is not.
@@ -46,7 +46,7 @@ class ContiguousArrayOfIntArray(
         _CONTIGUOUS_ARRAY_TYPE_CONSTRAINT.verify(
             attr, constraint_context=constraint_context
         )
-        attr = cast(ArrayAttr[ArrayAttr[IntegerAttr]], attr)
+        attr = cast(ArrayOfIntArrayAttr, attr)
 
         # Flatten all integer values from all inner arrays
         flat_values = [e.value.data for inner in attr.data for e in inner.data]
@@ -60,3 +60,81 @@ class ContiguousArrayOfIntArray(
     ) -> "ContiguousArrayOfIntArray":
         # No type variables to map in this constraint
         return self
+
+
+def verify_reshape_like_types(
+    collapsed_type: ShapedType,
+    expanded_type: ShapedType,
+    reassociation: ArrayAttr[ArrayAttr[IntegerAttr]],
+):
+    """
+    Verify that collapsed and expanded types conform to reassociation mapping.
+    """
+    expanded_rank = len(expanded_type.get_shape())
+    collapsed_rank = len(collapsed_type.get_shape())
+
+    if expanded_rank < collapsed_rank:
+        raise VerifyException(
+            f"expected the expanded type, {expanded_type} to have a higher (or same) rank "
+            f"than the collapsed type, {collapsed_type}."
+        )
+
+    if collapsed_rank != len(reassociation):
+        raise VerifyException(
+            f"expected collapsed rank ({collapsed_rank}) to equal the number of "
+            f"reassociation maps ({len(reassociation)})."
+        )
+
+    # Check that the total reassociation dimensions match the expanded type's rank.
+    total_reassociation_dims = sum(len(rm) for rm in reassociation)
+    if total_reassociation_dims != expanded_rank:
+        raise VerifyException(
+            f"expected the total number of reassociation dimensions ({total_reassociation_dims}) "
+            f"to equal the expanded type's rank ({expanded_rank})."
+        )
+
+    verify_reshape_like_shapes_are_compatible(
+        collapsed_shape=collapsed_type.get_shape(),
+        expanded_shape=expanded_type.get_shape(),
+        reassociation=reassociation,
+    )
+
+
+def verify_reshape_like_shapes_are_compatible(
+    collapsed_shape: tuple[int, ...],
+    expanded_shape: tuple[int, ...],
+    reassociation: ArrayOfIntArrayAttr,
+):
+    """
+    Verify that collapsed and expanded shapes adhere to reassociation mapping.
+    """
+    expanded_dim_start = 0
+
+    for map_idx, rm in enumerate(reassociation):
+        found_dynamic = False
+        linearized_static = 1
+
+        # Look at the next `len(rm)` dims in expanded_shape
+        for dim in expanded_shape[expanded_dim_start : expanded_dim_start + len(rm)]:
+            if dim == -1:
+                found_dynamic = True
+            else:
+                linearized_static *= dim
+
+        if found_dynamic:
+            # if any is dynamic, the collapsed must be dynamic too
+            if not collapsed_shape[map_idx] == -1:
+                raise VerifyException(
+                    f"expected dimension {map_idx} of collapsed type to be dynamic "
+                    f"since one or more of the corresponding dimensions in the "
+                    f"expanded type is dynamic"
+                )
+        else:
+            # all static → product must match
+            if collapsed_shape[map_idx] != linearized_static:
+                raise VerifyException(
+                    f"expected dimension {map_idx} of collapsed type to be static "
+                    f"value of {linearized_static}"
+                )
+
+        expanded_dim_start += len(rm)

commit 39fb626e86f7c6fe1c4945bc612ed25c6ccaedef

dialects: (tensor) Fix ExpandShapeOp parsing and add MLIR tests for the expand shape op
Fixes: e5ae7c81f2479619238badaebecb5de929f19094

xdsl/dialects/tensor.py
@@ -394,7 +394,7 @@ class ExpandShapeOp(IRDLOperation):
         printer.print_ssa_value(self.src)
         printer.print_string(" ")
         printer.print_attribute(self.reassociation)
-        printer.print_string(" static_output_shape ")
+        printer.print_string(" output_shape ")
         print_dynamic_index_list(
             printer,
             self.DYNAMIC_INDEX,

tests/filecheck/dialects/tensor/ops.mlir
@@ -25,9 +25,9 @@
 // CHECK-NEXT:   %insert1 = tensor.insert %extract1 into %tensor[%index, %index1] : tensor<?x?xf32>
 // CHECK-NEXT:   %fromelements = tensor.from_elements %index, %index1 : tensor<2xindex>
 // CHECK-NEXT:   %res_collapse1 = tensor.collapse_shape %tensor [[0 : i64, 1 : i64], [2 : i64]] : tensor<?x?xf32> into tensor<4x1xf32>
-// CHECK-NEXT:   %expanded = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] static_output_shape [%dim1, 1, 1, %dim2] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
-// CHECK-NEXT:   %expanded_with_attr_dict = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] static_output_shape [%dim1, 1, 1, %dim2] {test_attr = 42 : i8} : tensor<?x?xf32> into tensor<?x1x1x?xf32>
-// CHECK-NEXT:   %expanded_generic = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] static_output_shape [%dim1, 1, 1, %dim2] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+// CHECK-NEXT:   %expanded = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] output_shape [%dim1, 1, 1, %dim2] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+// CHECK-NEXT:   %expanded_with_attr_dict = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] output_shape [%dim1, 1, 1, %dim2] {test_attr = 42 : i8} : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+// CHECK-NEXT:   %expanded_generic = tensor.expand_shape %tensor [[0 : i64, 1 : i64, 2 : i64], [3 : i64]] output_shape [%dim1, 1, 1, %dim2] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
 // CHECK-NEXT: }
 
 // CHECK-GENERIC: "builtin.module"() ({

tests/filecheck/mlir-conversion/with-mlir/dialects/tensor/ops.mlir
@@ -18,6 +18,11 @@
 %extracted = tensor.extract %t2[%i1] : tensor<2xf32>
 %tensor_with_ins = tensor.insert %extracted into %t2[%i1] : tensor<2xf32>
 %fromelements = tensor.from_elements %i1, %i1: tensor<2xindex>
+%index, %index1, %tensor = "test.op"() : () -> (index, index, tensor<?x?xf32>)
+%expanded = tensor.expand_shape %tensor [[0, 1, 2], [3]] output_shape [%dim1, 1, 1, %dim2] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+%expanded_with_attr_dict = tensor.expand_shape %tensor [[0, 1, 2], [3]] output_shape [%dim1, 1, 1, %dim2] {test_attr = 42 : i8} : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+%expanded_generic = "tensor.expand_shape"(%tensor, %dim1, %dim2) <{reassociation = [[0 : i64, 1 : i64, 2 : i64], [3 : i64]], static_output_shape = array<i64: -9223372036854775808, 1, 1, -9223372036854775808>}> : (tensor<?x?xf32>, index, index) -> tensor<?x1x1x?xf32>
+
 
 
 // CHECK:       module {
@@ -39,4 +44,8 @@
 // CHECK-NEXT:  %extracted = tensor.extract %1[%2] : tensor<2xf32>
 // CHECK-NEXT:  %{{.*}} = tensor.insert %extracted into %1[%2] : tensor<2xf32>
 // CHECK-NEXT:  %{{.*}} = tensor.from_elements %2, %2 : tensor<2xindex>
+// CHECK-NEXT:  %7:3 = "test.op"() : () -> (index, index, tensor<?x?xf32>)
+// CHECK-NEXT:  %expanded = tensor.expand_shape %7#2 [[0, 1, 2], [3]] output_shape [%dim, 1, 1, %dim_0] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+// CHECK-NEXT:  %expanded_2 = tensor.expand_shape %7#2 [[0, 1, 2], [3]] output_shape [%dim, 1, 1, %dim_0] {test_attr = 42 : i8} : tensor<?x?xf32> into tensor<?x1x1x?xf32>
+// CHECK-NEXT:  %expanded_3 = tensor.expand_shape %7#2 [[0, 1, 2], [3]] output_shape [%dim, 1, 1, %dim_0] : tensor<?x?xf32> into tensor<?x1x1x?xf32>
 // CHECK-NEXT: }