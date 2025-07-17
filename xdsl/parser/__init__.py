b036fb545e807ee099aae41c528a7e7e47937384
Sasha Lopoukhine <superlopuh@gmail.com>
2025-07-17 13:27:02 +0100

    documentation: automatically generate the navigation for code reference (#4813)

--- xdsl/parser/__init__.py
+++ xdsl/parser/__init__.py
@@ -1,7 +1,6 @@
-# To avoid circular imports, affine_parser needs to be imported first
-
 # TID 251 enforces to not import from nested modules
 # We need to skip it here to allow importing from here instead.
+# To avoid circular imports, affine_parser needs to be imported first
 from .affine_parser import *  # noqa: TID251
 from .attribute_parser import *  # noqa: TID251
 from .base_parser import *  # noqa: TID251
