48169cdcefc6c40560bf4aabb3d742fbb30e5b5a
Sasha Lopoukhine <superlopuh@gmail.com>
2025-07-18 10:57:32 +0100

    API: add EqIntConstraint (#4827)

--- tests/irdl/test_int_constraint.py
+++ tests/irdl/test_int_constraint.py
@@ -2,7 +2,7 @@ import re
 
 import pytest
 
-from xdsl.irdl import AtLeast, ConstraintContext
+from xdsl.irdl import AtLeast, ConstraintContext, EqIntConstraint
 from xdsl.utils.exceptions import VerifyException
 
 
@@ -28,3 +28,12 @@ def test_at_least():
         AtLeast(2).verify(1, ConstraintContext())
     with pytest.raises(VerifyException):
         AtLeast(2).verify(0, ConstraintContext())
+
+
+def test_eq():
+    one_constr = EqIntConstraint(1)
+    one_constr.verify(1, ConstraintContext())
+    with pytest.raises(VerifyException, match="Invalid value 2, expected 1"):
+        one_constr.verify(2, ConstraintContext())
+    assert one_constr.can_infer(set())
+    assert one_constr.infer(ConstraintContext()) == 1
