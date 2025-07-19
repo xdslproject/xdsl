2f31920934a3ab317ca8dcd34a22476e4d2796a5
Sasha Lopoukhine <superlopuh@gmail.com>
2025-07-19 09:57:34 +0100

    core: add RangeLengthConstraint

--- xdsl/irdl/constraints.py
+++ xdsl/irdl/constraints.py
@@ -963,6 +963,60 @@ class RangeConstraint(Generic[AttributeCovT], ABC):
 
 
 @dataclass(frozen=True)
+class RangeLengthConstraint(RangeConstraint[AttributeCovT]):
+    """
+    Constrain an attribute range with the given length.
+    """
+
+    constraint: RangeConstraint[AttributeCovT]
+    """The constraint that the variable must satisfy."""
+
+    length: IntConstraint
+    """The length that the range must have"""
+
+    def verify(
+        self,
+        attrs: Sequence[Attribute],
+        constraint_context: ConstraintContext,
+    ) -> None:
+        self.verify_length(len(attrs), constraint_context)
+        self.constraint.verify(attrs, constraint_context)
+
+    def verify_length(self, length: int, constraint_context: ConstraintContext) -> None:
+        try:
+            self.length.verify(length, constraint_context)
+        except VerifyException as e:
+            raise VerifyException(
+                "incorrect length for range variable:\n" + str(e)
+            ) from e
+
+    def variables(self) -> set[str]:
+        return self.constraint.variables() | self.length.variables()
+
+    def can_infer(
+        self, var_constraint_names: AbstractSet[str], *, length_known: bool
+    ) -> bool:
+        length_known = length_known or self.length.can_infer(var_constraint_names)
+        return self.constraint.can_infer(
+            var_constraint_names, length_known=length_known
+        )
+
+    def infer(
+        self, context: ConstraintContext, *, length: int | None
+    ) -> Sequence[AttributeCovT]:
+        if length is None:
+            length = self.length.infer(context)
+        return self.constraint.infer(context, length=length)
+
+    def mapping_type_vars(
+        self, type_var_mapping: dict[TypeVar, AttrConstraint]
+    ) -> RangeLengthConstraint[AttributeCovT]:
+        return RangeLengthConstraint(
+            self.constraint.mapping_type_vars(type_var_mapping), self.length
+        )
+
+
+@dataclass(frozen=True)
 class RangeVarConstraint(RangeConstraint[AttributeCovT]):
     """
     Constrain an attribute range with the given constraint, and constrain all occurences
