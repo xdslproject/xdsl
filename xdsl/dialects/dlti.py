0e0c425ee57f5fdcc6e78d2b6135eb0d4d7fc4a2
Mathieu Fehr <mathieu.fehr@gmail.com>
2025-07-23 17:21:38 +0100

    dialects: Add dlti.dl_spec attribute
    stack-info: PR: https://github.com/xdslproject/xdsl/pull/4864, branch: math-fehr/stack/8

--- xdsl/dialects/dlti.py
+++ xdsl/dialects/dlti.py
@@ -8,9 +8,11 @@ https://mlir.llvm.org/docs/Dialects/DLTIDialect/
 
 from __future__ import annotations
 
-from xdsl.dialects.builtin import StringAttr
+from xdsl.dialects.builtin import ArrayAttr, StringAttr
 from xdsl.ir import Attribute, Dialect, ParametrizedAttribute, TypeAttribute
 from xdsl.irdl import irdl_attr_definition
+from xdsl.parser import AttrParser
+from xdsl.printer import Printer
 from xdsl.utils.exceptions import VerifyException
 
 
@@ -31,4 +33,45 @@ class DataLayoutEntryAttr(ParametrizedAttribute):
             raise VerifyException("key must be a string or a type attribute")
 
 
-DLTI = Dialect("dlti", [], [DataLayoutEntryAttr])
+@irdl_attr_definition
+class DataLayoutSpecAttr(ParametrizedAttribute):
+    """
+    An attribute to represent a data layout specification.
+
+    A data layout specification is a list of entries that specify (partial) data
+    layout information. It is expected to be attached to operations that serve as
+    scopes for data layout requests.
+
+    https://mlir.llvm.org/docs/Dialects/DLTIDialect/#datalayoutspecattr
+    """
+
+    name = "dlti.dl_spec"
+
+    # In MLIR, this is a DataLayoutEntryInterface.
+    entries: ArrayAttr[DataLayoutEntryAttr]
+
+    @classmethod
+    def parse_parameters(
+        cls, parser: AttrParser
+    ) -> tuple[ArrayAttr[DataLayoutEntryAttr]]:
+        def parse_entry() -> DataLayoutEntryAttr:
+            entry = parser.parse_attribute()
+            parser.parse_punctuation("=")
+            value = parser.parse_attribute()
+            return DataLayoutEntryAttr(entry, value)
+
+        entries = parser.parse_comma_separated_list(parser.Delimiter.ANGLE, parse_entry)
+
+        return (ArrayAttr(entries),)
+
+    def print_parameters(self, printer: Printer):
+        def print_entry(entry: DataLayoutEntryAttr):
+            printer.print_attribute(entry.key)
+            printer.print_string(" = ")
+            printer.print_attribute(entry.value)
+
+        with printer.in_angle_brackets():
+            printer.print_list(self.entries, print_entry)
+
+
+DLTI = Dialect("dlti", [], [DataLayoutEntryAttr, DataLayoutSpecAttr])
