1f04177afd4968abc33109aaab4e5f03bbdd87ea
EdmundGoodman <egoodman3141@gmail.com>
2025-07-23 01:10:33 +0100

    Finish failing tests for PDL rewriting DSL API

--- tests/filecheck/frontend/programs/pdl.py
+++ tests/filecheck/frontend/programs/pdl.py
@@ -12,14 +12,14 @@ The following code snippets show early suggestions for the structure of this API
 # Derived from <https://github.com/xdslproject/xdsl/pull/1137>
 
 @rewrite_pattern_query
-def zero_to_one_query(root: arith.ConstantOp):
-    zero = IntegerAttr(0, 32)
+def zero_to_one_query(root: arith.arith.ConstantOp):
+    zero = builtin.IntegerAttr(0, 32)
     return root.value == zero
 
 @query_rewrite_pattern(rewrite_pattern_query)
-def zero_to_one(rewriter: PatternRewriter, root: arith.ConstantOp):
-    one = IntegerAttr(1, 32)
-    rewriter.replace_matched_op(arith.ConstantOp(one))
+def zero_to_one(rewriter: PatternRewriter, root: arith.arith.ConstantOp):
+    one = builtin.IntegerAttr(1, 32)
+    rewriter.replace_matched_op(arith.arith.ConstantOp(one))
 ````
 
 ```python
@@ -60,34 +60,47 @@ pdl.pattern : benefit(2) {
 ```
 """
 
-from xdsl.dialects.arith import ConstantOp
-from xdsl.dialects.builtin import IntegerAttr, ModuleOp
+from xdsl.dialects import arith, builtin, pdl
+from xdsl.frontend.pyast.context import PyASTContext
+from xdsl.ir import SSAValue
 from xdsl.pattern_rewriter import PatternRewriter
 
-# IR code generation snippet
-# ```python
-# from xdsl.frontend.pyast.context import PyASTContext
-# ctx = PyASTContext()
-# ctx.register_type(int, builtin.i32)
-# ctx.register_type(ConstantOp, ...)
-# ctx.register_type(PatternRewriter, ...)
-# ctx.register_function(IntegerAttr, ...)
-# ctx.register_function(ConstantOp, ...)
-# ctx.register_function(PatternRewriter.replace_op, ...)
-# @ctx.pdl_rewrite
-# ```
+
+def integer_attr_as_pdl_attribute(
+    value: int | builtin.IntAttr, value_type: int
+) -> pdl.AttributeOp:
+    """Get a `pdl.attribute` for an xDSL `IntegerAttr`."""
+    return pdl.AttributeOp(builtin.IntegerAttr(value, value_type))
 
 
-def constant_replace(rewriter: PatternRewriter, matched_operation: ConstantOp) -> None:
+def arith_constant_as_pdl_operation(value: SSAValue[pdl.Attribute]) -> pdl.OperationOp:
+    """Get a `pdl.operation` for an `arith.constant` constructor."""
+    return pdl.OperationOp(
+        "arith.constant",
+        attribute_value_names=(builtin.StringAttr("value"),),
+        attribute_values=(value,),
+    )
+
+
+ctx = PyASTContext()
+ctx.register_function(builtin.IntegerAttr, integer_attr_as_pdl_attribute)
+ctx.register_function(arith.ConstantOp, arith_constant_as_pdl_operation)
+ctx.register_function(PatternRewriter.replace_op, pdl.ReplaceOp)
+
+
+@ctx.parse_program  # TODO: Change to `@ctx.pdl_rewrite` when implemented
+def constant_replace(
+    rewriter: PatternRewriter, matched_operation: arith.ConstantOp
+) -> None:
     """Replace a constant operation with a constant i32 with value one."""
-    new_attribute = IntegerAttr(1, 32)
-    new_operation = ConstantOp(new_attribute)
+    new_attribute = builtin.IntegerAttr(1, 32)
+    new_operation = arith.ConstantOp(new_attribute)
     rewriter.replace_op(matched_operation, new_operation)
 
 
 # Check that the DSL correctly rewrites on the xDSL data structures
-matched_operation = ConstantOp(IntegerAttr(0, 32))
-module = ModuleOp([matched_operation])
+matched_operation = arith.ConstantOp(builtin.IntegerAttr(0, 32))
+module = builtin.ModuleOp([matched_operation])
 rewriter = PatternRewriter(matched_operation)
 
 print(module)
@@ -102,15 +115,7 @@ print(module)
 # CHECK-NEXT:  }
 
 # Check that the extracted module results in the correct PDL rewrite
-print("""\
-builtin.module {
-  pdl.rewrite %2 {
-    %3 = pdl.attribute = 1 : i32
-    %4 = pdl.operation "arith.constant" {"value" = %3} -> (%0 : !pdl.type)
-    pdl.replace %2 with %4
-  }
-}
-""")
+print(constant_replace.module)
 # CHECK:       builtin.module {
 # CHECK-NEXT:    pdl.rewrite %2 {
 # CHECK-NEXT:        %3 = pdl.attribute = 1 : i32
