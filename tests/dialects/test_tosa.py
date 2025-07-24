5004a6f6f4c7af0436f28744468cf82febbe0750
Jake Davies <jakedves@gmail.com>
2025-07-24 14:33:02 +0100

    implement `tosa.add` and test cases for verify

---
+++ tests/dialects/test_tosa.py
@@ -0,0 +1,33 @@
+import pytest
+
+from xdsl.dialects.builtin import (
+    Float32Type,
+    IntegerType,
+    Signedness,
+    TensorType,
+    i32,
+    f32,
+)
+from xdsl.dialects.tosa import AddOp
+from xdsl.utils.test_value import create_ssa_value
+from xdsl.utils.exceptions import VerifyException
+
+
+tensor_i = create_ssa_value(TensorType(i32, [1, 2, 3, 4]))
+tensor_flat = create_ssa_value(TensorType(i32, [1, 1, 1, 1]))
+tensor_f = create_ssa_value(TensorType(f32, [1, 2, 3, 4]))
+
+
+def test_valid_cases():
+    AddOp(operands=[tensor_i, tensor_i], result_types=[tensor_i.type]).verify_()
+    AddOp(operands=[tensor_i, tensor_flat], result_types=[tensor_i.type]).verify_()
+
+
+def test_invalid_cases():
+    with pytest.raises(VerifyException):
+        AddOp(operands=[tensor_i, tensor_f], result_types=[tensor_f.type]).verify_()
+
+    with pytest.raises(VerifyException):
+        AddOp(
+            operands=[tensor_i, tensor_flat], result_types=[tensor_flat.type]
+        ).verify_()
--- xdsl/dialects/tosa.py
+++ xdsl/dialects/tosa.py
@@ -18,6 +18,7 @@ from xdsl.irdl import (
     prop_def,
     result_def,
 )
+from xdsl.utils.exceptions import VerifyException
 
 
 @irdl_op_definition
@@ -81,6 +82,46 @@ class AddOp(IRDLOperation):
     output = result_def(TensorType)
 
     assembly_format = "$in1 `,` $in2 attr-dict `:` `(` type($in1) `,` type($in2) `)` `->` type($output)"
+
+    def verify_(self) -> None:
+        """
+        Verify that the two input tensors are compatible, and that the result type can be constructed. For this,
+        both tensors must have the same rank. They should either have the same number of elements per dim, or
+        if there is only one element it can be broadcast implcitly.
+        """
+        t1 = self.in1.type
+        t2 = self.in2.type
+        t_out = self.output.type
+
+        if not (t1.get_num_dims() == t2.get_num_dims() == t_out.get_num_dims()):
+            raise VerifyException(
+                "'tosa.add' rank mismatch between input and output tensors"
+            )
+
+        if not (
+            t1.get_element_type() == t2.get_element_type() == t_out.get_element_type()
+        ):
+            raise VerifyException(
+                "'tosa.add' element type mismatch between inputs and output tensors"
+            )
+
+        # tosa allows for implcit broadcasting (i.e. same dims but any '1' element dimension
+        # can broadcast across that dimension) so we should check for same shapes, besides
+        # '1's can appear anywhere
+        s1, s2, s_out = t1.get_shape(), t2.get_shape(), t_out.get_shape()
+
+        for dim_in1, dim_in2, dim_out in zip(s1, s2, s_out):
+            # check that the shapes are compatible or have '1' to be broadcast
+            if dim_in1 != dim_in2 and 1 not in (dim_in1, dim_in2):
+                raise VerifyException(
+                    f"'tosa.add' shapes mismatch along axis: {dim_in1} and {dim_in2}"
+                )
+
+            # check that t_out is made from only broadcasting the operand types
+            if dim_out != max(dim_in1, dim_in2):
+                raise VerifyException(
+                    "'tosa.add' incompatible result type from operand types"
+                )
 
 
 TOSA = Dialect(
