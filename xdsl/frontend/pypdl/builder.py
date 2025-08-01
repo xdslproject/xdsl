b47b5580cabb82dad5a2ce180d350bff8676c38c
EdmundGoodman <egoodman3141@gmail.com>
2025-08-01 14:46:23 +0100

    Separate func and return operation passes

--- xdsl/frontend/pypdl/builder.py
+++ xdsl/frontend/pypdl/builder.py
@@ -3,9 +3,10 @@ from dataclasses import dataclass
 from xdsl.dialects.builtin import ModuleOp
 from xdsl.frontend.pyast.utils.builder import PyASTBuilder
 from xdsl.frontend.pypdl.transforms.func_to_pdl_rewrite import (
-    FuncToPdlRewritePattern,
-    PatternRewriteWalker,
+    FuncOpToPdlRewritePattern,
+    ReturnOpToPdlRewritePattern,
 )
+from xdsl.pattern_rewriter import GreedyRewritePatternApplier, PatternRewriteWalker
 
 
 @dataclass
@@ -15,5 +16,12 @@ class PyPDLRewriteBuilder(PyASTBuilder):
     def build(self) -> ModuleOp:
         """Build a PDL rewrite from the builder state."""
         module = super().build()
-        PatternRewriteWalker(FuncToPdlRewritePattern()).rewrite_module(module)
+        PatternRewriteWalker(
+            GreedyRewritePatternApplier(
+                [
+                    FuncOpToPdlRewritePattern(),
+                    ReturnOpToPdlRewritePattern(),
+                ]
+            )
+        ).rewrite_module(module)
         return module
--- xdsl/frontend/pypdl/transforms/func_to_pdl_rewrite.py
+++ xdsl/frontend/pypdl/transforms/func_to_pdl_rewrite.py
@@ -15,28 +15,32 @@ from xdsl.pattern_rewriter import (
 
 
 @dataclass
-class FuncToPdlRewritePattern(RewritePattern):
+class FuncOpToPdlRewritePattern(RewritePattern):
     """Rewrite pattern that transforms a function into a PDL rewrite operation."""
 
     @op_type_rewrite_pattern
-    def match_and_rewrite(
-        self, op: func.FuncOp | func.ReturnOp, rewriter: PatternRewriter
-    ):
-        if isinstance(op, func.FuncOp):
-            pdl_root = test.TestPureOp(result_types=(pdl.OperationType(),))
-            op.detach_region(func_body := op.regions[0])
-            rewrite_root = pdl_root.results[0]
-            for arg in func_body.block.args:
-                arg.replace_by(rewrite_root)
-                func_body.block.erase_arg(arg)
+    def match_and_rewrite(self, op: func.FuncOp, rewriter: PatternRewriter):
+        pdl_root = test.TestPureOp(result_types=(pdl.OperationType(),))
+        op.detach_region(func_body := op.regions[0])
+        rewrite_root = pdl_root.results[0]
+        for arg in func_body.block.args:
+            arg.replace_by(rewrite_root)
+            func_body.block.erase_arg(arg)
 
-            pdl_pattern = pdl.PatternOp(
-                benefit=1,
-                sym_name=op.sym_name.data,
-                body=Region(
-                    Block([pdl_root, pdl.RewriteOp(root=rewrite_root, body=func_body)])
-                ),
-            )
-            rewriter.replace_matched_op(pdl_pattern)
-        else:
-            rewriter.erase_matched_op()
+        pdl_pattern = pdl.PatternOp(
+            benefit=1,
+            sym_name=op.sym_name.data,
+            body=Region(
+                Block([pdl_root, pdl.RewriteOp(root=rewrite_root, body=func_body)])
+            ),
+        )
+        rewriter.replace_matched_op(pdl_pattern)
+
+
+@dataclass
+class ReturnOpToPdlRewritePattern(RewritePattern):
+    """Rewrite pattern that erases return ops in PDL rewrite operations."""
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: func.ReturnOp, rewriter: PatternRewriter):
+        rewriter.erase_matched_op()
