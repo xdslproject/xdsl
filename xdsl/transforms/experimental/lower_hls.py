338d596ef2df3122c139ce0c3d9e8fdaf41243f7
Sasha Lopoukhine <superlopuh@gmail.com>
2025-08-04 14:59:23 +0100

    revert unnecessary changes

--- xdsl/transforms/experimental/lower_hls.py
+++ xdsl/transforms/experimental/lower_hls.py
@@ -28,6 +28,7 @@ from xdsl.dialects.llvm import (
 )
 from xdsl.dialects.scf import ForOp, ParallelOp, YieldOp
 from xdsl.ir import Block, Operation, OpResult, Region, Use
+from xdsl.irdl import VarOperand, VarOpResult
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     GreedyRewritePatternApplier,
@@ -326,10 +327,10 @@ class SCFParallelToHLSPipelinedFor(RewritePattern):
         ii = ConstantOp.from_int_and_width(1, i32)
         hls_pipeline_op: Operation = PragmaPipelineOp(ii)
 
-        lb = op.lowerBound
-        ub = op.upperBound
-        step = op.step
-        res = op.res
+        lb: VarOperand = op.lowerBound
+        ub: VarOperand = op.upperBound
+        step: VarOperand = op.step
+        res: VarOpResult = op.res
 
         for i in range(len(lb)):
             cast(OpResult, lb[i]).op.detach()
