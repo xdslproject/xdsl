d2f258239157473c24f633e9e20008205fb61b38
Sasha Lopoukhine <superlopuh@gmail.com>
2025-08-04 14:40:40 +0100

    dialects: (ptr) canonicalize PtrAdd with zero (#4953)

--- xdsl/transforms/canonicalization_patterns/ptr.py
+++ xdsl/transforms/canonicalization_patterns/ptr.py
@@ -1,4 +1,5 @@
-from xdsl.dialects import ptr
+from xdsl.dialects import arith, ptr
+from xdsl.dialects.builtin import IntegerAttr
 from xdsl.pattern_rewriter import (
     PatternRewriter,
     RewritePattern,
@@ -24,3 +25,14 @@ class RedundantToPtr(RewritePattern):
 
         if origin.source.type == op.res.type:
             rewriter.replace_matched_op((), (origin.source,))
+
+
+class PtrAddZero(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: ptr.PtrAddOp, rewriter: PatternRewriter, /):
+        if (
+            (isinstance(offset_op := op.offset.owner, arith.ConstantOp))
+            and isinstance(offset_op.value, IntegerAttr)
+            and offset_op.value.value.data == 0
+        ):
+            rewriter.replace_matched_op((), (op.addr,))
