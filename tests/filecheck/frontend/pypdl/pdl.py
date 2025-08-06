72e04bddcd20b117149b65187f5b791a69f650f9
EdmundGoodman <egoodman3141@gmail.com>
2025-08-06 18:19:17 +0100

    Add todo list

--- tests/filecheck/frontend/pypdl/pdl.py
+++ tests/filecheck/frontend/pypdl/pdl.py
@@ -25,6 +25,11 @@ pdl.pattern : benefit(2) {
 ```
 """
 
+# TODO:
+# - Add None type support
+# - Add if..else support
+# - Add method support
+
 from xdsl.dialects import arith, builtin, pdl
 from xdsl.frontend import pypdl
 from xdsl.ir import Operation
