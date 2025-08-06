05346d29e3428a53cbf4286bd6f05980c1b08be3
EdmundGoodman <egoodman3141@gmail.com>
2025-08-06 17:51:55 +0100

    Move PyPDLContext into __init__

--- xdsl/frontend/pypdl/__init__.py
+++ xdsl/frontend/pypdl/__init__.py
@@ -1,5 +1,16 @@
-from xdsl.frontend.pypdl.context import PyPDLContext
+from dataclasses import dataclass
 
-__all__ = [
-    "PyPDLContext",
-]
+from xdsl.frontend.pyast.context import PyASTContext
+from xdsl.frontend.pypdl.transforms.func_to_pdl_rewrite import FuncToPdlRewrite
+from xdsl.transforms.desymref import FrontendDesymrefyPass
+
+
+@dataclass
+class PyPDLContext(PyASTContext):
+    """Encapsulate the mapping between Python and IR types and operations."""
+
+    def __init__(self):
+        super().__init__(
+            post_transforms=[FrontendDesymrefyPass(), FuncToPdlRewrite()],
+            post_callback=None,
+        )
