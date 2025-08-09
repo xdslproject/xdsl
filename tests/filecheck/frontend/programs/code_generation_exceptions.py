8a48af05b81d49c8be80444e1caf2ef4578161c4
EdmundGoodman <egoodman3141@gmail.com>
2025-08-09 16:30:57 +0100

    Add failing test for deprecating for loops

--- tests/filecheck/frontend/programs/code_generation_exceptions.py
+++ tests/filecheck/frontend/programs/code_generation_exceptions.py
@@ -1,12 +1,32 @@
 # RUN: python %s | filecheck %s
 
 from collections.abc import Callable
+from ctypes import c_size_t
 
+from xdsl.dialects import builtin
 from xdsl.frontend.pyast.context import CodeContext
 from xdsl.frontend.pyast.program import FrontendProgram
 from xdsl.frontend.pyast.utils.exceptions import CodeGenerationException
 
 p = FrontendProgram()
+p.register_type(c_size_t, builtin.IndexType())
+try:
+    with CodeContext(p):
+        # CHECK: For loops are currently not supported!
+
+        def test_for_unsupported(end: c_size_t):
+            for _ in range(
+                end  # pyright: ignore[reportArgumentType]
+            ):
+                pass
+            return
+
+    p.compile(desymref=False)
+    exit(1)
+except NotImplementedError as e:
+    print(e)
+
+
 try:
     with CodeContext(p):
         # CHECK: Unsupported function argument type: 'Callable[..., None]'
@@ -17,7 +37,6 @@ try:
     exit(1)
 except CodeGenerationException as e:
     print(e.msg)
-
 
 try:
     with CodeContext(p):
