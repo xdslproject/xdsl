8210f09dfea3030f101c5b5e03555fd6dfb3fea1
EdmundGoodman <egoodman3141@gmail.com>
2025-08-10 11:53:15 +0100

    Port across short dialect tests

--- tests/filecheck/frontend/dialects/scf.py
+++ tests/filecheck/frontend/dialects/scf.py
@@ -3,77 +3,90 @@
 from ctypes import c_size_t
 
 from xdsl.dialects import bigint, builtin
-from xdsl.frontend.pyast.context import CodeContext
-from xdsl.frontend.pyast.program import FrontendProgram
+from xdsl.frontend.pyast.context import PyASTContext
 from xdsl.frontend.pyast.utils.exceptions import CodeGenerationException
 
-p = FrontendProgram()
-p.register_type(bool, builtin.i1)
-p.register_type(int, bigint.bigint)
-p.register_type(c_size_t, builtin.IndexType())
-p.register_type(float, builtin.f64)
+ctx = PyASTContext(post_transforms=[])
+ctx.register_type(bool, builtin.i1)
+ctx.register_type(int, bigint.bigint)
+ctx.register_type(c_size_t, builtin.IndexType())
+ctx.register_type(float, builtin.f64)
 
 
-with CodeContext(p):
-    # CHECK:      %{{.*}} = scf.if %{{.*}} -> (!bigint.bigint) {
-    # CHECK-NEXT:   %{{.*}} = symref.fetch @x : !bigint.bigint
-    # CHECK-NEXT:   scf.yield %{{.*}} : !bigint.bigint
-    # CHECK-NEXT: } else {
-    # CHECK-NEXT:   %{{.*}} = symref.fetch @y : !bigint.bigint
-    # CHECK-NEXT:   scf.yield %{{.*}} : !bigint.bigint
-    # CHECK-NEXT: }
-    def test_if_expr(cond: bool, x: int, y: int) -> int:
-        return x if cond else y
-
-    # CHECK:      scf.if %{{.*}} {
-    # CHECK-NEXT: }
-    def test_if_I(cond: bool):
-        if cond:
-            pass
-        else:
-            pass
-        return
-
-    # CHECK:      %{{.*}} = symref.fetch @a : i1
-    # CHECK-NEXT: scf.if %{{.*}} {
-    # CHECK-NEXT: } else {
-    # CHECK-NEXT:   %{{.*}} = symref.fetch @b : i1
-    # CHECK-NEXT:   scf.if %{{.*}} {
-    # CHECK-NEXT:   } else {
-    # CHECK-NEXT:     %{{.*}} = symref.fetch @c : i1
-    # CHECK-NEXT:     scf.if %{{.*}} {
-    # CHECK-NEXT:     }
-    # CHECK-NEXT:   }
-    # CHECK-NEXT: }
-    def test_if_II(a: bool, b: bool, c: bool):
-        if a:
-            pass
-        elif b:
-            pass
-        elif c:
-            pass
-        return
-
-    # CHECK:      %{{.*}} = symref.fetch @cond : i1
-    # CHECK-NEXT: scf.if %{{.*}} {
-    # CHECK-NEXT: }
-    def test_if_III(cond: bool):
-        if cond:
-            pass
-        return
+# CHECK:      %{{.*}} = scf.if %{{.*}} -> (!bigint.bigint) {
+# CHECK-NEXT:   %{{.*}} = symref.fetch @x : !bigint.bigint
+# CHECK-NEXT:   scf.yield %{{.*}} : !bigint.bigint
+# CHECK-NEXT: } else {
+# CHECK-NEXT:   %{{.*}} = symref.fetch @y : !bigint.bigint
+# CHECK-NEXT:   scf.yield %{{.*}} : !bigint.bigint
+# CHECK-NEXT: }
+@ctx.parse_program
+def test_if_expr(cond: bool, x: int, y: int) -> int:
+    return x if cond else y
 
 
-p.compile(desymref=False)
-print(p.textual_format())
+print(test_if_expr.module)
+
+
+# CHECK:      scf.if %{{.*}} {
+# CHECK-NEXT: }
+@ctx.parse_program
+def test_if_I(cond: bool):
+    if cond:
+        pass
+    else:
+        pass
+    return
+
+
+print(test_if_I.module)
+
+
+# CHECK:      %{{.*}} = symref.fetch @a : i1
+# CHECK-NEXT: scf.if %{{.*}} {
+# CHECK-NEXT: } else {
+# CHECK-NEXT:   %{{.*}} = symref.fetch @b : i1
+# CHECK-NEXT:   scf.if %{{.*}} {
+# CHECK-NEXT:   } else {
+# CHECK-NEXT:     %{{.*}} = symref.fetch @c : i1
+# CHECK-NEXT:     scf.if %{{.*}} {
+# CHECK-NEXT:     }
+# CHECK-NEXT:   }
+# CHECK-NEXT: }
+@ctx.parse_program
+def test_if_II(a: bool, b: bool, c: bool):
+    if a:
+        pass
+    elif b:
+        pass
+    elif c:
+        pass
+    return
+
+
+print(test_if_II.module)
+
+
+# CHECK:      %{{.*}} = symref.fetch @cond : i1
+# CHECK-NEXT: scf.if %{{.*}} {
+# CHECK-NEXT: }
+@ctx.parse_program
+def test_if_III(cond: bool):
+    if cond:
+        pass
+    return
+
+
+print(test_if_III.module)
+
+
+# CHECK: Expected the same types for if expression, but got !bigint.bigint and f64.
+@ctx.parse_program
+def test_type_mismatch_in_if_expr(cond: bool, x: int, y: float) -> int:
+    return x if cond else y  # pyright: ignore[reportReturnType]
 
 
 try:
-    with CodeContext(p):
-        # CHECK: Expected the same types for if expression, but got !bigint.bigint and f64.
-        def test_type_mismatch_in_if_expr(cond: bool, x: int, y: float) -> int:
-            return x if cond else y  # pyright: ignore[reportReturnType]
-
-    p.compile(desymref=False)
-    print(p.textual_format())
+    test_type_mismatch_in_if_expr.module
 except CodeGenerationException as e:
     print(e.msg)
