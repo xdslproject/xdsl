b038107ff429e9494b1eb3ec795a70053fd177ba
Hendrik Klug <hendrik.klug@gmail.com>
2025-08-16 16:23:02 +0000

    wip towards EmitCTypeConstr as AnyOf constr

--- xdsl/dialects/emitc.py
+++ xdsl/dialects/emitc.py
@@ -7,12 +7,14 @@ Those can be translated to C/C++ via the Cpp emitter.
 See external [documentation](https://mlir.llvm.org/docs/Dialects/EmitC/).
 """
 
-from collections.abc import Iterable, Sequence
-from typing import Annotated, cast
+from __future__ import annotations
 
-from typing_extensions import TypeVar
+import functools
+import operator
+from collections.abc import Iterable, Sequence
 
 from xdsl.dialects.builtin import (
+    AnyAttr,
     ArrayAttr,
     BFloat16Type,
     ContainerType,
@@ -23,11 +25,15 @@ from xdsl.dialects.builtin import (
     IndexType,
     IntAttr,
     IntegerAttr,
-    IntegerType,
     ShapedType,
     StringAttr,
     TensorType,
     TupleType,
+    i1,
+    i8,
+    i16,
+    i32,
+    i64,
 )
 from xdsl.ir import (
     Attribute,
@@ -38,9 +44,11 @@ from xdsl.ir import (
     TypeAttribute,
 )
 from xdsl.irdl import (
-    AttrConstraint,
-    ConstraintContext,
+    AnyOf,
+    BaseAttr,
+    EqAttrConstraint,
     IRDLOperation,
+    ParamAttrConstraint,
     ParsePropInAttrDict,
     irdl_attr_definition,
     irdl_op_definition,
@@ -54,23 +62,17 @@ from xdsl.printer import Printer
 from xdsl.utils.exceptions import VerifyException
 from xdsl.utils.hints import isa
 
-import functools
-import operator
-
-EmitCIntegerTypeBitwidthConstr = reduce(
-    operator.or, 
-    (
-        EqAttrConstraint(bw) for bw in (1, 8, 16, 32, 64)
-    )
+EmitCIntegerTypeBitwidthConstr = functools.reduce(
+    operator.or_, (EqAttrConstraint(bw) for bw in (i1, i8, i16, i32, i64))
 )
 
-EmitCIntegerTypeConstr = ParamAttrConstr(
-	IntegerAttr,
-	(
-		EmitCIntegerTypeBitwidthConstr,
-		AnyAttr()
-	)
+EmitCIntegerTypeConstr = ParamAttrConstraint(
+    IntegerAttr, (EmitCIntegerTypeBitwidthConstr, AnyAttr())
 )
+"""
+Supported integer types in EmitC.
+See external [documentation](https://github.com/llvm/llvm-project/blob/main/mlir/lib/Dialect/EmitC/IR/EmitC.cpp#L96).
+"""
 
 
 EmitCFloatTypeConstr = (
@@ -83,191 +85,6 @@ EmitCFloatTypeConstr = (
 Supported floating-point type in EmitC.
 See external [documentation](https://github.com/llvm/llvm-project/blob/main/mlir/lib/Dialect/EmitC/IR/EmitC.cpp#L117)
 """
-
-
-def is_pointer_wide_type(type_attr: Attribute) -> bool:
-    """Check if a type is a pointer-wide type."""
-    match type_attr:
-        case EmitC_PtrDiffT() | EmitC_SignedSizeT() | EmitC_SizeT():
-            return True
-        case _:
-            return False
-
-
-def is_integer_index_or_opaque_type(
-    type_attr: Attribute,
-) -> bool:
-    """
-    Check if a type is an integer, index, or opaque type.
-
-    The emitC opaque type is not implemented yet so this function currently checks
-    only for integer and index types.
-    See external [documentation](https://github.com/llvm/llvm-project/blob/main/mlir/lib/Dialect/EmitC/IR/EmitC.cpp#L112).
-    """
-    return (
-        _is_supported_integer_type(type_attr)
-        or isinstance(type_attr, IndexType)
-        or is_pointer_wide_type(type_attr)
-    )
-
-
-class EmitCTypeConstraint(AttrConstraint):
-    """Constraint that verifies an attribute is a supported EmitC type."""
-
-    def verify(
-        self, attr: Attribute, constraint_context: ConstraintContext | None = None
-    ) -> None:
-        if not self.is_supported_emitc_type(attr):
-            raise VerifyException(f"Type {attr} is not a supported EmitC type")
-
-    def mapping_type_vars(
-        self, type_var_mapping: dict[TypeVar, AttrConstraint]
-    ) -> AttrConstraint:
-        # No type variables to map in this constraint
-        return self
-
-    @staticmethod
-    def is_supported_emitc_type(type_attr: Attribute) -> bool:
-        """
-        Check if a type is supported by EmitC.
-        See [MLIR implementation](https://github.com/llvm/llvm-project/blob/main/mlir/lib/Dialect/EmitC/IR/EmitC.cpp#L62).
-        """
-        match type_attr:
-            case IntegerType():
-                return _is_supported_integer_type(type_attr)
-            case IndexType():
-                return True
-            case EmitC_OpaqueType():
-                return True
-            case EmitC_ArrayType():
-                elem_type = cast(Attribute, type_attr.get_element_type())
-                return not isinstance(
-                    elem_type, EmitC_ArrayType
-                ) and EmitCTypeConstraint.is_supported_emitc_type(elem_type)
-            case EmitC_PointerType():
-                return EmitCTypeConstraint.is_supported_emitc_type(
-                    type_attr.pointee_type
-                )
-            case Float16Type() | BFloat16Type() | Float32Type() | Float64Type():
-                return True
-            case TensorType():
-                if not type_attr.has_static_shape():
-                    return False
-                elem_type = cast(Attribute, type_attr.get_element_type())
-                if isinstance(elem_type, EmitC_ArrayType):
-                    return False
-                return EmitCTypeConstraint.is_supported_emitc_type(elem_type)
-            case TupleType():
-                return all(
-                    not isinstance(t, EmitC_ArrayType)
-                    and EmitCTypeConstraint.is_supported_emitc_type(t)
-                    for t in type_attr.types
-                )
-            case EmitC_PtrDiffT() | EmitC_SignedSizeT() | EmitC_SizeT():
-                return True
-            case _:
-                return False
-
-
-emitc_type_constr = EmitCTypeConstraint()
-
-
-@irdl_attr_definition
-class EmitC_ArrayType(
-    ParametrizedAttribute, TypeAttribute, ShapedType, ContainerType[AttributeCovT]
-):
-    """EmitC array type"""
-
-    name = "emitc.array"
-
-    shape: ArrayAttr[IntAttr]
-    element_type: AttributeCovT
-
-    def __init__(
-        self,
-        shape: Iterable[int | IntAttr],
-        element_type: AttributeCovT,
-    ):
-        shape = ArrayAttr(
-            [IntAttr(dim) if isinstance(dim, int) else dim for dim in shape]
-        )
-        super().__init__(shape, element_type)
-
-    def verify(self) -> None:
-        if not self.shape.data:
-            raise VerifyException("EmitC array shape must not be empty")
-
-        for dim_attr in self.shape.data:
-            if dim_attr.data < 0:
-                raise VerifyException(
-                    "EmitC array dimensions must have non-negative size"
-                )
-
-        element_type = self.get_element_type()
-
-        if isinstance(element_type, EmitC_ArrayType):
-            raise VerifyException(
-                "EmitC array element type cannot be another EmitC_ArrayType."
-            )
-
-        # Check that the element type is a supported EmitC type.
-        if not self._is_valid_element_type(element_type):
-            raise VerifyException(
-                f"EmitC array element type '{element_type}' is not a supported EmitC type."
-            )
-
-    def get_num_dims(self) -> int:
-        return len(self.shape.data)
-
-    def get_shape(self) -> tuple[int, ...]:
-        return tuple(i.data for i in self.shape.data)
-
-    def get_element_type(self) -> AttributeCovT:
-        return self.element_type
-
-    @classmethod
-    def parse_parameters(cls, parser: AttrParser):
-        with parser.in_angle_brackets():
-            shape, type = parser.parse_ranked_shape()
-            return ArrayAttr(IntAttr(dim) for dim in shape), type
-
-    def print_parameters(self, printer: Printer) -> None:
-        with printer.in_angle_brackets():
-            printer.print_list(
-                self.shape, lambda dim: printer.print_string(f"{dim.data}"), "x"
-            )
-            printer.print_string("x")
-            printer.print_attribute(self.element_type)
-
-    def _is_valid_element_type(self, element_type: Attribute) -> bool:
-        """
-        Check if the element type is valid for EmitC_ArrayType.
-        See external [documentation](https://github.com/llvm/llvm-project/blob/main/mlir/include/mlir/Dialect/EmitC/IR/EmitCTypes.td#L77).
-        """
-        return is_integer_index_or_opaque_type(element_type) or is_supported_float_type(
-            element_type
-        )
-
-
-@irdl_attr_definition
-class EmitC_LValueType(ParametrizedAttribute, TypeAttribute):
-    """
-    EmitC lvalue type.
-    Values of this type can be assigned to and their address can be taken.
-    See [tablegen definition](https://github.com/llvm/llvm-project/blob/main/mlir/include/mlir/Dialect/EmitC/IR/EmitCTypes.td#L87)
-    """
-
-    name = "emitc.lvalue"
-    value_type: EmitCTypeConstr
-
-    def verify(self) -> None:
-        """
-        Verify the LValueType.
-        See [MLIR implementation](https://github.com/llvm/llvm-project/blob/main/mlir/lib/Dialect/EmitC/IR/EmitC.cpp#L1095)
-        """
-        # Check that the wrapped type is valid. This especially forbids nested lvalue types.
-        if isinstance(self.value_type, EmitC_ArrayType):
-            raise VerifyException("!emitc.lvalue cannot wrap !emitc.array type")
 
 
 @irdl_attr_definition
@@ -284,18 +101,6 @@ class EmitC_OpaqueType(ParametrizedAttribute, TypeAttribute):
             raise VerifyException(
                 "pointer not allowed as outer type with !emitc.opaque, use !emitc.ptr instead"
             )
-
-
-@irdl_attr_definition
-class EmitC_PointerType(ParametrizedAttribute, TypeAttribute):
-    """EmitC pointer type"""
-
-    name = "emitc.ptr"
-    pointee_type: TypeAttribute
-
-    def verify(self) -> None:
-        if isinstance(self.pointee_type, EmitC_LValueType):
-            raise VerifyException("pointers to lvalues are not allowed")
 
 
 @irdl_attr_definition
@@ -328,6 +133,132 @@ class EmitC_SizeT(ParametrizedAttribute, TypeAttribute):
     """
 
     name = "emitc.size_t"
+
+
+EmitCPointerWideTypesConstr = AnyOf(
+    [EmitC_PtrDiffT(), EmitC_SignedSizeT(), EmitC_SizeT()]
+)
+
+EmitCArrayElementTypeConstr = AnyOf(
+    [
+        EmitCIntegerTypeConstr,
+        IndexType(),
+        EmitC_OpaqueType,
+        EmitCPointerWideTypesConstr,
+        EmitCFloatTypeConstr,
+    ]
+)
+"""
+Supported element types in EmitC arrays.
+See external [documentation](https://github.com/llvm/llvm-project/blob/main/mlir/include/mlir/Dialect/EmitC/IR/EmitCTypes.td#L77).
+"""
+
+
+@irdl_attr_definition
+class EmitC_ArrayType(
+    ParametrizedAttribute, TypeAttribute, ShapedType, ContainerType[AttributeCovT]
+):
+    """EmitC array type"""
+
+    name = "emitc.array"
+
+    shape: ArrayAttr[IntAttr]
+
+    element_type: EmitCArrayElementTypeConstr
+
+    def __init__(
+        self,
+        shape: Iterable[int | IntAttr],
+        element_type: AttributeCovT,
+    ):
+        shape = ArrayAttr(
+            [IntAttr(dim) if isinstance(dim, int) else dim for dim in shape]
+        )
+        super().__init__(shape, element_type)
+
+    def verify(self) -> None:
+        if not self.shape.data:
+            raise VerifyException("EmitC array shape must not be empty")
+
+        for dim_attr in self.shape.data:
+            if dim_attr.data < 0:
+                raise VerifyException(
+                    "EmitC array dimensions must have non-negative size"
+                )
+
+        element_type = self.get_element_type()
+
+        if isinstance(element_type, EmitC_ArrayType):
+            raise VerifyException(
+                "EmitC array element type cannot be another EmitC_ArrayType."
+            )
+
+    def get_num_dims(self) -> int:
+        return len(self.shape.data)
+
+    def get_shape(self) -> tuple[int, ...]:
+        return tuple(i.data for i in self.shape.data)
+
+    def get_element_type(self) -> AttributeCovT:
+        return self.element_type
+
+    @classmethod
+    def parse_parameters(cls, parser: AttrParser):
+        with parser.in_angle_brackets():
+            shape, type = parser.parse_ranked_shape()
+            return ArrayAttr(IntAttr(dim) for dim in shape), type
+
+    def print_parameters(self, printer: Printer) -> None:
+        with printer.in_angle_brackets():
+            printer.print_list(
+                self.shape, lambda dim: printer.print_string(f"{dim.data}"), "x"
+            )
+            printer.print_string("x")
+            printer.print_attribute(self.element_type)
+
+
+@irdl_attr_definition
+class EmitC_LValueType(ParametrizedAttribute, TypeAttribute):
+    """
+    EmitC lvalue type.
+    Values of this type can be assigned to and their address can be taken.
+    See [tablegen definition](https://github.com/llvm/llvm-project/blob/main/mlir/include/mlir/Dialect/EmitC/IR/EmitCTypes.td#L87)
+    """
+
+    name = "emitc.lvalue"
+    value_type: EmitCTypeConstr
+
+    def verify(self) -> None:
+        """
+        Verify the LValueType.
+        See [MLIR implementation](https://github.com/llvm/llvm-project/blob/main/mlir/lib/Dialect/EmitC/IR/EmitC.cpp#L1095)
+        """
+        # Check that the wrapped type is valid. This especially forbids nested lvalue types.
+        if isinstance(self.value_type, EmitC_ArrayType):
+            raise VerifyException("!emitc.lvalue cannot wrap !emitc.array type")
+
+
+@irdl_attr_definition
+class EmitC_PointerType(ParametrizedAttribute, TypeAttribute):
+    """EmitC pointer type"""
+
+    name = "emitc.ptr"
+    pointee_type: EmitC_LValueType
+
+
+EmitCTypeConstr = AnyOf(
+    [
+        EmitCIntegerTypeConstr,
+        EmitCFloatTypeConstr,
+        IndexType(),
+        EmitC_OpaqueType,
+        EmitCPointerWideTypesConstr,
+        EmitC_ArrayType,
+        EmitC_PointerType,
+        TensorType,
+        TupleType,
+    ]
+)
 
 
 @irdl_op_definition
