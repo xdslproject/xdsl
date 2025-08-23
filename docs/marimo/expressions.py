45012a9ed7df28604dd384555d0aec438d187645
Tobias Grosser <tobias@grosser.es>
2025-08-23 13:36:59 +0100

    Install micropip

--- docs/marimo/expressions.py
+++ docs/marimo/expressions.py
@@ -7,6 +7,11 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
+    # Uncomment the following two lines to install local version of xDSL.
+    # Adjust version string as required
+    import micropip
+    await micropip.install("xdsl @ http://127.0.0.1:8000/xdsl-0.0.0-py3-none-any.whl")
+
     from xdsl.listlang import printtest
     return (mo, printtest)
 
