efff021e708dc7ebaad8652a080aba0f8828b732
Tobias Grosser <tobias@grosser.es>
2025-08-27 07:59:53 +0100

    Support all python versions

--- xdsl/frontend/listlang/main.py
+++ xdsl/frontend/listlang/main.py
@@ -1,7 +1,7 @@
 import io
 import re
 from dataclasses import dataclass
-from typing import cast
+from typing import Generic, TypeVar, cast
 
 from xdsl.builder import Builder
 from xdsl.dialects import arith, builtin, scf
@@ -43,9 +43,6 @@ GT_CMP = Punctuation(re.compile(r">"), "greater than comparator")
 LTE_CMP = Punctuation(re.compile(r"<="), "less than or equal comparator")
 GTE_CMP = Punctuation(re.compile(r">="), "greater than or equal comparator")
 
-BOOL_AND = Punctuation(re.compile(r"&&"), "boolean and")
-BOOL_OR = Punctuation(re.compile(r"\|\|"), "boolean or")
-BOOL_NEG = Punctuation(re.compile(r"!"), "boolean negation")
 
 LPAREN = Punctuation(re.compile(r"\("), "left parenthesis")
 RPAREN = Punctuation(re.compile(r"\)"), "right parenthesis")
@@ -62,10 +59,13 @@ class Location:
     pos: int
 
 
+T = TypeVar("T")
+
+
 @dataclass
-class Located:
+class Located(Generic[T]):  # noqa: UP046
     loc: Location
-    value: object
+    value: T
 
     def __bool__(self) -> bool:
         return bool(self.value)
@@ -118,7 +118,7 @@ class CodeCursor:
 
     def next_regex(
         self, regex: re.Pattern[str]
-    ) -> Located:
+    ) -> Located[re.Match[str] | None]:
         match = self.peek_regex(regex)
         if match.value is not None:
             self.pos = match.value.end()
@@ -126,7 +126,7 @@ class CodeCursor:
 
     def peek_regex(
         self, regex: re.Pattern[str]
-    ) -> Located:
+    ) -> Located[re.Match[str] | None]:
         pos = self._whitespace_end()
         return Located(Location(pos), regex.match(self.code, pos))
 
@@ -163,7 +163,7 @@ class TypedExpression:
 ## Utils
 
 
-def parse_opt_punct(ctx: ParsingContext, punct: Punctuation) -> Located:
+def parse_opt_punct(ctx: ParsingContext, punct: Punctuation) -> Located[bool]:
     """
     Returns True if the punctuation was successfully parsed.
     """
@@ -177,7 +177,7 @@ def parse_punct(ctx: ParsingContext, punct: Punctuation) -> Location:
     return located.loc
 
 
-def parse_opt_identifier(ctx: ParsingContext) -> Located:
+def parse_opt_identifier(ctx: ParsingContext) -> Located[str | None]:
     matched = ctx.cursor.next_regex(IDENT)
     return Located(
         matched.loc,
@@ -185,13 +185,13 @@ def parse_opt_identifier(ctx: ParsingContext) -> Located:
     )
 
 
-def parse_identifier(ctx: ParsingContext) -> Located:
+def parse_identifier(ctx: ParsingContext) -> Located[str]:
     if (ident := parse_opt_identifier(ctx)).value is None:
         raise ParseError.from_ctx(ctx, "expected variable identifier")
     return Located(ident.loc, ident.value)
 
 
-def parse_opt_integer(ctx: ParsingContext) -> Located:
+def parse_opt_integer(ctx: ParsingContext) -> Located[int | None]:
     matched = ctx.cursor.next_regex(INTEGER)
     return Located(
         matched.loc,
@@ -199,7 +199,7 @@ def parse_opt_integer(ctx: ParsingContext) -> Located:
     )
 
 
-def parse_integer(ctx: ParsingContext) -> Located:
+def parse_integer(ctx: ParsingContext) -> Located[int]:
     if (lit := parse_opt_integer(ctx)).value is None:
         raise ParseError.from_ctx(ctx, "expected integer constant")
     return Located(lit.loc, lit.value)
@@ -210,7 +210,7 @@ def parse_integer(ctx: ParsingContext) -> Located:
 
 def _parse_opt_expr_atom(
     ctx: ParsingContext, builder: Builder
-) -> Located:
+) -> Located[TypedExpression | None]:
     # Parse parenthesis expression.
     if parse_opt_punct(ctx, LPAREN):
         expr = parse_expr(ctx, builder)
@@ -296,24 +296,6 @@ def _parse_opt_expr_atom(
         val.result.name_hint = "true"
         return Located(true.loc, TypedExpression(val.result, ListLangBool()))
 
-    # Parse boolean negation.
-    if neg := parse_opt_punct(ctx, BOOL_NEG):
-        to_negate = _parse_expr_atom(ctx, builder)
-        if not isinstance(to_negate.value.typ, ListLangBool):
-            raise ParseError(
-                to_negate.loc.pos,
-                f"expected {ListLangBool()} type for negation, "
-                f"got {to_negate.value.typ}",
-            )
-        true = builder.insert_op(
-            arith.ConstantOp(builtin.IntegerAttr(1, XDSL_BOOL))
-        )
-        negated = builder.insert_op(
-            arith.XOrIOp(to_negate.value.value, true.result)
-        )
-        negated.result.name_hint = f"not_{to_negate.value.value.name_hint}"
-        return Located(neg.loc, TypedExpression(negated.result, ListLangBool()))
-
     # Parse binding.
     if (ident := parse_opt_identifier(ctx)).value is not None:
         if ident.value not in ctx.bindings:
@@ -324,16 +306,8 @@ def _parse_opt_expr_atom(
     return Located(Location(ctx.cursor.pos), None)
 
 
-def _parse_expr_atom(
-    ctx: ParsingContext, builder: Builder
-) -> Located:
-    if (expr := _parse_opt_expr_atom(ctx, builder)).value is None:
-        raise ParseError(expr.loc.pos, "expected expression atom")
-    return Located(expr.loc, expr.value)
-
-
 class BinaryOp:
-    def parse_opt_glyph(self, ctx: ParsingContext) -> Located:
+    def parse_opt_glyph(self, ctx: ParsingContext) -> Located[bool]:
         """
         Returns true if the expected glyph was found.
         """
@@ -342,20 +316,20 @@ class BinaryOp:
     def build(
         self,
         builder: Builder,
-        lhs: Located,
-        rhs: Located,
+        lhs: Located[TypedExpression],
+        rhs: Located[TypedExpression],
     ) -> TypedExpression: ...
 
 
 class Multiplication(BinaryOp):
-    def parse_opt_glyph(self, ctx: ParsingContext) -> Located:
+    def parse_opt_glyph(self, ctx: ParsingContext) -> Located[bool]:
         return parse_opt_punct(ctx, STAR)
 
     def build(
         self,
         builder: Builder,
-        lhs: Located,
-        rhs: Located,
+        lhs: Located[TypedExpression],
+        rhs: Located[TypedExpression],
     ) -> TypedExpression:
         if not isinstance(lhs.value.typ, ListLangInt):
             raise ParseError(
@@ -381,14 +355,14 @@ class Multiplication(BinaryOp):
 
 
 class Addition(BinaryOp):
-    def parse_opt_glyph(self, ctx: ParsingContext) -> Located:
+    def parse_opt_glyph(self, ctx: ParsingContext) -> Located[bool]:
         return parse_opt_punct(ctx, PLUS)
 
     def build(
         self,
         builder: Builder,
-        lhs: Located,
-        rhs: Located,
+        lhs: Located[TypedExpression],
+        rhs: Located[TypedExpression],
     ) -> TypedExpression:
         if not isinstance(lhs.value.typ, ListLangInt):
             raise ParseError(
@@ -418,14 +392,14 @@ class Comparator(BinaryOp):
     glyph: Punctuation
     arith_code: str
 
-    def parse_opt_glyph(self, ctx: ParsingContext) -> Located:
+    def parse_opt_glyph(self, ctx: ParsingContext) -> Located[bool]:
         return parse_opt_punct(ctx, self.glyph)
 
     def build(
         self,
         builder: Builder,
-        lhs: Located,
-        rhs: Located,
+        lhs: Located[TypedExpression],
+        rhs: Located[TypedExpression],
     ) -> TypedExpression:
         if not isinstance(lhs.value.typ, ListLangInt):
             raise ParseError(
@@ -451,91 +425,23 @@ class Comparator(BinaryOp):
         return TypedExpression(cmpi_op.result, ListLangBool())
 
 
-class BoolAnd(BinaryOp):
-    def parse_opt_glyph(self, ctx: ParsingContext) -> Located:
-        return parse_opt_punct(ctx, BOOL_AND)
-
-    def build(
-        self,
-        builder: Builder,
-        lhs: Located,
-        rhs: Located,
-    ) -> TypedExpression:
-        if not isinstance(lhs.value.typ, ListLangBool):
-            raise ParseError(
-                lhs.loc.pos,
-                f"expected expression of type {ListLangBool()} "
-                f"in boolean and, got {lhs.value.typ}",
-            )
-
-        if not isinstance(rhs.value.typ, ListLangBool):
-            raise ParseError(
-                rhs.loc.pos,
-                f"expected expression of type {ListLangBool()} "
-                f"in boolean and, got {rhs.value.typ}",
-            )
-
-        and_op = builder.insert_op(
-            arith.AndIOp(lhs.value.value, rhs.value.value)
-        )
-        and_op.result.name_hint = (
-            f"{lhs.value.value.name_hint}_and_{rhs.value.value.name_hint}"
-        )
-        return TypedExpression(and_op.result, lhs.value.typ)
-
-
-class BoolOr(BinaryOp):
-    def parse_opt_glyph(self, ctx: ParsingContext) -> Located:
-        return parse_opt_punct(ctx, BOOL_OR)
-
-    def build(
-        self,
-        builder: Builder,
-        lhs: Located,
-        rhs: Located,
-    ) -> TypedExpression:
-        if not isinstance(lhs.value.typ, ListLangBool):
-            raise ParseError(
-                lhs.loc.pos,
-                f"expected expression of type {ListLangBool()} "
-                f"in boolean or, got {lhs.value.typ}",
-            )
-
-        if not isinstance(rhs.value.typ, ListLangBool):
-            raise ParseError(
-                rhs.loc.pos,
-                f"expected expression of type {ListLangBool()} "
-                f"in boolean or, got {rhs.value.typ}",
-            )
-
-        or_op = builder.insert_op(arith.OrIOp(lhs.value.value, rhs.value.value))
-        or_op.result.name_hint = (
-            f"{lhs.value.value.name_hint}_or_{rhs.value.value.name_hint}"
-        )
-        return TypedExpression(or_op.result, lhs.value.typ)
-
-
 PARSE_BINOP_PRIORITY: tuple[tuple[BinaryOp, ...], ...] = (
     (Multiplication(),),
     (Addition(),),
     (
         Comparator(EQUAL_CMP, "eq"),
+        Comparator(LT_CMP, "ult"),
+        Comparator(GT_CMP, "ugt"),
         Comparator(LTE_CMP, "ule"),
         Comparator(GTE_CMP, "uge"),
-        Comparator(GT_CMP, "ugt"),
-        Comparator(LT_CMP, "ult"),
-    ),
-    (
-        BoolAnd(),
-        BoolOr(),
     ),
 )
 
 
 def parse_opt_expr(
     ctx: ParsingContext, builder: Builder
-) -> Located:
-    def priority_level_parser(level: int) -> Located:
+) -> Located[TypedExpression | None]:
+    def priority_level_parser(level: int) -> Located[TypedExpression | None]:
         if level == 0:
             return _parse_opt_expr_atom(ctx, builder)
 
@@ -561,14 +467,14 @@ def parse_opt_expr(
             )
             lhs = Located(lhs.loc, expr)
 
-        return cast(Located, lhs)
+        return cast(Located[TypedExpression | None], lhs)
 
     return priority_level_parser(len(PARSE_BINOP_PRIORITY))
 
 
 def parse_expr(
     ctx: ParsingContext, builder: Builder
-) -> Located:
+) -> Located[TypedExpression]:
     if (expr := parse_opt_expr(ctx, builder)).value is None:
         raise ParseError(expr.loc.pos, "expected expression")
     return Located(expr.loc, expr.value)
@@ -579,7 +485,7 @@ def parse_expr(
 
 def parse_opt_let_statement(
     ctx: ParsingContext, builder: Builder
-) -> Located:
+) -> Located[bool]:
     """
     Parses a let statement and adds its binding to the provided context if it
     is there. Returns True if a binding was found, False otherwise.
@@ -608,7 +514,7 @@ def parse_opt_let_statement(
     return let
 
 
-def parse_opt_statement(ctx: ParsingContext, builder: Builder) -> Located:
+def parse_opt_statement(ctx: ParsingContext, builder: Builder) -> Located[bool]:
     return parse_opt_let_statement(ctx, builder)
 
 
@@ -617,7 +523,7 @@ def parse_opt_statement(ctx: ParsingContext, builder: Builder) -> Located:
 
 def parse_block_content(
     ctx: ParsingContext, builder: Builder
-) -> Located:
+) -> Located[Located[TypedExpression | None]]:
     """
     Parses the content of a block and returns its trailing expression, if there
     is one. The first location is the start of the block content, while the
@@ -636,7 +542,7 @@ def parse_block_content(
 
 def parse_block(
     ctx: ParsingContext, builder: Builder
-) -> Located:
+) -> Located[Located[TypedExpression | None]]:
     """
     Parses a block and returns its trailing expression, if there is one. The
     first location is the start of the block content, while the second location
@@ -661,7 +567,7 @@ def parse_block(
 
 def parse_program(
     code: str, builder: Builder
-) -> Located:
+) -> Located[TypedExpression | None]:
     """
     Parses a program.
     If the program has a result expression, returns it. The location of the
@@ -690,3 +596,4 @@ if __name__ == "__main__":
     output = program_to_mlir(program)
 
     print(output)
+    print()
