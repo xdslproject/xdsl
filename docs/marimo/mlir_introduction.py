008da9d63c070b005c3ba4600bf31764799152e7
Mathieu Fehr <mathieu.fehr@gmail.com>
2025-09-02 02:45:24 +0100

    Some new stuff

--- docs/marimo/mlir_introduction.py
+++ docs/marimo/mlir_introduction.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.14.11"
+__generated_with = "0.14.17"
 app = marimo.App()
 
 
@@ -56,6 +56,7 @@ def _(mo):
         ```
     
         Note the following:
+    
         * An MLIR program is a sequence of operations (arith.constant, arith.addi in this example)
         * Variables are prefixed with a `%` sign
         * Operations:
@@ -63,11 +64,11 @@ def _(mo):
             * Produce results (all produce one result here, but they can produce zero or multiple)
             * Have a list of operands (the add has two operands %x and %y, and the constants have none)
             * Can have additional static information (2 and 3 in the arith.constant)
-            * Specify the types of their operands and results (i32 for all operations here)
+            * Specify some of the types of their operands and results (i32 for all operations here)
     
     
         To try to understand this more, you can change the following example, and see how the generated MLIR code change.
-        Try to assign constants to variables, and keep one operator per line (this will be explained later). Also, try to use booleans (`true`, `false`, `&&`, `||`) and comparisons (`==`, `!=`, `<`, `>`, `<=`, `>=`).
+        Try to use booleans (`true`, `false`, `&&`, `||`) and comparisons (`==`, `!=`, `<`, `>`, `<=`, `>=`).
     
         ```rust
         let a = true;
@@ -76,6 +77,7 @@ def _(mo):
         ```
     
         Some things you might have noticed:
+    
         * Booleans are represented as `i1` (a single bit integer)
         * All comparisons operations are encoded as `arith.cmpi` operations, with an opcode (eq, ult, ule, ...) and two operands
         """
@@ -87,16 +89,16 @@ def _(mo):
 def _(mo):
     mo.md(
         r"""
-        ## Single-Static Assignment (SSA)
+        ## Static Single-Assignment (SSA)
     
-        Show this code:
-        ```
-        c = (3 + 4) + 5
-        ```
-        * Make people add more operators to it.
-        * Show the form we get (SSA form)
+        MLIR IR uses single static-assignment form (SSA), meaning that each variable are defined once. Since our language does not have mutability, the effect is that we cannot "shadow" variables.
     
-        Explain that we cannot reassign variables.
+        Look how the following code is implemented in MLIR IR:
+        ```rust
+        let c = 4 + 5;
+        let c = c + 2;
+        c
+        ```
         """
     )
     return
@@ -108,16 +110,13 @@ def _(mo):
         r"""
         ## Applying compilation passes
     
-        Now that we have code, we can apply optimizations on it!
+        Now that we have showed some MLIR IR code, let's see how we can manipulate MLIR IR with passes. Here are a few important MLIR IR passes that are relevant for most compilers:
     
-        Show the following optimizations:
-        * DCE
-        * CSE
-        * Constant folding
+        * `dce` (Dead code elimination): This pass removes code that is not used.
+        * `cse` (Common subexpression elimination): This pass merges operations that are exactly the same.
+        * `constant-fold-interp` (Constant folding): This pass constant fold operations, so `3 + 4` gets rewritten to `7`.
     
-        Explain
-        * Explain each what they do, and how they change the code.
-        * Let people try them out on their own code.
+        Here is a code snippet where you can apply a pass pipeline to MLIR code, and see the effect of each pass. Try to understand what each pass does, and look how reordering passes may have an effect on the resulting operations.
         """
     )
     return
@@ -129,11 +128,28 @@ def _(mo):
         r"""
         ## Control flow with Regions
     
-        * scf.if example
-            * Explain with a figure how this is executed
-            * Let people try it with nested ifs, etc...
-            * Explain yield
-            * Let them try simple optimizations
+        Now that we have showed how computations work, let's look at how to model control-flow.
+    
+        ### `scf.if`
+    
+        As a simple first example of control-flow, let's look at a ternary condition. The following example computes the minimum between `x` and `y`:
+    
+        ```rust
+        let x = 5
+        let y = 7
+        if x < y then {x} else {y}
+        ```
+    
+        We can see the following in the generated MLIR IR:
+    
+        * The `if` is represented using an `scf.if` operation. `scf` stands for "structured control-flow".
+        * `scf.if` contains two **regions**. A region is a section of code that contains another list of operation (we will explain this more in details later).
+        * Only one region is executed, this logic comes from `scf.if`. In general, operations that have regions decide when they are executed through compilation passes.
+        * Each region ends with an `scf.yield`, this is the "value" that is leaving the region when the region gets executed, and that gets returned by the `scf.if` operation.
+    
+        Try to change this program, and look at the effect of different optimization passes!
+    
+        ### `scf.for`
     
         * scf.for example
             * Show how this is compiled to scf.for
