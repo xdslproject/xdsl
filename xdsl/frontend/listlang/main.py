34ea5e742cfcd308fad7ab41d267fa0f021e6d3b
Mathieu Fehr <mathieu.fehr@gmail.com>
2025-09-05 21:00:49 +0100

    Fix bug?

--- xdsl/frontend/listlang/main.py
+++ xdsl/frontend/listlang/main.py
@@ -307,7 +307,7 @@ def _parse_opt_expr_atom_with_methods(
 ) -> Located[TypedExpression | None]:
     if (x := _parse_opt_expr_atom(ctx, builder)).value is None:
         return x
-    x = Located(x.loc, x.value)
+    x: Located[TypedExpression | None] = Located(x.loc, x.value)
 
     while parse_opt_punct(ctx, SINGLE_PERIOD):
         method_name = parse_identifier(ctx)
@@ -364,7 +364,7 @@ def _parse_opt_expr_atom_with_methods(
         x = Located(x.loc, method.build(builder, x, lambda_info))
         x.value.value.name_hint = "_" + method.name
 
-    return cast(Located[TypedExpression | None], x)
+    return x
 
 
 ### Binary operators
