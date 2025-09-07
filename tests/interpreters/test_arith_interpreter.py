595ee16be2c2ee7842795f079e7ae6e59f353221
Théo Degioanni <30992420+Moxinilian@users.noreply.github.com>
2025-09-07 18:52:43 +0100

    fix arith bugs

--- tests/interpreters/test_arith_interpreter.py
+++ tests/interpreters/test_arith_interpreter.py
@@ -4,7 +4,7 @@ from math import copysign, isnan
 
 import pytest
 
-from xdsl.dialects import arith, test
+from xdsl.dialects import arith, builtin, func, test
 from xdsl.dialects.arith import (
     AddfOp,
     AddiOp,
@@ -27,6 +27,8 @@ from xdsl.dialects.arith import (
 from xdsl.dialects.builtin import IndexType, IntegerType, ModuleOp, Signedness, i8, i32
 from xdsl.interpreter import Interpreter
 from xdsl.interpreters.arith import ArithFunctions
+from xdsl.interpreters.func import FuncFunctions
+from xdsl.ir import Block, Region
 
 interpreter = Interpreter(ModuleOp([]))
 interpreter.register_implementations(ArithFunctions())
@@ -91,9 +93,9 @@ def test_muli(lhs_value: int, rhs_value: int):
 @pytest.mark.parametrize("lhs_value", [1, 0, -1, 127])
 @pytest.mark.parametrize("rhs_value", [1, 0, -1, 127])
 def test_andi(lhs_value: int, rhs_value: int):
-    muli = AndIOp(lhs_op, rhs_op)
+    andi = AndIOp(lhs_op, rhs_op)
 
-    ret = interpreter.run_op(muli, (lhs_value, rhs_value))
+    ret = interpreter.run_op(andi, (lhs_value, rhs_value))
 
     assert len(ret) == 1
     assert ret[0] == lhs_value & rhs_value
@@ -102,9 +104,9 @@ def test_andi(lhs_value: int, rhs_value: int):
 @pytest.mark.parametrize("lhs_value", [1, 0, -1, 127])
 @pytest.mark.parametrize("rhs_value", [1, 0, -1, 127])
 def test_ori(lhs_value: int, rhs_value: int):
-    muli = OrIOp(lhs_op, rhs_op)
+    ori = OrIOp(lhs_op, rhs_op)
 
-    ret = interpreter.run_op(muli, (lhs_value, rhs_value))
+    ret = interpreter.run_op(ori, (lhs_value, rhs_value))
 
     assert len(ret) == 1
     assert ret[0] == lhs_value | rhs_value
@@ -113,12 +115,29 @@ def test_ori(lhs_value: int, rhs_value: int):
 @pytest.mark.parametrize("lhs_value", [1, 0, -1, 127])
 @pytest.mark.parametrize("rhs_value", [1, 0, -1, 127])
 def test_xori(lhs_value: int, rhs_value: int):
-    muli = XOrIOp(lhs_op, rhs_op)
+    xori = XOrIOp(lhs_op, rhs_op)
 
-    ret = interpreter.run_op(muli, (lhs_value, rhs_value))
+    ret = interpreter.run_op(xori, (lhs_value, rhs_value))
 
     assert len(ret) == 1
     assert ret[0] == lhs_value ^ rhs_value
+
+
+def test_xori_i1():
+    ops = [
+        (lhs_op := arith.ConstantOp(builtin.IntegerAttr(1, builtin.i1))),
+        (rhs_op := arith.ConstantOp(builtin.IntegerAttr(1, builtin.i1))),
+        (xor_op := XOrIOp(lhs_op, rhs_op)),
+        func.ReturnOp(xor_op),
+    ]
+
+    func_op = func.FuncOp("main", ((), ()), Region([Block(ops)]))
+    interpreter.register_implementations(FuncFunctions())
+
+    ret = interpreter.call_op(func_op, ())
+
+    assert len(ret) == 1
+    assert ret[0] == 0
 
 
 @pytest.mark.parametrize("lhs_value", [1, 0, -1, 127])
