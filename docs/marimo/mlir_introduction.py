4d73a44cf309ec514724d87679896176be1b903a
Sasha Lopoukhine <superlopuh@gmail.com>
2025-09-08 00:39:08 +0100

    documentation: check pass exercise outputs (#5185)
    Kind of hardcoded need to double check that they're correct, not sure
    how to construct automated tests for these.

--- docs/marimo/mlir_introduction.py
+++ docs/marimo/mlir_introduction.py
@@ -588,10 +588,10 @@ def _(mo):
 
 
 @app.cell(hide_code=True)
-def _(mo):
+def _(check_optimizations, mo):
     mo.md(
-        r"""
-    ### How to optimize these examples?
+        rf"""
+    ### How to optimize these examples? {check_optimizations}
 
     For each of the following programs, can you find out which passes should be applied?
     """
@@ -599,34 +599,45 @@ def _(mo):
     return
 
 
-@app.cell(hide_code=True)
+@app.cell
 def _(mo):
+    get_checkboxes_state, set_checkboxes_state = mo.state(False)
+
     def build_example(num: int, mlir_str: str) -> tuple[list[mo.ui.checkbox], mo.vstack]:
         title = mo.md(f"#### Example {num}")
         pass_md = mo.md("`" * 3 + "mlir\n" + mlir_str + "`" * 3)
-        pass_boxes = [mo.ui.checkbox(label="cse"), mo.ui.checkbox(label="dce"), mo.ui.checkbox(label="constant-fold-interp")]
+        pass_boxes = [
+            mo.ui.checkbox(label=label, on_change=set_checkboxes_state)
+            for label in ("cse", "dce", "constant-fold-interp")
+        ]
         pass_mo = mo.vstack([title, pass_md, *pass_boxes])
         return (pass_boxes, pass_mo)
 
-    pass_1_mlir = r"""%x = arith.constant 3 : i32
-    %y = arith.constant 15 : i32
-    %res = arith.subi %x, %y : i32
+    # cse
+    pass_1_mlir = r"""%b = arith.addi %a, %a : i32
+    %c = arith.addi %a, %a : i32
+    %d = arith.addi %b, %c : i32
+    printf.print_format "{}", %d : i32
     """
     pass_1_boxes, pass_1_mo = build_example(1, pass_1_mlir)
 
-    pass_2_mlir = r"""%t = arith.addi %x, %x : i32
-    %t2 = arith.addi %t, %t : i32
-    printf.print_format "{}", %t2 : i32
+    # dce
+    pass_2_mlir = r"""%b = arith.addi %a, %a : i32
+    %c = arith.addi %a, %a : i32
+    %d = arith.addi %b, %b : i32
+    printf.print_format "{}", %d : i32
     """
     pass_2_boxes, pass_2_mo = build_example(2, pass_2_mlir)
 
-    pass_3_mlir = r"""%t = arith.muli %x, %y : i32
-    %u = arith.muli %x, %y : i32
-    %z = arith.addi %t, %u : i32
-    printf.print_format "{}", %z : i32
+    # constant-fold-interp
+    pass_3_mlir = r"""%x = arith.constant 3 : i32
+    %y = arith.constant 15 : i32
+    %res = arith.subi %x, %y : i32
+    printf.print_format "{}", %res : i32
     """
     pass_3_boxes, pass_3_mo = build_example(3, pass_3_mlir)
 
+    # nothing
     pass_4_mlir = r"""%t = arith.addi %x, %y : i32
     %t2 = arith.addi %y, %x : i32
     %res = arith.addi %t, %t2 : i32
@@ -634,7 +645,7 @@ def _(mo):
     """
     pass_4_boxes, pass_4_mo = build_example(4, pass_4_mlir)
 
-
+    # nothing
     pass_5_mlir = r"""%c1 = arith.constant -1 : i32
     %c0 = arith.constant 0 : i32
     %t = arith.addi %x, %c0 : i32
@@ -644,6 +655,7 @@ def _(mo):
     """
     pass_5_boxes, pass_5_mo = build_example(5, pass_5_mlir)
 
+    # dce,constant-fold-interp
     pass_6_mlir = r"""%c2 = arith.constant 2 : i32
     %c4 = arith.constant 4 : i32
     %u = arith.addi %c2, %c4 : i32
@@ -654,7 +666,42 @@ def _(mo):
     pass_6_boxes, pass_6_mo = build_example(6, pass_6_mlir)
 
     mo.vstack([mo.hstack([pass_1_mo, pass_2_mo]), mo.md("<br>"), mo.hstack([pass_3_mo, pass_4_mo]), mo.md("<br>"), mo.hstack([pass_5_mo, pass_6_mo])])
-    return
+    return (
+        get_checkboxes_state,
+        pass_1_boxes,
+        pass_2_boxes,
+        pass_3_boxes,
+        pass_4_boxes,
+        pass_5_boxes,
+        pass_6_boxes,
+    )
+
+
+@app.cell
+def _(
+    get_checkboxes_state,
+    mo,
+    pass_1_boxes,
+    pass_2_boxes,
+    pass_3_boxes,
+    pass_4_boxes,
+    pass_5_boxes,
+    pass_6_boxes,
+):
+    get_checkboxes_state
+
+    boxess = (pass_1_boxes, pass_2_boxes, pass_3_boxes, pass_4_boxes, pass_5_boxes, pass_6_boxes)
+    values = "_".join(
+        "".join(str(int(box.value)) for box in boxes)
+        for boxes in boxess
+    )
+    expected_values = "100_010_001_000_000_011"
+
+    check_optimizations = "✅" if values == expected_values else "❌"
+    values, check_optimizations
+
+    mo.hstack((mo.md("✅ Correct!" if values == expected_values else "❌ At least one exercise is incorrect"),), justify="center")
+    return (check_optimizations,)
 
 
 @app.cell(hide_code=True)
