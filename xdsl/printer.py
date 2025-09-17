0b511d93e7cf9a602b64136a2201a0c59931adec
Jake Davies <jakedves@gmail.com>
2025-09-17 11:59:46 +0100

    add printing code

--- xdsl/printer.py
+++ xdsl/printer.py
@@ -409,6 +409,17 @@ class Printer(BasePrinter):
         else:
             self.print_string(f"{value:d}")
 
+    def print_dimension_list(self, dims: Sequence[int]):
+        if len(dims) == 0:
+            self.print_string("[]")
+            return
+
+        self.print_list(
+            dims,
+            lambda x: self.print_int(x) if x != -1 else self.print_string("?"),
+            "x",
+        )
+
     def print_attribute(self, attribute: Attribute) -> None:
         # Print builtin attributes
         if isinstance(attribute, BuiltinAttribute):
