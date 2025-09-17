acdc5b42ad74393ab093fcd9a6d2c26d987d12c3
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-09-17 15:36:23 +0200

    more links

--- xdsl/transforms/convert_pdl_to_pdl_interp/__init__.py
+++ xdsl/transforms/convert_pdl_to_pdl_interp/__init__.py
@@ -1,4 +1,5 @@
 """
 The pdl-to-pdl-interp conversion works by translating declarative [pdl patterns][xdsl.dialects.pdl] into a set of
-predicates that are then converted to imperative code in the pdl_interp dialect.
+[predicates][xdsl.transforms.convert_pdl_to_pdl_interp.predicate] that are then converted to imperative code in
+the [pdl_interp dialect][xdsl.dialects.pdl_interp].
 """
