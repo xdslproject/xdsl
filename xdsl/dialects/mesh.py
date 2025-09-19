d9edd348527ac41f52c658e3a508495d4ae80b00
Jake Davies <jakedves@gmail.com>
2025-09-19 10:40:11 +0100

    introduce mesh (shard) dialect and `mesh.mesh` op

---
+++ xdsl/dialects/mesh.py
@@ -0,0 +1,37 @@
+from xdsl.dialects.builtin import (
+    SymbolNameConstraint,
+    I64,
+    DenseArrayBase,
+)
+from xdsl.dialects.utils.dimension_list import DimensionList
+from xdsl.ir import Dialect
+from xdsl.irdl import (
+    IRDLOperation,
+    irdl_op_definition,
+    prop_def,
+    traits_def,
+)
+from xdsl.traits import SymbolOpInterface
+
+
+@irdl_op_definition
+class MeshOp(IRDLOperation):
+    name = "mesh.mesh"
+
+    sym_name = prop_def(SymbolNameConstraint())
+    shape = prop_def(DenseArrayBase[I64])
+
+    traits = traits_def(SymbolOpInterface())
+
+    assembly_format = "$sym_name `(` `shape` `=` custom<DimensionList>($shape) `)` attr-dict"
+
+    custom_directives = (DimensionList,)
+
+
+Mesh = Dialect(
+    "mesh",
+    [
+        MeshOp,
+    ],
+    [],
+)
