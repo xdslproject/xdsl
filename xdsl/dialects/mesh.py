8d46b5389f22fcd0f6eb84a653b7ba6d61b6ddd7
Jake <jakedves@gmail.com>
2025-10-01 15:30:02 +0100

    dialects: (mesh) Implement `mesh.sharding`  (#5292)
    `mesh.sharding` is the main operation used in the mesh dialect.
    Implemented with xDSL roundtrip, MLIR conversion, and simple verify test
    cases.
    
    Couldn't get the parsing to infer `i16` from untyped array syntax but
    don't think the workaround is too horrible for the syntax it wanted
    parsed/printed.

--- xdsl/dialects/mesh.py
+++ xdsl/dialects/mesh.py
@@ -1,4 +1,5 @@
 from collections.abc import Sequence
+from enum import auto
 from typing import TypeAlias
 
 from xdsl.dialects.builtin import (
@@ -7,27 +8,37 @@ from xdsl.dialects.builtin import (
     ArrayAttr,
     BytesAttr,
     DenseArrayBase,
+    FlatSymbolRefAttr,
     SymbolNameConstraint,
     i16,
+    i64,
 )
-from xdsl.dialects.utils.dimension_list import DimensionList
+from xdsl.dialects.utils import DimensionList, DynamicIndexList
 from xdsl.ir import (
     Attribute,
     Dialect,
+    EnumAttribute,
     OpaqueSyntaxAttribute,
     ParametrizedAttribute,
+    SpacedOpaqueSyntaxAttribute,
+    TypeAttribute,
     VerifyException,
 )
 from xdsl.irdl import (
+    AttrSizedOperandSegments,
     IRDLOperation,
     irdl_attr_definition,
     irdl_op_definition,
+    opt_prop_def,
     prop_def,
+    result_def,
     traits_def,
+    var_operand_def,
 )
 from xdsl.parser import AttrParser
 from xdsl.printer import Printer
-from xdsl.traits import SymbolOpInterface
+from xdsl.traits import Pure, SymbolOpInterface
+from xdsl.utils.str_enum import StrEnum
 
 MeshAxis: TypeAlias = I16
 """
@@ -106,6 +117,32 @@ class MeshAxesArrayAttr(ParametrizedAttribute, OpaqueSyntaxAttribute):
             )
 
 
+class ReductionKind(StrEnum):
+    "Reduction kind for mesh dialect"
+
+    SUM = auto()
+    MAX = auto()
+    MIN = auto()
+    PRODUCT = auto()
+    AVERAGE = auto()
+    BITWISE_AND = auto()
+    BITWISE_OR = auto()
+    BITWISE_XOR = auto()
+    GENERIC = auto()
+
+
+@irdl_attr_definition
+class ReductionKindAttr(EnumAttribute[ReductionKind], SpacedOpaqueSyntaxAttribute):
+    name = "mesh.partial"
+
+    assembly_format = "$value"
+
+
+@irdl_attr_definition
+class ShardingType(ParametrizedAttribute, TypeAttribute):
+    name = "mesh.sharding"
+
+
 @irdl_op_definition
 class MeshOp(IRDLOperation):
     name = "mesh.mesh"
@@ -128,12 +165,71 @@ class MeshOp(IRDLOperation):
             )
 
 
+@irdl_op_definition
+class ShardingOp(IRDLOperation):
+    """
+    Mesh dialect sharding operation.
+
+    Note: `halo_sizes` and `sharded_dims_offsets` are mutually exlcusive.
+
+    See [external documentation](https://mlir.llvm.org/docs/Dialects/Shard/#shardsharding-shardshardingop)
+    """
+
+    name = "mesh.sharding"
+
+    mesh = prop_def(FlatSymbolRefAttr)
+    split_axes = prop_def(MeshAxesArrayAttr)
+    partial_axes = opt_prop_def(MeshAxesAttr)
+    partial_type = opt_prop_def(ReductionKindAttr)
+    static_sharded_dims_offsets = prop_def(
+        DenseArrayBase[I64], default_value=DenseArrayBase[I64](i64, BytesAttr(b""))
+    )
+    dynamic_sharded_dims_offsets = var_operand_def(I64)
+    static_halo_sizes = prop_def(
+        DenseArrayBase[I64], default_value=DenseArrayBase[I64](i64, BytesAttr(b""))
+    )
+    dynamic_halo_sizes = var_operand_def(I64)
+
+    result = result_def(ShardingType)
+
+    irdl_options = [AttrSizedOperandSegments(as_property=True)]
+
+    traits = traits_def(
+        Pure(),
+    )
+
+    assembly_format = (
+        "$mesh `split_axes` "
+        + "`=` $split_axes (`partial` `=` $partial_type $partial_axes^)? "
+        + "(`halo_sizes` `=` custom<DynamicIndexList>($dynamic_halo_sizes, $static_halo_sizes)^)? "
+        + "(`sharded_dims_offsets` `=` "
+        + "custom<DynamicIndexList>($dynamic_sharded_dims_offsets, $static_sharded_dims_offsets)^)? "
+        + "attr-dict `:` type($result)"
+    )
+
+    custom_directives = (DynamicIndexList,)
+
+    def verify_(self) -> None:
+        dims_offsets = (
+            self.static_sharded_dims_offsets or self.dynamic_sharded_dims_offsets
+        )
+        halo_sizes = self.static_halo_sizes or self.dynamic_halo_sizes
+
+        if dims_offsets and halo_sizes:
+            raise VerifyException(
+                "'mesh.sharding' cannot use both `halo_sizes` and `sharded_dims_offsets`"
+            )
+
+
 Mesh = Dialect(
     "mesh",
     [
         MeshOp,
+        ShardingOp,
     ],
     [
+        ReductionKindAttr,
+        ShardingType,
         MeshAxesArrayAttr,
     ],
 )
