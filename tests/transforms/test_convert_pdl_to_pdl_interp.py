bc03258da63a105a4b1cae1e586e89f0beb262d1
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-10-04 17:45:49 +0200

    test_detect_roots

--- tests/transforms/test_convert_pdl_to_pdl_interp.py
+++ tests/transforms/test_convert_pdl_to_pdl_interp.py
@@ -1,3 +1,7 @@
+from xdsl.builder import ImplicitBuilder
+from xdsl.dialects import pdl
+from xdsl.ir import Block, Region
+from xdsl.transforms.convert_pdl_to_pdl_interp.conversion import PatternAnalyzer
 from xdsl.transforms.convert_pdl_to_pdl_interp.predicate import OperationPosition
 
 
@@ -11,3 +15,20 @@ def test_get_operation_depth():
     ).get_attribute("my_attr")
     assert pos.get_base_operation() is base
     assert pos.get_operation_depth() == 1
+
+
+def test_detect_roots():
+    body = Region([Block()])
+    block = body.first_block
+    with ImplicitBuilder(block):
+        type = pdl.TypeOp().result
+        op1 = pdl.OperationOp("op1", type_values=(type,)).op
+        op2 = pdl.OperationOp("op2").op
+        op1_res = pdl.ResultOp(0, op1).val
+        op3 = pdl.OperationOp("op3", operand_values=(op1_res,)).op
+
+        pdl.RewriteOp(None, name="rewrite")
+
+    pattern = pdl.PatternOp(1, "pattern", body)
+    roots = PatternAnalyzer().detect_roots(pattern)
+    assert roots == [op2, op3]
