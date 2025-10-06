a3564060344897e25c01c5f731aee63e9ef14220
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-10-06 12:47:15 +0200

    transformations: (convert_pdl_to_pdl_interp) PatternAnalyzer `detect_roots` (#5315)
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

---
+++ xdsl/transforms/convert_pdl_to_pdl_interp/conversion.py
@@ -0,0 +1,29 @@
+from xdsl.dialects import pdl
+from xdsl.ir import OpResult
+
+
+class PatternAnalyzer:
+    """Analyzes PDL patterns and extracts predicates"""
+
+    def detect_roots(self, pattern: pdl.PatternOp) -> list[OpResult[pdl.OperationType]]:
+        """Detect root operations in a pattern"""
+        used = {
+            operand.owner.parent_
+            for operation_op in pattern.body.ops
+            if isinstance(operation_op, pdl.OperationOp)
+            for operand in operation_op.operand_values
+            if isinstance(operand.owner, pdl.ResultOp | pdl.ResultsOp)
+        }
+
+        rewriter = pattern.body.block.last_op
+        assert isinstance(rewriter, pdl.RewriteOp)
+        if rewriter.root is not None:
+            if rewriter.root in used:
+                used.remove(rewriter.root)
+
+        roots = [
+            op.op
+            for op in pattern.body.ops
+            if isinstance(op, pdl.OperationOp) and op.op not in used
+        ]
+        return roots
