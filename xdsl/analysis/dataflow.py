1f5fc7780619c73d010ccf74f3ea97977faa4f4d
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-10-13 14:02:16 +0200

    ChangeResult

---
+++ xdsl/analysis/dataflow.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from enum import Enum
+
+
+class ChangeResult(Enum):
+    """
+    A result type used to indicate if a change happened.
+    """
+
+    NO_CHANGE = 0
+    CHANGE = 1
+
+    def __or__(self, other: ChangeResult) -> ChangeResult:
+        return ChangeResult.CHANGE if self == ChangeResult.CHANGE else other
