4286045150743e5d1fdcd61f39b4d0ba50ebbe0e
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-11-20 16:19:18 +0100

    after review

--- xdsl/backend/x86/verify_register_allocation.py
+++ xdsl/backend/x86/verify_register_allocation.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+from xdsl.backend.register_allocatable import HasRegisterConstraints
 from xdsl.context import Context
 from xdsl.dialects import builtin, x86_func
 from xdsl.ir import SSAValue
@@ -14,15 +15,23 @@ class X86VerifyRegAlloc(ModulePass):
     def _process_function(self, func: x86_func.FuncOp) -> None:
         alive: set[SSAValue] = set()
         for op in reversed(func.body.ops):
-            for r in op.results:
-                if r in alive:
-                    alive.remove(r)
-            for o in op.operands:
-                if o.type in op.result_types and o in alive:
-                    raise DiagnosticException(
-                        f"{o.name_hint} should not be read after in/out usage"
-                    )
-                alive.add(o)
+            if isinstance(op, HasRegisterConstraints):
+                ins, outs, inouts = op.get_register_constraints()
+                ins = list(ins)
+                outs = list(outs)
+                for inout in inouts:
+                    in_reg, out_reg = inout[0], inout[1]
+                    if in_reg in alive:
+                        raise DiagnosticException(
+                            f"{in_reg.name_hint} should not be read after in/out usage"
+                        )
+                    ins.append(in_reg)
+                    outs.append(out_reg)
+                for o in outs:
+                    if o in alive:
+                        alive.remove(o)
+                for i in ins:
+                    alive.add(i)
 
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
         for func in op.walk():
