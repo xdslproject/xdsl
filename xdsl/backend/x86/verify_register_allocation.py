bafa58ed7e40616f170be1b66933b7e63db54135
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-11-20 14:47:45 +0100

    in/out pass

--- xdsl/backend/x86/verify_register_allocation.py
+++ xdsl/backend/x86/verify_register_allocation.py
@@ -2,7 +2,9 @@ from dataclasses import dataclass
 
 from xdsl.context import Context
 from xdsl.dialects import builtin, x86_func
+from xdsl.ir import SSAValue
 from xdsl.passes import ModulePass
+from xdsl.utils.exceptions import DiagnosticException
 
 
 @dataclass(frozen=True)
@@ -10,7 +12,17 @@ class X86VerifyRegAlloc(ModulePass):
     name = "x86-verify-register-allocation"
 
     def _process_function(self, func: x86_func.FuncOp) -> None:
-        pass
+        alive: set[SSAValue] = set()
+        for op in reversed(func.body.ops):
+            for r in op.results:
+                if r in alive:
+                    alive.remove(r)
+            for o in op.operands:
+                if o.type in op.result_types and o in alive:
+                    raise DiagnosticException(
+                        f"{o.name_hint} should not be read after in/out usage"
+                    )
+                alive.add(o)
 
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
         for func in op.walk():
