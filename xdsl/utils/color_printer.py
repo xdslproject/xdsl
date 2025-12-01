25164685e7eed6b8ae5a6eb0ec893632aebc6609
Alex Rice <alexrice999@hotmail.co.uk>
2025-12-01 11:09:17 +0000

    Add docstring

--- xdsl/utils/color_printer.py
+++ xdsl/utils/color_printer.py
@@ -9,12 +9,28 @@ from xdsl.utils.colors import RESET, Colors
 class ColorPrinter(BasePrinter):
     """
     A printer for printing colored text to a terminal window.
+    Colors are set by calling the `colored` context manager.
     """
 
     _in_color_block: bool = field(default=False, init=False)
 
     @contextmanager
     def colored(self, color: Colors | None):
+        """
+        No-op if given color is None or the printer is already in a color block.
+        Otherwise, anything printed in this context manager will be printed in
+        the given color.
+
+        When nesting this context manager, the color is determined by the outermost
+        call. For example:
+        ```python
+        with printer.colored(Colors.BLUE):
+            with printer.colored(Colors.RED):
+                printer.print_string("test")
+        ```
+        will print "test" in blue, and not red.
+        """
+
         if self._in_color_block or color is None:
             yield
         else:
