724c387d0eb3aa53c1bfe92080c8d98f92e25bfd
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-12-13 11:35:28 -0600

    change block check
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

--- xdsl/transforms/canonicalization_patterns/scf.py
+++ xdsl/transforms/canonicalization_patterns/scf.py
@@ -85,7 +85,7 @@ class SingleBlockExecuteInliner(RewritePattern):
         self, op: scf.ExecuteRegionOp, rewriter: PatternRewriter
     ) -> None:
         assert op.region.first_block is not None
-        if op.region.first_block.next_block is not None:
+        if op.region.first_block is not op.region.last_block:
             return
         replace_op_with_region(rewriter, op, op.region)
 
