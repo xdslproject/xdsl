0307bfe3825e1a1a87dccda7211666cfad9dd68a
sueszli <jabaryyahya@gmail.com>
2025-12-27 02:15:43 +0100

    (backend) add llvmlite boilerplate

---
+++ tests/backend/llvmlite/test_llvmlite_convert.py
@@ -0,0 +1,24 @@
+import llvmlite.ir as ir
+import pytest
+
+from xdsl.backend.llvmlite.convert import convert_module
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.dialects.test import TestOp
+
+
+def test_convert_empty_module():
+    module = ModuleOp([])
+    llvm_module = convert_module(module)
+    assert isinstance(llvm_module, ir.Module)
+    assert isinstance(str(llvm_module), str)
+    assert '; ModuleID = ""' in str(llvm_module)
+
+
+def test_convert_module_with_op_raises():
+    op = TestOp()
+    module = ModuleOp([op])
+
+    with pytest.raises(
+        NotImplementedError, match="Conversion not implemented for op: test.op"
+    ):
+        convert_module(module)
---
+++ xdsl/backend/llvmlite/convert.py
@@ -0,0 +1,16 @@
+import llvmlite.ir as ir
+
+from xdsl.dialects.builtin import ModuleOp
+
+
+def convert_module(module: ModuleOp) -> ir.Module:
+    # TODO: map xDSL llvm dialect to llvmlite IR
+    # see: https://llvmlite.readthedocs.io/en/latest/user-guide/ir/index.html
+    # see: https://gist.github.com/sueszli/6aea9f54e7305e9e7dfdb31fd999b68e
+
+    llvm_module = ir.Module()
+
+    for op in module.ops:
+        raise NotImplementedError(f"Conversion not implemented for op: {op.name}")
+
+    return llvm_module
