ffca9551b9a577431f26441095d15daa50552758
Yahya Jabary <61852663+sueszli@users.noreply.github.com>
2025-12-29 08:15:55 +0100

    backend: (llvm) add numba/llvmlite boilerplate (#5516)
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>

---
+++ tests/backend/llvm/test_convert.py
@@ -0,0 +1,24 @@
+import pytest
+
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.dialects.test import TestOp
+
+ir = pytest.importorskip("llvmlite.ir")
+from xdsl.backend.llvm.convert import convert_module  # noqa: E402
+
+
+def test_convert_empty_module():
+    module = ModuleOp([])
+    llvm_module = convert_module(module)
+    assert isinstance(llvm_module, ir.Module)
+    assert llvm_module.name == ""
+
+
+def test_convert_module_with_op_raises():
+    op = TestOp()
+    module = ModuleOp([op])
+
+    with pytest.raises(
+        NotImplementedError, match="Conversion not implemented for op: test.op"
+    ):
+        convert_module(module)
