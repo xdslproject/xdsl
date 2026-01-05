d83e070b9fdb9f5ea7f7a94b6a03a2c02b517650
Sasha Lopoukhine <superlopuh@gmail.com>
2026-01-05 10:32:43 +0100

    testing: use unittest.mock instead of manual override for pyclip_copy

--- xdsl/interactive/_pasteboard.py
+++ xdsl/interactive/_pasteboard.py
@@ -3,18 +3,8 @@ We use pyclip to copy text to the local pasteboard, but pyclip does not have typ
 annotations. We add our own mirror of the copy function here.
 """
 
-from collections.abc import Callable
-
-_test_pyclip_callback: Callable[[str], None] | None = None
-"""Used in tests."""
-
 
 def pyclip_copy(text: str) -> None:
-    global _test_pyclip_callback
-    if _test_pyclip_callback is not None:
-        _test_pyclip_callback(text)
-        return
-
     import pyclip  # pyright: ignore[reportMissingTypeStubs]
 
     pyclip.copy(text)  # pyright: ignore[reportUnknownMemberType]
