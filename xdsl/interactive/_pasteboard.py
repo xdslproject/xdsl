4f04e601e7ac216a80b31a906f567e10c585ea00
sueszli <jabaryyahya@gmail.com>
2026-01-07 03:36:39 +0100

    generate pyclip stubs

--- xdsl/interactive/_pasteboard.py
+++ xdsl/interactive/_pasteboard.py
@@ -3,8 +3,8 @@ We use pyclip to copy text to the local pasteboard, but pyclip does not have typ
 annotations. We add our own mirror of the copy function here.
 """
 
+import pyclip
+
 
 def pyclip_copy(text: str) -> None:
-    import pyclip  # pyright: ignore[reportMissingTypeStubs]
-
     pyclip.copy(text)  # pyright: ignore[reportUnknownMemberType]
