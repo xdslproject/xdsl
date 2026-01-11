deffc5cdefcaf46484b43256a7a42e0499738c73
Timo Nicolai <timo.nicolai94@gmail.com>
2026-01-11 14:07:14 +0100

    testing: remove dummy return value in pdl test (#5544)
    Remove the dummy return type in tests/filecheck/frontend/pypdl/pdl.py that
    seems to no longer be require (although it is unclear what commit fixed this).
    
    Note that a bare return is still required, otherwise the test will fail.

--- tests/filecheck/frontend/pypdl/pdl.py
+++ tests/filecheck/frontend/pypdl/pdl.py
@@ -42,10 +42,9 @@ ctx.register_function(erase_op, pdl.EraseOp)
 
 
 @ctx.parse_program
-def constant_replace(matched_operation: arith.ConstantOp) -> arith.ConstantOp:
+def constant_replace(matched_operation: arith.ConstantOp):
     erase_op(matched_operation)
-    # NOTE: A value is returned due to limitations of PyAST's current function implementation
-    return matched_operation
+    return
 
 
 # Check that the DSL correctly rewrites on the xDSL data structures
