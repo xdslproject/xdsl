391b70a7f9d211f90aad8b95cb2645cdbdcdc11b
sueszli <jabaryyahya@gmail.com>
2026-01-15 22:50:11 +0100

    backend: (llvm) convert ret op

--- tests/filecheck/backend/llvm/convert_op.mlir
+++ tests/filecheck/backend/llvm/convert_op.mlir
@@ -2,17 +2,35 @@
 
 builtin.module {
   "llvm.func"() <{
-    sym_name = "empty",
+    sym_name = "return_void",
     function_type = !llvm.func<void ()>,
     CConv = #llvm.cconv<ccc>,
     linkage = #llvm.linkage<external>,
     visibility_ = 0 : i64
   }> ({
+    "llvm.return"() : () -> ()
   }) : () -> ()
+
+  // CHECK: define void @"return_void"()
+  // CHECK-NEXT: {
+  // CHECK-NEXT: entry:
+  // CHECK-NEXT:   ret void
+  // CHECK-NEXT: }
+
+  "llvm.func"() <{
+    sym_name = "return_arg",
+    function_type = !llvm.func<i32 (i32)>,
+    CConv = #llvm.cconv<ccc>,
+    linkage = #llvm.linkage<external>,
+    visibility_ = 0 : i64
+  }> ({
+  ^bb0(%arg0 : i32):
+    "llvm.return"(%arg0) : (i32) -> ()
+  }) : () -> ()
+
+  // CHECK: define i32 @"return_arg"(i32 %".1")
+  // CHECK-NEXT: {
+  // CHECK-NEXT: entry:
+  // CHECK-NEXT:   ret i32 %".1"
+  // CHECK-NEXT: }
 }
-// CHECK: ; ModuleID = ""
-// Target triple is architecture dependent.
-// CHECK-NEXT: target triple = "{{[a-zA-Z0-9_.-]+}}"
-// CHECK-NEXT: target datalayout = ""
-// CHECK-EMPTY:
-// CHECK-NEXT: declare void @"empty"()
---
+++ xdsl/backend/llvm/convert_op.py
@@ -0,0 +1,41 @@
+from llvmlite import ir
+
+from xdsl.dialects import llvm
+from xdsl.ir import Operation, SSAValue
+
+
+def _convert_return(
+    op: Operation, builder: ir.IRBuilder, val_map: dict[SSAValue, ir.Value]
+):
+    if op.operands:
+        builder.ret(val_map[op.operands[0]])
+    else:
+        builder.ret_void()
+
+
+def convert_op(
+    op: Operation,
+    builder: ir.IRBuilder,
+    val_map: dict[SSAValue, ir.Value],
+):
+    """
+    Convert an xDSL operation to an llvmlite LLVM IR.
+
+    Side effects:
+        Mutates val_map by adding entries for the operation's results.
+
+    Args:
+        op: The xDSL operation to convert
+        builder: The LLVM IR builder for constructing instructions
+        val_map: The Mapping from xDSL SSA values to LLVM IR values.
+                 This dictionary is mutated to store the LLVM IR value produced by this operation for
+                 use by subsequent operations.
+
+    Raises:
+        NotImplementedError: If the operation is not supported.
+    """
+    match op:
+        case llvm.ReturnOp() | _ if op.name == "llvm.return":
+            _convert_return(op, builder, val_map)
+        case _:
+            raise NotImplementedError(f"Conversion not implemented for op: {op.name}")
