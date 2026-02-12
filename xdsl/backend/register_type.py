e5e3cd6d014f3d46961a6b2f47c634e69f570245
Sasha Lopoukhine <superlopuh@gmail.com>
2026-02-12 17:50:35 +0000

    backend: raise error for infinite register clash during class definition

--- xdsl/backend/register_type.py
+++ xdsl/backend/register_type.py
@@ -27,10 +27,41 @@ from xdsl.utils.exceptions import VerifyException
 class RegisterType(ParametrizedAttribute, TypeAttribute, ABC):
     """
     An abstract register type for target ISA-specific dialects.
+
+    Registers have a name, as used in assembly, and an index as used in the binary
+    encoding.
+
+    Some approaches for register allocation have stages where values are assigned to a
+    fixed set of registers that is distinct from the registers that exist on a target
+    platform, to separate the graph coloring from roles of registers in the target ABI.
+    In orger to support this scenario, negative indices are allowed in the index,
+    denoting an infinite register set without any representation in the ABI.
+    These are printed with a prefix as defined by the `infinite_register_prefix`
+    class method, which must not be a prefix of any of the register names defined by
+    the `index_by_name` class method.
     """
 
     index: IntAttr | NoneAttr
     register_name: StringAttr
+
+    def __init_subclass__(cls) -> None:
+        # Detect register names clashing with the infinite register prefix
+        try:
+            prefix = cls.infinite_register_prefix()
+            names = cls.index_by_name()
+        except NotImplementedError:
+            # Skip for abstract subclasses
+            return
+
+        clashing_register_names = tuple(
+            register_name for register_name in names if register_name.startswith(prefix)
+        )
+
+        if clashing_register_names:
+            raise ValueError(
+                f"Infinite register prefix '{prefix}' clashes with register names "
+                f"{list(clashing_register_names)}."
+            )
 
     @classmethod
     def unallocated(cls) -> Self:
