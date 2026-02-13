43740bb345ce624cd4ef4a9d83a8a9cf032251bc
osmanyasar05 <osmanyas05@gmail.com>
2026-02-13 17:11:48 +0000

    add tests rv32/64

---
+++ tests/backend/riscv/test_preallocated copy.py
@@ -0,0 +1,72 @@
+from xdsl.backend.register_allocatable import RegisterAllocatableOperation
+from xdsl.builder import Builder
+from xdsl.dialects import riscv, riscv_func, rv32
+from xdsl.dialects.builtin import SymbolRefAttr
+
+
+def test_gather_allocated():
+    @Builder.implicit_region
+    def no_preallocated_body() -> None:
+        v1 = rv32.GetRegisterOp(riscv.Registers.UNALLOCATED_INT).res
+        v2 = rv32.GetRegisterOp(riscv.Registers.UNALLOCATED_INT).res
+        _ = riscv.AddOp(v1, v2).rd
+
+    pa_regs = set(
+        RegisterAllocatableOperation.iter_all_used_registers(no_preallocated_body)
+    )
+
+    assert pa_regs == set()
+
+    @Builder.implicit_region
+    def one_preallocated_body() -> None:
+        reg1 = riscv.Registers.UNALLOCATED_INT
+        v1 = rv32.GetRegisterOp(reg1).res
+        v2 = rv32.GetRegisterOp(riscv.Registers.A7).res
+        _ = riscv.AddOp(v1, v2).rd
+
+    pa_regs = set(
+        RegisterAllocatableOperation.iter_all_used_registers(one_preallocated_body)
+    )
+
+    assert pa_regs == {riscv.Registers.A7}
+
+    @Builder.implicit_region
+    def repeated_preallocated_body() -> None:
+        v1 = rv32.GetRegisterOp(riscv.Registers.UNALLOCATED_INT).res
+        v2 = rv32.GetRegisterOp(riscv.Registers.A7).res
+        sum1 = riscv.AddOp(v1, v2).rd
+        _ = riscv.AddiOp(sum1, 1, rd=riscv.Registers.A7).rd
+
+    pa_regs = set(
+        RegisterAllocatableOperation.iter_all_used_registers(repeated_preallocated_body)
+    )
+
+    assert pa_regs == {riscv.Registers.A7}
+
+    @Builder.implicit_region
+    def multiple_preallocated_body() -> None:
+        v1 = rv32.GetRegisterOp(riscv.Registers.UNALLOCATED_INT).res
+        v2 = rv32.GetRegisterOp(riscv.Registers.A7).res
+        sum1 = riscv.AddOp(v1, v2).rd
+        _ = riscv.AddiOp(sum1, 1, rd=riscv.Registers.A6).rd
+
+    pa_regs = set(
+        RegisterAllocatableOperation.iter_all_used_registers(multiple_preallocated_body)
+    )
+
+    assert pa_regs == {riscv.Registers.A6, riscv.Registers.A7}
+
+    @Builder.implicit_region
+    def func_call_preallocated_body() -> None:
+        v1 = rv32.GetRegisterOp(riscv.Registers.UNALLOCATED_INT).res
+        v2 = rv32.GetRegisterOp(riscv.Registers.S0).res
+        riscv_func.CallOp(SymbolRefAttr("hello"), (v1, v2), ())
+
+    pa_regs = set(
+        RegisterAllocatableOperation.iter_all_used_registers(
+            func_call_preallocated_body
+        )
+    )
+
+    assert len(pa_regs) == 36
+    assert riscv.Registers.S0 in pa_regs
---
+++ tests/dialects/test_rv32.py
@@ -0,0 +1,44 @@
+import pytest
+
+from xdsl.dialects import riscv, rv32
+from xdsl.dialects.builtin import (
+    IntegerAttr,
+    Signedness,
+)
+from xdsl.traits import ConstantLike
+from xdsl.transforms.canonicalization_patterns.riscv import get_constant_value
+from xdsl.utils.exceptions import VerifyException
+
+
+def test_immediate_pseudo_inst():
+    lb, ub = Signedness.SIGNLESS.value_range(32)
+    assert ub == 4294967296
+    assert lb == -2147483648
+
+    # Pseudo-Instruction with custom handling
+    with pytest.raises(VerifyException):
+        rv32.LiOp(ub, rd=riscv.Registers.A0)
+
+    with pytest.raises(VerifyException):
+        rv32.LiOp(lb - 1, rd=riscv.Registers.A0)
+
+    rv32.LiOp(ub - 1, rd=riscv.Registers.A0)
+    rv32.LiOp(lb, rd=riscv.Registers.A0)
+
+
+def test_get_constant_value():
+    # Test 32-bit LiOp
+    li_op = rv32.LiOp(1)
+    li_val = get_constant_value(li_op.rd)
+    assert li_val == IntegerAttr.from_int_and_width(1, 32)
+    # LiOp implements ConstantLikeInterface so it also has a get_constant_value method:
+    constantlike = li_op.get_trait(ConstantLike)
+    assert constantlike is not None
+    assert constantlike.get_constant_value(li_op) == IntegerAttr.from_int_and_width(
+        1, 32
+    )
+
+    # Test 32-bit zero register
+    zero_op = rv32.GetRegisterOp(riscv.Registers.ZERO)
+    zero_val = get_constant_value(zero_op.res)
+    assert zero_val == IntegerAttr.from_int_and_width(0, 32)
