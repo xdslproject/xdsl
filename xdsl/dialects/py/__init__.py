6975251b41a7ec4378e96a422ba951f095c08d09
Ken Jin <kenjin4096@gmail.com>
2026-02-16 18:34:21 +0000

    Add warning message

--- xdsl/dialects/py/__init__.py
+++ xdsl/dialects/py/__init__.py
@@ -19,6 +19,8 @@ This module contains the definition of the Python semantics dialect.
 We only guarantee preservation of most[1] Python semantics, but do not guarantee
 preservation of AST or bytecode.
 
+The Python dialect is under heavy development, and the definitions are not yet stable!
+
 [1]: Assumptions:
         1. We assume no exceptions are raised in the code.
 """
