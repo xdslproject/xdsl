333cfd94067a0204aa2bfdae44a235d2b61d2d95
Ken Jin <kenjin4096@gmail.com>
2026-02-18 16:07:13 +0000

    Remove deprecated IntegerAttr use

--- tests/dialects/test_py.py
+++ tests/dialects/test_py.py
@@ -1,10 +1,10 @@
 import xdsl.dialects.py as py
-from xdsl.dialects.builtin import IntegerAttr, StringAttr
+from xdsl.dialects.builtin import IntegerAttr, StringAttr, i64
 
 
 def test_custom_inits():
-    c1 = py.PyConstOp(IntegerAttr.from_int_and_width(1, 64))
-    c2 = py.PyConstOp(IntegerAttr.from_int_and_width(2, 64))
+    c1 = py.PyConstOp(IntegerAttr(1, i64))
+    c2 = py.PyConstOp(IntegerAttr(2, i64))
     b1 = py.PyBinOp(op=StringAttr("add"), lhs=c1.results[0], rhs=c2.results[0])
     assert b1.operands[0] is c1.results[0]
     assert b1.operands[1] is c2.results[0]
