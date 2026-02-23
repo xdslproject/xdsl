ddc6baba32e0107ac0bf2fc3d472abffe0781b36
RunoBoy <dyselinck.run@gmail.com>
2026-02-23 14:03:18 +0100

    Create PDL_Region with the new Region Type

---
+++ xdsl/dialects/pdl_region.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from xdsl.dialects.pdl import TypeType, ValueType
+from xdsl.ir import (
+    Dialect,
+    ParametrizedAttribute,
+    TypeAttribute,
+)
+from xdsl.irdl import (
+    IRDLOperation,
+    irdl_attr_definition,
+    irdl_op_definition,
+    opt_operand_def,
+    result_def,
+)
+
+
+@irdl_op_definition
+class RegionOp(IRDLOperation):
+    name = "pdl.region"
+    value_type = opt_operand_def(TypeType)
+    value = result_def(ValueType)
+
+    assembly_format = "(`:` $value_type^)? attr-dict"
+
+
+@irdl_attr_definition
+class RegionType(ParametrizedAttribute, TypeAttribute):
+    name = "pdl.region"
+
+
+PDL_Region = Dialect(
+    "pdl_region",
+    [
+        RegionOp
+    ],
+    [
+        RegionType
+    ]
+)
