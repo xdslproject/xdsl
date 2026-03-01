4077f731ceacf146fc095ce594ef88b63801a973
jumerckx <31353884+jumerckx@users.noreply.github.com>
2026-03-01 10:54:10 +0100

    ematch: add run_get_class_representative

--- xdsl/interpreters/ematch.py
+++ xdsl/interpreters/ematch.py
@@ -4,6 +4,7 @@ from typing import Any
 from xdsl.dialects import ematch, equivalence
 from xdsl.interpreter import Interpreter, InterpreterFunctions, impl, register_impls
 from xdsl.ir import OpResult, SSAValue
+from xdsl.utils.hints import isa
 
 
 @register_impls
@@ -39,3 +40,31 @@ class EmatchFunctions(InterpreterFunctions):
 
         # Value is not an eclass result, return it as a single-element tuple
         return ((val,),)
+
+    @impl(ematch.GetClassRepresentativeOp)
+    def run_get_class_representative(
+        self,
+        interpreter: Interpreter,
+        op: ematch.GetClassRepresentativeOp,
+        args: tuple[Any, ...],
+    ) -> tuple[Any, ...]:
+        """
+        Get one of the values in the equivalence class of v.
+        Returns the first operand of the equivalence class.
+        """
+        assert len(args) == 1
+        val = args[0]
+
+        if val is None:
+            return (val,)
+
+        assert isa(val, SSAValue)
+
+        if isinstance(val, OpResult):
+            defining_op = val.owner
+            if isinstance(defining_op, equivalence.AnyClassOp):
+                leader = self.eclass_union_find.find(defining_op)
+                return (leader.operands[0],)
+
+        # Value is not an eclass result, return it as-is
+        return (val,)
