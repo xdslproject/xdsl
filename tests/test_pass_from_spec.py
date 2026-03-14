7e4df5cd66826c185f56cbbd968b9f9e21255e5d
Sasha Lopoukhine <superlopuh@gmail.com>
2026-03-14 18:28:06 +0000

    core: factor out arg spec logic from ModulePass (#5722)

--- tests/test_pass_from_spec.py
+++ tests/test_pass_from_spec.py
@@ -82,11 +82,14 @@ def test_pass_instantiation():
 @pytest.mark.parametrize(
     "spec, error_msg",
     [
-        (ArgSpec("wrong", {"a": (1,)}), "Cannot create Pass simple"),
+        (
+            ArgSpec("wrong", {"a": (1,)}),
+            "Spec name mismatch: got 'wrong', expected 'simple'.",
+        ),
         (ArgSpec("simple", {}), 'requires argument "a"'),
         (
             ArgSpec("simple", {"a": (1,), "no": ()}),
-            'Provided arguments ["no"] not found in expected pass arguments ["a", "b"]',
+            'Provided arguments ["no"] not found in expected arguments ["a", "b"]',
         ),
         (ArgSpec("simple", {"a": ()}), "Argument must contain a value"),
         (ArgSpec("simple", {"a": ("test",)}), "Incompatible types"),
@@ -100,7 +103,7 @@ def test_pass_instantiation_error(spec: ArgSpec, error_msg: str):
     """
     Test all possible failure modes in pass instantiation
     """
-    with pytest.raises(Exception, match=re.escape(error_msg)):
+    with pytest.raises(ValueError, match=re.escape(error_msg)):
         SimplePass.from_pass_spec(spec)
 
 
