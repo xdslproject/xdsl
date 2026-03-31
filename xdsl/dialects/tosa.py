3f3a48c7344becc00b1422cc62959b5493caead5
Ivan Ho <38537881+hhkit@users.noreply.github.com>
2026-03-31 19:24:01 +0100

    PR suggestions

--- xdsl/dialects/tosa.py
+++ xdsl/dialects/tosa.py
@@ -92,6 +92,7 @@ def are_tosa_broadcastable(lhs: Attribute, rhs: Attribute, out: Attribute):
 class RoundingMode(StrEnum):
     """
     Rounding mode for `tosa.rescale`
+    See external [documentation](https://github.com/llvm/llvm-project/blob/fef02d48c08db859ef83f84232ed78bd9d1c323a/mlir/include/mlir/Dialect/Tosa/IR/TosaOpBase.td#L470).
     """
 
     SINGLE_ROUND = "SINGLE_ROUND"
@@ -116,6 +117,7 @@ class RoundingModeAttr(EnumAttribute[RoundingMode]):
 class NanMode(StrEnum):
     """
     Supported NaN propagation strategies
+    See external [documentation](https://github.com/llvm/llvm-project/blob/fef02d48c08db859ef83f84232ed78bd9d1c323a/mlir/include/mlir/Dialect/Tosa/IR/TosaOpBase.td#L462).
     """
 
     PROPAGATE = "PROPAGATE"
