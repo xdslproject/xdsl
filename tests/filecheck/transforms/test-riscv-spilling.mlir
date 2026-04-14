58c057c17fc9f4310c81bb56aa9dda6fd53f2c7d
Aidan Wong <anominosgamer@gmail.com>
2026-04-14 11:57:51 +0100

    use riscv stack instead

--- tests/filecheck/transforms/test-riscv-spilling.mlir
+++ tests/filecheck/transforms/test-riscv-spilling.mlir
@@ -1,4 +1,4 @@
-// RUN: xdsl-opt --split-input-file -p test-riscv-spilling %s | filecheck %s
+// RUN: xdsl-opt --split-input-file -p test-riscv-spilling,convert-riscv-stack-to-riscv %s | filecheck %s
 builtin.module {
   riscv_func.func @main() {
     %0, %1, %2 = "test.op"() : () -> (!riscv.reg, !riscv.reg, !riscv.reg)
@@ -11,7 +11,7 @@ builtin.module {
 // CHECK:       builtin.module {
 // CHECK-NEXT:    riscv_func.func @main() {
 // CHECK-NEXT:      %0 = rv32.get_register : !riscv.reg<sp>
-// CHECK-NEXT:      %1 = riscv.addi %0, -16 : (!riscv.reg<sp>) -> !riscv.reg<sp>
+// CHECK-NEXT:      %1 = riscv.addi %0, -12 : (!riscv.reg<sp>) -> !riscv.reg<sp>
 // CHECK-NEXT:      %2, %3, %4 = "test.op"() : () -> (!riscv.reg, !riscv.reg, !riscv.reg)
 // CHECK-NEXT:      riscv.sw %0, %4, 0 : (!riscv.reg<sp>, !riscv.reg) -> ()
 // CHECK-NEXT:      %5 = riscv.add %2, %3 : (!riscv.reg, !riscv.reg) -> !riscv.reg
@@ -21,7 +21,7 @@ builtin.module {
 // CHECK-NEXT:      riscv.sw %0, %6, 8 : (!riscv.reg<sp>, !riscv.reg) -> ()
 // CHECK-NEXT:      %8 = riscv.lw %0, 4 : (!riscv.reg<sp>) -> !riscv.reg
 // CHECK-NEXT:      %9 = riscv.add %8, %3 : (!riscv.reg, !riscv.reg) -> !riscv.reg
-// CHECK-NEXT:      %10 = riscv.addi %0, 16 : (!riscv.reg<sp>) -> !riscv.reg<sp>
+// CHECK-NEXT:      %10 = riscv.addi %0, 12: (!riscv.reg<sp>) -> !riscv.reg<sp>
 // CHECK-NEXT:      riscv_func.return
 // CHECK-NEXT:    }
 // CHECK-NEXT:  }
--- xdsl/transforms/test_riscv_spilling.py
+++ xdsl/transforms/test_riscv_spilling.py
@@ -4,46 +4,14 @@ from itertools import islice
 from ordered_set import OrderedSet
 
 from xdsl.backend.riscv.register_stack import RiscvRegisterStack
+from xdsl.builder import Builder
 from xdsl.context import Context
-from xdsl.dialects import builtin, riscv_func
-from xdsl.dialects.riscv.ops import AddiOp, LwOp, SwOp
+from xdsl.dialects import builtin, riscv, riscv_func
 from xdsl.dialects.riscv.registers import IntRegisterType, Registers
-from xdsl.dialects.rv32 import GetRegisterOp
-from xdsl.ir import Operation, ParametrizedAttribute, SSAValue
-from xdsl.irdl import (
-    IRDLOperation,
-    irdl_attr_definition,
-    irdl_op_definition,
-    operand_def,
-    result_def,
-)
+from xdsl.ir import Operation, SSAValue
 from xdsl.passes import ModulePass
-from xdsl.rewriter import InsertPoint, Rewriter
-
-
-@irdl_attr_definition
-class SpillType(ParametrizedAttribute):
-    name = "spilling.type"
-
-
-@irdl_op_definition
-class SpillOp(IRDLOperation):
-    name = "spilling.spill_op"
-    value = operand_def()
-    result = result_def(SpillType())
-
-    def __init__(self, val: SSAValue):
-        super().__init__(operands=[val], result_types=[SpillType()])
-
-
-@irdl_op_definition
-class LoadOp(IRDLOperation):
-    name = "spilling.load_op"
-    value = operand_def(SpillType())
-    result = result_def()
-
-    def __init__(self, val: SSAValue):
-        super().__init__(operands=[val], result_types=[Registers.UNALLOCATED_INT])
+from xdsl.rewriter import InsertPoint
+from xdsl.utils.hints import isa
 
 
 @dataclass(frozen=True)
@@ -100,14 +68,16 @@ class SpillPass(ModulePass):
         if not spills:
             return
 
-        spill_ops = tuple(SpillOp(spill_val) for spill_val in spills)
-        for spill_op in spill_ops:
-            Rewriter.insert_op(spill_op, InsertPoint.before(inner_op))
-        # change all uses after pmov_op to results of pmov
-        for src, spill_op in zip(spills, spill_ops):
-            dst = spill_op.result
-            src.replace_uses_with_if(
-                dst, lambda use: spill_op.is_before_in_block(use.operation)
+        builder = Builder(InsertPoint.before(inner_op))
+        for spill_val in spills:
+            alloca_op = riscv.stack.AllocaOp(builtin.i32)
+            spill_op = riscv.stack.StoreOp(alloca_op, spill_val)
+            builder.insert_op(alloca_op)
+            builder.insert_op(spill_op)
+
+            # replace all uses after spill to the ref
+            spill_val.replace_uses_with_if(
+                alloca_op.ref, lambda use: spill_op.is_before_in_block(use.operation)
             )
 
     def insert_load(
@@ -117,16 +87,22 @@ class SpillPass(ModulePass):
         if not loads:
             return OrderedSet([])
 
-        load_ops = tuple(LoadOp(load_val) for load_val in loads)
-        for load_op in load_ops:
-            Rewriter.insert_op(load_op, InsertPoint.before(inner_op))
-        # change all uses after pmov_op to results of pmov
-        for src, load_op in zip(loads, load_ops):
-            dst = load_op.result
-            src.replace_uses_with_if(
-                dst, lambda use: load_op.is_before_in_block(use.operation)
+        assert isa(loads, OrderedSet[SSAValue[riscv.stack.StackSlotType]])
+
+        load_results = OrderedSet[SSAValue]([])
+        builder = Builder(InsertPoint.before(inner_op))
+
+        for load_val in loads:
+            load_op = riscv.stack.LoadOp(load_val)
+            builder.insert_op(load_op)
+            # replace all uses of the ref after load to the loaded result
+            load_val.replace_uses_with_if(
+                load_op.rd,
+                lambda use: load_op.is_before_in_block(use.operation),
             )
-        return OrderedSet(i.result for i in load_ops)
+            load_results.add(load_op.rd)
+
+        return load_results
 
     def get_die_set(self, func_op: riscv_func.FuncOp):
         """Create set of values that die at each operation."""
@@ -138,61 +114,6 @@ class SpillPass(ModulePass):
             die[inner_op] = uses - seen_vals
             seen_vals |= uses
         return die
-
-
-class ResolveSpillingOps(ModulePass):
-    def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
-        for func_op in op.walk():
-            if not isinstance(func_op, riscv_func.FuncOp):
-                continue
-            spill_ops = tuple(op for op in func_op.walk() if isinstance(op, SpillOp))
-            load_ops = tuple(op for op in func_op.walk() if isinstance(op, LoadOp))
-
-            # Map each spill value with its own stack offset
-            offset_by_value: dict[SSAValue, int] = {}
-            offset = 0
-            for spill_op in spill_ops:
-                offset_by_value[spill_op.result] = offset
-                offset += 4  # assume 32 bit values for now
-
-            # Get stack pointer for Lw/Sw to reference
-            stack_pointer = GetRegisterOp(Registers.SP)
-            Rewriter.insert_op(stack_pointer, InsertPoint.at_start(func_op.body.block))
-
-            # Resolve loads
-            for load_op in load_ops:
-                Rewriter.replace_op(
-                    load_op,
-                    LwOp(stack_pointer, offset_by_value[load_op.value]),
-                )
-
-            # Resolve spills
-            for spill_op in spill_ops:
-                store_op = SwOp(
-                    stack_pointer, spill_op.value, offset_by_value[spill_op.result]
-                )
-                # Use insert/erase since we are dropping the spill_op's result
-                Rewriter.insert_op(store_op, InsertPoint.before(spill_op))
-                Rewriter.erase_op(spill_op)
-
-            # Manage stack pointer
-            # Align stack to 16 bytes
-            stack_size = (offset + 15) & ~15
-            if stack_size > 0:
-                Rewriter.insert_op(
-                    AddiOp(stack_pointer, -stack_size, rd=Registers.SP),
-                    InsertPoint.after(stack_pointer),
-                )
-
-                for block in func_op.body.blocks:
-                    ret_op = block.last_op
-                    if not isinstance(ret_op, riscv_func.ReturnOp):
-                        continue
-
-                    Rewriter.insert_op(
-                        AddiOp(stack_pointer, stack_size, rd=Registers.SP),
-                        InsertPoint.before(ret_op),
-                    )
 
 
 @dataclass(frozen=True)
@@ -208,6 +129,5 @@ class TestRiscvSpillingPass(ModulePass):
             Registers.T2,
         )
         SpillPass().apply(ctx, op)
-        ResolveSpillingOps().apply(ctx, op)
 
         RiscvRegisterStack.DEFAULT_ALLOCATABLE_REGISTERS = defaults
