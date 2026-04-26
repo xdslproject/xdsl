6aa7c474a46c8077d6c38b8a681e6b473fd97bb3
Sasha Lopoukhine <superlopuh@gmail.com>
2026-04-26 13:07:52 +0100

    missing note

--- xdsl/irdl/__init__.py
+++ xdsl/irdl/__init__.py
@@ -1,5 +1,6 @@
 # TID 251 enforces to not import from nested irdl
 # We need to skip it here to allow importing from here instead.
+# If adding banned imports, add them also to pyproject.toml.
 from .attributes import *  # noqa: TID251
 from .constraints import *  # noqa: TID251
 from .operations import *  # noqa: TID251
