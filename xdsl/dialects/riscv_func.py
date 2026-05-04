f81df1a06bb1267a7060f5d52c28a4eaa673b955
Sasha Lopoukhine <superlopuh@gmail.com>
2026-05-04 22:17:10 +0100

    backend: use RegisterAllocatedMemoryEffect for all_used_registers

--- xdsl/dialects/riscv_func.py
+++ xdsl/dialects/riscv_func.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 import itertools
-from collections.abc import Generator, Sequence
+from collections.abc import Sequence
 
 from xdsl.backend.assembly_printer import AssemblyPrintable, AssemblyPrinter
-from xdsl.backend.register_type import RegisterResource, RegisterType
+from xdsl.backend.register_type import RegisterResource
 from xdsl.dialects import riscv
 from xdsl.dialects.builtin import (
     I8,
@@ -168,15 +168,6 @@ class CallOp(riscv.RISCVInstruction):
 
     def assembly_line_args(self) -> tuple[riscv.AssemblyInstructionArg | None, ...]:
         return (self.callee.string_value(),)
-
-    def iter_used_registers(self) -> Generator[RegisterType, None, None]:
-        # These registers are not guaranteed to hold the same values when the callee
-        # returns, according to the RISC-V calling convention.
-        # https://riscv.org/wp-content/uploads/2015/01/riscv-calling.pdf
-        yield from riscv.Registers.A
-        yield from riscv.Registers.T
-        yield from riscv.Registers.FA
-        yield from riscv.Registers.FT
 
 
 class FuncOpCallableInterface(CallableOpInterface):
