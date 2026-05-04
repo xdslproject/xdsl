8562c7d4b66cbb3edf53cd614bf7405a58cda77d
Sasha Lopoukhine <superlopuh@gmail.com>
2026-05-04 10:19:04 +0100

    minor refactor to use helpers

--- tests/dialects/test_dialects.py
+++ tests/dialects/test_dialects.py
@@ -27,13 +27,12 @@ def test_no_memory_effects_exclusive():
 
     for dialect_factory in all_dialects.values():
         for op in dialect_factory().operations:
-            effects_traits = tuple(
-                trait for trait in op.traits.traits if isinstance(trait, MemoryEffect)
-            )
-            if any(isinstance(t, NoMemoryEffect) for t in (effects_traits)):
-                if other_traits := tuple(
+            effects_traits = op.get_traits_of_type(MemoryEffect)
+            if effects_traits and op.has_trait(NoMemoryEffect):
+                other_traits = tuple(
                     trait
                     for trait in effects_traits
                     if not isinstance(trait, NoMemoryEffect)
-                ):
+                )
+                if other_traits:
                     assert not other_traits, op.name
