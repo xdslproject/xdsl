38b1b5b8e67a66e34e4a3850a82a2f253dfaa23c
szerdick <szerdick@student.ethz.ch>
2026-05-06 15:26:15 +0100

    remove scratch files that tested softmax lowering

--- testlowering.sh
+++
@@ -1,7 +0,0 @@
-uv run xdsl-opt -p decompose-softmax testlowering.mlir
-
-
-uv run xdsl-opt -p decompose-softmax,lower-exp-to-polynomial testlowering.mlir
-
-
-uv run xdsl-opt -p decompose-softmax,lower-exp-to-polynomial,expand-polynomial-eval testlowering.mlir
