f1f0141e36717c906e62bac01bbf75cc64aa0739
sueszli <jabaryyahya@gmail.com>
2026-05-14 13:17:48 +0200

    refactor: use negated isinstance check for FastMathAttr

--- xdsl/dialects/llvm.py
+++ xdsl/dialects/llvm.py
@@ -3161,7 +3161,7 @@ class FExp2Op(IRDLOperation):
         arg: Operation | SSAValue,
         fast_math: FastMathAttr | FastMathFlag | None = None,
     ):
-        if isinstance(fast_math, FastMathFlag | str | None):
+        if not isinstance(fast_math, FastMathAttr):
             fast_math = FastMathAttr(fast_math)
         super().__init__(
             operands=[arg],
