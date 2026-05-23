db99334daa6031d3aaa655b8abf7d5680e5cd5bd
Sasha Lopoukhine <superlopuh@gmail.com>
2026-05-23 22:24:36 +0100

    testing: add TestRegisterType to test dialect (#6112)

--- tests/backend/test_register.py
+++ tests/backend/test_register.py
@@ -4,6 +4,7 @@ import pytest
 
 from xdsl.backend.register_type import RegisterType
 from xdsl.dialects.builtin import IntAttr, NoneAttr, StringAttr
+from xdsl.dialects.test import TestRegisterType
 from xdsl.irdl import irdl_attr_definition
 from xdsl.utils.exceptions import VerifyException
 
@@ -29,86 +30,73 @@ def test_register_clashes():
                 return "x"
 
 
-@irdl_attr_definition
-class TestRegister(RegisterType):
-    name = "test.reg"
-
-    @classmethod
-    def index_by_name(cls) -> dict[str, int]:
-        return {"x0": 0, "x1": 1, "a0": 0, "a1": 1}
-
-    @classmethod
-    def infinite_register_prefix(cls):
-        return "y"
-
-
 def test_unallocated_register():
-    assert not TestRegister.unallocated().is_allocated
-    assert TestRegister.from_name("x0").is_allocated
+    assert not TestRegisterType.unallocated().is_allocated
+    assert TestRegisterType.from_name("x0").is_allocated
 
 
 def test_register_from_string():
     # Register with valid ABI name is fine
-    assert TestRegister.from_name("x0").register_name == StringAttr("x0")
-    assert TestRegister.from_name("x0").index == IntAttr(0)
-    assert TestRegister.from_name("x1").register_name == StringAttr("x1")
-    assert TestRegister.from_name("x1").index == IntAttr(1)
+    assert TestRegisterType.from_name("x0").register_name == StringAttr("x0")
+    assert TestRegisterType.from_name("x0").index == IntAttr(0)
+    assert TestRegisterType.from_name("x1").register_name == StringAttr("x1")
+    assert TestRegisterType.from_name("x1").index == IntAttr(1)
 
     # Register with infinite ABI name is fine
-    assert TestRegister.from_name("y0").register_name == StringAttr("y0")
-    assert TestRegister.from_name("y0").index == IntAttr(-1)
-    assert TestRegister.from_name("y1").register_name == StringAttr("y1")
-    assert TestRegister.from_name("y1").index == IntAttr(-2)
+    assert TestRegisterType.from_name("y0").register_name == StringAttr("y0")
+    assert TestRegisterType.from_name("y0").index == IntAttr(-1)
+    assert TestRegisterType.from_name("y1").register_name == StringAttr("y1")
+    assert TestRegisterType.from_name("y1").index == IntAttr(-2)
 
     # Infinite prefix but not a number
     with pytest.raises(
         VerifyException,
         match="Invalid register name yy for register type test.reg",
     ):
-        TestRegister.from_name("yy")
+        TestRegisterType.from_name("yy")
 
     # Incorrect name
     with pytest.raises(
         VerifyException, match="Invalid register name z0 for register type test.reg"
     ):
-        TestRegister.from_name("z0")
+        TestRegisterType.from_name("z0")
 
 
 def test_invalid_register_name():
     with pytest.raises(
         VerifyException, match="Invalid register name foo for register type test.reg."
     ):
-        TestRegister.from_name("foo")
+        TestRegisterType.from_name("foo")
 
 
 def test_invalid_index():
     with pytest.raises(
         AssertionError, match="Infinite index must be positive, got -1."
     ):
-        TestRegister.infinite_register(-1)
+        TestRegisterType.infinite_register(-1)
 
     with pytest.raises(
         VerifyException, match="Invalid index 1 for unallocated register."
     ):
-        TestRegister(IntAttr(1), StringAttr(""))
+        TestRegisterType(IntAttr(1), StringAttr(""))
 
     with pytest.raises(
         VerifyException, match="Missing index for register x1, expected 1."
     ):
-        TestRegister(NoneAttr(), StringAttr("x1"))
+        TestRegisterType(NoneAttr(), StringAttr("x1"))
 
     with pytest.raises(
         VerifyException, match="Invalid index 2 for register x1, expected 1."
     ):
-        TestRegister(IntAttr(2), StringAttr("x1"))
+        TestRegisterType(IntAttr(2), StringAttr("x1"))
 
 
 def test_name_by_index():
-    assert TestRegister.abi_name_by_index() == {0: "a0", 1: "a1"}
+    assert TestRegisterType.abi_name_by_index() == {0: "a0", 1: "a1"}
 
 
 def test_from_index():
-    assert TestRegister.from_index(0) == TestRegister.from_name("a0")
-    assert TestRegister.from_index(1) == TestRegister.from_name("a1")
-    assert TestRegister.from_index(-1) == TestRegister.infinite_register(0)
-    assert TestRegister.from_index(-2) == TestRegister.infinite_register(1)
+    assert TestRegisterType.from_index(0) == TestRegisterType.from_name("a0")
+    assert TestRegisterType.from_index(1) == TestRegisterType.from_name("a1")
+    assert TestRegisterType.from_index(-1) == TestRegisterType.infinite_register(0)
+    assert TestRegisterType.from_index(-2) == TestRegisterType.infinite_register(1)
