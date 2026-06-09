866bf81520fe1cde9344fe48832734059647e7b4
Edward Stow <edward.stow@riverlane.com>
2026-06-09 15:58:06 +0100

    allow any shape type to maintain better backwards compatibility

--- xdsl/dialects/builtin.py
+++ xdsl/dialects/builtin.py
@@ -1668,7 +1668,7 @@ class TensorType(
     @staticmethod
     def constr(
         element_type: IRDLAttrConstraint[AttributeInvT] | None = None,
-        shape: IRDLAttrConstraint[ArrayAttr[IntAttr]] | None = None,
+        shape: IRDLAttrConstraint | None = None,
     ) -> AttrConstraint[TensorType[AttributeInvT]]:
         return cast(
             AttrConstraint[TensorType[AttributeInvT]],
