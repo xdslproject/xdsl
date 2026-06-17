3ec6e29ebd0a5570f3f0756d27c84f9653a5fc38
Alex Rice <alexrice999@hotmail.co.uk>
2026-06-17 14:59:11 +0100

    utils: (DisjointSet) add roots

--- xdsl/utils/disjoint_set.py
+++ xdsl/utils/disjoint_set.py
@@ -4,10 +4,25 @@ Generic implementation of a disjoint set data structure.
 See external [documentation](https://en.wikipedia.org/wiki/Disjoint-set_data_structure).
 """
 
-from collections.abc import Hashable, Sequence
+from collections.abc import Hashable, Iterator, Sequence
 from typing import Generic
 
 from typing_extensions import TypeVar
+
+
+class RootsIterator(Iterator[int]):
+    _parent: Iterator[int]
+    _index: int
+
+    def __init__(self, parent: Iterator[int]):
+        self._parent = parent
+        self._index = 0
+
+    def __next__(self) -> int:
+        while (root := next(self._parent)) != self._index:
+            self._index += 1
+        self._index += 1
+        return root
 
 
 class IntDisjointSet:
@@ -123,6 +138,12 @@ class IntDisjointSet:
     def connected(self, lhs: int, rhs: int) -> bool:
         return self[lhs] == self[rhs]
 
+    def roots(self) -> Iterator[int]:
+        """
+        Returns an iterator over the roots of the disjoint set.
+        """
+        return RootsIterator(iter(self._parent))
+
 
 _T = TypeVar("_T", bound=Hashable)
 
@@ -214,6 +235,13 @@ class DisjointSet(Generic[_T]):
         return self._base.connected(
             self._index_by_value[lhs], self._index_by_value[rhs]
         )
+
+    def roots(self) -> Iterator[_T]:
+        """
+        Returns an iterator over the roots of the disjoint set.
+        """
+        for root in self._base.roots():
+            yield self._values[root]
 
     def __str__(self) -> str:
         values_by_root: dict[_T, list[_T]] = {}
