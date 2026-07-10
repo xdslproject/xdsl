b7bf83b88e3167beecbef54a0d2562eac0c93dd2
SaderSponge <saderfing@gmail.com>
2026-07-10 21:19:32 +0200

    Adapting tests of pybytecode

--- tests/dialects/test_py.py
+++ tests/dialects/test_py.py
@@ -1,10 +1,10 @@
-import xdsl.dialects.py as py
+import xdsl.dialects.pybytecode as pyb
 from xdsl.dialects.builtin import IntegerAttr, StringAttr, i64
 
 
 def test_custom_inits():
-    c1 = py.PyConstOp(IntegerAttr(1, i64))
-    c2 = py.PyConstOp(IntegerAttr(2, i64))
-    b1 = py.PyBinOp(op=StringAttr("add"), lhs=c1.results[0], rhs=c2.results[0])
+    c1 = pyb.PyConstOp(IntegerAttr(1, i64))
+    c2 = pyb.PyConstOp(IntegerAttr(2, i64))
+    b1 = pyb.PyBinOp(op=StringAttr("add"), lhs=c1.results[0], rhs=c2.results[0])
     assert b1.operands[0] is c1.results[0]
     assert b1.operands[1] is c2.results[0]
