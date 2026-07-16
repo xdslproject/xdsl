a8a8076dfe05ca37c13cdfe9ed74d19e8e1f9dd2
Sami El Akkad <94119348+Samielakkad@users.noreply.github.com>
2026-07-16 20:27:00 +0800

    CI: enable RUF003 lint rule (#6267)

--- xdsl/dialects/emitc.py
+++ xdsl/dialects/emitc.py
@@ -440,7 +440,7 @@ class EmitC_CallOpaqueOp(IRDLOperation):
     callee = prop_def(StringAttr)
     args = opt_prop_def(ArrayAttr)
     template_args = opt_prop_def(ArrayAttr)
-    # The SSA‐value operands of the call
+    # The SSA-value operands of the call
     call_args = var_operand_def()
     res = var_result_def()
 
