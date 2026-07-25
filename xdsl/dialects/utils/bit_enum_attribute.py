dcf67bf51cd23f96a54cd98feada354434ed29c0
Sasha Lopoukhine <superlopuh@gmail.com>
2026-07-25 13:46:54 +0100

    core: remove deprecated BitEnumAttribute.flags (#6299)

--- xdsl/dialects/utils/bit_enum_attribute.py
+++ xdsl/dialects/utils/bit_enum_attribute.py
@@ -5,7 +5,7 @@ from contextlib import nullcontext
 from dataclasses import dataclass
 from typing import ClassVar, Generic, cast, get_args, get_origin
 
-from typing_extensions import TypeVar, deprecated
+from typing_extensions import TypeVar
 
 from xdsl.ir import Data
 from xdsl.parser import AttrParser, Parser
@@ -82,11 +82,6 @@ class BitEnumAttribute(Data[frozenset[EnumType]], Generic[EnumType]):
             raise TypeError("Only direct inheritance from BitEnumAttribute is allowed.")
 
         cls.enum_type = enum_type
-
-    @property
-    @deprecated("Please use .data instead")
-    def flags(self) -> set[EnumType]:
-        return set(self.data)
 
     @classmethod
     def parse_parameter(cls, parser: AttrParser) -> frozenset[EnumType]:
